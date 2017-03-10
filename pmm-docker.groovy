788242a8a1d188454df061736895e456850fb23f
Mykola Marzhan <delgod@delgod.com>
2017-03-10 17:20:10 +0200

    add pmm-docker pipeline

---
+++ pmm-docker.groovy
@@ -0,0 +1,54 @@
+pipeline {
+    agent { label 'docker' }
+    parameters {
+        string (
+            defaultValue: 'centos7',
+            description: '',
+            name : 'GIT_BRANCH')
+        string (
+            defaultValue: 'perconalab/pmm-server',
+            description: '',
+            name : 'TAG')
+        string (
+            defaultValue: '1.1.2',
+            description: '',
+            name : 'VERSION')
+    }
+
+    stages {
+        stage ('Prepare') {
+            steps {
+                slackSend channel: '@mykola', color: '#FFFF00', message: "[Docker] ${TAG}: build started ${env.BUILD_URL}"
+                git poll: false, branch: GIT_BRANCH, url: 'https://github.com/percona/pmm-server.git'
+                sh """
+                    export FULL_TAG="${TAG}:${VERSION}-dev\$(date -u '+%Y%m%d%H%M')"
+                    echo \$FULL_TAG > FULL_TAG
+                """
+                archiveArtifacts 'FULL_TAG'
+            }
+        }
+        
+        stage ('Build container') {
+            steps {
+                sh 'docker build --no-cache -t \$(cat FULL_TAG) .'
+            }
+        }
+        
+        stage ('Push container') {
+            steps {
+                sh '''
+                    docker push \$(cat FULL_TAG)
+                    docker rmi  \$(cat FULL_TAG)
+                '''
+            }
+        }
+    }
+    post {
+        success {
+            slackSend channel: '@mykola', color: '#00FF00', message: "[Docker] ${TAG}: build finished"
+        }
+        failure {
+            slackSend channel: '@mykola', color: '#FF0000', message: "[Docker] ${TAG}: build failed"
+        }
+    }
+}
