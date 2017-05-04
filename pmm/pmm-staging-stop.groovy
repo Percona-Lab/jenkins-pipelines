4335981d26402ef31bac4c45a2c47d8ff9cdb3bf
Mykola Marzhan <delgod@delgod.com>
2017-05-04 12:47:37 +0300

    rework pmm-staging-stop pipeline

--- pmm/pmm-staging-stop.groovy
+++ pmm/pmm-staging-stop.groovy
@@ -1,49 +1,64 @@
-def userInput = ''
 def VMList = ''
 
-stage('Ask input') {
-    node('virtualbox') {
-        VMList = sh returnStdout: true, script: '''
-            set +o xtrace
-
-            for VM_NAME in $(VBoxManage list runningvms | cut -d '"' -f 2); do
-                IP=$(
-                    grep eth0: /tmp/$VM_NAME-console.log \
-                        | cut -d "|" -f 4 \
-                        | sed -e "s/ //g"
-                )
-                OWNER=$(
-                    VBoxManage showvminfo $VM_NAME \
-                        | grep Groups: \
-                        | sed -r "s/Groups:\\s+//; s/,\\/pmm-staging//"
-                )
-                printf "$VM_NAME\\t$IP\\t$OWNER\\n"
-            done
-        '''
-        echo """
-            What VM do you want to stop?
-            please copy VM name below and press 'Input requested' button
-
-$VMList
-        """
+pipeline {
+    agent {
+        label 'virtualbox'
+    }
+    parameters {
+        string(
+            defaultValue: 'list-all-vms',
+            description: 'Name or IP of VM to stop. Also you can set "list-all-vms" value, in this case list of current VMs will be shown and pipeline will ask you VM again.',
+            name: 'VM')
     }
 
-    timeout(time:10, unit:'MINUTES') {
-        userInput = input message: 'What VM do you want to stop?', parameters: [string(defaultValue: '', description: '', name: 'Name or IP')]
-        if ( !VMList.toLowerCase().contains(userInput.toLowerCase())) {
-            echo  'Unknown VM'
-            error 'Unknown VM'
+    stages {
+        stage('Ask input') {
+            steps {
+                script {
+                    VMList = sh returnStdout: true, script: '''
+                        set +o xtrace
+
+                        for VM_NAME in $(VBoxManage list runningvms | cut -d '"' -f 2); do
+                            IP=$(
+                                grep eth0: /tmp/$VM_NAME-console.log \
+                                    | cut -d "|" -f 4 \
+                                    | sed -e "s/ //g"
+                            )
+                            OWNER=$(
+                                VBoxManage showvminfo $VM_NAME \
+                                | grep Groups: \
+                                | sed -r "s/Groups:\\s+//"
+                            )
+                            printf "$VM_NAME\\t$IP\\t$OWNER\\n"
+                        done
+                    '''
+                    if ( "${VM}" == "list-all-vms" ) {
+                        echo """
+                            What VM do you want to stop?
+                            please copy VM name below and press 'Input requested' button
+                        """
+                        echo "${VMList}"
+                        timeout(time:10, unit:'MINUTES') {
+                            VM = input message: 'What VM do you want to stop?', parameters: [string(defaultValue: '', description: '', name: 'Name or IP')]
+                        }
+                    }
+                    if ( !VMList.toLowerCase().contains(VM.toLowerCase())) {
+                        echo 'Unknown VM'
+                        error 'Unknown VM'
+                    }
+                }
+            }
         }
-    }
-}
 
-node('virtualbox') {
-    stage('Stop VM') {
-        sh """
-            set -o errexit
-            VM_NAME=\$(echo '$VMList' | grep '$userInput' | awk '{print\$1}')
-            VBoxManage controlvm "\$VM_NAME" poweroff
-            VBoxManage unregistervm --delete "\$VM_NAME"
-        """
+        stage('Destroy VM') {
+            steps {
+                sh """
+                    set -o errexit
+                    VM_NAME=\$(echo '${VMList}' | grep '${VM}' | awk '{print\$1}')
+                    VBoxManage controlvm "\$VM_NAME" poweroff
+                    VBoxManage unregistervm --delete "\$VM_NAME"
+                """
+            }
+        }
     }
 }
