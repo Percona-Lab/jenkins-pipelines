fd675aa99f037c6e6f052a8a1bd1e886b6d552fa
Mykola Marzhan <mykola.marzhan@percona.com>
2017-07-21 12:25:15 +0300

    archive error output and logs only in case of errors

--- mysql/fb-mysql-5.6.groovy
+++ mysql/fb-mysql-5.6.groovy
@@ -90,8 +90,13 @@ void runMTR(String CMAKE_BUILD_TYPE) {
     archiveArtifacts "junit-${CMAKE_BUILD_TYPE}.xml"
     pushArtifactFile("junit-${CMAKE_BUILD_TYPE}.xml")
 
-    archiveArtifacts "${CMAKE_BUILD_TYPE}/destdir/usr/local/${GIT_BRANCH}/mysql-test/var/**/*.err"
-    archiveArtifacts "${CMAKE_BUILD_TYPE}/destdir/usr/local/${GIT_BRANCH}/mysql-test/var/**/*.log"
+    script {
+        def fileName = sh(returnStdout: true, script: "ls ${CMAKE_BUILD_TYPE}/destdir/usr/local/${GIT_BRANCH}/mysql-test/var/**/*.err || :").trim()
+        if (fileName != "" && fileExists(fileName)) {
+            archiveArtifacts "${CMAKE_BUILD_TYPE}/destdir/usr/local/${GIT_BRANCH}/mysql-test/var/**/*.err"
+            archiveArtifacts "${CMAKE_BUILD_TYPE}/destdir/usr/local/${GIT_BRANCH}/mysql-test/var/**/*.log"
+        }
+    }
 }
 
 void pushArtifactFile(String FILE_NAME) {
