c49dc50afe8a243e0c438ba6f4c537a34d54454f
Jenkins <jenkins@percona.com>
2017-12-20 10:20:31 -0500

    up PMM to 1.5.4

--- pmm/pmm-server-package.groovy
+++ pmm/pmm-server-package.groovy
@@ -36,7 +36,7 @@ pipeline {
             description: 'publish result package to internal or external repository',
             name: 'DESTINATION')
         string(
-            defaultValue: '1.5.3',
+            defaultValue: '1.5.4',
             description: 'version of result package',
             name: 'VERSION')
     }
