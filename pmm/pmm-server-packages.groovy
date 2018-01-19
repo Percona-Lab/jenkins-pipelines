740c89add4fcd3789f89146208b85caf8dbedafc
PMM Jenkins <dev-services@percona.com>
2018-01-19 19:29:28 +0000

    up PMM to 1.6.1

--- pmm/pmm-server-packages.groovy
+++ pmm/pmm-server-packages.groovy
@@ -21,7 +21,7 @@ pipeline {
             description: 'publish result package to internal or external repository',
             name: 'DESTINATION')
         string(
-            defaultValue: '1.6.0',
+            defaultValue: '1.6.1',
             description: 'version of result package',
             name: 'VERSION')
     }
