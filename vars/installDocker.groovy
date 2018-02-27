9a9ff6d07a753c517cc33ae7f40d7881defb17a1
Mykola Marzhan <mykola.marzhan@percona.com>
2018-02-27 23:43:21 +0200

    cut variables during docker installation

--- vars/installDocker.groovy
+++ vars/installDocker.groovy
@@ -3,7 +3,7 @@ def call() {
         sudo yum -y install git epel-release
         sudo yum -y install awscli
         curl -fsSL get.docker.com -o get-docker.sh
-        sh get-docker.sh
+        env -i sh get-docker.sh
         sudo usermod -aG docker `id -u -n`
         sudo mkdir -p /etc/docker
         echo '{"experimental": true}' | sudo tee /etc/docker/daemon.json
