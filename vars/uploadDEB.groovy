2089b60b819f3bc82e4eb75b7b2efc245787f24b
Mykola Marzhan <mykola.marzhan@percona.com>
2018-03-12 13:52:54 +0200

    add uploadDEB function

---
+++ vars/uploadDEB.groovy
@@ -0,0 +1,32 @@
+def call() {
+    node('master') {
+        deleteDir()
+        unstash 'debs'
+        unstash 'gitCommit'
+        withCredentials([sshUserPrivateKey(credentialsId: 'repo.ci.percona.com', keyFileVariable: 'KEY_PATH', usernameVariable: 'USER')]) {
+            sh """
+                export path_to_build="UPLOAD/pmm/${JOB_NAME}/\$(cat shortCommit)-${BUILD_NUMBER}"
+
+                dsc=`find results -name '*.dsc'`
+                if [ -f "\${dsc}" ]; then
+                    ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com \
+                        mkdir -p \${path_to_build}/source/debian
+                    scp -o StrictHostKeyChecking=no -i ${KEY_PATH} \
+                        `dirname \${dsc}`/* \
+                        ${USER}@repo.ci.percona.com:\${path_to_build}/source/debian/
+                fi
+
+                for deb in \$(find result-repo results -name '*.deb'); do
+                    dist=`echo \${deb} | sed -re 's/.*\\.([^.]+)_amd64.deb/\\1/'`
+                    path_to_dist=\${path_to_build}/binary/debian/\${dist}/x86_64
+                    ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com \
+                        mkdir -p \${path_to_dist}
+                    scp -o StrictHostKeyChecking=no -i ${KEY_PATH} \
+                        \${deb} \
+                        ${USER}@repo.ci.percona.com:\${path_to_dist}/
+                done
+            """
+        }
+        deleteDir()
+    }
+}
