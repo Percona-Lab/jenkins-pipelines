c85078b608eeaaa6e8efe5ae48ab21c14246ddc0
Mykola Marzhan <mykola.marzhan@percona.com>
2018-03-14 12:49:57 +0200

    make AmazonLinux compatible

--- vars/installDocker.groovy
+++ vars/installDocker.groovy
@@ -1,9 +1,9 @@
 def call() {
     sh '''
         sudo yum -y install git epel-release
-        sudo yum -y install awscli
+        sudo yum -y install '/usr/bin/aws'
         curl -fsSL get.docker.com -o get-docker.sh
-        env -i sh get-docker.sh
+        env -i sh get-docker.sh || sudo yum -y install docker
         sudo usermod -aG docker `id -u -n`
         sudo mkdir -p /etc/docker
         echo '{"experimental": true}' | sudo tee /etc/docker/daemon.json
