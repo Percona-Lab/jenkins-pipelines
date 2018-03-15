2ace83addbdc6b109324c67f9a70264875dac493
Mykola Marzhan <mykola.marzhan@percona.com>
2018-03-15 21:33:34 +0200

    improve sync

--- vars/sync2Prod.groovy
+++ vars/sync2Prod.groovy
@@ -7,34 +7,41 @@ def call(String DESTINATION) {
             withCredentials([sshUserPrivateKey(credentialsId: 'repo.ci.percona.com', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
                 sh """
                     ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com ' \
+                        set -o errexit
+                        set -o xtrace
+
                         pushd ${path_to_build}/binary
-                            for rhel in \$(ls -1 redhat); do
+                            for rhel in `ls -1 redhat`; do
                                 export dest_path=/srv/repo-copy/${DESTINATION}/\${rhel}
 
                                 # RPMS
                                 mkdir -p \${dest_path}/RPMS
-                                for arch in \$(ls -1 redhat/\${rhel}); do
+                                for arch in `ls -1 redhat/\${rhel}`; do
                                     repo_path=\${dest_path}/RPMS/\${arch}
                                     mkdir -p \${repo_path}
-                                    cp -av redhat/\${rhel}/\${arch}/*.rpm \${repo_path}/
+                                    if [ `ls redhat/\${rhel}/\${arch}/*.rpm | wc -l` -gt 0 ]; then
+                                        cp -av redhat/\${rhel}/\${arch}/*.rpm \${repo_path}/
+                                    fi
                                     createrepo --update \${repo_path}
                                 done
 
                                 # SRPMS
-                                src_rpm=\$(find ../source/redhat -type f -name '*.src.rpm')
                                 mkdir -p \${dest_path}/SRPMS
-                                cp -v \${src_rpm} \${dest_path}/SRPMS/
+                                if [ `find ../source/redhat -name '*.src.rpm' | wc -l` -gt 0 ]; then
+                                    cp -v `find ../source/redhat -name '*.src.rpm'` \${dest_path}/SRPMS/
+                                fi
                                 createrepo --update \${dest_path}/SRPMS
                             done
 
-                            for dist in \$(ls -1 debian); do
-                                for deb in \$(find debian/\${dist} -type f -name '*.deb'); do
+                            for dist in `ls -1 debian`; do
+                                for deb in `find debian/\${dist} -name '*.deb'`; do
                                     repopush --password ${SIGN_PASSWORD} --deb \${deb} --verbose --repo ${DESTINATION}
                                 done
 
                                 # source deb
-                                dsc=\$(find ../source -type f -name '*.dsc')
-                                repopush --password ${SIGN_PASSWORD} --dsc \${dsc} --verbose --repo ${DESTINATION} --distribution \${dist}
+                                for dsc in `find ../source -name '*.dsc'`; do
+                                    repopush --password ${SIGN_PASSWORD} --dsc \${dsc} --verbose --repo ${DESTINATION} --distribution \${dist}
+                                done
                             done
                         popd
 
