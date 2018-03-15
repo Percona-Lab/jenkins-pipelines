daa2b12b0cba0c3b878643dccec927b34affbbfb
Mykola Marzhan <mykola.marzhan@percona.com>
2018-03-15 21:35:59 +0200

    add pmm-server-autobuild pipeline

---
+++ pmm/pmm-server-autobuild.groovy
@@ -0,0 +1,120 @@
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+]) _
+
+pipeline {
+    environment {
+        DESTINATION = 'laboratory'
+    }
+    agent {
+        label 'large-amazon'
+    }
+    parameters {
+        string(
+            defaultValue: 'master',
+            description: 'Tag/Branch for percona-images repository',
+            name: 'GIT_BRANCH')
+    }
+    options {
+        skipDefaultCheckout()
+        disableConcurrentBuilds()
+    }
+    stages {
+        stage('Prepare') {
+            steps {
+                installDocker()
+
+                git poll: true, branch: GIT_BRANCH, url: 'http://github.com/Percona-Lab/pmm-submodules'
+                sh '''
+                    git reset --hard
+                    sudo git clean -xdf
+                    git submodule update --init --jobs 10 \
+                        sources/pmm-update \
+                        sources/pmm-server \
+                        sources/grafana-dashboards \
+                        sources/pmm-server-packaging \
+                        sources/qan-api/src/github.com/percona/qan-api \
+                        sources/qan-app/src/github.com/percona/qan-app \
+                        sources/pmm-manage/src/github.com/percona/pmm-manage \
+                        sources/pmm-managed/src/github.com/percona/pmm-managed \
+                        sources/rds_exporter/src/github.com/percona/rds_exporter
+
+                    git rev-parse HEAD         > gitCommit
+                    git rev-parse --short HEAD > shortCommit
+                '''
+                stash includes: 'gitCommit,shortCommit', name: 'gitCommit'
+                slackSend channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
+            }
+        }
+        stage('Build server packages') {
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    sh '''
+                        sg docker -c "
+                            export PATH=$PATH:$(pwd -P)/build/bin
+
+                            # 1st-party
+                            build-server-rpm percona-dashboards grafana-dashboards
+                            build-server-rpm pmm-manage
+                            build-server-rpm pmm-managed
+                            build-server-rpm percona-qan-api qan-api
+                            build-server-rpm percona-qan-app qan-app
+                            build-server-rpm pmm-server
+                            build-server-rpm pmm-update
+
+                            # 3rd-party
+                            build-server-rpm consul
+                            build-server-rpm orchestrator
+                            build-server-rpm rds_exporter
+                            build-server-rpm prometheus
+                            build-server-rpm grafana
+                        "
+                    '''
+                }
+                stash includes: 'tmp/pmm-server/RPMS/*/*/*.rpm', name: 'rpms'
+                uploadRPM()
+            }
+        }
+        stage('Build server docker') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                    sh """
+                        sg docker -c "
+                            docker login -u "${USER}" -p "${PASS}"
+                        "
+                    """
+                }
+                sh '''
+                    sg docker -c "
+                        export PUSH_DOCKER=1
+                        export DOCKER_TAG=perconalab/pmm-server:$(date -u '+%Y%m%d%H%M')
+
+                        ./build/bin/build-server-docker
+                    "
+                '''
+            }
+        }
+        stage('Sign packages') {
+            steps {
+                signRPM()
+            }
+        }
+        stage('Push to public repository') {
+            steps {
+                sync2Prod(DESTINATION)
+            }
+        }
+    }
+    post {
+        always {
+            script {
+                if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
+                    slackSend channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build finished"
+                } else {
+                    slackSend channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result}"
+                }
+            }
+        }
+    }
+}
