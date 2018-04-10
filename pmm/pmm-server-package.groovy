24b60e5135ffd07b71d88fca49450843fd8d10ef
Mykola Marzhan <mykola.marzhan@percona.com>
2018-04-10 12:36:48 +0300

    remove old pipelines

--- pmm/pmm-server-package.groovy
+++
@@ -1,6 +0,0 @@
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
-    $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
-]) _
-
-buildPipeline('pmm-server', 'percona/pmm-server')
