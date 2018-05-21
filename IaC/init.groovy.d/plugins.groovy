9ab8abe7b0c6e71f6f3604abc38da70b08559400
Mykola Marzhan <mykola.marzhan@percona.com>
2018-05-21 11:29:52 +0300

    update plugins list

--- IaC/init.groovy.d/plugins.groovy
+++ IaC/init.groovy.d/plugins.groovy
@@ -3,7 +3,7 @@ import java.util.logging.Logger
 def logger = Logger.getLogger("")
 def installed = false
 def initialized = false
-def pluginParameter="ec2 ssh-slaves google-login matrix-auth jobConfigHistory compress-buildlog"
+def pluginParameter="%LIST%"
 def plugins = pluginParameter.split()
 logger.info("" + plugins)
 def instance = Jenkins.getInstance()
