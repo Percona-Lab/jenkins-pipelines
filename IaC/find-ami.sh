77648459d305fbf97932016cce53eec6cafaaaa2
Mykola Marzhan <mykola.marzhan@percona.com>
2018-07-27 12:06:56 +0300

    update relevant OSes

--- IaC/find-ami.sh
+++ IaC/find-ami.sh
@@ -3,18 +3,19 @@
 set -o errexit
 #set -o xtrace
 
-#export AWS_DEFAULT_REGION=us-west-2
-export AWS_DEFAULT_REGION=us-east-2
+export AWS_DEFAULT_REGION=us-west-2
+#export AWS_DEFAULT_REGION=us-east-2
 
 declare -A prod_code=(
     ["micro-amazon"]="Name=name,Values=amzn-ami-hvm-*-x86_64-gp2"
+    ["micro-amazon2"]="Name=name,Values=amzn2-ami-hvm-*-x86_64-gp2"
+    ["min-centos-6-x32"]="Name=product-code,Values=pwwpwelgj5jk5cie2erh4h3e"
     ["min-centos-6-x64"]="Name=product-code,Values=6x5jmcajty9edm3f211pqjfn2"
     ["min-centos-7-x64"]="Name=product-code,Values=aw0evgkw8e5c1q413zgy5pjce"
     ["min-trusty-x64"]="Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"
     ["min-xenial-x64"]="Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"
-    ["min-zesty-x64"]="Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-zesty-17.04-amd64-server-*"
+    ["min-bionic-x64"]="Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"
     ["min-artful-x64"]="Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-artful-17.10-amd64-server-*"
-    ["min-wheezy-x64"]="Name=name,Values=debian-wheezy-amd64-hvm-*"
     ["min-jessie-x64"]="Name=product-code,Values=3f8t6t8fp5m9xx18yzwriozxi"
     ["min-stretch-x64"]="Name=product-code,Values=55q52qvgjfpdj2fpfy9mb1lo4"
 )
