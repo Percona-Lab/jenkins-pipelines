c26bbc027a28ff32c7398802f336c443ccc91bb2
Mykola Marzhan <mykola.marzhan@percona.com>
2018-08-16 20:37:36 +0300

    add sqs policy

--- IaC/pxb.cd/terraform/termination.tf
+++ IaC/pxb.cd/terraform/termination.tf
@@ -7,6 +7,31 @@ resource "aws_sqs_queue" "jenkins" {
   }
 }
 
+resource "aws_sqs_queue_policy" "jenkins" {
+  queue_url = "${aws_sqs_queue.jenkins.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.jenkins.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_cloudwatch_event_rule.aws-stop.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
+
 # notify about instance interruption from AWS side
 resource "aws_cloudwatch_event_rule" "aws-stop" {
   name        = "${var.cloud_name}-aws-stop"
