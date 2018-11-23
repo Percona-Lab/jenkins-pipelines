347ec4959099afc5b93c4025c9d9894f248c7848
puneet0191 <puneet.0191@gmail.com>
2018-11-23 12:27:24 +0530

    PMM-3289 Adding Codecept-Saucelabs Pipelines file

---
+++ pmm/pmm-codecept-saucelabs.groovy
@@ -0,0 +1,106 @@
+void runStaging(String DOCKER_VERSION, CLIENT_VERSION, CLIENTS) {
+    stagingJob = build job: 'aws-staging-start', parameters: [
+        string(name: 'DOCKER_VERSION', value: DOCKER_VERSION),
+        string(name: 'CLIENT_VERSION', value: CLIENT_VERSION),
+        string(name: 'CLIENTS', value: CLIENTS),
+        string(name: 'NOTIFY', value: 'false'),
+        string(name: 'DAYS', value: '1')
+    ]
+    env.VM_IP = stagingJob.buildVariables.IP
+    env.VM_NAME = stagingJob.buildVariables.VM_NAME
+    env.PMM_URL = "http://${VM_IP}"
+}
+
+void destroyStaging(IP) {
+    build job: 'aws-staging-stop', parameters: [
+        string(name: 'VM', value: IP),
+    ]
+}
+
+pipeline {
+    agent {
+        label 'micro-amazon'
+    }
+    parameters {
+        string(
+            defaultValue: 'PMM-2850',
+            description: 'Tag/Branch for pmm-qa repository',
+            name: 'GIT_BRANCH')
+        string(
+            defaultValue: 'perconalab/pmm-server:dev-latest',
+            description: 'PMM Server docker container version (image-name:version-tag)',
+            name: 'DOCKER_VERSION')
+        string(
+            defaultValue: 'dev-latest',
+            description: 'PMM Client version',
+            name: 'CLIENT_VERSION')
+    }
+    options {
+        skipDefaultCheckout()
+        disableConcurrentBuilds()
+    }
+
+    stages {
+        stage('Prepare') {
+            steps {
+                // clean up workspace and fetch pmm-qa repository
+                deleteDir()
+                git poll: false, branch: GIT_BRANCH, url: 'https://github.com/Percona-QA/pmm-qa.git'
+
+                slackSend channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
+
+                sh '''
+                    curl --silent --location https://rpm.nodesource.com/setup_7.x | sudo bash -
+                    sudo yum -y install nodejs
+
+                    export PATH=$PATH:/usr/local/node/bin
+                    npm install -g codeceptjs webdriverio
+                '''
+            }
+        }
+        stage('Start staging') {
+            steps {
+                runStaging(DOCKER_VERSION, CLIENT_VERSION, '--addclient=ps,1')
+            }
+        }
+        stage('Sanity check') {
+            steps {
+                sh "curl --silent --insecure '${PMM_URL}/prometheus/targets' | grep localhost:9090"
+            }
+        }
+        stage('Sleep') {
+            steps {
+                sleep 300
+            }
+        }
+        stage('Run Grafana Test') {
+            steps {
+                sauce('SauceLabsKey') {
+                    sauceconnect(options: '', sauceConnectPath: '') {
+                        sh """
+                            export PATH=$PATH:/usr/local/node/bin:\$(pwd -P)/node_modules/protractor/bin
+                            codeceptjs run --steps --verbose tests/verifyRemoteInstances_test.js -o '{ "helpers": {"WebDriverIO": {"url": "${PMM_URL}", user": ${SAUCE_USERNAME}, "key": ${SAUCE_ACCESS_KEY}, "host": "ondemand.saucelabs.com"}}}'
+                        """
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            // stop staging
+            destroyStaging(VM_NAME)
+            script {
+                if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
+                    saucePublisher()
+                    junit '**/testresults/*xmloutput*.xml'
+                    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'screenshots', reportFiles: 'pmm-qan-report.html, pmm-test-grafana-report.html', reportName: 'HTML Report', reportTitles: ''])
+                    slackSend channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build finished"
+                } else {
+                    slackSend channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result}"
+                }
+            }
+            deleteDir()
+        }
+    }
+}
