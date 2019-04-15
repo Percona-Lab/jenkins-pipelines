34183fc37ef4f87d851577f367d5de0e85dc9217
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2019-04-15 13:16:48 +0300

    PMM-3668 add job for building ami image for PMM2

---
+++ pmm/pmm2-ami.groovy
@@ -0,0 +1,64 @@
+pipeline {
+    environment {
+        specName = 'AMI'
+    }
+    agent {
+        label 'awscli'
+    }
+    parameters {
+        string(
+            defaultValue: 'master',
+            description: 'Tag/Branch for percona-images repository',
+            name: 'GIT_BRANCH')
+    }
+    options {
+        skipDefaultCheckout()
+        disableConcurrentBuilds()
+    }
+    triggers {
+        upstream upstreamProjects: 'pmm2-server-autobuild', threshold: hudson.model.Result.SUCCESS
+    }
+
+    stages {
+        stage('Prepare') {
+            steps {
+                slackSend channel: '#pmm-ci', color: '#FFFF00', message: "[${specName}]: build started - ${BUILD_URL}"
+                git poll: true, branch: GIT_BRANCH, url: "https://github.com/Percona-Lab/percona-images.git"
+                sh """
+                    make clean
+                    make deps
+                """
+            }
+        }
+
+        stage('Build Image') {
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    sh """
+                        ~/bin/packer build -only amazon-ebs -color=false packer/pmm2.json \
+                            | tee build.log
+                    """
+                }
+                sh 'tail build.log | grep us-east-1 | cut -d " " -f 2 > IMAGE'
+                stash includes: 'IMAGE', name: 'IMAGE'
+                archiveArtifacts 'IMAGE'
+            }
+        }
+    }
+
+    post {
+        always {
+            deleteDir()
+        }
+        success {
+            script {
+                unstash 'IMAGE'
+                def IMAGE = sh(returnStdout: true, script: "cat IMAGE").trim()
+                slackSend channel: '#pmm-ci', color: '#00FF00', message: "[${specName}]: build finished - ${IMAGE}"
+            }
+        }
+        failure {
+            slackSend channel: '#pmm-ci', color: '#FF0000', message: "[${specName}]: build failed"
+        }
+    }
+}
