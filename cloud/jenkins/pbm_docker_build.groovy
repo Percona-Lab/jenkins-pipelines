6e7d069369cdf87c6e062523e8fe7729ae11d1cb
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2019-05-23 13:57:08 +0300

    CLOUD-215 push docker images to scan.connect.redhat.com

--- cloud/jenkins/pbm_docker_build.groovy
+++ cloud/jenkins/pbm_docker_build.groovy
@@ -11,8 +11,27 @@ void build(String IMAGE_PREFIX){
         sudo rm -rf ./vendor
     """
 }
-
-void pushImage(String IMAGE_PREFIX){
+void pushImageToRhel(String IMAGE_PREFIX){
+     withCredentials([usernamePassword(credentialsId: 'scan.connect.redhat.com-psmdb-containers', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh """
+            IMAGE_PREFIX=${IMAGE_PREFIX}
+            GIT_FULL_COMMIT=\$(git rev-parse HEAD)
+            GIT_SHORT_COMMIT=\${GIT_FULL_COMMIT:0:7}
+            IMAGE_ID=\$(docker images -q perconalab/percona-server-mongodb-operator:master-\$IMAGE_PREFIX)
+            IMAGE_NAME='percona-server-mongodb-operator'
+            IMAGE_TAG="master-\$GIT_SHORT_COMMIT-\$IMAGE_PREFIX"
+            if [ -n "\${IMAGE_ID}" ]; then
+                sg docker -c "
+                    docker login -u '${USER}' -p '${PASS}' scan.connect.redhat.com
+                    docker tag \${IMAGE_ID} scan.connect.redhat.com/ospid-5690f369-d04c-45f9-8195-5acb27d80ebf/\$IMAGE_NAME:\$IMAGE_TAG
+                    docker push scan.connect.redhat.com/ospid-5690f369-d04c-45f9-8195-5acb27d80ebf/\$IMAGE_NAME:\$IMAGE_TAG
+                    docker logout
+                "
+            fi
+        """
+    }
+}
+void pushImageToDocker(String IMAGE_PREFIX){
      withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh """
             IMAGE_PREFIX=${IMAGE_PREFIX}
@@ -58,19 +77,29 @@ pipeline {
                 stash includes: "source/**", name: "sourceFILES"
             }
         }
-        stage('Build PSMDB docker images') {
+        stage('Build PBM docker images') {
             steps {
                 unstash "sourceFILES"
                 echo 'Build PBM docker images'
                 build('backup')
-                
-                echo 'Push PBM docker images to docker hub'
-                pushImage('backup')
+            }
+        }
+        stage('Push PBM image to Docker registry') {
+            steps {
+                pushImageToDocker('backup')
+            }
+        }
+        stage('Push PBM image to RHEL registry') {
+            steps {
+                pushImageToRhel('backup')
             }
         }
     }
     post {
         always {
+            sh '''
+                sudo docker rmi -f \$(sudo docker images -q) || true
+            '''
             deleteDir()
         }
     }
--- cloud/jenkins/psmdb_docker_build.groovy
+++ cloud/jenkins/psmdb_docker_build.groovy
@@ -1,18 +1,59 @@
 void build(String IMAGE_PREFIX){
     sh """
         cd ./source/
-        docker build --no-cache --squash -t perconalab/percona-server-mongodb-operator:master-${IMAGE_PREFIX} -f build/Dockerfile.${IMAGE_PREFIX} .
+        DOCKER_FILE_PREFIX=\$(echo ${IMAGE_PREFIX} | tr -d '.')
+        docker build --no-cache --squash -t perconalab/percona-server-mongodb-operator:master-${IMAGE_PREFIX} -f build/Dockerfile.\$DOCKER_FILE_PREFIX .
     """
 }
 
-void pushImage(String IMAGE_PREFIX){
+void pushImageToDocker(String IMAGE_PREFIX){
     withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh """
             IMAGE_PREFIX=${IMAGE_PREFIX}
             sg docker -c "
                 docker login -u '${USER}' -p '${PASS}'
                 docker push perconalab/percona-server-mongodb-operator:master-${IMAGE_PREFIX}
+                docker logout
             "
+        """
+    }
+}
+void pushImageToRhelOperator(){
+     withCredentials([usernamePassword(credentialsId: 'scan.connect.redhat.com-psmdb-operator', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh """
+            GIT_FULL_COMMIT=\$(git rev-parse HEAD)
+            GIT_SHORT_COMMIT=\${GIT_FULL_COMMIT:0:7}
+            IMAGE_ID=\$(docker images -q perconalab/percona-server-mongodb-operator:master)
+            IMAGE_NAME='percona-server-mongodb-operator'
+            IMAGE_TAG="master-\$GIT_SHORT_COMMIT"
+            if [ -n "\${IMAGE_ID}" ]; then
+                sg docker -c "
+                    docker login -u '${USER}' -p '${PASS}' scan.connect.redhat.com
+                    docker tag \${IMAGE_ID} scan.connect.redhat.com/ospid-e90784a0-8fe7-4122-9c91-f0ce60be8314/\$IMAGE_NAME:\$IMAGE_TAG
+                    docker push scan.connect.redhat.com/ospid-e90784a0-8fe7-4122-9c91-f0ce60be8314/\$IMAGE_NAME:\$IMAGE_TAG
+                    docker logout
+                "
+            fi
+        """
+    }
+}
+void pushImageToRhel(String IMAGE_PREFIX){
+     withCredentials([usernamePassword(credentialsId: 'scan.connect.redhat.com-psmdb-containers', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh """
+            IMAGE_PREFIX=${IMAGE_PREFIX}
+            GIT_FULL_COMMIT=\$(git rev-parse HEAD)
+            GIT_SHORT_COMMIT=\${GIT_FULL_COMMIT:0:7}
+            IMAGE_ID=\$(docker images -q perconalab/percona-server-mongodb-operator:master-\$IMAGE_PREFIX)
+            IMAGE_NAME='percona-server-mongodb-operator'
+            IMAGE_TAG="master-\$GIT_SHORT_COMMIT-\$IMAGE_PREFIX"
+            if [ -n "\${IMAGE_ID}" ]; then
+                sg docker -c "
+                    docker login -u '${USER}' -p '${PASS}' scan.connect.redhat.com
+                    docker tag \${IMAGE_ID} scan.connect.redhat.com/ospid-5690f369-d04c-45f9-8195-5acb27d80ebf/\$IMAGE_NAME:\$IMAGE_TAG
+                    docker push scan.connect.redhat.com/ospid-5690f369-d04c-45f9-8195-5acb27d80ebf/\$IMAGE_NAME:\$IMAGE_TAG
+                    docker logout
+                "
+            fi
         """
     }
 }
@@ -51,7 +92,7 @@ pipeline {
             }
         }
 
-        stage('Build PSMDBO docker images') {
+        stage('Build PSMDBO operator docker image') {
             steps {
                 unstash "sourceFILES"
                 withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
@@ -60,6 +101,7 @@ pipeline {
                         sg docker -c "
                             docker login -u '${USER}' -p '${PASS}'
                             ./e2e-tests/build
+                            docker logout
                         "
                     '''
                 }
@@ -70,22 +112,33 @@ pipeline {
             steps {
                 unstash "sourceFILES"
                 echo 'Build PSMDB docker images'
-                build('mongod36')
-                build('mongod40')
-                
-                echo 'Push PSMDB docker images to docker hub'
-                pushImage('mongod36')
-                pushImage('mongod40')
-                
-                sh '''
-                    sudo rm -rf ./source/build
-                '''
+                build('mongod3.6')
+                build('mongod4.0')
+            }
+        }
+
+        stage('Push PSMDB images to Docker registry') {
+            steps {
+                pushImageToDocker('mongod3.6')
+                pushImageToDocker('mongod4.0')
+            }
+        }
+
+        stage('Push PSMDB images to RHEL registry') {
+            steps {
+                pushImageToRhel('mongod3.6')
+                pushImageToRhel('mongod4.0')
+                pushImageToRhelOperator()
             }
         }
     }
 
     post {
         always {
+            sh '''
+                sudo docker rmi -f \$(sudo docker images -q) || true
+                sudo rm -rf ./source/build
+            '''
             deleteDir()
         }
     }
