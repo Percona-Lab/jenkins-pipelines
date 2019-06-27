3d7898414fc10e15e11c3b306657e91e73b74636
puneet0191 <puneet.0191@gmail.com>
2019-06-27 17:05:21 +0530

    PMM-4230 change default version

--- pmm/aws-staging-start.groovy
+++ pmm/aws-staging-start.groovy
@@ -36,7 +36,7 @@ pipeline {
             description: 'MySQL Community Server version',
             name: 'MS_VERSION')
         choice(
-            choices: ['11', '10.8', '9.6'],
+            choices: ['10.8', '11', '9.6'],
             description: "Which version of PostgreSQL",
             name: 'PGSQL_VERSION')
         string(
