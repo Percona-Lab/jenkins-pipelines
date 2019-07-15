ea727d9395b2e1b10c49936263c9ee2cd6c34cce
puneet0191 <puneet.0191@gmail.com>
2019-07-15 13:18:47 +0530

    PMM-4230 Adding UI tests Job for pmm2.x

---
+++ pmm/pmm2-ui-tests.groovy
@@ -0,0 +1,111 @@
+void runStaging(String DOCKER_VERSION, CLIENT_VERSION, CLIENTS) {
+    stagingJob = build job: 'aws-staging-start', parameters: [
+        string(name: 'DOCKER_VERSION', value: DOCKER_VERSION),
+        string(name: 'CLIENT_VERSION', value: CLIENT_VERSION),
+        string(name: 'CLIENTS', value: CLIENTS),
+        string(name: 'NOTIFY', value: 'false'),
+        string(name: 'DAYS', value: '1')
+    ]
+    env.VM_IP = stagingJob.buildVariables.IP
+    env.VM_NAME = stagingJob.buildVariables.VM_NAME
+    env.PMM_URL = "http://${VM_IP}"
+}
+
+void destroyStaging(IP) {
+    build job: 'aws-staging-stop', parameters: [
+        string(name: 'VM', value: IP),
+    ]
+}
+
+pipeline {
+    agent {
+        label 'micro-amazon'
+    }
+    parameters {
+        string(
+            defaultValue: 'master',
+            description: 'Tag/Branch for pmm-qa repository',
+            name: 'GIT_BRANCH')
+        string(
+            defaultValue: 'perconalab/pmm-server:dev-latest',
+            description: 'PMM Server docker container version (image-name:version-tag)',
+            name: 'DOCKER_VERSION')
+        string(
+            defaultValue: 'dev-latest',
+            description: 'PMM Client version',
+            name: 'CLIENT_VERSION')
+    }
+    options {
+        skipDefaultCheckout()
+        disableConcurrentBuilds()
+    }
+    triggers {
+        upstream upstreamProjects: 'pmm2-server-autobuild', threshold: hudson.model.Result.SUCCESS
+    }
+    stages {
+        stage('Prepare') {
+            steps {
+                // clean up workspace and fetch pmm-qa repository
+                deleteDir()
+                git poll: false, branch: GIT_BRANCH, url: 'https://github.com/percona/pmm-qa.git'
+
+                slackSend channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
+
+                sh '''
+                    curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+                    sudo yum -y install nodejs
+
+                    export PATH=$PATH:/usr/local/node/bin
+                    npm install
+                '''
+            }
+        }
+        stage('Start staging') {
+            steps {
+                runStaging(DOCKER_VERSION, CLIENT_VERSION, '--addclient=ps,1 --addclient=mo,2 --with-replica  --addclient=pgsql,1 --pmm2')
+            }
+        }
+        stage('Sanity check') {
+            steps {
+                sh 'timeout 100 bash -c \'while [[ "$(curl -s -o /dev/null -w \'\'%{http_code}\'\' \${PMM_URL}/ping)" != "200" ]]; do sleep 5; done\' || false'
+            }
+        }
+        stage('Sleep') {
+            steps {
+                sleep 180
+            }
+        }
+        stage('Run Grafana Test') {
+            steps {
+                sauce('SauceLabsKey') {
+                    sauceconnect(options: '', sauceConnectPath: '') {
+                        sh """
+                            sed -i 's/{SAUCE_USER_KEY}/${SAUCE_ACCESS_KEY}/g' codecept.json
+                            ./node_modules/.bin/codeceptjs run-multiple parallel --reporter mocha-multi -o '{ "helpers": {"WebDriverIO": {"url": "${PMM_URL}"}}}' --grep '(?=.*)^(?!.*@visual-test)'
+                        """
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            // stop staging
+            destroyStaging(VM_NAME)
+            script {
+                if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
+                    saucePublisher()
+                    junit 'tests/output/parallel_chunk*/chrome_report.xml'
+                    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'tests/output/', reportFiles: 'parallel_chunk1__browser_chrome__1/result.html, parallel_chunk2__browser_chrome__2/result.html', reportName: 'HTML Report', reportTitles: ''])
+                    slackSend channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build finished"
+                } else {
+                    slackSend channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result}"
+                }
+            }
+            sh '''
+                sudo rm -r node_modules/
+            '''
+            deleteDir()
+        }
+    }
+}
