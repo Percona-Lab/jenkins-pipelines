48b8c1fec9e5ffc8b1dc2879289f97ef4076d02d
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2019-08-28 14:56:46 +0300

    CLOUD-365 check pbm image

--- cloud/jenkins/pbm_docker_build.groovy
+++ cloud/jenkins/pbm_docker_build.groovy
@@ -3,13 +3,26 @@ void build(String IMAGE_PREFIX){
         cd ./source
         SOURCE_ROOT_DIR=\$(pwd)
         mkdir -p src/github.com/percona
-        ln -s /mnt/jenkins/workspace/pbm-docker-build/source  src/github.com/percona/percona-backup-mongodb
+        ln -s \$SOURCE_ROOT_DIR  src/github.com/percona/percona-backup-mongodb
         cd src/github.com/percona/percona-backup-mongodb
         docker run --rm -v \$(pwd):/go/src/github.com/percona/percona-backup-mongodb -w /go/src/github.com/percona/percona-backup-mongodb golang:1.12 make
         cd \$SOURCE_ROOT_DIR
         docker build -t perconalab/percona-server-mongodb-operator:master-${IMAGE_PREFIX} -f docker/Dockerfile.common .
         sudo rm -rf ./vendor
     """
+}
+void checkImageForDocker(String IMAGE_PREFIX){
+     withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh """
+            IMAGE_PREFIX=${IMAGE_PREFIX}
+            IMAGE_NAME='percona-server-mongodb-operator'
+            sg docker -c "
+                docker login -u '${USER}' -p '${PASS}'
+                /usr/local/bin/trivy -o $WORKSPACE/trivy-hight-\$IMAGE_NAME-${IMAGE_PREFIX}.log --exit-code 0 --severity HIGH --quiet --auto-refresh perconalab/\$IMAGE_NAME:master-${IMAGE_PREFIX}
+                /usr/local/bin/trivy -o $WORKSPACE/trivy-critical-\$IMAGE_NAME-${IMAGE_PREFIX}.log --exit-code 1 --severity CRITICAL --quiet --auto-refresh perconalab/\$IMAGE_NAME:master-${IMAGE_PREFIX}
+            "
+        """
+    }
 }
 void pushImageToRhel(String IMAGE_PREFIX){
      withCredentials([usernamePassword(credentialsId: 'scan.connect.redhat.com-psmdb-containers', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
@@ -66,14 +79,17 @@ pipeline {
         stage('Prepare') {
             steps {
                 git branch: 'master', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
-                sh '''
+                sh """
+                    TRIVY_VERSION=\$(curl --silent 'https://api.github.com/repos/aquasecurity/trivy/releases/latest' | grep '"tag_name":' | tr -d '"' | sed -E 's/.*v(.+),.*/\\1/')
+                    wget https://github.com/aquasecurity/trivy/releases/download/v\${TRIVY_VERSION}/trivy_\${TRIVY_VERSION}_Linux-64bit.tar.gz
+                    sudo tar zxvf trivy_\${TRIVY_VERSION}_Linux-64bit.tar.gz -C /usr/local/bin/
                     # sudo is needed for better node recovery after compilation failure
                     # if building failed on compilation stage directory will have files owned by docker user
                     sudo git reset --hard
                     sudo git clean -xdf
                     sudo rm -rf source
                     ./cloud/local/checkout
-                '''
+                """
                 stash includes: "source/**", name: "sourceFILES"
             }
         }
@@ -94,13 +110,22 @@ pipeline {
                 pushImageToRhel('backup')
             }
         }
+        stage('Check PBM Docker image') {
+            steps {
+                checkImageForDocker('backup')
+            }
+        }
     }
     post {
         always {
+            archiveArtifacts '*.log'
             sh '''
-                sudo docker rmi -f \$(sudo docker images -q) || true
+                sudo docker rmi -f \$(sudo docker images -q | uniq) || true
             '''
             deleteDir()
+        }
+        failure {
+            slackSend channel: '#cloud-dev-ci', color: '#FF0000', message: "Building of PBM image failed. Please check the log ${BUILD_URL}"
         }
     }
 }
