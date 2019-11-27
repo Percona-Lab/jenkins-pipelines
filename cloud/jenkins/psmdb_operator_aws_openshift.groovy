4f5dc35258b99688403aca737e22bd1c4c3812af
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2019-11-27 15:09:45 +0200

    CLOUD-452 Automate PSMDB Operator tests on AWS 'OpenShift-3.11'

--- cloud/jenkins/psmdb_operator_aws_openshift.groovy
+++ cloud/jenkins/psmdb_operator_aws_openshift.groovy
@@ -26,15 +26,7 @@ void runTest(String TEST_NAME) {
             echo Skip $TEST_NAME test
         else
             cd ./source
-            export IMAGE=perconalab/percona-xtradb-cluster-operator:${env.GIT_BRANCH}
-            if [ -n "${IMAGE_PXC}" ]; then
-                export IMAGE_PXC=${IMAGE_PXC}
-            fi
-
-            if [ -n "${IMAGE_PROXY}" ]; then
-                export IMAGE_PROXY=${IMAGE_PROXY}
-            fi
-
+            export IMAGE=perconalab/percona-server-mongodb-operator:${env.GIT_BRANCH}
             source $HOME/google-cloud-sdk/path.bash.inc
             ./e2e-tests/$TEST_NAME/run
             touch $VERSION-$TEST_NAME
@@ -57,24 +49,12 @@ pipeline {
     parameters {
         string(
             defaultValue: 'master',
-            description: 'Tag/Branch for percona/percona-xtradb-cluster-operator repository',
+            description: 'Tag/Branch for percona/percona-server-mongodb-operator repository',
             name: 'GIT_BRANCH')
         string(
-            defaultValue: 'https://github.com/percona/percona-xtradb-cluster-operator',
-            description: 'percona-xtradb-cluster-operator repository',
+            defaultValue: 'https://github.com/percona/percona-server-mongodb-operator',
+            description: 'percona-server-mongodb-operator repository',
             name: 'GIT_REPO')
-        string(
-            defaultValue: 'master',
-            description: 'Tag/Branch for percona/percona-xtradb-cluster-operator repository',
-            name: 'GIT_BRANCH')
-        string(
-            defaultValue: '',
-            description: 'PXC image',
-            name: 'IMAGE_PXC')
-        string(
-            defaultValue: '',
-            description: 'PXC proxy image',
-            name: 'IMAGE_PROXY')
     }
     environment {
         TF_IN_AUTOMATION = 'true'
@@ -132,7 +112,7 @@ pipeline {
                         cd ./source/
                         sg docker -c "
                             docker login -u '${USER}' -p '${PASS}'
-                            export IMAGE=perconalab/percona-xtradb-cluster-operator:$GIT_BRANCH
+                            export IMAGE=perconalab/percona-server-mongodb-operator:$GIT_BRANCH
                             ./e2e-tests/build
                             docker logout
                         "
@@ -179,29 +159,20 @@ pipeline {
                }
             }
         }
-        stage('E2E Basic Tests') {
+        stage('E2E Scaling') {
             steps {
                 runTest('init-deploy')
                 runTest('limits')
-                runTest('monitoring')
-                runTest('affinity')
-           }
-        }
-        stage('E2E Scaling') {
-            steps {
                 runTest('scaling')
-                runTest('scaling-proxysql')
             }
         }
         stage('E2E Backups') {
             steps {
-                runTest('recreate')
                 runTest('demand-backup')
                 runTest('scheduled-backup')
             }
         }
     }
-
     post {
         always {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'openshift-cicd'], file(credentialsId: 'aws-openshift-key-pub', variable: 'AWS_NODES_KEY_PUB')]) {
