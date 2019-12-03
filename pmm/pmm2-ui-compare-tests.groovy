1f455dfdc93e82c5df9bc620b7fd1df4d021e6bf
Puneet Kala <puneet.0191@gmail.com>
2019-12-03 18:46:54 +0530

    PMM-5039 Adding Job for UI Compare (#303)

---
+++ pmm/pmm2-ui-compare-tests.groovy
@@ -0,0 +1,150 @@
+void runStaging(String DOCKER_VERSION, CLIENT_VERSION, CLIENTS, CLIENT_INSTANCE, SERVER_IP) {
+    stagingJob = build job: 'aws-staging-start', parameters: [
+        string(name: 'DOCKER_VERSION', value: DOCKER_VERSION),
+        string(name: 'CLIENT_VERSION', value: CLIENT_VERSION),
+        string(name: 'CLIENTS', value: CLIENTS),
+        string(name: 'CLIENT_INSTANCE', value: CLIENT_INSTANCE),
+        string(name: 'SERVER_IP', value: SERVER_IP),
+        string(name: 'NOTIFY', value: 'false'),
+        string(name: 'DAYS', value: '1')
+    ]
+    env.VM_IP = stagingJob.buildVariables.IP
+    env.VM_NAME = stagingJob.buildVariables.VM_NAME
+    def clientInstance = "yes";
+    if ( CLIENT_INSTANCE == clientInstance ) {
+        env.PMM_URL = "http://admin:admin@${SERVER_IP}"
+        env.PMM_UI_URL = "https://${SERVER_IP}"
+    }
+    else
+    {
+        env.PMM_URL = "http://admin:admin@${VM_IP}"
+        env.PMM_UI_URL = "https://${VM_IP}/"
+    }
+}
+
+
+void destroyStaging(IP) {
+    build job: 'aws-staging-stop', parameters: [
+        string(name: 'VM', value: IP),
+    ]
+}
+
+pipeline {
+    agent {
+        label 'large-amazon'
+    }
+    parameters {
+        string(
+            defaultValue: 'PMM-5039',
+            description: 'Tag/Branch for pmm-qa repository',
+            name: 'GIT_BRANCH')
+        string(
+            defaultValue: 'perconalab/pmm-server:dev-latest',
+            description: 'PMM Server docker container version (image-name:version-tag)',
+            name: 'DOCKER_VERSION')
+        string(
+            defaultValue: 'dev-latest',
+            description: 'PMM Client version',
+            name: 'CLIENT_VERSION')
+        choice(
+            choices: ['no', 'yes'],
+            description: "Use this instance only as a client host",
+            name: 'CLIENT_INSTANCE')
+        string (
+            defaultValue: '',
+            description: 'Value for Server Public IP, to use this instance just as client',
+            name: 'SERVER_IP')
+    }
+    options {
+        skipDefaultCheckout()
+        disableConcurrentBuilds()
+    }
+    stages {
+        stage('Prepare') {
+            steps {
+                // clean up workspace and fetch pmm-qa repository
+                deleteDir()
+                git poll: false, branch: GIT_BRANCH, url: 'https://github.com/percona/pmm-qa.git'
+
+                slackSend channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
+
+                sh '''
+                    sudo yum -y update --security
+                    sudo yum -y install docker
+                    sudo usermod -aG docker ec2-user
+                    sudo service docker start
+                    curl https://intoli.com/install-google-chrome.sh | bash
+                    /usr/bin/google-chrome-stable --version
+                    sudo ln -s /usr/bin/google-chrome-stable /usr/bin/chromium
+                '''
+            }
+        }
+        stage('Start staging') {
+            steps {
+                runStaging(DOCKER_VERSION, CLIENT_VERSION, '--addclient=ps,1 --addclient=mo,2 --with-replica  --addclient=pgsql,1 --addclient=pxc,1 --with-proxysql --pmm2', CLIENT_INSTANCE, SERVER_IP)
+            }
+        }
+        stage('Sanity check') {
+            steps {
+                sh 'timeout 100 bash -c \'while [[ "$(curl -s -o /dev/null -w \'\'%{http_code}\'\' \${PMM_URL}/ping)" != "200" ]]; do sleep 5; done\' || false'
+            }
+        }
+        stage('Sleep') {
+            steps {
+                sleep 300
+            }
+        }
+        stage('Run Grafana Test') {
+            steps {
+                sauce('SauceLabsKey') {
+                    sauceconnect(options: '', sauceConnectPath: '') {
+                        sh """
+                            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+                            . ~/.nvm/nvm.sh
+                            nvm install 10.6.0
+                            sudo rm -f /usr/bin/node
+                            sudo ln -s ~/.nvm/versions/node/v10.6.0/bin/node /usr/bin/node
+                            npm install
+                            node -v
+                            npm -v
+                            ./node_modules/.bin/codeceptjs run --config codecept_puppeteer.json -o '{ "helpers": {"Puppeteer": {"url": "${PMM_UI_URL}"}}}' --steps --debug --reporter mochawesome --reporter-options cdn=true,inline=true
+                        """
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            // stop staging
+            sh '''
+                curl --insecure ${PMM_URL}/logs.zip --output logs.zip
+                sed -i 's+/mnt/workspace/pmm2-ui-compare-tests/+../+g' mochawesome-report/mochawesome.html
+            '''
+            destroyStaging(VM_NAME)
+            script {
+                if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
+                    saucePublisher()
+                    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'mochawesome-report/', reportFiles: 'mochawesome.html', reportName: 'HTML Report', reportTitles: ''])
+                    slackSend channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build finished - ${BUILD_URL} "
+                    archiveArtifacts artifacts: 'mochawesome-report/mochawesome.html'
+                    archiveArtifacts artifacts: 'logs.zip'
+                    archiveArtifacts artifacts: 'tests/output/*.png'
+                } else {
+                    saucePublisher()
+                    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'mochawesome-report/', reportFiles: 'mochawesome.html', reportName: 'HTML Report', reportTitles: ''])
+                    slackSend channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result} - ${BUILD_URL}"
+                    archiveArtifacts artifacts: 'mochawesome-report/mochawesome.html'
+                    archiveArtifacts artifacts: 'logs.zip'
+                    archiveArtifacts artifacts: 'tests/output/*.png'
+                    archiveArtifacts artifacts: 'tests/screenshots/base/*.png'
+                    archiveArtifacts artifacts: 'tests/screenshots/diff/*.png'
+                }
+            }
+            sh '''
+                sudo rm -r node_modules/
+            '''
+            deleteDir()
+        }
+    }
+}
