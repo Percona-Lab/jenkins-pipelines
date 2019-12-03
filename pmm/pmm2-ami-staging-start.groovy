b20670074cd0642aeabc2c4de6a361ba16de6a08
puneet0191 <puneet.0191@gmail.com>
2019-12-03 18:45:21 +0530

    PMM-4871 Adding extra wait seconds

--- pmm/pmm2-ami-staging-start.groovy
+++ pmm/pmm2-ami-staging-start.groovy
@@ -151,7 +151,7 @@ pipeline {
                         if [ -n "$SSH_KEY" ]; then
                             echo '$SSH_KEY' | ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no admin@\$(cat IP_PUBLIC) 'cat - >> .ssh/authorized_keys'
                         fi
-                        sleep 60
+                        sleep 300
                     """
                 } 
                 script {
