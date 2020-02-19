cab715365e053bc6ff744f5fc7718652b83437dd
Ivan Pylypenko <ivan.pylypenko@percona.com>
2020-02-19 18:20:23 +0200

    CLOUD-515 LKE test platform

---
+++ cloud/jenkins/pxc_operator_lke_version.groovy
@@ -0,0 +1,285 @@
+void CreateCluster(String CLUSTER_PREFIX) {
+    sh """
+            retry() {
+                local max=$1
+                local delay=$2
+                shift 2 # cut delay and max args
+                local n=1
+
+                until "$@"; do
+                    if [[ $n -ge $max ]]; then
+                        echo "The command '$@' has failed after $n attempts."
+                        exit 1
+                    fi
+                    ((n++))
+                    sleep $delay
+                done
+            }
+
+            CLUSTER_ID="$(linode-cli lke cluster-create --version ${LKE_VERSION} --label $CLUSTER_NAME-${CLUSTER_PREFIX} --region us-central --node_pools.count 3 --tags jenkins,$CLUSTER_NAME-${CLUSTER_PREFIX} --node_pools.type g6-standard-2 --json | jq '.[].id')"
+
+            KUBECONF=$(retry 10 60 linode-cli lke kubeconfig-view "${CLUSTER_ID}" --json)
+            echo ${KUBECONF} | jq '.[].kubeconfig' | sed 's/\"//g' | base64 -D > /tmp/$CLUSTER_NAME-${CLUSTER_PREFIX}
+            export KUBECONFIG=/tmp/$CLUSTER_NAME-${CLUSTER_PREFIX}
+        """
+}
+
+void ShutdownCluster(String CLUSTER_PREFIX) {
+    sh """
+        linode-cli lke cluster-delete $(linode-cli lke clusters-list --json | jq '.[] | select(.label == "'"${CLUSTER_NAME}-${CLUSTER_PREFIX}"'").id' )
+    """
+
+}
+void pushArtifactFile(String FILE_NAME) {
+    echo "Push $FILE_NAME file to S3!"
+
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+        sh """
+            touch ${FILE_NAME}
+            S3_PATH=s3://percona-jenkins-artifactory/\$JOB_NAME/${env.GIT_SHORT_COMMIT}
+            aws s3 ls \$S3_PATH/${FILE_NAME} || :
+            aws s3 cp --quiet ${FILE_NAME} \$S3_PATH/${FILE_NAME} || :
+        """
+    }
+}
+
+void popArtifactFile(String FILE_NAME) {
+    echo "Try to get $FILE_NAME file from S3!"
+
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+        sh """
+            S3_PATH=s3://percona-jenkins-artifactory/\$JOB_NAME/${env.GIT_SHORT_COMMIT}
+            aws s3 cp --quiet \$S3_PATH/${FILE_NAME} ${FILE_NAME} || :
+        """
+    }
+}
+
+testsResultsMap = [:]
+
+void setTestsresults() {
+    testsResultsMap.each { file ->
+        pushArtifactFile("${file.key}")
+    }
+}
+
+void runTest(String TEST_NAME, String CLUSTER_PREFIX) {
+    try {
+        echo "The $TEST_NAME test was started!"
+        popArtifactFile("${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}")
+        sh """
+            if [ -f "${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}" ]; then
+                echo Skip $TEST_NAME test
+            else
+                cd ./source
+                if [ -n "${PXC_OPERATOR_IMAGE}" ]; then
+                    export IMAGE=${PXC_OPERATOR_IMAGE}
+                else
+                    export IMAGE=perconalab/percona-xtradb-cluster-operator:${env.GIT_BRANCH}
+                fi
+
+                if [ -n "${IMAGE_PXC}" ]; then
+                    export IMAGE_PXC=${IMAGE_PXC}
+                fi
+
+                if [ -n "${IMAGE_PROXY}" ]; then
+                    export IMAGE_PROXY=${IMAGE_PROXY}
+                fi
+
+                if [ -n "${IMAGE_BACKUP}" ]; then
+                    export IMAGE_BACKUP=${IMAGE_BACKUP}
+                fi
+
+                if [ -n "${IMAGE_PMM}" ]; then
+                    export IMAGE_PMM=${IMAGE_PMM}
+                fi
+
+                export KUBECONFIG=/tmp/$CLUSTER_NAME-${CLUSTER_PREFIX}
+                ./e2e-tests/$TEST_NAME/run
+            fi
+        """
+        pushArtifactFile("${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}")
+        testsResultsMap["${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}"] = 'passed'
+    }
+    catch (exc) {
+        currentBuild.result = 'FAILURE'
+    }
+
+    echo "The $TEST_NAME test was finished!"
+}
+void installRpms() {
+    sh '''
+        sudo yum install -y https://repo.percona.com/yum/percona-release-latest.noarch.rpm || true
+        sudo percona-release enable-only tools
+        sudo yum install -y percona-xtrabackup-80 jq | true
+    '''
+}
+pipeline {
+    environment {
+        CLOUDSDK_CORE_DISABLE_PROMPTS = 1
+    }
+    parameters {
+        string(
+            defaultValue: 'master',
+            description: 'Tag/Branch for percona/percona-xtradb-cluster-operator repository',
+            name: 'GIT_BRANCH')
+        string(
+            defaultValue: 'https://github.com/percona/percona-xtradb-cluster-operator',
+            description: 'percona-xtradb-cluster-operator repository',
+            name: 'GIT_REPO')
+        string(
+            defaultValue: '1.14',
+            description: 'LKE version',
+            name: 'LKE_VERSION')
+        string(
+            defaultValue: '',
+            description: 'Operator image: perconalab/percona-xtradb-cluster-operator:master',
+            name: 'PXC_OPERATOR_IMAGE')
+        string(
+            defaultValue: '',
+            description: 'PXC image: perconalab/percona-xtradb-cluster-operator:master-pxc',
+            name: 'IMAGE_PXC')
+        string(
+            defaultValue: '',
+            description: 'PXC proxy image: perconalab/percona-xtradb-cluster-operator:master-proxysql',
+            name: 'IMAGE_PROXY')
+        string(
+            defaultValue: '',
+            description: 'Backup image: perconalab/percona-xtradb-cluster-operator:master-backup',
+            name: 'IMAGE_BACKUP')
+        string(
+            defaultValue: '',
+            description: 'PMM image: perconalab/percona-server-mongodb-operator:master-pmm',
+            name: 'IMAGE_PMM')
+    }
+    agent {
+        label 'docker'
+    }
+    options {
+        skipDefaultCheckout()
+        disableConcurrentBuilds()
+    }
+
+    stages {
+        stage('Prepare') {
+            steps {
+                git branch: 'master', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
+                sh """
+                    # sudo is needed for better node recovery after compilation failure
+                    # if building failed on compilation stage directory will have files owned by docker user
+                    sudo git reset --hard
+                    sudo git clean -xdf
+                    sudo rm -rf source
+                    ./cloud/local/checkout $GIT_REPO $GIT_BRANCH
+                """
+                stash includes: "source/**", name: "sourceFILES"
+
+                installRpms()
+                sh '''
+                    pip3 install --upgrade linode-cli
+
+                    curl -s https://storage.googleapis.com/kubernetes-helm/helm-v2.16.1-linux-amd64.tar.gz \
+                        | sudo tar -C /usr/local/bin --strip-components 1 -zvxpf -
+                    curl -s -L https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz \
+                        | sudo tar -C /usr/local/bin --strip-components 1 --wildcards -zxvpf - '*/oc'
+                '''
+                withCredentials([file(credentialsId: 'cloud-secret-file', variable: 'CLOUD_SECRET_FILE'), file(credentialsId: 'LINODE-CONFIG', variable: 'LKE_CLIENT_FILE')]) {
+                    sh '''
+                        mkdir ${HOME}/.config || true
+                        cp $CLOUD_SECRET_FILE ./source/e2e-tests/conf/cloud-secret.yml
+                        cp ${LKE_CLIENT_FILE} ${HOME}/.local/linode-cli
+                    '''
+                }
+            }
+        }
+        stage('Build docker image') {
+            steps {
+                unstash "sourceFILES"
+                withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                    sh '''
+                        if [ -n "${PXC_OPERATOR_IMAGE}" ]; then
+                            echo "SKIP: Build is not needed, PXC operator image was set!"
+                        else
+                            cd ./source/
+                            sg docker -c "
+                                docker login -u '${USER}' -p '${PASS}'
+                                export IMAGE=perconalab/percona-xtradb-cluster-operator:$GIT_BRANCH
+                                ./e2e-tests/build
+                                docker logout
+                            "
+                            sudo rm -rf ./build
+                        fi
+                    '''
+                }
+            }
+        }
+        stage('Run Tests') {
+            environment {
+                CLOUDSDK_CORE_DISABLE_PROMPTS = 1
+                GIT_SHORT_COMMIT = sh(script: 'git -C source rev-parse --short HEAD', , returnStdout: true).trim()
+                VERSION = "${env.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}"
+                CLUSTER_NAME = sh(script: "echo jenkins-pxc-${GIT_SHORT_COMMIT} | tr '[:upper:]' '[:lower:]'", , returnStdout: true).trim()
+            }
+            parallel {
+                stage('E2E Basic Tests') {
+                    steps {
+                        CreateCluster('basic')
+                        runTest('init-deploy', 'basic')
+                        runTest('storage', 'basic')
+                        runTest('limits', 'basic')
+                        runTest('monitoring', 'basic')
+                        runTest('monitoring-2-0', 'basic')
+                        runTest('affinity', 'basic')
+                        ShutdownCluster('basic')
+                   }
+                }
+                stage('E2E Scaling') {
+                    steps {
+                        CreateCluster('scaling')
+                        runTest('scaling', 'scaling')
+                        runTest('scaling-proxysql', 'scaling')
+                        runTest('upgrade', 'scaling')
+                        runTest('upgrade-consistency', 'scaling')
+                        ShutdownCluster('scaling')
+                    }
+                }
+                stage('E2E SelfHealing') {
+                    steps {
+                        CreateCluster('selfhealing')
+                        runTest('self-healing', 'selfhealing')
+                        runTest('self-healing-advanced', 'selfhealing')
+                        runTest('operator-self-healing', 'selfhealing')
+                        runTest('one-pod', 'selfhealing')
+                        runTest('auto-tuning', 'selfhealing')
+                        ShutdownCluster('selfhealing')
+                    }
+                }
+                stage('E2E Backups') {
+                    steps {
+                        CreateCluster('backups')
+                        runTest('recreate', 'backups')
+                        runTest('demand-backup', 'backups')
+                        runTest('scheduled-backup', 'backups')
+                        ShutdownCluster('backups')
+                    }
+                }
+                stage('E2E BigData') {
+                    steps {
+                        CreateCluster('bigdata')
+                        runTest('big-data', 'bigdata')
+                        ShutdownCluster('bigdata')
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            setTestsresults()
+            sh '''
+                sudo docker rmi -f \$(sudo docker images -q) || true
+                sudo rm -rf ./*
+            '''
+            deleteDir()
+        }
+    }
+}
