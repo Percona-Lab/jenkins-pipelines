936844c0630c368e43dfa75bd2a06faa522f447c
Mykyta Solomko <mykyta.solomko@percona.com>
2020-03-04 11:27:12 +0200

    Signed tags

--- pmm/pmm2-server-release.groovy
+++ pmm/pmm2-server-release.groovy
@@ -35,6 +35,9 @@ pipeline {
             defaultValue: '2.0.0',
             description: 'PMM2 Server version',
             name: 'VERSION')
+        string(
+            description: 'Password for PGP key',
+            name: 'PGP_KEY_PASS')
     }
     stages {
         stage('Get Docker RPMs') {
@@ -140,8 +143,13 @@ pipeline {
                                 rm -fr $package
                                 mkdir $package
                                 pushd $package >/dev/null
-                                    git clone https://github.com/${repo["$package"]} ./
+                                    git clone --recursive git@github.com:${repo["$package"]} ./
                                     git checkout $SHA
+                                    git update submodules
+                                    git tag -s v$VERSION \
+                                            -m "Version $VERSION"
+                                    git push --tags
+
                                     FULL_SHA=$(git rev-parse HEAD)
 
                                     echo "$FULL_SHA"
