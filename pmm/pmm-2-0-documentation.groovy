bda8d6edb41f0ac3d148e969336cdec38e6d4848
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-04-07 19:43:03 +0300

    ENG-814 Fix destination path

--- pmm/pmm-2-0-documentation.groovy
+++ pmm/pmm-2-0-documentation.groovy
@@ -51,7 +51,7 @@ pipeline {
                     sh '''
                         echo BRANCH=${BRANCH_NAME}
                         DEST_HOST='docs-rsync-endpoint.int.percona.com'
-                        rsync --delete-before -avzr -O -e "ssh -p2222 -i \${KEY_PATH}"  build/html/ \${USER}@\${DEST_HOST}:/data/websites_data/\${PUBLISH_TARGET}/doc/percona-monitoring-and-management/2.0/
+                        rsync --delete-before -avzr -O -e "ssh -o StrictHostKeyChecking=no -p2222 -i \${KEY_PATH}"  build/html/ \${USER}@\${DEST_HOST}:/data/websites_data/\${PUBLISH_TARGET}/doc/percona-monitoring-and-management/2.x/
                     '''
                 }
             }
