b009453f00f7ad2e6973646d0378f591181e2549
puneet0191 <puneet.0191@gmail.com>
2020-05-04 15:23:46 +0530

    PMM-5767 Fixing comments

--- pmm/pmm2-ui-tests.groovy
+++ pmm/pmm2-ui-tests.groovy
@@ -92,6 +92,12 @@ pipeline {
                 installDocker()
                 sh '''
                     sudo yum -y update --security
+                    sudo usermod -aG docker ec2-user
+                    sudo service docker start
+                    sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+                    sudo chmod +x /usr/local/bin/docker-compose
+                    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+                    sudo docker-compose --version
                     sudo yum -y install jq svn
                 '''
             }
