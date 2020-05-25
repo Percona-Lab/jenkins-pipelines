546132d8863306eed93b0b7c0186f92d1db88800
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-05-25 13:28:23 +0300

    CLOUD-541 Fix openshift

--- cloud/jenkins/psmdb_operator_aws_openshift-4.groovy
+++ cloud/jenkins/psmdb_operator_aws_openshift-4.groovy
@@ -188,7 +188,7 @@ pipeline {
         }
         stage('Create AWS Infrastructure') {
             steps {
-                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'openshift-cicd'], file(credentialsId: 'aws-openshift-41-key-pub', variable: 'AWS_NODES_KEY_PUB'), file(credentialsId: 'psmdb-openshift-secret-file', variable: 'OPENSHIFT_CONF_FILE')]) {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'openshift-cicd'], file(credentialsId: 'aws-openshift-41-key-pub', variable: 'AWS_NODES_KEY_PUB'), file(credentialsId: 'psmdb-openshift4-secret-file', variable: 'OPENSHIFT_CONF_FILE')]) {
                      sh """
                          mkdir openshift
                          cp $OPENSHIFT_CONF_FILE ./openshift/install-config.yaml
