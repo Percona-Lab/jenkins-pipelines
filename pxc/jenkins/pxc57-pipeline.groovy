8f30e0e7c28f9296cab366bf5dcb39880b214b35
rameshvs02 <rameshvs02@gmail.com>
2020-06-02 22:02:18 +0530

    Bug fix : Added CentOS8 in pxc57-pipeline.groovy

--- pxc/jenkins/pxc57-pipeline.groovy
+++ pxc/jenkins/pxc57-pipeline.groovy
@@ -33,7 +33,7 @@ pipeline {
 	        name: 'GALERA3_BRANCH',
 	        trim: true)
         choice(
-            choices: 'centos:6\ncentos:7\nubuntu:xenial\nubuntu:bionic\ndebian:jessie\ndebian:stretch',
+            choices: 'centos:6\ncentos:7\ncentos:8\nubuntu:xenial\nubuntu:bionic\nubuntu:focal\ndebian:jessie\ndebian:stretch\ndebian:buster',
             description: 'OS version for compilation',
             name: 'DOCKER_OS')
         choice(
