a5fd046f97c42d1b18a2d4bd897dcb6d029273ff
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-06-04 10:24:10 +0300

    PPG jenkins jobs.

---
+++ ppg/ppg-11-with-vanila-components-parallel.groovy
@@ -0,0 +1,68 @@
+library changelog: false, identifier: "lib@master", retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+])
+
+def moleculeDir = "molecule/ppg/pg-11-with-vanila-components"
+pipeline {
+  agent {
+      label 'micro-amazon'
+  }
+  environment {
+      PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin'
+  }
+  parameters {
+        choice(
+            name: 'REPO',
+            description: 'Repo for testing',
+            choices: [
+                'testing',
+                'release',
+                'experimental'
+            ]
+        )
+        choice(
+            name: 'VERSION',
+            description: 'PG version for test',
+            choices: [
+                'ppg-11.5',
+                'ppg-11.6',
+                'ppg-11.7',
+                'ppg-11.8'
+            ]
+        )
+  }
+  options {
+          withCredentials(moleculeDistributionJenkinsCreds())
+          disableConcurrentBuilds()
+  }
+    stages {
+        stage('Checkout') {
+          steps {
+                deleteDir()
+                git poll: false, branch: 'master', url: 'https://github.com/Percona-QA/package-testing.git'
+            }
+        }
+        stage ('Prepare') {
+          steps {
+                script {
+                   installMolecule()
+             }
+           }
+        }
+        stage('Test') {
+          steps {
+                script {
+                    moleculeParallelTest(ppgOperatingSystems(), moleculeDir)
+                }
+            }
+         }
+  }
+    post {
+    always {
+      script {
+          moleculeParallelPostDestroy(ppgOperatingSystems(), moleculeDir)
+         }
+      }
+   }
+}
