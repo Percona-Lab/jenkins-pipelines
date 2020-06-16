08844a139e2c79293afa11d5eb048ccc68a16697
Puneet Kala <puneet.0191@gmail.com>
2020-06-16 12:43:26 +0530

    PMM-6072 Adding Upgrade Job and upgrade matrix job (#466)
    * PMM-6072 Adding Upgrade test, upgrade matrix.
    
    * PMM-6072 adding Cron

---
+++ pmm/pmm2-upgrade-tests-matrix.groovy
@@ -0,0 +1,97 @@
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+]) _
+void runUpgradeJob(String GIT_BRANCH, DOCKER_VERSION, CLIENT_VERSION, PMM_SERVER_LATEST) {
+    upgradeJob = build job: 'pmm2-upgrade-tests', parameters: [
+        string(name: 'GIT_BRANCH', value: GIT_BRANCH),
+        string(name: 'CLIENT_VERSION', value: CLIENT_VERSION),
+        string(name: 'DOCKER_VERSION', value: DOCKER_VERSION),
+        string(name: 'PMM_SERVER_LATEST', value: PMM_SERVER_LATEST)
+    ]
+}
+
+pipeline {
+    agent {
+        label 'large-amazon'
+    }
+    parameters {
+        string(
+            defaultValue: 'PMM-2.0',
+            description: 'Tag/Branch for grafana-dashboards repository',
+            name: 'GIT_BRANCH')
+        string(
+            defaultValue: '2.7.1',
+            description: 'dev-latest PMM Server Version',
+            name: 'PMM_SERVER_LATEST')
+    }
+    options {
+        skipDefaultCheckout()
+        disableConcurrentBuilds()
+    }
+    triggers { cron('0 0 * * SAT') }
+    stages {
+        stage('Run Upgrade Matrix-1') {
+            parallel {
+                stage('Upgrade from 2.3.0'){
+                    steps {
+                        script {
+                            runUpgradeJob(GIT_BRANCH,'2.3.0', '2.3.0', PMM_SERVER_LATEST );
+                        }
+                    }
+                }
+                stage('Upgrade from 2.4.0'){
+                    steps {
+                        script {
+                            runUpgradeJob(GIT_BRANCH,'2.4.0', '2.4.0', PMM_SERVER_LATEST );
+                        }
+                    }
+                }
+                stage('Upgrade from 2.5.0'){
+                    steps {
+                        script {
+                            runUpgradeJob(GIT_BRANCH,'2.5.0', '2.5.0', PMM_SERVER_LATEST );
+                        }
+                    }
+                }
+            }
+        }
+        stage('Run Upgrade Matrix-2') {
+            parallel {
+                stage('Upgrade from 2.6.0'){
+                    steps {
+                        script {
+                            runUpgradeJob(GIT_BRANCH,'2.6.0', '2.6.0', PMM_SERVER_LATEST );
+                        }
+                    }
+                }
+                stage('Upgrade from 2.6.1'){
+                    steps {
+                        script {
+                            runUpgradeJob(GIT_BRANCH,'2.6.1', '2.6.1', PMM_SERVER_LATEST );
+                        }
+                    }
+                }
+                stage('Upgrade from 2.7.0'){
+                    steps {
+                        script {
+                            runUpgradeJob(GIT_BRANCH,'2.7.0', '2.7.0', PMM_SERVER_LATEST );
+                        }
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            script {
+                if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
+                    slackSend channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build finished - ${BUILD_URL} "
+                } else {
+                    slackSend channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result} - ${BUILD_URL}"
+                }
+            }
+            deleteDir()
+        }
+    }
+}
---
+++ pmm/pmm2-upgrade-tests.groovy
@@ -0,0 +1,189 @@
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+]) _
+void runStaging(String DOCKER_VERSION, CLIENT_VERSION, CLIENTS) {
+    stagingJob = build job: 'aws-staging-start', parameters: [
+        string(name: 'DOCKER_VERSION', value: "percona/pmm-server:${DOCKER_VERSION}"),
+        string(name: 'CLIENT_VERSION', value: CLIENT_VERSION),
+        string(name: 'DOCKER_ENV_VARIABLE', value: '-e DISABLE_TELEMETRY=true -e DATA_RETENTION=48h'),
+        string(name: 'CLIENTS', value: CLIENTS),
+        string(name: 'NOTIFY', value: 'false'),
+        string(name: 'DAYS', value: '1')
+    ]
+    env.VM_IP = stagingJob.buildVariables.IP
+    env.VM_NAME = stagingJob.buildVariables.VM_NAME
+    env.PMM_URL = "http://admin:admin@${VM_IP}"
+    env.PMM_UI_URL = "http://${VM_IP}"
+}
+
+void destroyStaging(IP) {
+    build job: 'aws-staging-stop', parameters: [
+        string(name: 'VM', value: IP),
+    ]
+}
+
+void checkUpgrade(String PMM_VERSION) {
+    withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
+        sh """
+            ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no ${USER}@${VM_IP} '
+                export PMM_VERSION=${PMM_VERSION}
+                echo "Checking";
+                sudo chmod 755 /srv/pmm-qa/pmm-tests/check_upgrade.sh
+                bash -xe /srv/pmm-qa/pmm-tests/check_upgrade.sh ${PMM_VERSION}
+            '
+        """
+    }
+}
+
+pipeline {
+    agent {
+        label 'large-amazon'
+    }
+    environment {
+        REMOTE_AWS_MYSQL_USER=credentials('pmm-dev-mysql-remote-user')
+        REMOTE_AWS_MYSQL_PASSWORD=credentials('pmm-dev-remote-password')
+        REMOTE_AWS_MYSQL57_HOST=credentials('pmm-dev-mysql57-remote-host')
+        REMOTE_MYSQL_HOST=credentials('mysql-remote-host')
+        REMOTE_MYSQL_USER=credentials('mysql-remote-user')
+        REMOTE_MYSQL_PASSWORD=credentials('mysql-remote-password')
+        REMOTE_MONGODB_HOST=credentials('qa-remote-mongodb-host')
+        REMOTE_MONGODB_USER=credentials('qa-remote-mongodb-user')
+        REMOTE_MONGODB_PASSWORD=credentials('qa-remote-mongodb-password')
+        REMOTE_POSTGRESQL_HOST=credentials('qa-remote-pgsql-host')
+        REMOTE_POSTGRESQL_USER=credentials('qa-remote-pgsql-user')
+        REMOTE_POSTGRESSQL_PASSWORD=credentials('qa-remote-pgsql-password')
+        REMOTE_PROXYSQL_HOST=credentials('qa-remote-proxysql-host')
+        REMOTE_PROXYSQL_USER=credentials('qa-remote-proxysql-user')
+        REMOTE_PROXYSQL_PASSWORD=credentials('qa-remote-proxysql-password')
+    }
+    parameters {
+        string(
+            defaultValue: 'PMM-2.0',
+            description: 'Tag/Branch for grafana-dashboards repository',
+            name: 'GIT_BRANCH')
+        choice(
+            choices: ['2.3.0', '2.4.0', '2.5.0', '2.6.0', '2.6.1', '2.7.0'],
+            description: 'PMM Server Version to test for Upgrade',
+            name: 'DOCKER_VERSION')
+        choice(
+            choices: ['2.3.0', '2.4.0', '2.5.0', '2.6.0', '2.6.1', '2.7.0'],
+            description: 'PMM Client Version to test for Upgrade',
+            name: 'CLIENT_VERSION')
+        string(
+            defaultValue: '2.7.1',
+            description: 'dev-latest PMM Server Version',
+            name: 'PMM_SERVER_LATEST')
+    }
+    options {
+        skipDefaultCheckout()
+    }
+    stages {
+        stage('Prepare') {
+            steps {
+                // clean up workspace and fetch pmm-qa repository
+                deleteDir()
+                git poll: false, branch: GIT_BRANCH, url: 'https://github.com/percona/grafana-dashboards.git'
+
+                slackSend channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
+                installDocker()
+                sh '''
+                    sudo yum -y update --security
+                    sudo yum -y install jq svn
+                    sudo usermod -aG docker ec2-user
+                    sudo service docker start
+                    sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+                    sudo chmod +x /usr/local/bin/docker-compose
+                    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+                    sudo docker-compose --version
+                '''
+            }
+        }
+        stage('Start staging') {
+            steps {
+                runStaging(DOCKER_VERSION, CLIENT_VERSION, '--addclient=ps,1')
+            }
+        }
+        stage('Sanity check') {
+            steps {
+                sh 'timeout 100 bash -c \'while [[ "$(curl -s -o /dev/null -w \'\'%{http_code}\'\' \${PMM_URL}/ping)" != "200" ]]; do sleep 5; done\' || false'
+            }
+        }
+        stage('Sleep') {
+            steps {
+                sleep 60
+            }
+        }
+        stage('Check Packages before Upgrade') {
+            steps {
+                checkUpgrade(DOCKER_VERSION);
+            }
+        }
+        stage('Run Upgrade Tests & Remote Instances Tests') {
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'PMM_AWS_DEV', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    sh """
+                        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+                        . ~/.nvm/nvm.sh
+                        nvm install 12.14.1
+                        sudo rm -f /usr/bin/node
+                        sudo ln -s ~/.nvm/versions/node/v12.14.1/bin/node /usr/bin/node
+                        pushd pmm-app/
+                        npm install
+                        node -v
+                        npm -v
+                        sudo yum install -y gettext
+                        envsubst < env.list > env.generated.list
+                        popd
+                        pushd pmm-app/
+                        sed -i 's+http://localhost/+${PMM_UI_URL}/+g' pr.codecept.js
+                        export PWD=\$(pwd);
+                        sudo docker run -e PMM_SERVER_LATEST=${PMM_SERVER_LATEST} -e DOCKER_VERSION=${DOCKER_VERSION}  --env VM_IP=${VM_IP} --env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --env-file env.generated.list --net=host -v \$PWD:/tests codeception/codeceptjs:2.6.1 codeceptjs run-multiple parallel --debug --steps --reporter mocha-multi -c pr.codecept.js --grep '@pmm-upgrade'
+                        popd
+                    """
+                    }
+                }
+        }
+        stage('Check Packages after Upgrade') {
+            steps {
+                checkUpgrade(PMM_SERVER_LATEST);
+            }
+        }
+    }
+    post {
+        always {
+            // stop staging
+            sh '''
+                curl --insecure ${PMM_URL}/logs.zip --output logs.zip
+            '''
+            destroyStaging(VM_NAME)
+            sh '''
+                sudo chmod 777 -R pmm-app/tests/output
+                ./pmm-app/node_modules/.bin/mochawesome-merge pmm-app/tests/output/parallel_chunk*/*.json > pmm-app/tests/output/combine_results.json
+                ./pmm-app/node_modules/.bin/marge pmm-app/tests/output/combine_results.json --reportDir pmm-app/tests/output/ --inline --cdn --charts
+            '''
+            script {
+                if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
+                    junit 'pmm-app/tests/output/parallel_chunk*/*.xml'
+                    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'pmm-app/tests/output/', reportFiles: 'combine_results.html', reportName: 'HTML Report', reportTitles: ''])
+                    slackSend channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build finished - ${BUILD_URL} "
+                    archiveArtifacts artifacts: 'pmm-app/tests/output/combine_results.html'
+                    archiveArtifacts artifacts: 'logs.zip'
+                } else {
+                    junit 'pmm-app/tests/output/parallel_chunk*/*.xml'
+                    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'pmm-app/tests/output/', reportFiles: 'combine_results.html', reportName: 'HTML Report', reportTitles: ''])
+                    slackSend channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result} - ${BUILD_URL}"
+                    archiveArtifacts artifacts: 'pmm-app/tests/output/combine_results.html'
+                    archiveArtifacts artifacts: 'logs.zip'
+                    archiveArtifacts artifacts: 'pmm-app/tests/output/parallel_chunk*/*.png'
+                    archiveArtifacts artifacts: 'pmm-app/tests/output/video/*.mp4'
+                }
+            }
+            sh '''
+                sudo rm -r pmm-app/node_modules/
+                sudo rm -r pmm-app/tests/output
+            '''
+            deleteDir()
+        }
+    }
+}
