cdf91d20fa2821df0c1e462048b3c0c6ba2d0b53
rameshvs02 <rameshvs02@gmail.com>
2020-06-29 16:34:26 +0530

    Updates: PXC-57 QA framework pipeline job

--- pxc/jenkins/qa-pxc57-pipeline.groovy
+++ pxc/jenkins/qa-pxc57-pipeline.groovy
@@ -86,7 +86,7 @@ pipeline {
                 stage('Build PXB24') {
                     agent { label 'docker' }
                     steps {
-                        git branch: 'PXC-3309-Add-PXCQA-pipeline-jobs', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
+                        git branch: 'master', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
                         echo 'Checkout PXB24 sources'
                         sh '''
                             # sudo is needed for better node recovery after compilation failure
@@ -123,7 +123,7 @@ pipeline {
         stage('Build PXC57') {
                 agent { label 'docker-32gb' }
                 steps {
-                    git branch: 'PXC-3309-Add-PXCQA-pipeline-jobs', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
+                    git branch: 'master', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
                     echo 'Checkout PXC57 sources'
                     sh '''
                         # sudo is needed for better node recovery after compilation failure
@@ -159,7 +159,7 @@ pipeline {
         stage('Test PXC57') {
                 agent { label 'docker-32gb' }
                 steps {
-                    git branch: 'PXC-3309-Add-PXCQA-pipeline-jobs', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
+                    git branch: 'master', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
                     echo 'Test PXC57'
                     withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'c42456e5-c28d-4962-b32c-b75d161bff27', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                         sh '''
