8a99c2f24671a9ed737a594fff45e93b52f815d1
cap1984 <ivan.pylypenko@percona.com>
2020-07-09 12:46:21 +0300

    K8SPXC-356 Tests enhancement
    New functional test has been introduced
    Test timeout has been set

--- cloud/jenkins/pxc_operator_minikube.groovy
+++ cloud/jenkins/pxc_operator_minikube.groovy
@@ -233,6 +233,7 @@ pipeline {
                     runTest('upgrade-consistency')
                     runTest('self-healing-advanced')
                     runTest('operator-self-healing')
+                    runTest('users')
             }
             post {
                 always {
