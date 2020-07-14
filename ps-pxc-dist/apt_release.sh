93b486ef203953f7a38d7d419214901b9e0ae90d
Illia P <illia.pshonkin@percona.com>
2020-07-14 16:32:33 +0300

    DISTMYSQL-36: Create pipeline for releasing distributions

---
+++ ps-pxc-dist/apt_release.sh
@@ -0,0 +1,103 @@
+export GPG_TTY=$(tty)
+echo GPG_TTY is ${GPG_TTY}
+#
+DIRECTORIES=""
+source /tmp/args_pipeline
+
+# Create list of packages to push:
+for dir in $(printenv | grep _PATH | sed 's/^.*=//g'); do
+    DIRECTORIES+=" ${dir}"
+done
+
+if [[ -z "${DIRECTORIES}" ]]; then
+    echo "No PATH specified..."
+    exit 1
+fi
+#
+REPOCOMP=$(echo "${COMPONENT}" | tr '[:upper:]' '[:lower:]')
+REPOPUSH_ARGS=""
+
+#if [[ ${REMOVE_BEFORE_PUSH} = true ]] || [[ ${COMPONENT} != RELEASE ]]; then
+if [[ ${REMOVE_BEFORE_PUSH} = true ]]; then
+    REPOPUSH_ARGS+=" --remove-package"
+fi
+#fi
+#
+#
+if [[ ${REPOSITORY} == "PDPXC" ]]; then
+  REPOPATH_TMP="/srv/repo-copy/pdpxc-${REPOSITORY_VERSION}/apt"
+  if [ $REPOSITORY_VERSION_MAJOR == true ]; then
+    REPOPATH_TMP+=" /srv/repo-copy/pdpxc-$(echo ${REPOSITORY_VERSION} | awk -F"." 'BEGIN { OFS = "." }{ print $1, $2}' | sed 's/\.$//')/apt"
+  fi
+  export PATH="/usr/local/reprepro5/bin:${PATH}"
+fi
+
+if [[ ${REPOSITORY} == "PDPS" ]]; then
+  REPOPATH_TMP="/srv/repo-copy/pdps-${REPOSITORY_VERSION}/apt"
+  if [ $REPOSITORY_VERSION_MAJOR == true ]; then
+    REPOPATH_TMP+=" /srv/repo-copy/pdps-$(echo ${REPOSITORY_VERSION} | awk -F"." 'BEGIN { OFS = "." }{ print $1, $2}' | sed 's/\.$//')/apt"
+  fi
+  export PATH="/usr/local/reprepro5/bin:${PATH}"
+fi
+#
+for REPOPATH in $REPOPATH_TMP; do
+    for dir in $DIRECTORIES; do
+        #
+        set -e
+        echo "<*> reprepro binary is $(which reprepro)"
+        pushd /srv/UPLOAD/$dir/binary/debian
+        echo "Looking for Debian build directories..."
+        if [[ ${COMPONENT} != RELEASE ]]; then
+            CODENAMES=$(ls -1)
+        fi
+
+        echo Distributions are: ${CODENAMES}
+        popd
+        #
+        #######################################
+        # source pushing, it's a bit specific #
+        #######################################
+        #
+        if [[ ${REMOVE_LOCKFILE} == true ]]; then
+            echo "Removing lock file as requested..."
+            sudo rm -vf ${REPOPATH}/db/lockfile
+        fi
+        #
+        if [[ ${REPOCOMP} == release ]]; then
+            export REPOCOMP=main
+        # pushing sources
+            if [ -d /srv/UPLOAD/$dir/source/debian ]; then
+                cd /srv/UPLOAD/$dir/source/debian
+                DSC=$(find . -type f -name '*.dsc')
+                for DSC_FILE in ${DSC}; do
+                    echo "DSC file is ${DSC_FILE}"
+                    for _codename in ${CODENAMES}; do
+                        echo "===>DSC $DSC_FILE"
+                        repopush --gpg-pass=${SIGN_PASSWORD} --package=${DSC_FILE} --repo-path=${REPOPATH} --component=${REPOCOMP}  --codename=${_codename} --verbose ${REPOPUSH_ARGS}
+                        sleep 1
+                    done
+                done
+            fi
+        fi
+
+        #
+        #######################################
+        # binary pushing                      #
+        #######################################
+        cd /srv/UPLOAD/$dir/binary/debian
+
+        for _codename in ${CODENAMES}; do
+            pushd ${_codename}
+                DEBS=$(find . -type f -name '*.*deb' )
+                for _deb in ${DEBS}; do
+                    repopush --gpg-pass=${SIGN_PASSWORD} --package=${_deb} --repo-path=${REPOPATH} --component=${REPOCOMP} --codename=${_codename} --verbose ${REPOPUSH_ARGS}
+                done
+            #
+            popd
+        done
+        #
+    done
+done
+
+echo "Quickly checking pool integrity..."
+#reprepro -b ${REPOPATH} checkpool fast || exit $?
---
+++ ps-pxc-dist/rpm_release.sh
@@ -0,0 +1,66 @@
+set -x
+set -e
+#
+DIRECTORIES=""
+source /tmp/args_pipeline
+
+# Create list of packages to push:
+for dir in $(printenv | grep _PATH | sed 's/^.*=//g'); do
+    DIRECTORIES+=" ${dir}"
+done
+
+if [[ -z "${DIRECTORIES}" ]]; then
+    echo "No PATH specified..."
+    exit 1
+fi
+#
+REPOCOMP=$(echo "${COMPONENT}" | tr '[:upper:]' '[:lower:]')
+REPOPATH=${REPOSITORY}
+#
+if [[ ${REPOSITORY} == "PDPXC" ]]; then
+  REPOPATH_TMP="repo-copy/pdpxc-${REPOSITORY_VERSION}/yum"
+  if [ $REPOSITORY_VERSION_MAJOR == true ]; then
+    REPOPATH_TMP+=" repo-copy/pdpxc-$(echo ${REPOSITORY_VERSION} | awk -F"." 'BEGIN { OFS = "." }{ print $1, $2}' | sed 's/\.$//')/yum"
+  fi
+fi
+#
+if [[ ${REPOSITORY} == "PDPS" ]]; then
+  REPOPATH_TMP="repo-copy/pdps-${REPOSITORY_VERSION}/yum"
+  if [ $REPOSITORY_VERSION_MAJOR == true ]; then
+    REPOPATH_TMP+=" repo-copy/pdps-$(echo ${REPOSITORY_VERSION} | awk -F"." 'BEGIN { OFS = "." }{ print $1, $2}' | sed 's/\.$//')/yum"
+  fi
+fi
+#
+for REPOPATH in $REPOPATH_TMP; do
+    for dir in $DIRECTORIES; do
+        #
+        cd /srv/UPLOAD/${dir}
+        #
+        # getting the list of RH systems
+        RHVERS=$(ls -1 binary/redhat)
+        #
+        # source processing
+        if [ -d /srv/UPLOAD/$dir/source/redhat ]; then
+          SRCRPM=$(find source/redhat -name '*.src.rpm')
+          for rhel in ${RHVERS}; do 
+              mkdir -p /srv/${REPOPATH}/${REPOCOMP}/${rhel}/SRPMS
+              cp -v ${SRCRPM} /srv/${REPOPATH}/${REPOCOMP}/${rhel}/SRPMS
+          done
+        fi
+        #
+        # binary processing
+        pushd binary
+        for rhel in ${RHVERS}; do
+            mkdir -p /srv/${REPOPATH}/${REPOCOMP}/${rhel}/RPMS
+            for arch in $(ls -1 redhat/${rhel}); do
+                mkdir -p /srv/${REPOPATH}/${REPOCOMP}/${rhel}/RPMS/${arch}
+                cp -av redhat/${rhel}/${arch}/*.rpm /srv/${REPOPATH}/${REPOCOMP}/${rhel}/RPMS/${arch}/
+            done
+        done
+        #
+    done
+    #
+    createrepo --update /srv/${REPOPATH}/${REPOCOMP}/${rhel}/SRPMS
+    createrepo --update /srv/${REPOPATH}/${REPOCOMP}/${rhel}/RPMS/${arch}/
+    #
+done
