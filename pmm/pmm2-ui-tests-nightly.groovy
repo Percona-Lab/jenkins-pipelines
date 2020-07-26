d3464e5def91e2dba080bf651288e12532e33b01
puneet0191 <puneet.0191@gmail.com>
2020-07-26 16:47:47 +0530

    PMM-6263 add extra wait

--- pmm/pmm2-ui-tests-nightly.groovy
+++ pmm/pmm2-ui-tests-nightly.groovy
@@ -170,7 +170,7 @@ pipeline {
                 curl --data '{"enable_stt": true, "enable_telemetry": true}' -u admin:admin -X POST ${PMM_UI_URL}/v1/Settings/Change
                 curl -u admin:admin -X POST ${PMM_UI_URL}/v1/management/SecurityChecks/Start
                 """
-                sleep 300
+                sleep 600
                 sh """
                 curl --data '{"disable_stt": true, "enable_telemetry": true}' -u admin:admin -X POST ${PMM_UI_URL}/v1/Settings/Change
                 """
