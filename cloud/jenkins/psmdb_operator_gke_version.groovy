b61fcc4d828d318b4f477b6905d7cfb2056a275e
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-08-04 17:28:58 +0300

    K8SPSMDB-240 Add psmdb tag to cached tests

--- cloud/jenkins/psmdb_operator_gke_version.groovy
+++ cloud/jenkins/psmdb_operator_gke_version.groovy
@@ -76,10 +76,11 @@ void runTest(String TEST_NAME, String CLUSTER_PREFIX) {
     try {
         echo "The $TEST_NAME test was started!"
 
-        popArtifactFile("${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}")
+        MDB_TAG = sh(script: "if [ -n \"\${IMAGE_MONGOD}\" ] ; then echo ${IMAGE_MONGOD} | awk -F':' '{print \$2}'; else echo 'master'; fi", , returnStdout: true).trim()
+        popArtifactFile("${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}-$MDB_TAG")
 
         sh """
-            if [ -f "${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}" ]; then
+            if [ -f "${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}-$MDB_TAG" ]; then
                 echo Skip $TEST_NAME test
             else
                 cd ./source
@@ -106,8 +107,8 @@ void runTest(String TEST_NAME, String CLUSTER_PREFIX) {
                 ./e2e-tests/$TEST_NAME/run
             fi
         """
-        pushArtifactFile("${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}")
-        testsResultsMap["${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}"] = 'passed'
+        pushArtifactFile("${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}-$MDB_TAG")
+        testsResultsMap["${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}-$MDB_TAG"] = 'passed'
     }
     catch (exc) {
         currentBuild.result = 'FAILURE'
