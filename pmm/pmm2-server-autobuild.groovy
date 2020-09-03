a842ad844ef338869dd70e060f48d251b3a30317
Illia P <Sudokamikaze@users.noreply.github.com>
2020-09-03 15:17:45 +0300

    PMM-6212: Build dbaas-tools as well (#552)

--- pmm/pmm2-server-autobuild.groovy
+++ pmm/pmm2-server-autobuild.groovy
@@ -118,6 +118,7 @@ pipeline {
                             build-server-rpm pmm-server
                             build-server-rpm pmm-update
                             build-server-rpm dbaas-controller
+                            build-server-rpm dbaas-tools
 
                             # 3rd-party
                             build-server-rpm clickhouse
