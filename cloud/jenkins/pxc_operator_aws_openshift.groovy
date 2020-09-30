1c56d833be2399d7d26106517895aa5f6a6c7cf2
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-09-30 12:20:21 +0300

    Remove openshift

--- cloud/jenkins/pxc_operator_aws_openshift.groovy
+++ cloud/jenkins/pxc_operator_aws_openshift.groovy
@@ -190,36 +190,6 @@ pipeline {
 
             }
         }
-        stage('Build docker image') {
-            steps {
-                git branch: 'master', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
-                withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER'), file(credentialsId: 'cloud-secret-file', variable: 'CLOUD_SECRET_FILE')]) {
-                    sh '''
-                        # sudo is needed for better node recovery after compilation failure
-                        # if building failed on compilation stage directory will have files owned by docker user
-                        sudo git reset --hard
-                        sudo git clean -xdf
-                        sudo rm -rf source
-                        ./cloud/local/checkout $GIT_REPO $GIT_BRANCH
-
-                        cp $CLOUD_SECRET_FILE ./source/e2e-tests/conf/cloud-secret.yml
-
-                        if [ -n "${PXC_OPERATOR_IMAGE}" ]; then
-                            echo "SKIP: Build is not needed, PXC operator image was set!"
-                        else
-                            cd ./source/
-                            sg docker -c "
-                                docker login -u '${USER}' -p '${PASS}'
-                                export IMAGE=perconalab/percona-xtradb-cluster-operator:$GIT_BRANCH
-                                ./e2e-tests/build
-                                docker logout
-                            "
-                            sudo rm -rf ./build
-                        fi
-                    '''
-                }
-            }
-        }
         stage('Create AWS Infrastructure') {
             steps {
                 IsRunTestsInClusterWide()
@@ -230,97 +200,6 @@ pipeline {
                         sudo git reset --hard
                         sudo git clean -xdf
                     """
-
-                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'openshift-cicd'], file(credentialsId: 'aws-openshift-key-pub', variable: 'AWS_NODES_KEY_PUB')]) {
-                     sshagent(['aws-openshift-key']) {
-                         sh """
-                            pushd ./aws-openshift-automation
-                                make infrastructure
-                                sleep 400
-                            popd
-                    """
-                    }
-               }
-
-            }
-        }
-        stage('Install and conigure Openshift') {
-            steps {
-                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'openshift-cicd'], file(credentialsId: 'aws-openshift-key-pub', variable: 'AWS_NODES_KEY_PUB')]) {
-                     sshagent(['aws-openshift-key']) {
-                         sh """
-                            pushd ./aws-openshift-automation
-                                make openshift
-                            popd
-                         """
-                    }
-                    retry(3) {
-                        sh """
-                            pushd ./aws-openshift-automation
-                                sleep 120
-                                oc login \$(terraform output master-url) --insecure-skip-tls-verify=true -u=real-admin -p=123
-                            popd
-                        """
-                    }
-                }
-            }
-        }
-        stage('E2E Basic Tests') {
-            options {
-                timeout(time: 3, unit: 'HOURS')
-            }
-            steps {
-                runTest('init-deploy')
-                runTest('limits')
-                runTest('monitoring')
-                runTest('affinity')
-                runTest('one-pod')
-                runTest('auto-tuning')
-                runTest('proxysql-sidecar-res-limits')
-                runTest('users')
-                runTest('haproxy')
-            }
-        }
-        stage('E2E Scaling') {
-            options {
-                timeout(time: 3, unit: 'HOURS')
-            }
-            steps {
-                runTest('scaling')
-                runTest('scaling-proxysql')
-                runTest('upgrade')
-                runTest('upgrade-consistency')
-                runTest('security-context')
-            }
-        }
-        stage('E2E Backups') {
-            options {
-                timeout(time: 3, unit: 'HOURS')
-            }
-            steps {
-                runTest('recreate')
-                runTest('restore-to-encrypted-cluster')
-                runTest('demand-backup')
-                runTest('demand-backup-encrypted-with-tls')
-                runTest('scheduled-backup')
-            }
-        }
-        stage('E2E BigData') {
-            options {
-                timeout(time: 3, unit: 'HOURS')
-            }
-            steps {
-                runTest('big-data')
-            }
-        }
-        stage('Make report') {
-            steps {
-                makeReport()
-                sh """
-                    echo "${TestsReport}" > TestsReport.xml
-                """
-                step([$class: 'JUnitResultArchiver', testResults: '*.xml', healthScaleFactor: 1.0])
-                archiveArtifacts '*.xml'
             }
         }
     }
