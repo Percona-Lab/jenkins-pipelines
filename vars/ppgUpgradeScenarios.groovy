30b1f250f5d1587150ab545315953ceb1f70d2e5
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-10-02 21:56:05 +0300

    PPG. Add ppg-13 options.

--- vars/ppgUpgradeScenarios.groovy
+++ vars/ppgUpgradeScenarios.groovy
@@ -1,3 +1,4 @@
 def call() {
-  return ['pg-11-minor-upgrade', 'pg-12-minor-upgrade', 'pg-12-major-upgrade']
+  return ['pg-11-minor-upgrade', 'pg-12-minor-upgrade', 'pg-12-major-upgrade',
+   'pg-13-minor-upgrade', 'pg-13-major-upgrade']
 }
