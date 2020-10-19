29340c23f33cbe7337a0ad50fc047310d2835e35
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-10-19 12:08:37 +0300

    Use newer versions for molecule and virtualenv.

--- vars/installMolecule.groovy
+++ vars/installMolecule.groovy
@@ -1,8 +1,9 @@
 def call() {
         sh """
-            sudo yum install -y gcc python3-pip python3-devel libselinux-python openssl-devel
-            sudo mkdir -p /usr/local/lib64/python3.7/site-packages
-            sudo rsync -aHv /usr/lib64/python2.7/site-packages/*selinux* /usr/local/lib64/python3.7/site-packages/
-            pip3 install --user molecule==2.22 pytest molecule-ec2 ansible wheel boto boto3 paramiko selinux
+            sudo yum install -y gcc python3-pip python3-devel libselinux-python3
+            sudo yum remove ansible -y
+            python3 -m venv virtenv
+            . virtenv/bin/activate
+            python3 -m pip install --upgrade molecule==3.0.2 testinfra pytest molecule-ec2==0.2 ansible==2.9.6
         """
 }
