113fae6dca542d88c8f772d430e8c29a03bf4932
rameshvs02 <rameshvs02@gmail.com>
2020-11-26 15:46:15 +0530

    Code cleanup

--- proxysql/qa-proxysql2-pipeline.groovy
+++ proxysql/qa-proxysql2-pipeline.groovy
@@ -65,7 +65,7 @@ pipeline {
         stage('Build ProxySQL') {
                 agent { label 'docker' }
                 steps {
-                    git branch: 'PSQLADM-273-Update-Jenkins-job', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
+                    git branch: 'master', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
                     echo 'Checkout ProxySQL sources'
                     sh '''
                         # sudo is needed for better node recovery after compilation failure
@@ -101,7 +101,7 @@ pipeline {
         stage('Test ProxySQL') {
                 agent { label 'docker' }
                 steps {
-                    git branch: 'PSQLADM-273-Update-Jenkins-job', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
+                    git branch: 'master', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
                     echo 'Test ProxySQL'
                     withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'c42456e5-c28d-4962-b32c-b75d161bff27', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                         sh '''
