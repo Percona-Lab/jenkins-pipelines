19a70948e266b69f347f4ccce56f5598432b1009
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-12-03 16:38:36 +0300

    PMM-7076 Push PMM Client to perconalab/ too

--- pmm/pmm2-server-release.groovy
+++ pmm/pmm2-server-release.groovy
@@ -182,16 +182,26 @@ pipeline {
                         docker save percona/pmm-server:\${VERSION} | xz > pmm-server-\${VERSION}.docker
 
                         docker pull \${DOCKER_CLIENT_VERSION}
-                        docker tag \${DOCKER_CLIENT_VERSION} percona/pmm-client:\${VERSION}
-                        docker tag \${DOCKER_CLIENT_VERSION} percona/pmm-client:\${DOCKER_MID}
-                        docker tag \${DOCKER_CLIENT_VERSION} percona/pmm-client:\${TOP_VER}
+
                         if [ \${TOP_VER} = 1 ]; then
                             docker tag \${DOCKER_CLIENT_VERSION} perconalab/pmm-client:latest
                             docker push perconalab/pmm-client:latest
                         fi
+
+                        docker tag \${DOCKER_CLIENT_VERSION} percona/pmm-client:\${VERSION}
+                        docker tag \${DOCKER_CLIENT_VERSION} percona/pmm-client:\${DOCKER_MID}
+                        docker tag \${DOCKER_CLIENT_VERSION} percona/pmm-client:\${TOP_VER}
                         docker push percona/pmm-client:\${VERSION}
                         docker push percona/pmm-client:\${DOCKER_MID}
                         docker push percona/pmm-client:\${TOP_VER}
+
+                        docker tag \${DOCKER_CLIENT_VERSION} perconalab/pmm-client:\${VERSION}
+                        docker tag \${DOCKER_CLIENT_VERSION} perconalab/pmm-client:\${DOCKER_MID}
+                        docker tag \${DOCKER_CLIENT_VERSION} perconalab/pmm-client:\${TOP_VER}
+                        docker push perconalab/pmm-client:\${VERSION}
+                        docker push perconalab/pmm-client:\${DOCKER_MID}
+                        docker push perconalab/pmm-client:\${TOP_VER}
+
                         docker save percona/pmm-client:\${VERSION} | xz > pmm-client-\${VERSION}.docker
                     "
                 """
