7545000eac3df97754ccb5c1a61b80d98821f467
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-12-22 12:59:31 +0200

    Install the latest 3.X helm

--- cloud/jenkins/pxc_operator_gke_version.groovy
+++ cloud/jenkins/pxc_operator_gke_version.groovy
@@ -236,8 +236,7 @@ pipeline {
                     gcloud components install alpha
                     gcloud components install kubectl
 
-                    curl -s https://get.helm.sh/helm-v3.2.3-linux-amd64.tar.gz \
-                        | sudo tar -C /usr/local/bin --strip-components 1 -zvxpf -
+                    curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
                     curl -s -L https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz \
                         | sudo tar -C /usr/local/bin --strip-components 1 --wildcards -zxvpf - '*/oc'
                     sudo sh -c "curl -s -L https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64 > /usr/local/bin/yq"
