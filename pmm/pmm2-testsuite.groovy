e60004b1a35647fddd666b6cc120aa940f3777b3
Puneet Kala <puneet.0191@gmail.com>
2021-01-13 17:33:17 +0530

    Fix PMM2 Testsuite after change of Amazon Linux (#732)
    * Changes for  the log file
    
    * dev-latest tag should be creating log file via journalctl

--- pmm/pmm2-testsuite.groovy
+++ pmm/pmm2-testsuite.groovy
@@ -74,6 +74,7 @@ void fetchAgentLog(String CLIENT_VERSION) {
                 set -o xtrace
                 export CLIENT_VERSION=${CLIENT_VERSION}
                 if [[ \$CLIENT_VERSION != http* ]]; then
+                    journalctl -u pmm-agent.service > /var/log/pmm-agent.log
                     sudo chmod 777 /var/log/pmm-agent.log
                 fi
                 if [[ -e /var/log/pmm-agent.log ]]; then
