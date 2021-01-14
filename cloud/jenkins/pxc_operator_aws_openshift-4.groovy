b9805eae61b805f74ce114daf6d9a5796bde483b
Tomislav Plavcic <tomislav.plavcic@percona.com>
2021-01-14 13:58:24 +0100

    Add tls e2e tests for PXCO openshift-4 pipelines

--- cloud/jenkins/pxc_operator_aws_openshift-4.groovy
+++ cloud/jenkins/pxc_operator_aws_openshift-4.groovy
@@ -270,6 +270,9 @@ pipeline {
                 runTest('haproxy')
                 runTest('monitoring-2-0')
                 runTest('validation-hook')
+                runTest('tls-issue-self')
+                runTest('tls-issue-cert-manager')
+                runTest('tls-issue-cert-manager-ref')
             }
         }
         stage('E2E Scaling') {
