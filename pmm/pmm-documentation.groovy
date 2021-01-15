a65d11b093bbba3c325c68aa6fe3d0bfddea7cf3
Paul Jacobs <paul.jacobs@percona.com>
2021-01-15 11:36:57 +0300

    New for PMM (all) - copy HTML from publish branch

---
+++ pmm/pmm-documentation.groovy
@@ -0,0 +1,54 @@
+pipeline {
+    agent {
+        label 'micro-amazon'
+    }
+    parameters {
+        string(
+            defaultValue: 'publish',
+            description: 'Tag/Branch for build',
+            name: 'BRANCH_NAME')
+        choice(
+            choices: ['test.percona.com'],
+            description: 'Publish to test server',
+            name: 'PUBLISH_TARGET')
+    }
+    options {
+        buildDiscarder(logRotator(daysToKeepStr: '14', numToKeepStr: '5'))
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                sh """
+                    sudo chmod 777 -R ./
+                """
+                git poll: false, branch: BRANCH_NAME, url: 'https://github.com/percona/pmm-doc.git'
+            stash name: "html-files", includes: "**"
+
+            }
+        }
+        stage('Doc Publish'){
+            agent {
+                label 'vbox-01.ci.percona.com'
+            }
+            steps{
+                unstash "html-files"
+                withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-deploy', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
+                    sh '''
+                        echo BRANCH=${BRANCH_NAME}
+                        DEST_HOST='docs-rsync-endpoint.int.percona.com'
+                        rsync --delete-before -avzr -O -e "ssh -o StrictHostKeyChecking=no -p2222 -i \${KEY_PATH}" ./* \${USER}@\${DEST_HOST}:/data/websites_data/\${PUBLISH_TARGET}/doc/percona-monitoring-and-management/
+                    '''
+                }
+            }
+        }
+    }
+    post {
+        always {
+            // stop staging
+            sh '''
+                sudo chmod 777 -R ./
+            '''
+            deleteDir()
+        }
+    }
+}
