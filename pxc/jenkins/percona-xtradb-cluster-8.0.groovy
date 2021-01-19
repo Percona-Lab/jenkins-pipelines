b9c33c4113b9e93f6c8189fefae80d6d5b2ae13a
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2021-01-20 01:52:45 +0200

    PXC-3515 pxc build automation

---
+++ pxc/jenkins/percona-xtradb-cluster-8.0.groovy
@@ -0,0 +1,274 @@
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+]) _
+
+void buildStage(String DOCKER_OS, String STAGE_PARAM) {
+    sh """
+        set -o xtrace
+        mkdir test
+        wget https://raw.githubusercontent.com/percona/percona-xtradb-cluster/${GIT_BRANCH}/build-ps/pxc_builder.sh -O pxc_builder.sh
+        pwd -P
+        ls -laR
+        export build_dir=\$(pwd -P)
+        docker run -u root -v \${build_dir}:\${build_dir} ${DOCKER_OS} sh -c "
+            set -o xtrace
+            cd \${build_dir}
+            bash -x ./pxc_builder.sh --builddir=\${build_dir}/test --install_deps=1
+            bash -x ./pxc_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${GIT_BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} --bin_release=${BIN_RELEASE} ${STAGE_PARAM}"
+    """
+}
+
+void cleanUpWS() {
+    sh """
+        sudo rm -rf ./*
+    """
+}
+
+def AWS_STASH_PATH
+
+pipeline {
+    agent {
+        label 'docker'
+    }
+    parameters {
+        string(
+            defaultValue: 'https://github.com/percona/percona-xtradb-cluster.git',
+            description: 'URL for percona-xtradb-cluster repository',
+            name: 'GIT_REPO')
+        string(
+            defaultValue: '8.0',
+            description: 'Tag/Branch for percona-xtradb-cluster repository',
+            name: 'GIT_BRANCH')
+        string(
+            defaultValue: '1',
+            description: 'RPM release value',
+            name: 'RPM_RELEASE')
+        string(
+            defaultValue: '1',
+            description: 'DEB release value',
+            name: 'DEB_RELEASE')
+        string(
+            defaultValue: '1',
+            description: 'BIN release value',
+            name: 'BIN_RELEASE')
+        string(
+            defaultValue: 'pxc-80',
+            description: 'PXC repo name',
+            name: 'PXC_REPO')
+        choice(
+            choices: 'laboratory\ntesting\nexperimental\nrelease',
+            description: 'Repo component to push packages to',
+            name: 'COMPONENT')
+    }
+    options {
+        skipDefaultCheckout()
+        disableConcurrentBuilds()
+        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        timestamps ()
+    }
+    stages {
+        stage('Create PXC source tarball') {
+            steps {
+                slackNotify("#releases", "#00FF00", "[${JOB_NAME}]: starting build for ${GIT_BRANCH}")
+                cleanUpWS()
+                buildStage("centos:7", "--get_sources=1")
+                sh '''
+                   REPO_UPLOAD_PATH=$(grep "UPLOAD" test/pxc-80.properties | cut -d = -f 2 | sed "s:$:${BUILD_NUMBER}:")
+                   AWS_STASH_PATH=$(echo ${REPO_UPLOAD_PATH} | sed  "s:UPLOAD/experimental/::")
+                   echo ${REPO_UPLOAD_PATH} > uploadPath
+                   echo ${AWS_STASH_PATH} > awsUploadPath
+                   cat test/pxc-80.properties
+                   cat uploadPath
+                   cat awsUploadPath
+                '''
+                script {
+                    AWS_STASH_PATH = sh(returnStdout: true, script: "cat awsUploadPath").trim()
+                }
+                stash includes: 'uploadPath', name: 'uploadPath'
+                pushArtifactFolder("source_tarball/", AWS_STASH_PATH)
+                uploadTarballfromAWS("source_tarball/", AWS_STASH_PATH, 'source')
+            }
+        }
+        stage('Build PXC generic source packages') {
+            parallel {
+                stage('Build PXC generic source rpm') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_tarball/", AWS_STASH_PATH)
+                        buildStage("centos:7", "--build_src_rpm=1")
+
+                        pushArtifactFolder("srpm/", AWS_STASH_PATH)
+                        uploadRPMfromAWS("srpm/", AWS_STASH_PATH)
+                    }
+                }
+                stage('Build PXC generic source deb') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_tarball/", AWS_STASH_PATH)
+                        buildStage("ubuntu:xenial", "--build_source_deb=1")
+
+                        pushArtifactFolder("source_deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS("source_deb/", AWS_STASH_PATH)
+                    }
+                }
+            }  //parallel
+        } // stage
+        stage('Build PXC RPMs/DEBs/Binary tarballs') {
+            parallel {
+                stage('Centos 7') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("srpm/", AWS_STASH_PATH)
+                        buildStage("centos:7", "--build_rpm=1")
+
+                        pushArtifactFolder("rpm/", AWS_STASH_PATH)
+                        uploadRPMfromAWS("rpm/", AWS_STASH_PATH)
+                    }
+                }
+                stage('Centos 8') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("srpm/", AWS_STASH_PATH)
+                        buildStage("centos:8", "--build_rpm=1")
+
+                        pushArtifactFolder("rpm/", AWS_STASH_PATH)
+                        uploadRPMfromAWS("rpm/", AWS_STASH_PATH)
+                    }
+                }
+                stage('Ubuntu Xenial(16.04)') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
+                        buildStage("ubuntu:xenial", "--build_deb=1")
+
+                        pushArtifactFolder("deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
+                    }
+                }
+                stage('Ubuntu Bionic(18.04)') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
+                        buildStage("ubuntu:bionic", "--build_deb=1")
+
+                        pushArtifactFolder("deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
+                    }
+                }
+                stage('Ubuntu Focal(20.04)') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
+                        buildStage("ubuntu:focal", "--build_deb=1")
+
+                        pushArtifactFolder("deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
+                    }
+                }
+                stage('Debian Stretch(9)') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
+                        buildStage("debian:stretch", "--build_deb=1")
+
+                        pushArtifactFolder("deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
+                    }
+                }
+                stage('Debian Buster(10)') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
+                        buildStage("debian:buster", "--build_deb=1")
+
+                        pushArtifactFolder("deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
+                    }
+                }
+                stage('Centos 7 tarball') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_tarball/", AWS_STASH_PATH)
+                        buildStage("centos:7", "--build_tarball=1")
+
+                        pushArtifactFolder("tarball/", AWS_STASH_PATH)
+                        uploadTarballfromAWS("tarball/", AWS_STASH_PATH, 'binary')
+                    }
+                }
+                stage('Centos 7 debug tarball') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_tarball/", AWS_STASH_PATH)
+                        buildStage("centos:7", "--debug=1")
+
+                        pushArtifactFolder("debug/", AWS_STASH_PATH)
+                    }
+                }
+            }
+        }
+
+        stage('Sign packages') {
+            steps {
+                signRPM()
+                signDEB()
+            }
+        }
+        stage('Push to public repository') {
+            steps {
+                // sync packages
+                sync2ProdAutoBuild(PXC_REPO, COMPONENT)
+            }
+        }
+
+    }
+    post {
+        success {
+            slackNotify("#releases", "#00FF00", "[${JOB_NAME}]: build has been finished successfully for ${GIT_BRANCH}")
+            deleteDir()
+        }
+        failure {
+            slackNotify("#releases", "#FF0000", "[${JOB_NAME}]: build failed for ${GIT_BRANCH}")
+            deleteDir()
+        }
+        always {
+            sh '''
+                sudo rm -rf ./*
+            '''
+            deleteDir()
+        }
+    }
+}
