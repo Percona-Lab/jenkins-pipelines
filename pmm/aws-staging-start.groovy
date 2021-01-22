e5a8dddb0edfd30efcab8ad4dc6b7a3e7b805769
Puneet Kala <puneet.0191@gmail.com>
2021-01-22 13:00:13 +0530

    PMM-7352 Update AWS-Staging Job to use Library for spot Instance (#749)

--- pmm/aws-staging-start.groovy
+++ pmm/aws-staging-start.groovy
@@ -3,6 +3,11 @@ import hudson.slaves.*
 import jenkins.model.Jenkins
 import hudson.plugins.sshslaves.SSHLauncher
 
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+]) _
+
 pipeline {
     agent {
         label 'awscli'
@@ -162,120 +167,7 @@ pipeline {
 
         stage('Run VM') {
             steps {
-                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                    sh '''
-                        export VM_NAME=\$(cat VM_NAME)
-                        export OWNER=\$(cat OWNER_FULL)
-                        export SUBNET=\$(
-                            aws ec2 describe-subnets \
-                                --region us-east-2 \
-                                --output text \
-                                --filters "Name=tag:aws:cloudformation:stack-name,Values=pmm-staging" \
-                                --query 'Subnets[].SubnetId' \
-                                | tr '\t' '\n' \
-                                | sort --random-sort \
-                                | head -1
-                        )
-                        export SG1=\$(
-                            aws ec2 describe-security-groups \
-                                --region us-east-2 \
-                                --output text \
-                                --filters "Name=tag:aws:cloudformation:stack-name,Values=pmm-staging" \
-                                          "Name=group-name,Values=HTTP" \
-                                --query 'SecurityGroups[].GroupId'
-                        )
-                        export SG2=\$(
-                            aws ec2 describe-security-groups \
-                                --region us-east-2 \
-                                --output text \
-                                --filters "Name=tag:aws:cloudformation:stack-name,Values=pmm-staging" \
-                                          "Name=group-name,Values=SSH" \
-                                --query 'SecurityGroups[].GroupId'
-                        )
-
-                        echo '{
-                            "DryRun": false,
-                            "InstanceCount": 1,
-                            "InstanceInterruptionBehavior": "terminate",
-                            "LaunchSpecification": {
-                                "BlockDeviceMappings": [
-                                    {
-                                        "DeviceName": "/dev/xvda",
-                                        "Ebs": {
-                                            "DeleteOnTermination": true,
-                                            "VolumeSize": 20,
-                                            "VolumeType": "gp2"
-                                        }
-                                    }
-                                ],
-                                "EbsOptimized": false,
-                                "ImageId": "ami-0a0ad6b70e61be944",
-                                "UserData": "c3VkbyB5dW0gaW5zdGFsbCAteSBqYXZhLTEuOC4wLW9wZW5qZGsKCnN1ZG8gL3Vzci9zYmluL2FsdGVybmF0aXZlcyAtLXNldCBqYXZhIC91c3IvbGliL2p2bS9qcmUtMS44LjAtb3Blbmpkay54ODZfNjQvYmluL2phdmEKCnN1ZG8gL3Vzci9zYmluL2FsdGVybmF0aXZlcyAtLXNldCBqYXZhYyAvdXNyL2xpYi9qdm0vanJlLTEuOC4wLW9wZW5qZGsueDg2XzY0L2Jpbi9qYXZhYwoKc3VkbyB5dW0gcmVtb3ZlIGphdmEtMS43Cg==",
-                                "InstanceType": "t3.large",
-                                "KeyName": "jenkins",
-                                "Monitoring": {
-                                    "Enabled": false
-                                },
-                                "IamInstanceProfile": {
-                                    "Name": "jenkins-pmm-slave"
-                                },
-                                "SecurityGroupIds": [
-                                    "security-group-id-1",
-                                    "security-group-id-2"
-                                ],
-                                "SubnetId": "subnet-id"
-                            },
-                            "SpotPrice": "0.040",
-                            "Type": "persistent"
-                        }' \
-                            | sed -e "s/subnet-id/\${SUBNET}/" \
-                            | sed -e "s/security-group-id-1/\${SG1}/" \
-                            | sed -e "s/security-group-id-2/\${SG2}/" \
-                            > config.json
-
-                        REQUEST_ID=\$(
-                            aws ec2 request-spot-instances \
-                                --output text \
-                                --region us-east-2 \
-                                --cli-input-json file://config.json \
-                                --query SpotInstanceRequests[].SpotInstanceRequestId
-                        )
-                        echo \$REQUEST_ID > REQUEST_ID
-
-                        until [ -s IP ]; do
-                            sleep 1
-                            aws ec2 describe-instances \
-                                --filters "Name=spot-instance-request-id,Values=\${REQUEST_ID}" \
-                                --query 'Reservations[].Instances[].PublicIpAddress' \
-                                --output text \
-                                --region us-east-2 \
-                                | tee IP
-                        done
-
-                        aws ec2 describe-instances \
-                            --filters "Name=spot-instance-request-id,Values=\${REQUEST_ID}" \
-                            --query 'Reservations[].Instances[].InstanceId' \
-                            --output text \
-                            --region us-east-2 \
-                            | tee ID
-
-                        VOLUMES=$(
-                            aws ec2 describe-instances \
-                                --region us-east-2 \
-                                --output text \
-                                --instance-ids \$(cat ID) \
-                                --query 'Reservations[].Instances[].BlockDeviceMappings[].Ebs.VolumeId'
-                        )
-
-                        aws ec2 create-tags  \
-                            --region us-east-2 \
-                            --resources \$REQUEST_ID \$(cat ID) \$VOLUMES \
-                            --tags Key=Name,Value=\$VM_NAME \
-                                   Key=iit-billing-tag,Value=pmm-staging \
-                                   Key=stop-after-days,Value=${DAYS} \
-                                   Key=owner,Value=\$OWNER
-                    '''
-                }
+                launchSpotInstance('t3.large', '0.040', 20)
                 withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
                     sh """
                         until ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no ${USER}@\$(cat IP) 'java -version; sudo yum install -y java-1.8.0-openjdk; sudo /usr/sbin/alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java; java -version;' ; do
@@ -317,10 +209,6 @@ pipeline {
                             sudo git clone --single-branch --branch \${PMM_QA_GIT_BRANCH} https://github.com/percona/pmm-qa.git .
                             sudo git checkout \${PMM_QA_GIT_COMMIT_HASH}
                             sudo svn export https://github.com/Percona-QA/percona-qa.git/trunk/get_download_link.sh
-                            cd pmm-tests/
-                            sudo svn export https://github.com/puneet0191/pmm-workloads.git/trunk/mysql/schema_table_query.php
-                            sudo chmod 755 schema_table_query.php
-                            cd ../
                             sudo chmod 755 get_download_link.sh
                         popd
                     """
