d09e3dc4245f1e47d9a68c446917f7d2fbcc88b3
Puneet Kala <puneet.0191@gmail.com>
2021-01-26 11:34:41 +0530

    Fix Config generation issue with latest minikube

--- pmm/kubernetes-cluster-staging.groovy
+++ pmm/kubernetes-cluster-staging.groovy
@@ -127,7 +127,7 @@ pipeline {
                                 set -o errexit
                                 set -o xtrace
                                 sudo yum -y install curl
-                                sudo curl -Lo /usr/local/sbin/minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+                                sudo curl -Lo /usr/local/sbin/minikube https://github.com/kubernetes/minikube/releases/download/v1.16.0/minikube-linux-amd64
                                 sudo chmod +x /usr/local/sbin/minikube
                                 sudo ln -s /usr/local/sbin/minikube /usr/sbin/minikube
                                 alias kubectl='minikube kubectl --'
@@ -151,6 +151,7 @@ pipeline {
                                 set -o xtrace
                                 export PATH=\$PATH:/usr/sbin
                                 minikube version
+                                rm ~/.kube/config && minikube delete
                                 minikube config set cpus 8
                                 minikube config set memory 29000
                                 minikube config set kubernetes-version 1.16.15
