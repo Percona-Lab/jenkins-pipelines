db7ef10269872965ab8cff919c930b83f91822c5
Alexander Tymchuk <alexander.tymchuk@percona.com>
2021-01-27 08:10:44 +0300

    Fix: check if nvm exists, upgrade node to v12.20.1 (#743)
    The script fails to install nvm because when it already exists, so we need to perform a check to see if nvm is present.
    NodeJS upgraded to latest LTS v12.20.1.

--- pmm/pmm2-testsuite.groovy
+++ pmm/pmm2-testsuite.groovy
@@ -134,11 +134,13 @@ pipeline {
                 deleteDir()
                 slackSend channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
                 sh '''
-                    curl -o - https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+                    if [ ! -f ~/.nvm/nvm.sh ]; then
+                        curl -o - https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+                    ]
                     . ~/.nvm/nvm.sh
-                    nvm install 12.14.1
+                    nvm install 12.20.1
                     sudo rm -f /usr/bin/node
-                    sudo ln -s ~/.nvm/versions/node/v12.14.1/bin/node /usr/bin/node
+                    sudo ln -s ~/.nvm/versions/node/v12.20.1/bin/node /usr/bin/node
                     npm install tap-junit
                 '''
             }
