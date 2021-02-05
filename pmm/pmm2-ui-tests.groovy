96551e5f4a7dded8abffd718147831587b0fbdf1
Puneet Kala <puneet.0191@gmail.com>
2021-02-05 16:49:54 +0530

    Fix symlink on docker compose (#787)
    * Fixing the symlink  issue
    
    * Fix symlink when file exist

--- pmm/pmm2-ui-tests.groovy
+++ pmm/pmm2-ui-tests.groovy
@@ -162,7 +162,7 @@ pipeline {
                     sudo service docker start
                     sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
                     sudo chmod +x /usr/local/bin/docker-compose
-                    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+                    sudo ln -sfn /usr/local/bin/docker-compose /usr/bin/docker-compose
                     sudo docker-compose --version
                 '''
             }
