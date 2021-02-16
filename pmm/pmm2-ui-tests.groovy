bb23c92f4c0a15f82832bd72d16ed2fad058a850
Ivana Wzientková <ivana.wzientkova@percona.com>
2021-02-16 11:02:58 +0100

    add '/'

--- pmm/pmm2-ui-tests.groovy
+++ pmm/pmm2-ui-tests.groovy
@@ -20,12 +20,12 @@ void runStagingServer(String DOCKER_VERSION, CLIENT_VERSION, CLIENTS, CLIENT_INS
     env.VM_NAME = stagingJob.buildVariables.VM_NAME
     def clientInstance = "yes";
     if ( CLIENT_INSTANCE == clientInstance ) {
-        env.PMM_URL = "http://admin:admin@${SERVER_IP}"
+        env.PMM_URL = "http://admin:admin@${SERVER_IP}/"
         env.PMM_UI_URL = "http://${SERVER_IP}/"
     }
     else
     {
-        env.PMM_URL = "http://admin:admin@${VM_IP}"
+        env.PMM_URL = "http://admin:admin@${VM_IP}/"
         env.PMM_UI_URL = "http://${VM_IP}/"
     }
 }
