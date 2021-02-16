1421e851f8f443ec75c62ef12fa4b184e928e2df
Puneet Kala <puneet.0191@gmail.com>
2021-02-16 16:28:43 +0530

    Fixing issue with path

--- pmm/pmm2-ui-tests.groovy
+++ pmm/pmm2-ui-tests.groovy
@@ -57,12 +57,12 @@ void runStagingClient(String DOCKER_VERSION, CLIENT_VERSION, CLIENTS, CLIENT_INS
     def clientInstance = "yes";
     if ( CLIENT_INSTANCE == clientInstance ) {
         env.PMM_URL = "http://admin:admin@${SERVER_IP}"
-        env.PMM_UI_URL = "http://${SERVER_IP}"
+        env.PMM_UI_URL = "http://${SERVER_IP}/"
     }
     else
     {
         env.PMM_URL = "http://admin:admin@${VM_IP}"
-        env.PMM_UI_URL = "http://${VM_IP}"
+        env.PMM_UI_URL = "http://${VM_IP}/"
     }
 }
 
