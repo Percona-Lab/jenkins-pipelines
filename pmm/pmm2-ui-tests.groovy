ac64f97c7fe04c4954a66107f8c77efc337b3937
puneet0191 <puneet.0191@gmail.com>
2021-02-25 14:31:45 +0530

    PMM-7484 change default branch

--- pmm/pmm2-ui-tests.groovy
+++ pmm/pmm2-ui-tests.groovy
@@ -140,7 +140,7 @@ pipeline {
             description: 'Value for Server Public IP, to use this instance just as client',
             name: 'SERVER_IP')
         string(
-            defaultValue: 'master',
+            defaultValue: 'PMM-7482',
             description: 'Tag/Branch for pmm-qa repository',
             name: 'PMM_QA_GIT_BRANCH')
     }
