94d97aafeedacab2d591459bf9def63caa5bcbe7
Ivana Wzientková <ivana.wzientkova@percona.com>
2021-03-02 13:28:48 +0100

    fix indentation

--- pmm/pmm2-ui-tests-nightly.groovy
+++ pmm/pmm2-ui-tests-nightly.groovy
@@ -156,10 +156,10 @@ pipeline {
                     sudo docker-compose --version
                     sudo mkdir -p /srv/pmm-qa || :
                     pushd /srv/pmm-qa
-                    sudo git clone --single-branch --branch \${PMM_QA_GIT_BRANCH} https://github.com/percona/pmm-qa.git .
-                    sudo git checkout \${PMM_QA_GIT_COMMIT_HASH}
-                    sudo chmod 755 pmm-tests/install-google-chrome.sh
-                    bash ./pmm-tests/install-google-chrome.sh
+                        sudo git clone --single-branch --branch \${PMM_QA_GIT_BRANCH} https://github.com/percona/pmm-qa.git .
+                        sudo git checkout \${PMM_QA_GIT_COMMIT_HASH}
+                        sudo chmod 755 pmm-tests/install-google-chrome.sh
+                        bash ./pmm-tests/install-google-chrome.sh
                     popd
                     sudo ln -s /usr/bin/google-chrome-stable /usr/bin/chromium
                 '''
