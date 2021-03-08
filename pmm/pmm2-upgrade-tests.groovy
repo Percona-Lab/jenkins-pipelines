fef0391eeab8f6b9049c99cd507bbdcf7d254d37
Puneet Kala <puneet.0191@gmail.com>
2021-03-08 11:03:11 +0530

    Remove unwanted Setup

--- pmm/pmm2-upgrade-tests.groovy
+++ pmm/pmm2-upgrade-tests.groovy
@@ -106,14 +106,9 @@ pipeline {
                 slackSend channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
                 installDocker()
                 sh '''
-                    sudo yum -y update --security
                     sudo yum -y install jq svn
                     sudo usermod -aG docker ec2-user
                     sudo service docker start
-                    sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-                    sudo chmod +x /usr/local/bin/docker-compose
-                    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-                    sudo docker-compose --version
                     sudo mkdir -p /srv/pmm-qa || :
                     pushd /srv/pmm-qa
                         sudo git clone --single-branch --branch \${PMM_QA_GIT_BRANCH} https://github.com/percona/pmm-qa.git .
