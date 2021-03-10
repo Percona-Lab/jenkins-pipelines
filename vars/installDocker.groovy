0bb225de093e4c99f72c48ef7ef0cea8943382f5
Illia Pshonkin <illia.pshonkin@percona.com>
2021-03-10 18:32:13 +0200

    PMM-7672: Update PMM.cd IaC and PMM's pipelines for AMZN Linux 2 workers (#839)

--- vars/installDocker.groovy
+++ vars/installDocker.groovy
@@ -1,12 +1,7 @@
 def call() {
     sh '''
-        sudo yum -y install git curl epel-release
-        sudo yum -y install python36
-        curl -fsSL https://bootstrap.pypa.io/pip/3.3/get-pip.py -o get-pip.py
-        sudo python3 get-pip.py
-        sudo env PATH=/usr/local/bin:${PATH} pip install awscli==1.15.19
-        curl -fsSL get.docker.com -o get-docker.sh
-        env -i sh get-docker.sh || sudo yum -y install docker
+        sudo amazon-linux-extras install epel -y
+        sudo yum -y install git curl docker
         sudo usermod -aG docker `id -u -n`
         sudo mkdir -p /etc/docker
         echo '{"experimental": true}' | sudo tee /etc/docker/daemon.json
