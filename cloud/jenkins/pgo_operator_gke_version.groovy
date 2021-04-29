65f7f40afa5032baba4d0293ff066605d011b3cf
cap1984 <ivan.pylypenko@percona.com>
2021-04-29 13:06:12 +0300

    RM-927 Fix s3 artifacts pathes

--- cloud/jenkins/pgo_operator_gke_version.groovy
+++ cloud/jenkins/pgo_operator_gke_version.groovy
@@ -78,11 +78,12 @@ void runTest(String TEST_NAME, String CLUSTER_PREFIX) {
         try {
             echo "The $TEST_NAME test was started!"
             testsReportMap[TEST_NAME] = 'failed'
-            popArtifactFile("${env.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME")
+            PPG_TAG = sh(script: "if [ -n \"\${PGO_POSTGRES_HA_IMAGE}\" ] ; then echo ${PGO_POSTGRES_HA_IMAGE} | awk -F':' '{print \$2}' | grep -oE '[A-Za-z0-9\\.]+-ppg[0-9]{2}' ; else echo 'main-ppg13'; fi", , returnStdout: true).trim()
+            popArtifactFile("${env.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}-$PPG_TAG")
 
             timeout(time: 90, unit: 'MINUTES') {
                 sh """
-                    if [ -f "${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}-$MDB_TAG" ]; then
+                    if [ -f "${params.GIT_BRANCH}-${env.GIT_SHORT_COMMIT}-$TEST_NAME-${params.GKE_VERSION}-$PPG_TAG" ]; then
                         echo Skip $TEST_NAME test
                     else
                         cd ./source
