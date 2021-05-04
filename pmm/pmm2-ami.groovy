599ebc1d4bd0d98baaf8b38b2dd4c3ca739e15aa
Puneet Kala <puneet.0191@gmail.com>
2021-05-04 20:09:44 +0530

    PMM-6014 Changes for RC on OVF and AMI builds (#933)
    * PMM-6014 Changes for RC on OVF and AMI builds
    
    * PMM-6014 switch repo's for RC and Dev-latest
    
    * PMM-6014 Fix release server repo
    
    * PMM-6014 send message only on one channel

--- pmm/pmm2-ami.groovy
+++ pmm/pmm2-ami.groovy
@@ -16,6 +16,10 @@ pipeline {
             defaultValue: 'master',
             description: 'Tag/Branch for percona-images repository',
             name: 'GIT_BRANCH')
+        choice(
+            choices: ['no', 'yes'],
+            description: "Build Release Candidate?",
+            name: 'RELEASE_CANDIDATE')
     }
     options {
         skipDefaultCheckout()
@@ -37,12 +41,40 @@ pipeline {
             }
         }
 
-        stage('Build Image') {
+        stage('Build Image Release Candidate') {
+            when {
+                expression { env.RELEASE_CANDIDATE == "yes" }
+            }
             steps {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                     sh """
                         set -o pipefail
-                        ~/bin/packer build -only amazon-ebs -color=false packer/pmm2.json \
+                        ~/bin/packer build \
+                        -var 'pmm_client_repos=original testing' \
+                        -var 'pmm_client_repo_name=percona-testing-x86_64' \
+                        -var 'pmm2_server_repo=testing' \
+                        -only amazon-ebs -color=false packer/pmm2.json \
+                            | tee build.log
+                    """
+                }
+                sh 'tail build.log | grep us-east-1 | cut -d " " -f 2 > IMAGE'
+                stash includes: 'IMAGE', name: 'IMAGE'
+                archiveArtifacts 'IMAGE'
+            }
+        }
+        stage('Build Image Dev-Latest') {
+            when {
+                expression { env.RELEASE_CANDIDATE == "no" }
+            }
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    sh """
+                        set -o pipefail
+                        ~/bin/packer build \
+                        -var 'pmm_client_repos=original experimental' \
+                        -var 'pmm_client_repo_name=percona-experimental-x86_64' \
+                        -var 'pmm2_server_repo=experimental' \
+                        -only amazon-ebs -color=false packer/pmm2.json \
                             | tee build.log
                     """
                 }
@@ -61,8 +93,16 @@ pipeline {
             script {
                 unstash 'IMAGE'
                 def IMAGE = sh(returnStdout: true, script: "cat IMAGE").trim()
-                slackSend botUser: true, channel: '#pmm-ci', color: '#00FF00', message: "[${specName}]: build finished - ${IMAGE}"
                 runPmm2AmiUITests(IMAGE)
+                if ("${RELEASE_CANDIDATE}" == "yes")
+                {
+                    currentBuild.description = "Release Candidate Build"
+                    slackSend botUser: true, channel: '#pmm-qa', color: '#00FF00', message: "[${specName}]: ${BUILD_URL} Release Candidate build finished - ${IMAGE}"
+                }
+                else
+                {
+                    slackSend botUser: true, channel: '#pmm-ci', color: '#00FF00', message: "[${specName}]: build finished - ${IMAGE}"
+                }
             }
         }
         failure {
