b5e1379862b3c83c76b5aae25b6e01cabd6cf8d3
puneet0191 <puneet.0191@gmail.com>
2021-05-05 19:20:24 +0530

    PMM-6014 Add option to delete submodules release branches

--- pmm/pmm2-prepare-release-candidate.groovy
+++ pmm/pmm2-prepare-release-candidate.groovy
@@ -37,9 +37,8 @@ void runPMM2OVFBuild(String GIT_BRANCH, String RELEASE_CANDIDATE) {
     ]
 }
 
-void setupReleaseBranches(String VERSION, String GIT_BRANCH) {
-
-    def pmm_submodules = [
+def pmm_submodules() {
+    return [
         "pmm",
         "pmm-admin",
         "pmm-agent",
@@ -52,7 +51,10 @@ void setupReleaseBranches(String VERSION, String GIT_BRANCH) {
         "pmm-ui-tests",
         "pmm-qa"
     ]
-    def dependent_submodules = [
+}
+
+def dependent_submodules() {
+    return [
         "mysqld_exporter",
         "grafana",
         "dbaas-controller",
@@ -65,15 +67,42 @@ void setupReleaseBranches(String VERSION, String GIT_BRANCH) {
         "azure_metrics_exporter",
         "percona-toolkit"
     ]
+}
+
+void deleteReleaseBranches(String VERSION, String GIT_BRANCH) {
+
+    pmm_submodules().each { submodule ->
+        println "Deleting Release branch for : $submodule"
+        deleteBranch(submodule, 'release-' + VERSION)
+    }
+    dependent_submodules().each { submodule ->
+        println "Deleting Release branch for : $submodule"
+        deleteBranch(submodule, 'pmm-' + VERSION)
+    }
+    withCredentials([sshUserPrivateKey(credentialsId: 'GitHub SSH Key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: '')]) {
+        sh """
+            set -o errexit
+            set -o xtrace
+
+            echo "/usr/bin/ssh -i "${SSHKEY}" -o StrictHostKeyChecking=no \\\"\\\$@\\\"" > github-ssh.sh
+            chmod 755 github-ssh.sh
+            export GIT_SSH=\$(pwd -P)/github-ssh.sh
+            git push origin --delete \${RELEASE_BRANCH}
+        """
+    }
+}
+
+void setupReleaseBranches(String VERSION, String GIT_BRANCH) {
+
     sh '''
         git branch \${RELEASE_BRANCH}
         git checkout \${RELEASE_BRANCH}
     '''
-    pmm_submodules.each { submodule ->
+    pmm_submodules().each { submodule ->
         println "Preparing Release branch for Submodule: $submodule"
         createBranch(submodule, 'release-' + VERSION)
     }
-    dependent_submodules.each { submodule ->
+    dependent_submodules().each { submodule ->
         println "Preparing Release branch for Submodule: $submodule"
         createBranch(submodule, 'pmm-' + VERSION)
     }
@@ -121,6 +150,32 @@ void createBranch(String SUBMODULE, String BRANCH)
     }
 }
 
+void deleteBranch(String SUBMODULE, String BRANCH)
+{
+    withCredentials([sshUserPrivateKey(credentialsId: 'GitHub SSH Key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: '')]) {
+        sh """
+            set -o errexit
+            set -o xtrace
+
+            echo "/usr/bin/ssh -i "${SSHKEY}" -o StrictHostKeyChecking=no \\\"\\\$@\\\"" > github-ssh.sh
+            chmod 755 github-ssh.sh
+            export GIT_SSH=\$(pwd -P)/github-ssh.sh
+            export SUBMODULE=${SUBMODULE}
+            export BRANCH=${BRANCH}
+            export submodule_url=\$(git config --file=.gitmodules submodule.\${SUBMODULE}.url)
+            export submodule_branch=\$(git config --file=.gitmodules submodule.\${SUBMODULE}.branch)
+            export ssh_submodule_url=\$(echo \$submodule_url | sed "s^https://github.com/^git@github.com:^g")
+            cd /tmp/
+            export submodule_branch_exist=\$(git ls-remote --heads \${submodule_url} \${submodule_branch} | wc -l)
+            if [[ \${submodule_branch_exist} != 0 ]]; then
+                git clone --branch \${submodule_branch} \${ssh_submodule_url}
+                cd \${SUBMODULE}
+                git push origin --delete \${submodule_branch}
+            fi
+        """
+    }
+}
+
 pipeline {
     agent {
         label 'micro-amazon'
@@ -128,12 +183,16 @@ pipeline {
     parameters {
         string(
             defaultValue: 'PMM-2.0',
-            description: 'Prepare Submodules from pmm-submodules branch',
+            description: 'Prepare Submodules from pmm-submodules branch, to delete release branch please pass actual submodule branch name & set Remove param to yes',
             name: 'GIT_BRANCH')
         string(
             defaultValue: '2.17.0',
             description: 'Release Version',
             name: 'VERSION')
+        choice(
+            choices: ['no', 'yes'],
+            description: 'Remove Release branches, Option to be used only to recreate release branches',
+            name: 'REMOVE_RELEASE_BRANCH')
     }
     stages {
         stage('Check if Release Branch Exist') {
@@ -146,6 +205,15 @@ pipeline {
                         returnStdout: true
                     ).trim()
                 }
+            }
+        }
+        stage('Remove Release branches for submodules') {
+            when {
+                expression { env.EXIST.toInteger() == 1 && env.REMOVE_RELEASE_BRANCH == "yes"}
+            }
+            steps {
+                git branch: env.RELEASE_BRANCH, credentialsId: 'GitHub SSH Key', poll: false, url: 'git@github.com:Percona-Lab/pmm-submodules'
+                deleteReleaseBranches(VERSION, GIT_BRANCH)
             }
         }
         stage('Checkout Submodules and Prepare for creating branches') {
@@ -163,11 +231,17 @@ pipeline {
             }
         }
         stage('Rewind Release Submodule') {
+            when {
+                expression { env.REMOVE_RELEASE_BRANCH == "no"}
+            }
             steps {
                 runSubmodulesRewind(RELEASE_BRANCH)
             }
         }
         stage('Autobuilds RC for Server & Client') {
+            when {
+                expression { env.REMOVE_RELEASE_BRANCH == "no"}
+            }
             parallel {
                 stage('Start PMM2 Server Autobuild') {
                     steps {
@@ -182,6 +256,9 @@ pipeline {
             }
         }
         stage('Autobuilds RC for OVF & AMI') {
+            when {
+                expression { env.REMOVE_RELEASE_BRANCH == "no"}
+            }
             parallel {
                 stage('Start AMI Build for RC') {
                     steps {
