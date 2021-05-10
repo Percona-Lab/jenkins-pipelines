b04f95ce9bcae8c3df10e4707fb42878c2ad4982
Illia P <illia.pshonkin@percona.com>
2021-05-10 16:53:50 +0300

    ENG-1011: Use our CentOS 6 AMI with modified EBS

--- IaC/ps80.cd/init.groovy.d/cloud.groovy
+++ IaC/ps80.cd/init.groovy.d/cloud.groovy
@@ -28,7 +28,7 @@ imageMap['us-west-2a.micro-amazon']      = 'ami-04b3d201cc869b037'
 imageMap['us-west-2a.min-centos-8-x64']  = 'ami-0155c31ea13d4abd2'
 imageMap['us-west-2a.min-centos-7-x64']  = 'ami-0686851c4e7b1a8e1'
 imageMap['us-west-2a.fips-centos-7-x64'] = 'ami-036d2cdf95d86d256'
-imageMap['us-west-2a.min-centos-6-x64']  = 'ami-0362922178e02e9f3'
+imageMap['us-west-2a.min-centos-6-x64']  = 'ami-0f3565dfdcdbfccfa'
 imageMap['us-west-2a.min-buster-x64']    = 'ami-0c7ea5497c02abcaf'
 imageMap['us-west-2a.min-focal-x64']     = 'ami-0554443e7f55d3eeb'
 imageMap['us-west-2a.min-bionic-x64']    = 'ami-090717c950a5c34d3'
@@ -208,9 +208,9 @@ initMap['micro-amazon'] = '''
 initMap['min-centos-6-x64'] = '''
     set -o xtrace
     if ! mountpoint -q /mnt; then
-        for DEVICE_NAME in $(lsblk -ndpbo NAME,SIZE | sort -n -r | awk '{print $1}'); do
+        for DEVICE_NAME in $(lsblk -ndbo NAME,SIZE | sort -n -r | awk '{print $1}'); do
             if ! grep -qs "${DEVICE_NAME}" /proc/mounts; then
-                DEVICE="${DEVICE_NAME}"
+                DEVICE="/dev/${DEVICE_NAME}"
                 break
             fi
         done
