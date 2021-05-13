dc0daec94b96c1c1022da804b18312af7162a011
sandraromanchenko <sandra.romanchenko@percona.com>
2021-05-13 17:07:23 +0300

    Add libselinux-python package and system-site-packages option

--- vars/installMolecule.groovy
+++ vars/installMolecule.groovy
@@ -1,8 +1,8 @@
 def call() {
         sh """
-            sudo yum install -y gcc python3-pip python3-devel libselinux-python3 python3-libselinux
+            sudo yum install -y gcc python3-pip python3-devel libselinux-python
             sudo yum remove ansible -y
-            python3 -m venv virtenv
+            python3 -m venv virtenv --system-site-packages
             . virtenv/bin/activate
             python3 -m pip install --upgrade molecule testinfra pytest molecule-ec2 ansible==2.9.6
         """
