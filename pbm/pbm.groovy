5c3a9021c29e97e8f0c5bbf4c8e7f582398280aa
sandraromanchenko <sandra.romanchenko@percona.com>
2021-05-13 17:26:31 +0300

    Fix

--- pbm/pbm.groovy
+++ pbm/pbm.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: "lib@master", retriever: modernSCM([
+library changelog: false, identifier: "lib@test_molecule_selinux", retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ])
