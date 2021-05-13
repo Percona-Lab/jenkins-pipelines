f9e4b10a9021f92a8dab3d8f100a584a5d68330b
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-05-13 23:05:13 +0300

    molecule: Install ansible as molecule deps.

--- vars/installMolecule.groovy
+++ vars/installMolecule.groovy
@@ -5,6 +5,7 @@ def call() {
             python3 -m venv virtenv
             . virtenv/bin/activate
             python3 --version
-            python3 -m pip install --upgrade molecule==3.2.4 testinfra pytest molecule-ec2==0.3 ansible==2.9.6
+            python3 -m pip install --upgrade setuptools
+            python3 -m pip install --upgrade molecule==3.3.0 testinfra pytest molecule-ec2==0.3 molecule[ansible]
         """
 }
