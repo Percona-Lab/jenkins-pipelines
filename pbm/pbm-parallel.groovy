8db648f33f2ece5ce7b8b2ad4e712ab7a75e9405
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-05-13 13:02:23 +0300

    Fix2

--- pbm/pbm-parallel.groovy
+++ pbm/pbm-parallel.groovy
@@ -44,9 +44,13 @@ pipeline {
         }
         stage ('Prepare') {
           steps {
-                script {
-                   installMolecule()
-             }
+                sh """
+                sudo yum install -y gcc python3-pip python3-devel
+                sudo yum remove ansible -y
+                python3 -m venv virtenv
+                . virtenv/bin/activate
+                python3 -m pip install --upgrade molecule==3.0.2 testinfra pytest molecule-ec2==0.2 ansible==2.9.6
+            """
            }
         }
         stage('Test') {
