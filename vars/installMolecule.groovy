6eacc2e5bc973f806708ce1716d6d84807775372
sandraromanchenko <sandra.romanchenko@percona.com>
2021-05-13 17:02:45 +0300

    Revert "Add libselinux-python package"
    This reverts commit 4e3dcd5bd12bfda6736d4bbbe2874320e3fa5ec0.

--- vars/installMolecule.groovy
+++ vars/installMolecule.groovy
@@ -1,8 +1,8 @@
 def call() {
         sh """
-            sudo yum install -y gcc python3-pip python3-devel libselinux-python
+            sudo yum install -y gcc python3-pip python3-devel
             sudo yum remove ansible -y
-            python3 -m venv virtenv --system-site-packages
+            python3 -m venv virtenv
             . virtenv/bin/activate
             python3 -m pip install --upgrade molecule testinfra pytest molecule-ec2 ansible==2.9.6 selinux
         """
