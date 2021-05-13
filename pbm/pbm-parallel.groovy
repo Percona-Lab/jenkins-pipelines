dc044099933f222a73f86384da1fcb09be817c78
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-05-13 12:50:10 +0300

    fix

--- pbm/pbm-parallel.groovy
+++ pbm/pbm-parallel.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: "lib@master", retriever: modernSCM([
+library changelog: false, identifier: "lib@add_molecule_deps", retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ])
--- vars/installMolecule.groovy
+++ vars/installMolecule.groovy
@@ -1,9 +1,9 @@
 def call() {
         sh """
-            sudo yum install -y gcc python3-pip python3-devel libselinux-python3 python3-libselinux
+            sudo yum install -y gcc python3-pip python3-devel libselinux-python3
             sudo yum remove ansible -y
             python3 -m venv virtenv
             . virtenv/bin/activate
-            python3 -m pip install --upgrade molecule testinfra pytest molecule-ec2 ansible==2.9.6
+            python3 -m pip install --upgrade molecule testinfra pytest molecule-ec2 ansible==2.9.6 selinux
         """
 }
