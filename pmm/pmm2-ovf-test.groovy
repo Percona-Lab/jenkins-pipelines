b000a70ccab4332daeca18a534cb8d7169a1915c
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2021-05-19 11:14:23 +0300

    Update pmm2-ovf-test.groovy

--- pmm/pmm2-ovf-test.groovy
+++ pmm/pmm2-ovf-test.groovy
@@ -5,7 +5,7 @@ void runUITests(CLIENT_VERSION, CLIENT_INSTANCE, SERVER_IP, GIT_BRANCH, CLIENTS)
         string(name: 'SERVER_IP', value: SERVER_IP),
         string(name: 'OVF_TEST', value: 'yes'),
         string(name: 'GIT_BRANCH', value: GIT_BRANCH)
-        text(name: 'CLIENTS', value: CLIENTS)
+        string(name: 'CLIENTS', value: CLIENTS)
     ]
     env.VM_IP = stagingJob.buildVariables.IP
     env.VM_NAME = stagingJob.buildVariables.VM_NAME
