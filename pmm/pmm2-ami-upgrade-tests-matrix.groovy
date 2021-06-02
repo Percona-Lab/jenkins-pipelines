1f423b72f433691dab234840a872a041a8152784
Puneet Kala <puneet.0191@gmail.com>
2021-06-02 15:55:50 +0530

    PMM2 Update dev-latest version to 2.19.0 (#1021)
    * Update PMM2 Client Version
    
    * Update PMM2-Client Version
    
    * Update pmm2-client
    
    * Add 2.18.0 In Upgrade tests
    
    * Adding AMI ID for 2.18.0 released version
    
    * Adding 2.18.0 to upgrade job
    
    * Adding 2.18.0 to upgrade matrix

--- pmm/pmm2-ami-upgrade-tests-matrix.groovy
+++ pmm/pmm2-ami-upgrade-tests-matrix.groovy
@@ -27,7 +27,7 @@ pipeline {
             description: 'Tag/Branch for pmm-qa repository',
             name: 'PMM_QA_GIT_BRANCH')
         string(
-            defaultValue: '2.18.0',
+            defaultValue: '2.19.0',
             description: 'dev-latest PMM Server Version',
             name: 'PMM_SERVER_LATEST')
         choice(
@@ -70,6 +70,13 @@ pipeline {
                     steps {
                         script {
                             runAMIUpgradeJob(GIT_BRANCH,'2.17.0', '2.17.0', PMM_SERVER_LATEST, ENABLE_TESTING_REPO, PMM_QA_GIT_BRANCH);
+                        }
+                    }
+                }
+                stage('Upgrade from 2.18.0'){
+                    steps {
+                        script {
+                            runAMIUpgradeJob(GIT_BRANCH,'2.18.0', '2.18.0', PMM_SERVER_LATEST, ENABLE_TESTING_REPO, PMM_QA_GIT_BRANCH);
                         }
                     }
                 }
