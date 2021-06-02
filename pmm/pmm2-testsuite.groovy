e07c0ac61410a08c450e431bf97368ecbb2e3a37
Puneet Kala <puneet.0191@gmail.com>
2021-06-02 15:13:49 +0530

    Update PMM2 Client Version

--- pmm/pmm2-testsuite.groovy
+++ pmm/pmm2-testsuite.groovy
@@ -122,7 +122,7 @@ pipeline {
             description: 'Commit hash for pmm-qa branch',
             name: 'PMM_QA_GIT_COMMIT_HASH')
         string(
-            defaultValue: '2.18.0',
+            defaultValue: '2.19.0',
             description: 'Commit hash for pmm-qa branch',
             name: 'PMM_VERSION')
     }
