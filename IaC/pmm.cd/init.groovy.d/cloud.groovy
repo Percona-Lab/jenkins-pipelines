2c39af55d29520beb10bc7e88b813c73ab5329d3
Illia P <illia.pshonkin@percona.com>
2021-06-24 15:49:04 +0300

    PMM-8279: Add AWSCLI v2 for docker & amazon executors on PMM.cd
    * And update permissions to pull the images from ECR for Artifactory user

--- IaC/pmm.cd/init.groovy.d/cloud.groovy
+++ IaC/pmm.cd/init.groovy.d/cloud.groovy
@@ -128,14 +128,30 @@ initMap['rpmMap'] = '''
         sudo sysctl -w fs.aio-max-nr=1048576 || true
         sudo sysctl -w fs.file-max=6815744 || true
         echo "*  soft  core  unlimited" | sudo tee -a /etc/security/limits.conf
-        PKGLIST="tar coreutils aws-cli"
+        sudo amazon-linux-extras install epel -y
+        PKGLIST="tar coreutils p7zip"
     fi
     until sudo yum makecache; do
         sleep 1
         echo try again
     done
     sudo yum -y install java-1.8.0-openjdk git ${PKGLIST} || :
-    sudo yum -y remove java-1.7.0-openjdk || :
+    sudo yum -y remove java-1.7.0-openjdk aws-cli || :
+
+    if [[ $SYSREL -eq 2 ]]; then
+        if ! $(aws --version | grep -q 'aws-cli/2'); then
+            find /tmp -maxdepth 1 -name "*aws*" | xargs sudo rm -rf
+
+            until curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"; do
+                sleep 1
+                echo try again
+            done
+
+            7za -o/tmp x /tmp/awscliv2.zip 
+            cd /tmp/aws && sudo ./install
+        fi
+    fi
+
     sudo install -o $(id -u -n) -g $(id -g -n) -d /mnt/jenkins
 '''
 
@@ -211,9 +227,22 @@ initMap['docker'] = '''
     done
     sudo amazon-linux-extras install epel -y
     sudo yum -y install java-1.8.0-openjdk git docker p7zip
-    sudo yum -y remove java-1.7.0-openjdk
+    sudo yum -y remove java-1.7.0-openjdk aws-cli
+
+    if ! $(aws --version | grep -q 'aws-cli/2'); then
+        find /tmp -maxdepth 1 -name "*aws*" | xargs sudo rm -rf
+
+        until curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"; do
+            sleep 1
+            echo try again
+        done
+
+        7za -o/tmp x /tmp/awscliv2.zip 
+        cd /tmp/aws && sudo ./install
+    fi
 
     sudo install -o $(id -u -n) -g $(id -g -n) -d /mnt/jenkins
+
     sudo sysctl net.ipv4.tcp_fin_timeout=15
     sudo sysctl net.ipv4.tcp_tw_reuse=1
     sudo sysctl net.ipv6.conf.all.disable_ipv6=1
