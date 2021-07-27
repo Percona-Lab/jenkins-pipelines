974f75dbcdc45713c486903fdf0ed056ad7cd5ac
Puneet Kala <puneet.0191@gmail.com>
2021-07-27 18:45:25 +0530

    PMM-8423 Increase disk size for testsuite (#1102)

--- pmm/aws-staging-start.groovy
+++ pmm/aws-staging-start.groovy
@@ -171,7 +171,7 @@ pipeline {
 
         stage('Run VM') {
             steps {
-                launchSpotInstance('t3.large', '0.040', 20)
+                launchSpotInstance('t3.large', '0.040', 25)
                 withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
                     sh """
                         until ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no ${USER}@\$(cat IP) 'java -version; sudo yum install -y java-1.8.0-openjdk; sudo /usr/sbin/alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java; java -version;' ; do
