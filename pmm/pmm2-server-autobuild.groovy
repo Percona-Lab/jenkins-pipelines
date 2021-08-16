6b01c335183100bb01f7951bb068a5f788f928f8
Nikita Beletskii <nikita.beletskii@percona.com>
2021-08-16 17:51:30 +0300

    fix RC

--- pmm/pmm2-server-autobuild.groovy
+++ pmm/pmm2-server-autobuild.groovy
@@ -154,17 +154,18 @@ pipeline {
                             ./build/bin/build-server-docker
 
                             if [ ! -z \${DOCKER_RC_TAG+x} ]; then
-                                docker tag  \\${DOCKER_TAG} public.ecr.aws/e7j3v3n0/pmm-server:${DOCKER_RC_TAG}
-                                docker push public.ecr.aws/e7j3v3n0/pmm-server:\${DOCKER_RC_TAG}
-                                docker rmi public.ecr.aws/e7j3v3n0/pmm-server:\${DOCKER_RC_TAG}
+                                docker tag  \\${DOCKER_TAG} perconalab/pmm-server:${DOCKER_RC_TAG}
+                                docker push perconalab/pmm-server:\${DOCKER_RC_TAG}
+                                docker rmi perconalab/pmm-server:\${DOCKER_RC_TAG}
                             fi
-                            docker tag  \\${DOCKER_TAG} public.ecr.aws/e7j3v3n0/pmm-server:${DOCKER_LATEST_TAG}
-                            docker tag  \\${DOCKER_TAG} perconalab/pmm-server:${DOCKER_LATEST_TAG}
+                            docker tag \\${DOCKER_TAG} public.ecr.aws/e7j3v3n0/pmm-server:${DOCKER_LATEST_TAG}
+                            docker tag \\${DOCKER_TAG} perconalab/pmm-server:${DOCKER_LATEST_TAG}
                             docker push \\${DOCKER_TAG}
                             docker push public.ecr.aws/e7j3v3n0/pmm-server:${DOCKER_LATEST_TAG}
                             docker push perconalab/pmm-server:${DOCKER_LATEST_TAG}
                             docker rmi  \\${DOCKER_TAG}
-                            docker rmi  public.ecr.aws/e7j3v3n0/pmm-server:${DOCKER_LATEST_TAG}
+                            docker rmi public.ecr.aws/e7j3v3n0/pmm-server:${DOCKER_LATEST_TAG}
+                            docker rmi perconalab/pmm-server:${DOCKER_LATEST_TAG}
                         "
                     '''
                 }
