6fe86adc154550569c4150b2d9ba92b52433a98f
Puneet Kala <puneet.0191@gmail.com>
2021-09-06 19:38:09 +0530

    PMM-8625 Adding more deployment (#1192)

--- pmm/pmm2-ami-upgrade-tests.groovy
+++ pmm/pmm2-ami-upgrade-tests.groovy
@@ -266,7 +266,7 @@ pipeline {
         }
         stage('Start Client Instance') {
             steps {
-                runStagingClient(CLIENT_VERSION, '--addclient=ps,1 --setup-with-custom-queries --setup-remote-db', 'yes', AMI_INSTANCE_IP, PMM_QA_GIT_BRANCH, ENABLE_TESTING_REPO)
+                runStagingClient(CLIENT_VERSION, '--addclient=modb,1 --addclient=pgsql,1 --addclient=ps,1 --setup-with-custom-queries --setup-remote-db', 'yes', AMI_INSTANCE_IP, PMM_QA_GIT_BRANCH, ENABLE_TESTING_REPO)
             }
         }
         stage('Sleep') {
