bcc17149565d5b18c409b985f4229e6d9cc212da
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2021-09-13 16:23:51 +0300

    ENG-7 sync version file to production

--- vars/sync2Prod.groovy
+++ vars/sync2Prod.groovy
@@ -72,6 +72,9 @@ def call(String DESTINATION, String SYNC_PMM_CLIENT) {
                         rsync -avt --bwlimit=50000 --delete --progress --exclude=rsync-* --exclude=*.bak \
                             /srv/repo-copy/apt/ \
                             10.10.9.209:/www/repo.percona.com/htdocs/apt/
+                        rsync -avt --bwlimit=50000 --delete --progress --exclude=rsync-* --exclude=*.bak \
+                            /srv/repo-copy/version \
+                            10.10.9.209:/www/repo.percona.com/htdocs/
 
                         # Clean CDN cache for repo.percona.com
                         bash -xe /usr/local/bin/clear_cdn_cache.sh
