9b6850c72773764a9c7219f999950d343965624e
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-09-15 11:14:59 +0300

    Restore pdmysql parallel job

---
+++ pdmysql/pdmysql-parallel.groovy
@@ -0,0 +1,103 @@
+library changelog: false, identifier: "lib@master", retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+])
+
+def operatingSystems = ['centos-6', 'centos-7', 'debian-9', 'debian-10', 'ubuntu-xenial', 'ubuntu-bionic', 'ubuntu-focal', 'rhel8']
+
+pipeline {
+  agent {
+      label 'min-centos-7-x64'
+  }
+  environment {
+      PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin';
+      MOLECULE_DIR = "molecule/pdmysql/${SCENARIO}";
+
+  }
+  parameters {
+        choice(
+            name: 'REPO',
+            description: 'Repo for testing',
+            choices: [
+                'testing',
+                'experimental',
+                'release'
+            ]
+        )
+        string(
+            defaultValue: '8.0.23',
+            description: 'PDMYSQL version for test',
+            name: 'VERSION'
+         )
+        string(
+            defaultValue: '2.0.18',
+            description: 'Proxysql version for test',
+            name: 'PROXYSQL_VERSION'
+         )
+        string(
+            defaultValue: '2.3.10',
+            description: 'HAProxy version for test',
+            name: 'HAPROXY_VERSION'
+         )
+        string(
+            defaultValue: '8.0.23',
+            description: 'PXB version for test',
+            name: 'PXB_VERSION'
+         )
+        string(
+            defaultValue: '3.3.1',
+            description: 'Percona toolkit version for test',
+            name: 'PT_VERSION'
+         )
+        string(
+            defaultValue: '3.1.4',
+            description: 'Percona orchestrator version for test',
+            name: 'ORCHESTRATOR_VERSION'
+         )
+        choice(
+            name: 'SCENARIO',
+            description: 'PDMYSQL scenario for test',
+            choices: pdmysqlScenarios()
+        )
+  }
+  options {
+          withCredentials(moleculeDistributionJenkinsCreds())
+          disableConcurrentBuilds()
+  }
+    stages {
+        stage('Set build name'){
+          steps {
+                    script {
+                        currentBuild.displayName = "${env.BUILD_NUMBER}-${env.SCENARIO}"
+                    }
+                }
+            }
+        stage('Checkout') {
+            steps {
+                deleteDir()
+                git poll: false, branch: 'master', url: 'https://github.com/Percona-QA/package-testing.git'
+            }
+        }
+        stage ('Prepare') {
+          steps {
+                script {
+                   installMolecule()
+             }
+           }
+        }
+        stage('Test') {
+          steps {
+                script {
+                    moleculeParallelTest(operatingSystems, env.MOLECULE_DIR)
+                }
+            }
+         }
+  }
+    post {
+        always {
+          script {
+              moleculeParallelPostDestroy(operatingSystems, env.MOLECULE_DIR)
+         }
+      }
+   }
+}
