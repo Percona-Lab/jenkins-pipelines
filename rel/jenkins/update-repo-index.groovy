0c230e9a922276b2006ccf9ccd0b5c932ec2817c
Serhii Stasiuk <serhii.stasiuk@percona.com>
2021-10-11 16:54:58 +0300

    [ENG-1061] Preparing PR for Percona-Lab

--- rel/jenkins/update-repo-index.groovy
+++ rel/jenkins/update-repo-index.groovy
@@ -1,6 +1,6 @@
 library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/AgileStas/jenkins-pipelines.git'
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
 pipeline {
