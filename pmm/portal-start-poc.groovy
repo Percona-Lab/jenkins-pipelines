dd2b15becba50aef883db5be0a0036675cfc296e
yurkovychv <vasyl.yurkovych@percona.com>
2021-10-19 18:19:26 +0300

    remove kubectl version check

--- pmm/portal-start-poc.groovy
+++ pmm/portal-start-poc.groovy
@@ -132,7 +132,6 @@ pipeline {
                         # Install kubectl
                         curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && chmod +x ./kubectl
                         sudo mv ./kubectl /usr/local/bin/kubectl
-                        kubectl version
                         
                         # Install minikube 
                         curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
