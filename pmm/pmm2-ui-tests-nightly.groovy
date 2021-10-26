f0eb6ea788da76007aecbe000125bb6089439ea4
Puneet Kala <puneet.0191@gmail.com>
2021-10-26 20:46:52 +0530

    PMM-7 change default MongoDb

--- pmm/pmm2-ui-tests-nightly.groovy
+++ pmm/pmm2-ui-tests-nightly.groovy
@@ -179,7 +179,7 @@ pipeline {
             description: "MariaDB Server version",
             name: 'MD_VERSION')
         choice(
-            choices: ['4.2', '4.4', '4.0', '3.6'],
+            choices: ['4.4', '4.2', '4.0', '3.6'],
             description: "Percona Server for MongoDB version",
             name: 'MO_VERSION')
         choice(
