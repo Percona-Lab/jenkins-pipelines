c6c1f68a98870df9c929cb35dabc147099e57f56
Serhii S. Stasiuk <AgileStas@users.noreply.github.com>
2021-10-26 11:09:15 +0300

    [ENG-1063] Added parameters to pipeline (#1255)

--- vars/updateRepoIndex.groovy
+++ vars/updateRepoIndex.groovy
@@ -1,10 +1,13 @@
-def call() {
+def call(String[] ITEMS_TO_ADD) {
+    def argsStr = ''
+    if (ITEMS_TO_ADD) {
+        argsStr = ITEMS_TO_ADD.join(' ')
+        argsStr += ' '
+    }
     sh '''
         set -o errexit
         set -o xtrace
-        #sudo
         wget https://raw.githubusercontent.com/Percona-Lab/release-aux/main/scripts/update-repo-index.sh -O ./update-repo-index.sh
-        #chmod u+x ./update-repo-index.sh
-        bash -x ./update-repo-index.sh > ./new-index.html
+        bash -x ./update-repo-index.sh ''' + argsStr + '''> ./new-index.html
     '''
 }
