8d0e38de6f7ccbbbdd4fad1cbccc5c1455a7efb5
Nikita Beletskii <nikita.beletskii@percona.com>
2021-11-02 23:53:43 +0300

    PMM-4466 add grafana migrator

--- pmm/pmm2-server-autobuild.groovy
+++ pmm/pmm2-server-autobuild.groovy
@@ -132,6 +132,8 @@ pipeline {
                             build-server-rpm victoriametrics
                             build-server-rpm alertmanager
                             build-server-rpm grafana
+                            build-server-rpm grafana-db-migrator
+
                         "
                     '''
                 }
