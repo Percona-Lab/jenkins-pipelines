eeccf2a87f2896786f49fae9c2abc4816189af8d
Puneet Kala <puneet.0191@gmail.com>
2021-12-08 20:36:57 +0530

    PMM-7 Fix failing permission test

--- pmm/pmm2-ami-upgrade-tests.groovy
+++ pmm/pmm2-ami-upgrade-tests.groovy
@@ -108,8 +108,8 @@ void fetchAgentLog(String CLIENT_VERSION) {
                 set -o xtrace
                 export CLIENT_VERSION=${CLIENT_VERSION}
                 if [[ \$CLIENT_VERSION != http* ]]; then
-                    journalctl -u pmm-agent.service > /var/log/pmm-agent.log
-                    sudo chmod 777 /var/log/pmm-agent.log
+                    sudo journalctl -u pmm-agent.service > /var/log/pmm-agent.log
+                    sudo chown ec2-user:ec2-user /var/log/pmm-agent.log
                 fi
                 if [[ -e /var/log/pmm-agent.log ]]; then
                     cp /var/log/pmm-agent.log .
