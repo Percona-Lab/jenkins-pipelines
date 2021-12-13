c6de94239d4ba49ee45df26fd9b477c0568da171
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2021-12-13 12:22:40 +0200

    Update pmm2-ui-tests.groovy

--- pmm/pmm2-ui-tests.groovy
+++ pmm/pmm2-ui-tests.groovy
@@ -128,9 +128,6 @@ pipeline {
     triggers {
         upstream upstreamProjects: 'pmm2-server-autobuild', threshold: hudson.model.Result.SUCCESS
     }
-    options {
-        skipDefaultCheckout()
-    }
     stages {
         stage('Prepare') {
             steps {
