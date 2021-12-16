adbdaa42585408206641aae82da033a47614d66b
Puneet Kala <puneet.0191@gmail.com>
2021-12-16 16:34:53 +0530

    PMM-7 adding version

--- pmm/pmm2-ui-tests-nightly.groovy
+++ pmm/pmm2-ui-tests-nightly.groovy
@@ -189,7 +189,7 @@ pipeline {
             description: "Which version of PostgreSQL",
             name: 'PGSQL_VERSION')
         choice(
-            choices: ['14.1', '14.0', '13.4', '12.8', '11.13'],
+            choices: ['14.1', '14.0', '13.5', '13.4', '12.9', '12.8', '11.14', '11.13'],
             description: 'Percona Distribution for PostgreSQL',
             name: 'PDPGSQL_VERSION')
         choice(
