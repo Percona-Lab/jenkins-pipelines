357c8aa79cd9157802b4e485cd15072f3e8366b6
Nikita Beletskii <nikita.beletskii@percona.com>
2022-01-03 16:37:11 +0300

    PMM-7 create rpmbuild image

---
+++ pmm/infrastructure/rpm-build.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent {
+        label 'docker-farm'
+    }
+    options {
+        skipStagesAfterUnstable()
+        buildDiscarder(logRotator(artifactNumToKeepStr: '10'))
+    }
+    stages {
+        stage('Prepare') {
+            steps {
+                git poll: true,
+                    branch: 'PMM-2.0',
+                    url: 'https://github.com/Percona-Lab/pmm-submodules.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh """
+                    cd build/rpmbuild-docker
+                    docker build --pull --squash --tag public.ecr.aws/e7j3v3n0/rpmbuild:2 .
+                """
+                withCredentials([[
+                    $class: 'AmazonWebServicesCredentialsBinding',
+                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                    credentialsId: 'ECRRWUser',
+                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    sh """
+                        aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin public.ecr.aws/e7j3v3n0
+                        docker push public.ecr.aws/e7j3v3n0/rpmbuild:2
+                    """
+                }
+            }
+        }
+    }
+}
