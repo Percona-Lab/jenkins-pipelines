13ad4543c264827b782e2f19e1b443804311eddc
Ege Güneş <ege.gunes@percona.com>
2022-01-20 14:59:51 +0300

    Add pipelines to test PSO (#1382)

---
+++ cloud/jenkins/ps_operator_eks.groovy
@@ -0,0 +1,299 @@
+void pushArtifactFile(String FILE_NAME) {
+    echo "Push $FILE_NAME file to S3!"
+
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+        sh """
+            touch ${FILE_NAME}
+            S3_PATH=s3://percona-jenkins-artifactory/\$JOB_NAME/\$(git -C source describe --always --dirty)
+            aws s3 ls \$S3_PATH/${FILE_NAME} || :
+            aws s3 cp --quiet ${FILE_NAME} \$S3_PATH/${FILE_NAME} || :
+        """
+    }
+}
+
+void popArtifactFile(String FILE_NAME) {
+    echo "Try to get $FILE_NAME file from S3!"
+
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+        sh """
+            S3_PATH=s3://percona-jenkins-artifactory/\$JOB_NAME/\$(git -C source describe --always --dirty)
+            aws s3 cp --quiet \$S3_PATH/${FILE_NAME} ${FILE_NAME} || :
+        """
+    }
+}
+
+TestsReport = '<testsuite name=\\"PSMO\\">\n'
+testsReportMap = [:]
+void makeReport() {
+    for ( test in testsReportMap ) {
+        TestsReport = TestsReport + "<testcase name=\\\"${test.key}\\\"><${test.value}/></testcase>\n"
+    }
+    TestsReport = TestsReport + '</testsuite>\n'
+}
+
+void runTest(String TEST_NAME) {
+    def retryCount = 0
+    waitUntil {
+        try {
+            echo "The $TEST_NAME test was started!"
+
+            VERSION = "${env.GIT_BRANCH}"
+            testsReportMap[TEST_NAME] = 'failure'
+
+            timeout(time: 90, unit: 'MINUTES') {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'eks-cicd'], file(credentialsId: 'eks-conf-file', variable: 'EKS_CONF_FILE')]) {
+                    sh """
+                        if [ -f "$VERSION-$TEST_NAME-${params.PLATFORM_VER}" ]; then
+                            echo Skip $TEST_NAME test
+                        else
+                            cd ./source
+                            if [ -n "${OPERATOR_IMAGE}" ]; then
+                                export IMAGE=${OPERATOR_IMAGE}
+                            else
+                                export IMAGE=perconalab/percona-server-mysql-operator:${env.GIT_BRANCH}
+                            fi
+
+                            if [ -n "${IMAGE_MYSQL}" ]; then
+                                export IMAGE_MYSQL=${IMAGE_MYSQL}
+                            fi
+
+                            if [ -n "${IMAGE_ORCHESTRATOR}" ]; then
+                                export IMAGE_ORCHESTRATOR=${IMAGE_ORCHESTRATOR}
+                            fi
+
+                            if [ -n "${IMAGE_PMM}" ]; then
+                                export IMAGE_PMM=${IMAGE_PMM}
+                            fi
+
+                            export PATH="/home/ec2-user/.local/bin:${HOME}/.krew/bin:$PATH"
+                            source $HOME/google-cloud-sdk/path.bash.inc
+                            export KUBECONFIG=~/.kube/config
+
+                            kubectl kuttl test --config ./e2e-tests/kuttl.yaml --test "${TEST_NAME}"
+                        fi
+                    """
+                }
+            }
+            testsReportMap[TEST_NAME] = 'passed'
+            return true
+        }
+        catch (exc) {
+            if (retryCount >= 2) {
+                currentBuild.result = 'FAILURE'
+                return true
+            }
+            retryCount++
+            return false
+        }
+    }
+
+    echo "The $TEST_NAME test was finished!"
+}
+
+void conditionalRunTest(String TEST_NAME) {
+    if ( TEST_NAME == 'default-cr' ) {
+        if ( params.GIT_BRANCH.contains('release-') ) {
+            runTest(TEST_NAME)
+        }
+        return 0
+    }
+    runTest(TEST_NAME)
+}
+
+void installRpms() {
+    sh """
+        sudo yum install -y https://repo.percona.com/yum/percona-release-latest.noarch.rpm || true
+        sudo percona-release enable-only tools
+        sudo yum install -y percona-xtrabackup-80 jq | true
+    """
+}
+pipeline {
+    parameters {
+        string(
+            defaultValue: 'main',
+            description: 'Tag/Branch for percona/percona-server-mysql-operator repository',
+            name: 'GIT_BRANCH')
+        string(
+            defaultValue: 'https://github.com/percona/percona-server-mysql-operator',
+            description: 'percona-server-mysql-operator repository',
+            name: 'GIT_REPO')
+        string(
+            defaultValue: '1.20',
+            description: 'EKS kubernetes version',
+            name: 'PLATFORM_VER')
+        string(
+            defaultValue: '',
+            description: 'Operator image: perconalab/percona-server-mysql-operator:main',
+            name: 'OPERATOR_IMAGE')
+        string(
+            defaultValue: '',
+            description: 'PS for MySQL image: perconalab/percona-server-mysql-operator:main-ps8.0',
+            name: 'IMAGE_MYSQL')
+        string(
+            defaultValue: '',
+            description: 'Orchestrator image: perconalab/percona-server-mysql-operator:main-orchestrator',
+            name: 'IMAGE_ORCHESTRATOR')
+        string(
+            defaultValue: '',
+            description: 'PMM image: perconalab/pmm-client:dev-latest',
+            name: 'IMAGE_PMM')
+    }
+    environment {
+        CLEAN_NAMESPACE = 1
+    }
+    agent {
+         label 'docker'
+    }
+    options {
+        buildDiscarder(logRotator(daysToKeepStr: '-1', artifactDaysToKeepStr: '-1', numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        skipDefaultCheckout()
+        disableConcurrentBuilds()
+    }
+
+    stages {
+        stage('Prepare') {
+            steps {
+                installRpms()
+                sh '''
+                    if [ ! -d $HOME/google-cloud-sdk/bin ]; then
+                        rm -rf $HOME/google-cloud-sdk
+                        curl https://sdk.cloud.google.com | bash
+                    fi
+
+                    source $HOME/google-cloud-sdk/path.bash.inc
+                    gcloud components update kubectl
+                    gcloud version
+
+                    curl -s https://get.helm.sh/helm-v3.2.3-linux-amd64.tar.gz \
+                        | sudo tar -C /usr/local/bin --strip-components 1 -zvxpf -
+
+                    sudo sh -c "curl -s -L https://github.com/mikefarah/yq/releases/download/v4.16.2/yq_linux_amd64 > /usr/local/bin/yq"
+                    sudo chmod +x /usr/local/bin/yq
+
+                    curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+                    sudo mv -v /tmp/eksctl /usr/local/bin
+
+                    cd "$(mktemp -d)"
+                    OS="$(uname | tr '[:upper:]' '[:lower:]')"
+                    ARCH="$(uname -m | sed -e 's/x86_64/amd64/')"
+                    KREW="krew-${OS}_${ARCH}"
+                    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/download/v0.4.2/${KREW}.tar.gz"
+                    tar zxvf "${KREW}.tar.gz"
+                    ./"${KREW}" install krew
+
+                    export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+                    kubectl krew install kuttl
+                '''
+
+            }
+        }
+        stage('Build docker image') {
+            steps {
+                git branch: 'master', url: 'https://github.com/Percona-Lab/jenkins-pipelines'
+                withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER'), file(credentialsId: 'cloud-secret-file', variable: 'CLOUD_SECRET_FILE')]) {
+                    sh '''
+                        sudo git reset --hard
+                        sudo git clean -xdf
+                        sudo rm -rf source
+                        ./cloud/local/checkout $GIT_REPO $GIT_BRANCH
+
+                        cp $CLOUD_SECRET_FILE ./source/e2e-tests/conf/cloud-secret.yml
+
+                        if [ -n "${OPERATOR_IMAGE}" ]; then
+                            echo "SKIP: Build is not needed, operator image was set!"
+                        else
+                            cd ./source/
+                            sg docker -c "
+                                docker login -u '${USER}' -p '${PASS}'
+                                export IMAGE=perconalab/percona-server-mysql-operator:$GIT_BRANCH
+                                ./e2e-tests/build
+                                docker logout
+                            "
+                            sudo rm -rf ./build
+                        fi
+                    '''
+                }
+            }
+        }
+        stage('Create EKS Infrastructure') {
+            steps {
+                sh '''
+cat <<-EOF > cluster.yaml
+# An example of ClusterConfig showing nodegroups with mixed instances (spot and on demand):
+---
+apiVersion: eksctl.io/v1alpha5
+kind: ClusterConfig
+
+metadata:
+    name: eks-psmo-cluster
+    region: eu-west-3
+    version: "$PLATFORM_VER"
+
+nodeGroups:
+    - name: ng-1
+      minSize: 3
+      maxSize: 5
+      instancesDistribution:
+        maxPrice: 0.15
+        instanceTypes: ["m5.xlarge", "m5.2xlarge"] # At least two instance types should be specified
+        onDemandBaseCapacity: 0
+        onDemandPercentageAboveBaseCapacity: 50
+        spotInstancePools: 2
+      tags:
+        'iit-billing-tag': 'jenkins-eks'
+EOF
+                '''
+
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'eks-cicd', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                     sh """
+                         export PATH=/home/ec2-user/.local/bin:$PATH
+                         source $HOME/google-cloud-sdk/path.bash.inc
+                         eksctl create cluster -f cluster.yaml
+                     """
+               }
+               stash includes: 'cluster.yaml', name: 'cluster_conf'
+
+            }
+        }
+        stage('E2E Basic Tests') {
+            options {
+                timeout(time: 3, unit: 'HOURS')
+            }
+            steps {
+                runTest('init-deploy')
+                runTest('monitoring')
+                runTest('semi-sync')
+                runTest('sidecars')
+            }
+        }
+        stage('Make report') {
+            steps {
+                makeReport()
+                sh """
+                    echo "${TestsReport}" > TestsReport.xml
+                """
+                step([$class: 'JUnitResultArchiver', testResults: '*.xml', healthScaleFactor: 1.0])
+                archiveArtifacts '*.xml'
+            }
+        }
+    }
+
+    post {
+        always {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'eks-cicd', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    unstash 'cluster_conf'
+                    sh """
+                        eksctl delete cluster -f cluster.yaml --wait
+                    """
+                }
+
+            sh '''
+                sudo docker rmi -f \$(sudo docker images -q) || true
+                sudo rm -rf $HOME/google-cloud-sdk
+                sudo rm -rf ./*
+            '''
+            deleteDir()
+        }
+    }
+}
