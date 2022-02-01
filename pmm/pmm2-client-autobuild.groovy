60f27363cf398e9e52a07f9a56649f50930bf4fa
Nikita Beletskii <nikita.beletskii@percona.com>
2022-02-01 12:24:01 +0300

    PMM-7 migrate client build job to new farm (#1367)
    * PMM-7 migrate client build job to a new farm

--- pmm/pmm2-client-autobuild.groovy
+++ pmm/pmm2-client-autobuild.groovy
@@ -4,9 +4,7 @@ library changelog: false, identifier: 'lib@master', retriever: modernSCM([
 ]) _
 
 pipeline {
-    agent {
-        label 'large-amazon'
-    }
+    agent none
     parameters {
         string(
             defaultValue: 'PMM-2.0',
@@ -25,141 +23,129 @@ pipeline {
         upstream upstreamProjects: 'pmm2-submodules-rewind', threshold: hudson.model.Result.SUCCESS
     }
     stages {
-        stage('Prepare') {
-            steps {
-                installDocker()
+        stage('Build PMM-Server') {
+            agent {
+                label 'docker-farm'
+            }
+            stages {
+                stage('Prepare') {
+                    steps {
+                        git poll: true, branch: GIT_BRANCH, url: 'http://github.com/Percona-Lab/pmm-submodules'
+                        sh '''
+                            git reset --hard
+                            sudo git clean -xdf
+                            git submodule update --init --jobs 10
+                            git submodule status
 
-                git poll: true, branch: GIT_BRANCH, url: 'http://github.com/Percona-Lab/pmm-submodules'
-                sh '''
-                    git reset --hard
-                    sudo git clean -xdf
-                    git submodule update --init --jobs 10
-                    git submodule status
+                            git rev-parse --short HEAD > shortCommit
+                            echo "UPLOAD/${DESTINATION}/${JOB_NAME}/pmm2/\$(cat VERSION)/${GIT_BRANCH}/\$(cat shortCommit)/${BUILD_NUMBER}" > uploadPath
+                        '''
+                        script {
+                            def versionTag = sh(returnStdout: true, script: "cat VERSION").trim()
+                            if ("${DESTINATION}" == "testing") {
+                                env.DOCKER_LATEST_TAG = "${versionTag}-rc${BUILD_NUMBER}"
+                                env.DOCKER_RC_TAG = "${versionTag}-rc"
+                            } else {
+                                env.DOCKER_LATEST_TAG = "dev-latest"
+                            }
+                        }
 
-                    git rev-parse --short HEAD > shortCommit
-                    echo "UPLOAD/${DESTINATION}/${JOB_NAME}/pmm2/\$(cat VERSION)/${GIT_BRANCH}/\$(cat shortCommit)/${BUILD_NUMBER}" > uploadPath
-                '''
-                script {
-                    def versionTag = sh(returnStdout: true, script: "cat VERSION").trim()
-                    if ("${DESTINATION}" == "testing") {
-                        env.DOCKER_LATEST_TAG = "${versionTag}-rc${BUILD_NUMBER}"
-                        env.DOCKER_RC_TAG = "${versionTag}-rc"
-                    } else {
-                        env.DOCKER_LATEST_TAG = "dev-latest"
+                        archiveArtifacts 'uploadPath'
+                        stash includes: 'uploadPath', name: 'uploadPath'
+                        archiveArtifacts 'shortCommit'
+                        slackSend botUser: true, channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
+                    }
+                }
+                stage('Build client source') {
+                    steps {
+                        sh './build/bin/build-client-source'
+                        stash includes: 'results/source_tarball/*.tar.*', name: 'source.tarball'
+                        uploadTarball('source')
+                    }
+                }
+                stage('Build client binary') {
+                    steps {
+                        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'pmm-staging-slave', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                            sh '''
+                                ./build/bin/build-client-binary
+                                aws s3 cp --acl public-read results/tarball/pmm2-client-*.tar.gz \
+                                    s3://pmm-build-cache/PR-BUILDS/pmm2-client/pmm2-client-latest-${BUILD_ID}.tar.gz
+                            '''
+                        }
+                        stash includes: 'results/tarball/*.tar.*', name: 'binary.tarball'
+                        uploadTarball('binary')
+                    }
+                }
+                stage('Build client docker') {
+                    steps {
+                        withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                            sh """
+                                echo "${PASS}" | docker login -u "${USER}" --password-stdin
+                            """
+                        }
+                        sh '''
+                            set -o xtrace
+
+                            export PUSH_DOCKER=1
+                            export DOCKER_CLIENT_TAG=perconalab/pmm-client:$(date -u '+%Y%m%d%H%M')
+
+                            ./build/bin/build-client-docker
+
+                            if [ ! -z \${DOCKER_RC_TAG+x} ]; then
+                                docker tag  \${DOCKER_CLIENT_TAG} perconalab/pmm-client:\${DOCKER_RC_TAG}
+                                docker push perconalab/pmm-client:\${DOCKER_RC_TAG}
+                                docker rmi perconalab/pmm-client:\${DOCKER_RC_TAG}
+                            fi
+                            docker tag  \${DOCKER_CLIENT_TAG} perconalab/pmm-client:\${DOCKER_LATEST_TAG}
+                            docker push \${DOCKER_CLIENT_TAG}
+                            docker push perconalab/pmm-client:\${DOCKER_LATEST_TAG}
+                            docker rmi  \${DOCKER_CLIENT_TAG}
+                            docker rmi  perconalab/pmm-client:\${DOCKER_LATEST_TAG}
+                        '''
+                        stash includes: 'results/docker/CLIENT_TAG', name: 'CLIENT_IMAGE'
+                        archiveArtifacts 'results/docker/CLIENT_TAG'
+                    }
+                }
+                stage('Build client source rpm') {
+                    steps {
+                        sh './build/bin/build-client-srpm centos:7'
+                        stash includes: 'results/srpm/pmm*-client-*.src.rpm', name: 'rpms'
+                        uploadRPM()
+                    }
+                }
+                stage('Build client binary rpm') {
+                    steps {
+                        sh './build/bin/build-client-rpm centos:7'
+                        sh './build/bin/build-client-rpm rockylinux:8'
+                        stash includes: 'results/rpm/pmm*-client-*.rpm', name: 'rpms'
+                        uploadRPM()
                     }
                 }
 
-                archiveArtifacts 'uploadPath'
-                stash includes: 'uploadPath', name: 'uploadPath'
-                archiveArtifacts 'shortCommit'
-                slackSend botUser: true, channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
-            }
-        }
-        stage('Build client source') {
-            steps {
-                sh '''
-                    sg docker -c "
-                        env
-                        ./build/bin/build-client-source
-                    "
-                '''
-                stash includes: 'results/source_tarball/*.tar.*', name: 'source.tarball'
-                uploadTarball('source')
-            }
-        }
-        stage('Build client binary') {
-            steps {
-                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'pmm-staging-slave', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                    sh '''
-                        sg docker -c "
-                            env
-                            ./build/bin/build-client-binary
-                        "
-                        aws s3 cp --acl public-read results/tarball/pmm2-client-*.tar.gz \
-                            s3://pmm-build-cache/PR-BUILDS/pmm2-client/pmm2-client-latest-${BUILD_ID}.tar.gz
-                    '''
+                stage('Build client source deb') {
+                    steps {
+                        sh './build/bin/build-client-sdeb ubuntu:bionic'
+                        stash includes: 'results/source_deb/*', name: 'debs'
+                        uploadDEB()
+                    }
                 }
-                stash includes: 'results/tarball/*.tar.*', name: 'binary.tarball'
-                uploadTarball('binary')
-            }
-        }
-        stage('Build client docker') {
-            steps {
-                withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                    sh """
-                        sg docker -c "
-                            docker login -u "${USER}" -p "${PASS}"
-                        "
-                    """
+                stage('Build client binary debs') {
+                    steps {
+                        sh './build/bin/build-client-deb debian:buster'
+                        sh './build/bin/build-client-deb debian:stretch'
+                        sh './build/bin/build-client-deb debian:bullseye'
+                        sh './build/bin/build-client-deb ubuntu:bionic'
+                        sh './build/bin/build-client-deb ubuntu:focal'
+                        stash includes: 'results/deb/*.deb', name: 'debs'
+                        uploadDEB()
+                    }
                 }
-                sh '''
-                    sg docker -c "
-                        set -o xtrace
-
-                        export PUSH_DOCKER=1
-                        export DOCKER_CLIENT_TAG=perconalab/pmm-client:$(date -u '+%Y%m%d%H%M')
-
-                        ./build/bin/build-client-docker
-
-                        if [ ! -z \${DOCKER_RC_TAG+x} ]; then
-                            docker tag  \\${DOCKER_CLIENT_TAG} perconalab/pmm-client:\${DOCKER_RC_TAG}
-                            docker push perconalab/pmm-client:\${DOCKER_RC_TAG}
-                            docker rmi perconalab/pmm-client:\${DOCKER_RC_TAG}
-                        fi
-                        docker tag  \\${DOCKER_CLIENT_TAG} perconalab/pmm-client:\${DOCKER_LATEST_TAG}
-                        docker push \\${DOCKER_CLIENT_TAG}
-                        docker push perconalab/pmm-client:\${DOCKER_LATEST_TAG}
-                        docker rmi  \\${DOCKER_CLIENT_TAG}
-                        docker rmi  perconalab/pmm-client:\${DOCKER_LATEST_TAG}
-                    "
-                '''
-                stash includes: 'results/docker/CLIENT_TAG', name: 'CLIENT_IMAGE'
-                archiveArtifacts 'results/docker/CLIENT_TAG'
-            }
-        }
-        stage('Build client source rpm') {
-            steps {
-                sh 'sg docker -c "./build/bin/build-client-srpm centos:7"'
-                stash includes: 'results/srpm/pmm*-client-*.src.rpm', name: 'rpms'
-                uploadRPM()
-            }
-        }
-        stage('Build client binary rpm') {
-            steps {
-                sh '''
-                    sg docker -c "
-                        env
-                        ./build/bin/build-client-rpm centos:7
-                        ./build/bin/build-client-rpm centos:8
-                    "
-                '''
-                stash includes: 'results/rpm/pmm*-client-*.rpm', name: 'rpms'
-                uploadRPM()
-            }
-        }
-
-        stage('Build client source deb') {
-            steps {
-                sh 'sg docker -c "./build/bin/build-client-sdeb ubuntu:bionic"'
-                stash includes: 'results/source_deb/*', name: 'debs'
-                uploadDEB()
-            }
-        }
-        stage('Build client binary debs') {
-            steps {
-                sh 'sg docker -c "./build/bin/build-client-deb debian:buster"'
-                sh 'sg docker -c "./build/bin/build-client-deb debian:stretch"'
-                sh 'sg docker -c "./build/bin/build-client-deb debian:bullseye"'
-                sh 'sg docker -c "./build/bin/build-client-deb ubuntu:bionic"'
-                sh 'sg docker -c "./build/bin/build-client-deb ubuntu:focal"'
-                stash includes: 'results/deb/*.deb', name: 'debs'
-                uploadDEB()
-            }
-        }
-        stage('Sign packages') {
-            steps {
-                signRPM()
-                signDEB()
+                stage('Sign packages') {
+                    steps {
+                        signRPM()
+                        signDEB()
+                    }
+                }
             }
         }
         stage('Push to public repository') {
@@ -171,11 +157,8 @@ pipeline {
                 sync2ProdPMM(DESTINATION, 'yes')
 
                 // upload tarball
-                deleteDir()
                 unstash 'binary.tarball'
-                sh '''
-                    scp -i ~/.ssh/id_rsa_downloads -P 2222 -o ConnectTimeout=1 -o StrictHostKeyChecking=no results/tarball/*.tar.* jenkins@jenkins-deploy.jenkins-deploy.web.r.int.percona.com:/data/downloads/TESTING/pmm/
-                '''
+                sh 'scp -i ~/.ssh/id_rsa_downloads -P 2222 -o ConnectTimeout=1 -o StrictHostKeyChecking=no results/tarball/*.tar.* jenkins@jenkins-deploy.jenkins-deploy.web.r.int.percona.com:/data/downloads/TESTING/pmm/'
             }
         }
     }
