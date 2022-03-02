b2aa36c068aacb24b5c92c41e2d70fde0a09465d
Nikita Beletskii <nikita.beletskii@percona.com>
2022-03-02 10:56:26 +0400

    fix

--- pmm/infrastructure/rpm-build.groovy
+++ pmm/infrastructure/rpm-build.groovy
@@ -32,7 +32,7 @@ pipeline {
                     credentialsId: 'ECRRWUser',
                     secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                     sh """
-                        docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) public.ecr.aws/e7j3v3n0
+                        docker login -u AWS -p \$(aws ecr get-login-password --region us-east-1) public.ecr.aws/e7j3v3n0
                         docker push public.ecr.aws/e7j3v3n0/rpmbuild:2
                     """
                 }
