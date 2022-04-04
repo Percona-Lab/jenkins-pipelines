8eae82e89e40c50f13d60c45545eed7c0dc04b92
Nikita Beletskii <nikita.beletskii@percona.com>
2022-04-04 14:10:25 +0700

    fix

--- vars/launchSpotInstance.groovy
+++ vars/launchSpotInstance.groovy
@@ -1,13 +1,23 @@
-def call(String INSTANCE_TYPE, String SPOT_PRICE, Int VOLUME) {
-    def SPOT_PRICE = SPOT_PRICE
-    def VOLUME = VOLUME
-    def INSTANCE_TYPE = INSTANCE_TYPE
-    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'pmm-staging-slave', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+def call(String INSTANCE_TYPE, String SPOT_PRICE, VOLUME) {
+   withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'pmm-staging-slave', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
         sh """
             export VM_NAME=\$(cat VM_NAME)
             export OWNER=\$(cat OWNER_FULL)
             export INSTANCE_TYPE=${INSTANCE_TYPE}
             export VOLUME=${VOLUME}
+
+            if [ "$SPOT_PRICE" = "FAIR" ]; then
+                export SPOT_PRICE=\$(
+                    aws ec2 describe-spot-price-history \
+                        --instance-types \$INSTANCE_TYPE \
+                        --region us-east-2 --output text \
+                        --product-description "Linux/UNIX (Amazon VPC)" | head -n 1 | awk '{ print \$5}'
+                )
+                echo SET PRICE: \$SPOT_PRICE
+                echo \$SPOT_PRICE > SPOT_PRICE
+            else
+                export SPOT_PRICE=${SPOT_PRICE}
+            fi
 
             export SUBNET=\$(
                 aws ec2 describe-subnets \
@@ -36,94 +46,66 @@ def call(String INSTANCE_TYPE, String SPOT_PRICE, Int VOLUME) {
                     --query 'SecurityGroups[].GroupId'
             )
 
-            while true
-            do
-                if [ "\$SPOT_PRICE" = "FAIR" ]; then
-                    export SPOT_PRICE=\$(
-                        aws ec2 describe-spot-price-history \
-                            --instance-types \$INSTANCE_TYPE \
-                            --region us-east-2 --output text \
-                            --product-description "Linux/UNIX (Amazon VPC)" | head -n 1 | awk '{ print \$5}'
-                    )
-                    echo SET PRICE: \$SPOT_PRICE
-                    echo \$SPOT_PRICE > SPOT_PRICE
-                else
-                    export SPOT_PRICE=\${SPOT_PRICE}
-                fi
-
-                echo '{
-                    "DryRun": false,
-                    "InstanceCount": 1,
-                    "InstanceInterruptionBehavior": "terminate",
-                    "LaunchSpecification": {
-                        "BlockDeviceMappings": [
-                            {
-                                "DeviceName": "/dev/xvda",
-                                "Ebs": {
-                                    "DeleteOnTermination": true,
-                                    "VolumeSize": VOLUME,
-                                    "VolumeType": "gp2"
-                                }
+            echo '{
+                "DryRun": false,
+                "InstanceCount": 1,
+                "InstanceInterruptionBehavior": "terminate",
+                "LaunchSpecification": {
+                    "BlockDeviceMappings": [
+                        {
+                            "DeviceName": "/dev/xvda",
+                            "Ebs": {
+                                "DeleteOnTermination": true,
+                                "VolumeSize": VOLUME,
+                                "VolumeType": "gp2"
                             }
-                        ],
-                        "EbsOptimized": false,
-                        "ImageId": "ami-00dfe2c7ce89a450b",
-                        "UserData": "c3VkbyB5dW0gaW5zdGFsbCAteSBqYXZhLTEuOC4wLW9wZW5qZGsKCnN1ZG8gL3Vzci9zYmluL2FsdGVybmF0aXZlcyAtLXNldCBqYXZhIC91c3IvbGliL2p2bS9qcmUtMS44LjAtb3Blbmpkay54ODZfNjQvYmluL2phdmEKCnN1ZG8gL3Vzci9zYmluL2FsdGVybmF0aXZlcyAtLXNldCBqYXZhYyAvdXNyL2xpYi9qdm0vanJlLTEuOC4wLW9wZW5qZGsueDg2XzY0L2Jpbi9qYXZhYwoKc3VkbyB5dW0gcmVtb3ZlIGphdmEtMS43Cg==",
-                        "InstanceType": "INSTANCE_TYPE",
-                        "KeyName": "jenkins",
-                        "Monitoring": {
-                            "Enabled": false
-                        },
-                        "IamInstanceProfile": {
-                            "Name": "pmm-staging-slave"
-                        },
-                        "SecurityGroupIds": [
-                            "security-group-id-1",
-                            "security-group-id-2"
-                        ],
-                        "SubnetId": "subnet-id"
+                        }
+                    ],
+                    "EbsOptimized": false,
+                    "ImageId": "ami-00dfe2c7ce89a450b",
+                    "UserData": "c3VkbyB5dW0gaW5zdGFsbCAteSBqYXZhLTEuOC4wLW9wZW5qZGsKCnN1ZG8gL3Vzci9zYmluL2FsdGVybmF0aXZlcyAtLXNldCBqYXZhIC91c3IvbGliL2p2bS9qcmUtMS44LjAtb3Blbmpkay54ODZfNjQvYmluL2phdmEKCnN1ZG8gL3Vzci9zYmluL2FsdGVybmF0aXZlcyAtLXNldCBqYXZhYyAvdXNyL2xpYi9qdm0vanJlLTEuOC4wLW9wZW5qZGsueDg2XzY0L2Jpbi9qYXZhYwoKc3VkbyB5dW0gcmVtb3ZlIGphdmEtMS43Cg==",
+                    "InstanceType": "INSTANCE_TYPE",
+                    "KeyName": "jenkins",
+                    "Monitoring": {
+                        "Enabled": false
                     },
-                    "SpotPrice": "SPOT_PRICE",
-                    "Type": "persistent"
-                }' \
-                    | sed -e "s/subnet-id/\${SUBNET}/" \
-                    | sed -e "s/security-group-id-1/\${SG1}/" \
-                    | sed -e "s/security-group-id-2/\${SG2}/" \
-                    | sed -e "s/SPOT_PRICE/\${SPOT_PRICE}/" \
-                    | sed -e "s/INSTANCE_TYPE/\${INSTANCE_TYPE}/" \
-                    | sed -e "s/VOLUME/\${VOLUME}/" \
-                    > config.json
+                    "IamInstanceProfile": {
+                        "Name": "pmm-staging-slave"
+                    },
+                    "SecurityGroupIds": [
+                        "security-group-id-1",
+                        "security-group-id-2"
+                    ],
+                    "SubnetId": "subnet-id"
+                },
+                "SpotPrice": "SPOT_PRICE",
+                "Type": "persistent"
+            }' \
+                | sed -e "s/subnet-id/\${SUBNET}/" \
+                | sed -e "s/security-group-id-1/\${SG1}/" \
+                | sed -e "s/security-group-id-2/\${SG2}/" \
+                | sed -e "s/SPOT_PRICE/\${SPOT_PRICE}/" \
+                | sed -e "s/INSTANCE_TYPE/\${INSTANCE_TYPE}/" \
+                | sed -e "s/VOLUME/\${VOLUME}/" \
+                > config.json
 
-                REQUEST_ID=\$(
-                    aws ec2 request-spot-instances \
-                        --output text \
-                        --region us-east-2 \
-                        --cli-input-json file://config.json \
-                        --query SpotInstanceRequests[].SpotInstanceRequestId
-                )
-                echo \$REQUEST_ID > REQUEST_ID
+            REQUEST_ID=\$(
+                aws ec2 request-spot-instances \
+                    --output text \
+                    --region us-east-2 \
+                    --cli-input-json file://config.json \
+                    --query SpotInstanceRequests[].SpotInstanceRequestId
+            )
+            echo \$REQUEST_ID > REQUEST_ID
 
-                ATTEMPS=10
-                until [ -s IP ]; do
-                    sleep 5
-                    aws ec2 describe-instances \
-                        --filters "Name=spot-instance-request-id,Values=\${REQUEST_ID}" \
-                        --query 'Reservations[].Instances[].PublicIpAddress' \
-                        --output text \
-                        --region us-east-2 \
-                        | tee IP
-                    ATTEMPS=$((ATTEMPS-1))
-                    if [ \$ATTEMPS -eq 0 ]; then
-                        break
-                    fi
-                done
-
-                if [ -s IP ]; then
-                    break
-                else
-                    aws ec2 --region us-east-2 cancel-spot-instance-requests --spot-instance-request-ids \$REQUEST_ID
-                    continue
-                fi
+            until [ -s IP ]; do
+                sleep 5
+                aws ec2 describe-instances \
+                    --filters "Name=spot-instance-request-id,Values=\${REQUEST_ID}" \
+                    --query 'Reservations[].Instances[].PublicIpAddress' \
+                    --output text \
+                    --region us-east-2 \
+                    | tee IP
             done
 
             aws ec2 describe-instances \
