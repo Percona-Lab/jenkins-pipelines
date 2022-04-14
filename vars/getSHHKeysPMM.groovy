5e08d2f27af6da975da48ddc4acbff53bb7ee325
Nikita Beletskii <nikita.beletskii@percona.com>
2022-04-14 14:24:59 +0700

    add more people

--- vars/getSHHKeysPMM.groovy
+++ vars/getSHHKeysPMM.groovy
@@ -1,7 +1,7 @@
 String call() {
     // You need to add your keys to https://github.com/settings/keys
     String sshKeys = ''
-    final List additional_keys = ['nikita-b', 'nailya', 'puneet0191'] // github users
+    final List additional_keys = ['nikita-b', 'nailya', 'puneet0191', 'BupycHuk', ] // github users
     additional_keys.each { item ->
         response = httpRequest "https://github.com/${item}.keys"
         sshKeys += response.content + '\n'
