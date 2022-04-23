9eb5d3c8a0a6e6f8d3e938e56a6580c88d1a0525
Nikita Beletskii <nikita.beletskii@percona.com>
2022-04-23 11:16:53 +0700

    PMM-7 add ARM version for rpmbuild

--- pmm/infrastructure/rpm-build.groovy
+++ pmm/infrastructure/rpm-build.groovy
@@ -13,30 +13,46 @@ pipeline {
         buildDiscarder(logRotator(artifactNumToKeepStr: '10'))
     }
     stages {
-        stage('Prepare') {
-            steps {
-                git poll: true,
-                    branch: SUBMODULES_GIT_BRANCH,
-                    url: 'https://github.com/Percona-Lab/pmm-submodules.git'
-            }
-        }
-        stage('Build') {
-            steps {
-                sh """
-                    cd build/rpmbuild-docker
-                    docker build --pull --squash --tag public.ecr.aws/e7j3v3n0/rpmbuild:2 .
-                """
-                withCredentials([[
-                    $class: 'AmazonWebServicesCredentialsBinding',
-                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                    credentialsId: 'ECRRWUser',
-                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                    sh """
-                        aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin public.ecr.aws/e7j3v3n0
-                        docker push public.ecr.aws/e7j3v3n0/rpmbuild:2
-                    """
+        stage('Build rpmbuild image') {
+            matrix {
+                agent {
+                    label "${AGENT}"
+                }
+                axes {
+                    axis {
+                        name 'AGENT'
+                        values 'docker-farm', 'docker-farm-arm'
+                    }
+                }
+                stages {
+                    stage('Prepare') {
+                        steps {
+                            git poll: true,
+                                branch: SUBMODULES_GIT_BRANCH,
+                                url: 'https://github.com/Percona-Lab/pmm-submodules.git'
+                        }
+                    }
+                    stage('Build') {
+                        steps {
+                            sh """
+                                cd build/rpmbuild-docker
+                                docker build --pull --squash --tag public.ecr.aws/e7j3v3n0/rpmbuild:2 .
+                            """
+                            withCredentials([[
+                                $class: 'AmazonWebServicesCredentialsBinding',
+                                accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                                credentialsId: 'ECRRWUser',
+                                secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                                sh """
+                                    aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin public.ecr.aws/e7j3v3n0
+                                    docker push public.ecr.aws/e7j3v3n0/rpmbuild:2
+                                """
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
+
