fa0811e4604f9307d84212d151edbfa1de5db8f2
Nikita Beletskii <nikita.beletskii@percona.com>
2022-04-25 18:21:23 +0700

    change branch to master

--- pmm/pmm2-ovf-staging-stop.groovy
+++ pmm/pmm2-ovf-staging-stop.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: 'lib@PMM-6800-add-ova-staging', retriever: modernSCM([
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
