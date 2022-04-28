514bdfc9bdaabddedffcbde1eba7b671a77c4a92
Nikita Beletskii <nikita.beletskii@percona.com>
2022-04-28 18:38:24 +0700

    PMM-7 fix color for prepare release candidate

--- pmm/pmm2-prepare-release-candidate.groovy
+++ pmm/pmm2-prepare-release-candidate.groovy
@@ -216,7 +216,7 @@ pipeline {
                     currentBuild.description = "$VERSION"
                     slackSend botUser: true,
                         channel: '#pmm-dev',
-                        color: '#FF0000',
+                        color: '#0892d0',
                         message: "Release candidate PMM $VERSION build has started. You can check progress at: ${BUILD_URL}"
                     env.EXIST = sh (
                         script: 'git ls-remote --heads https://github.com/Percona-Lab/pmm-submodules pmm-\${VERSION} | wc -l',
