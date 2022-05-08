5732bd33aead99b95d1c0e3b32f8723cdd029841
Nikita Beletskii <nikita.beletskii@percona.com>
2022-05-08 21:21:53 +0700

    PMM-7 add arm for rpmbuild (#1498)
    * PMM-7 add ARM version for rpmbuild
    
    * use none agent
    
    * move to dockerhub
    
    * fix
    
    * fix
    
    * remove tag

--- pmm/infrastructure/rpm-build.groovy
+++ pmm/infrastructure/rpm-build.groovy
@@ -1,7 +1,5 @@
 pipeline {
-    agent {
-        label 'docker-farm'
-    }
+    agent none
     parameters {
         string(
             defaultValue: 'PMM-2.0',
@@ -12,31 +10,57 @@ pipeline {
         skipStagesAfterUnstable()
         buildDiscarder(logRotator(artifactNumToKeepStr: '10'))
     }
+    environment {
+        IMAGE_REGISTRY = "public.ecr.aws/e7j3v3n0"
+    }
     stages {
-        stage('Prepare') {
-            steps {
-                git poll: true,
-                    branch: SUBMODULES_GIT_BRANCH,
-                    url: 'https://github.com/Percona-Lab/pmm-submodules.git'
-            }
-        }
-        stage('Build') {
-            steps {
-                sh """
-                    cd build/rpmbuild-docker
-                    docker build --pull --squash --tag public.ecr.aws/e7j3v3n0/rpmbuild:2 .
-                """
-                withCredentials([[
-                    $class: 'AmazonWebServicesCredentialsBinding',
-                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                    credentialsId: 'ECRRWUser',
-                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                    sh """
-                        aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin public.ecr.aws/e7j3v3n0
-                        docker push public.ecr.aws/e7j3v3n0/rpmbuild:2
-                    """
+        stage('Build rpmbuild image') {
+            matrix {
+                agent {
+                    label "agent-${ARCH}"
+                }
+                axes {
+                    axis {
+                        name 'ARCH'
+                        values 'amd64', 'arm64'
+                    }
+                }
+                stages {
+                    stage('Prepare') {
+                        steps {
+                            git poll: true,
+                                branch: SUBMODULES_GIT_BRANCH,
+                                url: 'https://github.com/Percona-Lab/pmm-submodules.git'
+                        }
+                    }
+                    stage('Build') {
+                        steps {
+                            sh """
+                                cd build/rpmbuild-docker
+                                docker build --pull --tag ${IMAGE_REGISTRY}/rpmbuild:${ARCH} .
+                            """
+                            withCredentials([[
+                                $class: 'AmazonWebServicesCredentialsBinding',
+                                accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                                credentialsId: 'ECRRWUser',
+                                secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                                sh """
+                                    aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin ${IMAGE_REGISTRY}
+                                    docker push ${IMAGE_REGISTRY}/rpmbuild:${ARCH}
+                                    docker manifest create ${IMAGE_REGISTRY}/rpmbuild:2 \
+                                        ${IMAGE_REGISTRY}/rpmbuild:amd64  \
+                                        ${IMAGE_REGISTRY}/rpmbuild:arm64
+                                    docker manifest annotate --arch amd64 ${IMAGE_REGISTRY}/rpmbuild:2 ${IMAGE_REGISTRY}/rpmbuild:amd64
+                                    docker manifest annotate --arch arm64 ${IMAGE_REGISTRY}/rpmbuild:2 ${IMAGE_REGISTRY}/rpmbuild:arm64
+
+                                    docker manifest push ${IMAGE_REGISTRY}/rpmbuild:2
+                                """
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
+
