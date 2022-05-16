7469e2cd510310d02d3d58bc587e05f03ff0edd8
Nikita Beletskii <nikita.beletskii@percona.com>
2022-05-16 10:12:44 +0000

    dont install jave + FAIR price

--- pmm/aws-portal-start.groovy
+++ pmm/aws-portal-start.groovy
@@ -94,10 +94,10 @@ pipeline {
 
         stage('Run VM') {
             steps {
-                launchSpotInstance('m5.2xlarge', '0.43', 25)
+                launchSpotInstance('m5.2xlarge', 'FAIR', 25)
                 withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
                     sh """
-                        until ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no ${USER}@\$(cat IP) 'java -version; sudo yum install -y java-1.8.0-openjdk; sudo /usr/sbin/alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java; java -version;' ; do
+                        until ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no ${USER}@\$(cat IP); do
                             sleep 5
                         done
 
