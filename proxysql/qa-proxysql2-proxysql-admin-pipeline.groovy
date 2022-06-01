134ec7730f7f71a467b1f96e5434eaa5943da762
Vadim Yalovets <vadim.yalovets@percona.com>
2022-06-01 17:30:30 +0300

    PSQLADM-361 Create a Jenkins job to build proxysql-admin and run test suites

--- proxysql/qa-proxysql2-proxysql-admin-pipeline.groovy
+++ proxysql/qa-proxysql2-proxysql-admin-pipeline.groovy
@@ -82,7 +82,7 @@ pipeline {
                         ./proxysql/checkout PROXYSQL
                     '''
 
-                    echo 'Build ProxySQL ${DOCKER_OS}'
+                    echo 'Build ProxySQL}'
                     withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'c42456e5-c28d-4962-b32c-b75d161bff27', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                         sh '''
                             aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin public.ecr.aws/e7j3v3n0
