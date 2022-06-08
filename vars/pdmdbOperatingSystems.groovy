e142499d7317c9db7b905bb22e38f016e4320e20
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2022-06-08 10:13:41 +0300

    PSMDB-1041 added ubuntu jammy

--- vars/pdmdbOperatingSystems.groovy
+++ vars/pdmdbOperatingSystems.groovy
@@ -1,3 +1,3 @@
 def call() {
-  return ['centos-7', 'debian-9', 'debian-10', 'debian-11', 'ubuntu-focal', 'ubuntu-bionic', 'rhel8']
+  return ['centos-7', 'debian-9', 'debian-10', 'debian-11', 'ubuntu-focal', 'ubuntu-bionic', 'rhel8', 'ubuntu-jammy']
 }
