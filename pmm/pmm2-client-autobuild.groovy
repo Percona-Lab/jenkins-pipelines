24a76fb9f444281b183fc818d98b54d310983ec9
Nikita Beletskii <nikita.beletskii@percona.com>
2022-06-15 08:16:02 +0000

    add ARM build

--- pmm/pmm2-client-autobuild.groovy
+++ pmm/pmm2-client-autobuild.groovy
@@ -117,6 +117,7 @@ pipeline {
                     steps {
                         sh './build/bin/build-client-rpm centos:7'
                         sh './build/bin/build-client-rpm rockylinux:8'
+                        sh './build/bin/build-client-rpm almalinux:9.0'
                         stash includes: 'results/rpm/pmm*-client-*.rpm', name: 'rpms'
                         uploadRPM()
                     }
