fad3bed4635a2554e5ed40caa6b98409a0161aef
Tomislav Plavcic <tomislav.plavcic@percona.com>
2022-06-28 15:33:51 +0200

    K8SPS-108 - Add IMAGE_PMM_SERVER_TAG and IMAGE_PMM_SERVER_REPO for PS (#1612)
    operator

--- cloud/jenkins/ps_operator_eks.groovy
+++ cloud/jenkins/ps_operator_eks.groovy
@@ -74,6 +74,14 @@ void runTest(String TEST_NAME) {
                                 export IMAGE_PMM=${IMAGE_PMM}
                             fi
 
+                            if [ -n "${IMAGE_PMM_SERVER_REPO}" ]; then
+                                export IMAGE_PMM_SERVER_REPO=${IMAGE_PMM_SERVER_REPO}
+                            fi
+
+                            if [ -n "${IMAGE_PMM_SERVER_TAG}" ]; then
+                                export IMAGE_PMM_SERVER_TAG=${IMAGE_PMM_SERVER_TAG}
+                            fi
+
                             export PATH="/home/ec2-user/.local/bin:${HOME}/.krew/bin:$PATH"
                             source $HOME/google-cloud-sdk/path.bash.inc
                             export KUBECONFIG=~/.kube/config
@@ -155,6 +163,14 @@ pipeline {
             defaultValue: '',
             description: 'PMM image: perconalab/pmm-client:dev-latest',
             name: 'IMAGE_PMM')
+        string(
+            defaultValue: '',
+            description: 'PMM server image repo: perconalab/pmm-server',
+            name: 'IMAGE_PMM_SERVER_REPO')
+        string(
+            defaultValue: '',
+            description: 'PMM server image tag: dev-latest',
+            name: 'IMAGE_PMM_SERVER_TAG')
     }
     environment {
         CLEAN_NAMESPACE = 1
