fd22a33c497ef79baf6c2f358a0d7b268a0b73b2
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2022-06-28 17:16:55 +0300

    skip cli tests stage

--- pmm/pmm2-ui-tests-matrix.groovy
+++ pmm/pmm2-ui-tests-matrix.groovy
@@ -126,13 +126,13 @@ pipeline {
                         }
                     }
                 }
-                stage('Run CLI tests @cli'){
-                    steps {
-                        script {
-                            runUITestsJob(GIT_BRANCH, GIT_COMMIT_HASH, DOCKER_VERSION, CLIENT_VERSION, '@cli', MYSQL_IMAGE, POSTGRES_IMAGE, MONGO_IMAGE, PROXYSQL_IMAGE, PMM_QA_GIT_BRANCH, '');
-                        }
-                    }
-                }
+//                 stage('Run CLI tests @cli'){
+//                     steps {
+//                         script {
+//                             runUITestsJob(GIT_BRANCH, GIT_COMMIT_HASH, DOCKER_VERSION, CLIENT_VERSION, '@cli', MYSQL_IMAGE, POSTGRES_IMAGE, MONGO_IMAGE, PROXYSQL_IMAGE, PMM_QA_GIT_BRANCH, '');
+//                         }
+//                     }
+//                 }
             }
         }
     }
