0c2c5849d4773c76ee9cc86d39159cf20f81f97c
Puneet Kaushik <puneet.kaushik@percona.com>
2022-07-01 18:44:03 +0530

    Update

--- pxc/jenkins/cross_version_pxc_57_80_test-pipeline.groovy
+++ pxc/jenkins/cross_version_pxc_57_80_test-pipeline.groovy
@@ -44,7 +44,7 @@ pipeline {
                     mkdir $ROOT_FS/lower_pxc_latest
                     cd $ROOT_FS/lower_pxc_latest
                     LOWER_PXC_TAR=lower-pxc-latest.tar.gz
-                    wget -cO - https://downloads.percona.com/downloads/TESTING/pxc-${LOWER_PXC_VERSION}/Percona-XtraDB-Cluster-${PXC57_PKG_VERSION}.Linux.x86_64.glibc2.17.tar.gz > ${LOWER_PXC_TAR}
+                    wget -qcO - https://downloads.percona.com/downloads/TESTING/pxc-${LOWER_PXC_VERSION}/Percona-XtraDB-Cluster-${PXC57_PKG_VERSION}.Linux.x86_64.glibc2.17.tar.gz > ${LOWER_PXC_TAR}
                     tar -xzf ${LOWER_PXC_TAR}
                     rm *.tar.gz
                     mv Percona-XtraDB-* Lower-Percona-XtraDB-latest
@@ -59,7 +59,7 @@ pipeline {
                     mkdir $ROOT_FS/upper_pxc_latest
                     cd $ROOT_FS/upper_pxc_latest
                     UPPER_PXC_TAR=upper-pxc-latest.tar.gz
-                    wget -cO - https://downloads.percona.com/downloads/TESTING/pxc-${UPPER_PXC_VERSION}/Percona-XtraDB-Cluster_${UPPER_PXC_VERSION}_Linux.x86_64.glibc2.17.tar.gz > ${UPPER_PXC_TAR}
+                    wget -qcO - https://downloads.percona.com/downloads/TESTING/pxc-${UPPER_PXC_VERSION}/Percona-XtraDB-Cluster_${UPPER_PXC_VERSION}_Linux.x86_64.glibc2.17.tar.gz > ${UPPER_PXC_TAR}
                     tar -xzf ${UPPER_PXC_TAR}
                     rm *.tar.gz
                     mv Percona-XtraDB-* Upper-Percona-XtraDB-latest
