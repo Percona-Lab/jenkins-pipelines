0a8d58f2ab6133db8e273ef77e6f2ab89bf50fdf
yurkovychv <vasyl.yurkovych@percona.com>
2022-07-13 17:54:50 +0300

    review changes

--- pmm/pmm2-portal-integration.groovy
+++ pmm/pmm2-portal-integration.groovy
@@ -58,17 +58,6 @@ pipeline {
                 git poll: false, branch: GIT_BRANCH, url: 'https://github.com/percona/pmm-ui-tests.git'
 
                 slackSend channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
-                sh '''
-                    sudo yum -y install svn
-                    sudo mkdir -p /srv/pmm-qa || :
-                    pushd /srv/pmm-qa
-                        sudo git clone --single-branch --branch \${PMM_QA_GIT_BRANCH} https://github.com/percona/pmm-qa.git .
-                        sudo git checkout \${PMM_QA_GIT_COMMIT_HASH}
-                        sudo chmod 755 pmm-tests/install-google-chrome.sh
-                        bash ./pmm-tests/install-google-chrome.sh
-                    popd
-                    sudo ln -s /usr/bin/google-chrome-stable /usr/bin/chromium
-                '''
             }
         }
         stage('Start Server Instance') {
