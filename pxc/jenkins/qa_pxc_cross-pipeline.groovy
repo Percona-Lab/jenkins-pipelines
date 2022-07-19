52e03377f70e1c83fa3b0e552aa51bb6075523d7
Puneet Kaushik <puneet.kaushik@percona.com>
2022-07-19 13:30:57 +0530

    update

--- pxc/jenkins/qa_pxc_cross-pipeline.groovy
+++ pxc/jenkins/qa_pxc_cross-pipeline.groovy
@@ -1,5 +1,7 @@
 pipeline {
-  agent docker
+   agent {
+        label 'docker'
+    }
   parameters {
     string(name: 'LOWER_PXC_VERSION', defaultValue: '5.7.38-31.59', description: 'PXC lower version tarball to download for testing')
     string(name: 'UPPER_PXC_VERSION', defaultValue: '8.0.27-18.1', description: 'PXC Upper version tarball to download for testing')
