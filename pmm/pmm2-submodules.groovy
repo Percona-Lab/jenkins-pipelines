5d7b97323227071a13aa0e901eea78e25b598a1e
Nikita Beletskii <nikita.beletskii@percona.com>
2022-07-25 15:58:19 +0700

    PMM-7 fix pmm2-submodules pipeline (#1656)

--- pmm/pmm2-submodules.groovy
+++ pmm/pmm2-submodules.groovy
@@ -275,10 +275,10 @@ pipeline {
                         // trigger workflow in GH to run some test there as well, pass server and client images as parameters
                         sh """
                             curl \
-                                -v -X POST \
-                                -H "Accept: application/vnd.github.v3+json" \ 
-                                -H "Authorization: token ${GITHUB_API_TOKEN}" \
-                                "https://api.github.com/repos/\$(echo $CHANGE_URL | cut -d '/' -f 4-5)/actions/workflows/jenkins-dispatch/dispatches" \
+                                -v -X POST \\
+                                -H "Accept: application/vnd.github.v3+json" \\
+                                -H "Authorization: token ${GITHUB_API_TOKEN}" \\
+                                "https://api.github.com/repos/\$(echo $CHANGE_URL | cut -d '/' -f 4-5)/actions/workflows/jenkins-dispatch/dispatches" \\
                                 -d '{"ref":"${GIT_BRANCH}","inputs":{"server_image":"${IMAGE}","client_image":"${CLIENT_IMAGE}","sha":"${GIT_COMMIT_HASH}"}}'
                         """
                     }
