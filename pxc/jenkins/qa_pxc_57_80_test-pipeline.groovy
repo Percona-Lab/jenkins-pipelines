5387073a58c18af9250299c3c3dd84a04cf07295
Puneet Kaushik <puneet.kaushik@percona.com>
2022-07-26 14:47:19 +0530

    update

--- pxc/jenkins/qa_pxc_57_80_test-pipeline.groovy
+++ pxc/jenkins/qa_pxc_57_80_test-pipeline.groovy
@@ -63,6 +63,7 @@ stages {
                     sudo apt install -y git wget socat curl gnupg2 numactl
                     curl -O https://repo.percona.com/apt/percona-release_latest.generic_all.deb
                     sudo apt install -y ./percona-release_latest.generic_all.deb
+                    sudo apt-get update
                     fi
                     ROOT_FS=$(pwd)
                     sudo killall -9 mysqld || true
