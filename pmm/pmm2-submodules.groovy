32a3d4f1f5e6dec7129855b21180539e35a6f1e0
Denys Kondratenko <denisok@users.noreply.github.com>
2022-07-27 17:40:59 +0200

    fix pipeline (#1676)

--- pmm/pmm2-submodules.groovy
+++ pmm/pmm2-submodules.groovy
@@ -282,7 +282,7 @@ pipeline {
                                 -H "Accept: application/vnd.github.v3+json" \
                                 -H "Authorization: token ${GITHUB_API_TOKEN}" \
                                 "https://api.github.com/repos/\$(echo $CHANGE_URL | cut -d '/' -f 4-5)/actions/workflows/jenkins-dispatch.yml/dispatches" \
-                                -d '{"ref":"${GIT_LOCAL_BRANCH}","inputs":{"server_image":"${IMAGE}","client_image":"${CLIENT_IMAGE}","sha":"${FB_COMMIT_HASH}"}}'
+                                -d '{"ref":"PMM-2.0","inputs":{"server_image":"${IMAGE}","client_image":"${CLIENT_IMAGE}","sha":"${FB_COMMIT_HASH}"}}'
                         """
                     }
                 }
