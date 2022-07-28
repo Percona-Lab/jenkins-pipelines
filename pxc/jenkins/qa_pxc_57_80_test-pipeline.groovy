c59659eb93f2a1c1e711c87cf8fe659938c5606a
Puneet Kaushik <puneet.kaushik@percona.com>
2022-07-28 11:34:31 +0530

    Update

--- pxc/jenkins/qa_pxc_57_80_test-pipeline.groovy
+++ pxc/jenkins/qa_pxc_57_80_test-pipeline.groovy
@@ -33,8 +33,7 @@ pipeline {
                 'min-bionic-x64',
                 'min-focal-x64',
                 'min-buster-x64',
-                'min-bullseye-x64',
-                'min-jammy-x64'
+                'min-bullseye-x64'
             ],
             description: 'Node to run tests',
             name: 'node_to_test'
@@ -62,7 +61,6 @@ stages {
                     sudo yum -y update
                     sudo yum install -y git wget tar socat
                     else
-                    lsb_release -a
                     sudo apt install -y git wget socat curl gnupg2 numactl
                     curl -O https://repo.percona.com/apt/percona-release_latest.generic_all.deb
                     sudo apt install -y ./percona-release_latest.generic_all.deb
