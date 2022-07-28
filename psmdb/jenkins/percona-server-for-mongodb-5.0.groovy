145bf95c3904a4f4726f1c15c47bd5c5bca489ab
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2022-07-28 15:05:55 +0300

    PSMDB-7 remove stretch builds

--- psmdb/jenkins/percona-server-for-mongodb-5.0.groovy
+++ psmdb/jenkins/percona-server-for-mongodb-5.0.groovy
@@ -191,19 +191,6 @@ pipeline {
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
-                stage('Debian Stretch(9)') {
-                    agent {
-                        label 'docker-32gb'
-                    }
-                    steps {
-                        cleanUpWS()
-                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
-                        buildStage("debian:stretch", "--build_deb=1")
-
-                        pushArtifactFolder("deb/", AWS_STASH_PATH)
-                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
-                    }
-                }
                 stage('Debian Buster(10)') {
                     agent {
                         label 'docker-32gb'
