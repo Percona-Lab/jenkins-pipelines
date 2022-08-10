0e08d92ea0385fd4613c0cad02f51db0c62bcb1c
Nikita Beletskii <nikita.beletskii@percona.com>
2022-08-10 02:19:09 +0200

    fix

--- pmm/pmm2-client-autobuild.groovy
+++ pmm/pmm2-client-autobuild.groovy
@@ -163,12 +163,12 @@ pipeline {
                     script {
                         unstash 'uploadPath'
                         def path_to_build = sh(returnStdout: true, script: "cat uploadPath").trim()
-                    }
-                    sh """
-                        ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com << 'ENDSSH'
-                        scp -P 2222 -o ConnectTimeout=1 -o StrictHostKeyChecking=no ${path_to_build}/binary/tarball/*.tar.gz jenkins@jenkins-deploy.jenkins-deploy.web.r.int.percona.com:/data/downloads/TESTING/pmm/'
+                        sh """
+                            ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com << 'ENDSSH'
+                            scp -P 2222 -o ConnectTimeout=1 -o StrictHostKeyChecking=no ${path_to_build}/binary/tarball/*.tar.gz jenkins@jenkins-deploy.jenkins-deploy.web.r.int.percona.com:/data/downloads/TESTING/pmm/'
 ENDSSH
-                    """        
+                        """      
+                    }  
                 }
             }
         }
