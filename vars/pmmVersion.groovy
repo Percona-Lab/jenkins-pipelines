d4847156b6a31e7298f7ec974d2c164e4972507a
Taras <70516245+taraskozub-percona@users.noreply.github.com>
2022-08-17 17:32:41 +0300

    Update pmmVersion.groovy
    Update PMM 2.30 ami

--- vars/pmmVersion.groovy
+++ vars/pmmVersion.groovy
@@ -18,7 +18,7 @@ def call(type='latest') {
     '2.28.0': 'ami-015cbf0312dd101c7',
     '2.29.0': 'ami-0e68224439dd6f200',
     '2.29.1': 'ami-01ce74cdab54cabcd',
-    '2.30.0': 'ami-0239e0f6ea66aacbf'
+    '2.30.0': 'ami-0d7968647bc2a3b93'
   ]
   List<String> versionsList = new ArrayList<>(versions.keySet());
   switch(type) {
