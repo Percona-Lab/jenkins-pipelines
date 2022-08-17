12ccd606e6696d959b4b42586e82d62206184106
Puneet Kala <puneet.0191@gmail.com>
2022-08-17 10:49:35 +0530

    PMM-7 Post client upgrade

--- pmm/pmm2-upgrade-tests.groovy
+++ pmm/pmm2-upgrade-tests.groovy
@@ -352,7 +352,7 @@ pipeline {
         }
         stage('Check Client Upgrade') {
             steps {
-                checkClientAfterUpgrade(PMM_SERVER_LATEST, "post");
+                checkClientAfterUpgrade(PMM_SERVER_LATEST, "postClientUpgrade");
                 sh """
                     export PWD=\$(pwd);
                     export CHROMIUM_PATH=/usr/bin/chromium
