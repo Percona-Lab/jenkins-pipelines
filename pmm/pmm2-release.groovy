4a3a92a7a6f35e30e59ce4bff4d7e5fef33661e4
Alex Tymchuk <alexander.tymchuk@gmail.com>
2022-08-23 20:58:30 +0300

    PMM-7 fix the syntax (brackets)

--- pmm/pmm2-release.groovy
+++ pmm/pmm2-release.groovy
@@ -495,7 +495,8 @@ ENDSSH
                         scp -i ~/.ssh/id_rsa_downloads -P 2222 -o ConnectTimeout=1 -o StrictHostKeyChecking=no pmm-server-\${VERSION}.docker pmm-server-\${VERSION}.sha256sum \${REPO_HOST}:/data/downloads/pmm2/\${VERSION}/docker/
                         scp -i ~/.ssh/id_rsa_downloads -P 2222 -o ConnectTimeout=1 -o StrictHostKeyChecking=no pmm-client-\${VERSION}.docker pmm-client-\${VERSION}.sha256sum \${REPO_HOST}:/data/downloads/pmm2/\${VERSION}/docker/
 ENDSSH
-                """
+                    """
+                }
                 deleteDir()
             }
         }
@@ -513,7 +514,8 @@ ENDSSH
                         sha256sum pmm-server-\${VERSION}.ova > pmm-server-\${VERSION}.sha256sum
                         scp -i ~/.ssh/id_rsa-downloads -P 2222 -o ConnectTimeout=1 -o StrictHostKeyChecking=no pmm-server-\${VERSION}.ova pmm-server-\${VERSION}.sha256sum \${REPO_HOST}:/data/downloads/pmm2/\${VERSION}/ova/
 ENDSSH
-                """
+                    """
+                }
                 deleteDir()
             }
         }
