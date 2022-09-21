bb89a6b3d1ab844365d8976b86dc864f0866db38
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-09-21 17:23:59 +0300

    DISTMYSQL-180: remove downgrade stages (#1765)

--- pdpxc/pdpxc-multi-parallel.groovy
+++ pdpxc/pdpxc-multi-parallel.groovy
@@ -166,33 +166,6 @@ pipeline {
                 }
             }
         }
-        stage ('Test minor downgrade') {
-            when {
-                expression { env.TO_REPO != 'release' }
-            }
-            steps {
-                script {
-                    try {
-                        build job: 'pdpxc-upgrade-parallel', parameters: [
-                        string(name: 'FROM_REPO', value: "${env.TO_REPO}"),
-                        string(name: 'FROM_VERSION', value: "${env.VERSION}"),
-                        string(name: 'TO_REPO', value: "${env.FROM_REPO}"),
-                        string(name: 'VERSION', value: "${env.FROM_VERSION}"),
-                        string(name: 'TESTING_BRANCH', value: "${env.TESTING_BRANCH}"),
-                        string(name: 'SCENARIO', value: "pdpxc-minor-upgrade"),
-                        string(name: 'PROXYSQL_VERSION', value: "${env.PROXYSQL_VERSION}"),
-                        string(name: 'PXB_VERSION', value: "${env.PXB_VERSION}"),
-                        string(name: 'PT_VERSION', value: "${env.PT_VERSION}"),
-                        string(name: 'HAPROXY_VERSION', value: "${env.HAPROXY_VERSION}"),
-                        ]
-                    }
-                    catch (err) {
-                        currentBuild.result = "FAILURE"
-                        echo "Stage 'Test minor downgrade' failed, but we continue"
-                    }
-                }
-            }
-        }
         stage ('Test haproxy') {
             when {
                 expression { env.TO_REPO != 'release' }
