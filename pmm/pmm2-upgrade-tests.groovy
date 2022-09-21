b9e7e4b4f1b8a499ff1274f7cf02e6477c5b9f71
Alex Tymchuk <alexander.tymchuk@gmail.com>
2022-09-21 12:35:40 +0300

    PMM-7 put back single quotes

--- pmm/pmm2-upgrade-tests.groovy
+++ pmm/pmm2-upgrade-tests.groovy
@@ -167,14 +167,14 @@ pipeline {
                     url: 'https://github.com/percona/pmm-ui-tests.git'
 
                 slackSend channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
-                sh """
+                sh '''
                     sudo mkdir -p /srv/pmm-qa || :
                     pushd /srv/pmm-qa
                         sudo git clone --single-branch --branch \${PMM_QA_GIT_BRANCH} https://github.com/percona/pmm-qa.git .
                         sudo git checkout \${PMM_QA_GIT_COMMIT_HASH}
                     popd
                     sudo ln -s /usr/bin/chromium-browser /usr/bin/chromium
-                """
+                '''
             }
         }
         stage('Start Server Instance') {
