76ab79c398e5daf55186a1ed44ccf319faba0ae3
Tomislav Plavcic <tomislav.plavcic@percona.com>
2022-09-26 20:55:41 +0200

    K8SPS-171 - Update minikube job for PS operator

--- cloud/jenkins/ps_operator_minikube.groovy
+++ cloud/jenkins/ps_operator_minikube.groovy
@@ -257,7 +257,7 @@ pipeline {
 
                         curl -s https://get.helm.sh/helm-v3.9.4-linux-amd64.tar.gz \
                             | sudo tar -C /usr/local/bin --strip-components 1 -zvxpf -
-                        sudo sh -c "curl -s -L https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64 > /usr/local/bin/yq"
+                        sudo sh -c "curl -s -L https://github.com/mikefarah/yq/releases/download/v4.16.2/yq_linux_amd64 > /usr/local/bin/yq"
                         sudo chmod +x /usr/local/bin/yq
 
 						cd "$(mktemp -d)"
