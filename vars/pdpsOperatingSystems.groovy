68dcab5abff08b763b53cfac7fb47fed22cd4298
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-09-28 14:16:31 +0300

    DISTMYSQL-224: add Ubuntu Jammy and update distros order

--- vars/pdpsOperatingSystems.groovy
+++ vars/pdpsOperatingSystems.groovy
@@ -1,3 +1,3 @@
 def call() {
-  return ['centos-7', 'debian-10', 'debian-11', 'ubuntu-bionic', 'ubuntu-focal', 'oracle-8', 'oracle-9']
+  return ['centos-7',  'oracle-8', 'oracle-9', 'debian-10', 'debian-11', 'ubuntu-bionic', 'ubuntu-focal', 'ubuntu-jammy']
 }
