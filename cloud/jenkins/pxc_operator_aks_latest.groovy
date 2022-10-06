d28b62fb150e007fe30599c57060f72553ff59b7
Natalia Marukovich <natalia.marukovich@percona.com>
2022-10-06 16:41:34 +0400

    fix credentials

--- cloud/jenkins/pxc_operator_aks_latest.groovy
+++ cloud/jenkins/pxc_operator_aks_latest.groovy
@@ -3,7 +3,7 @@ void CreateCluster(String CLUSTER_SUFFIX) {
         env.OPERATOR_NS = 'pxc-operator'
     }
 
-    withCredentials([azureServicePrincipal('TEST-AZURE')]) {
+    withCredentials([azureServicePrincipal('PERCONA-OPERATORS-SP')]) {
         sh """
             export KUBECONFIG=/tmp/$CLUSTER_NAME-${CLUSTER_SUFFIX}
             ret_num=0
@@ -12,8 +12,8 @@ void CreateCluster(String CLUSTER_SUFFIX) {
                 az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" -t "$AZURE_TENANT_ID"  --allow-no-subscriptions
                 az account show --query "{subscriptionId:id, tenantId:tenantId}"
                 az account list --all --output table
-                az aks create -g percona-operators --subscription  Pay-As-You-Go -n $CLUSTER_NAME-${CLUSTER_SUFFIX} --load-balancer-sku basic --enable-managed-identity --node-count 3 --node-vm-size Standard_B4ms --min-count 3 --max-count 3 --node-osdisk-size 30 --network-plugin kubenet  --generate-ssh-keys --enable-cluster-autoscaler --outbound-type loadbalancer --kubernetes-version ${params.PLATFORM_VER} -l eastus
-                az aks get-credentials --subscription  Pay-As-You-Go --resource-group percona-operators --name $CLUSTER_NAME-${CLUSTER_SUFFIX}
+                az aks create -g percona-operators --subscription  eng-cloud-dev -n $CLUSTER_NAME-${CLUSTER_SUFFIX} --load-balancer-sku basic --enable-managed-identity --node-count 3 --node-vm-size Standard_B4ms --min-count 3 --max-count 3 --node-osdisk-size 30 --network-plugin kubenet  --generate-ssh-keys --enable-cluster-autoscaler --outbound-type loadbalancer --kubernetes-version ${params.PLATFORM_VER} -l eastus
+                az aks get-credentials --subscription  eng-cloud-dev --resource-group percona-operators --name $CLUSTER_NAME-${CLUSTER_SUFFIX} --overwrite-existing
                 if [ \${ret_val} -eq 0 ]; then break; fi
                 ret_num=\$((ret_num + 1))
             done
@@ -22,12 +22,12 @@ void CreateCluster(String CLUSTER_SUFFIX) {
     }
 }
 void ShutdownCluster(String CLUSTER_SUFFIX) {
-    withCredentials([azureServicePrincipal('TEST-AZURE')]) {
+    withCredentials([azureServicePrincipal('PERCONA-OPERATORS-SP')]) {
         sh """
             export KUBECONFIG=/tmp/$CLUSTER_NAME-${CLUSTER_SUFFIX}
             az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" -t "$AZURE_TENANT_ID" --allow-no-subscriptions
             az account set -s "$AZURE_SUBSCRIPTION_ID"
-            az aks delete --name $CLUSTER_NAME-${CLUSTER_SUFFIX} --resource-group percona-operators --subscription Pay-As-You-Go  --yes
+            az aks delete --name $CLUSTER_NAME-${CLUSTER_SUFFIX} --resource-group percona-operators --subscription eng-cloud-dev  --yes
         """
     }
 }
@@ -403,12 +403,12 @@ pipeline {
             step([$class: 'JUnitResultArchiver', testResults: '*.xml', healthScaleFactor: 1.0])
             archiveArtifacts '*.xml'
 
-            withCredentials([azureServicePrincipal('TEST-AZURE')]) {
+            withCredentials([azureServicePrincipal('PERCONA-OPERATORS-SP')]) {
                 sh """
                     export CLUSTER_NAME=\$(echo jenkins-lat-pxc-\$(git -C source rev-parse --short HEAD) | tr '[:upper:]' '[:lower:]')
                     az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" -t "$AZURE_TENANT_ID" --allow-no-subscriptions
                     az account set -s "$AZURE_SUBSCRIPTION_ID"
-                    az group list --query "[?starts_with(name, $CLUSTER_NAME)].name | [0]" | xargs az aks delete --resource-group percona-operators --subscription  Pay-As-You-Go --yes || true
+                    az group list --query "[?starts_with(name, $CLUSTER_NAME)].name | [0]" | xargs az aks delete --resource-group percona-operators --subscription eng-cloud-dev --yes || true
                 """
             }
             sh '''
--- cloud/jenkins/pxc_operator_aks_version.groovy
+++ cloud/jenkins/pxc_operator_aks_version.groovy
@@ -13,7 +13,7 @@ void CreateCluster(String CLUSTER_SUFFIX) {
                 az account show --query "{subscriptionId:id, tenantId:tenantId}"
                 az account list --all --output table
                 az aks create -g percona-operators --subscription eng-cloud-dev -n $CLUSTER_NAME-${CLUSTER_SUFFIX} --load-balancer-sku basic --enable-managed-identity --node-count 3 --node-vm-size Standard_B4ms --min-count 3 --max-count 3 --node-osdisk-size 30 --network-plugin kubenet  --generate-ssh-keys --enable-cluster-autoscaler --outbound-type loadbalancer --kubernetes-version ${params.PLATFORM_VER} -l eastus
-                az aks get-credentials --subscription eng-cloud-dev --resource-group percona-operators --name $CLUSTER_NAME-${CLUSTER_SUFFIX}
+                az aks get-credentials --subscription eng-cloud-dev --resource-group percona-operators --name $CLUSTER_NAME-${CLUSTER_SUFFIX} --overwrite-existing
                 if [ \${ret_val} -eq 0 ]; then break; fi
                 ret_num=\$((ret_num + 1))
             done
