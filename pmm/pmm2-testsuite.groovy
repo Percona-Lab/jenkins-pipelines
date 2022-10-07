4109c3abb0fe0ee54e657d2dc2d1de1f2d94e922
Vadym Yarosh <vadym.yarosh@percona.com>
2022-10-07 13:16:13 +0200

    PMM-10849 nvm install fix

--- pmm/pmm2-testsuite.groovy
+++ pmm/pmm2-testsuite.groovy
@@ -95,6 +95,9 @@ void runCli() {
                 export tap="1"
                 export PMM_VERSION=${PMM_VERSION}
 
+//                sudo yum install nodejs npm
+                curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+                . ~/.nvm/nvm.sh
                 nvm install-latest-npm
                 npm install
                 npx playwright install
