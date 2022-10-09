5423351453499ac82422d3cb779dd8fa9e02c644
Alex Tymchuk <alexander.tymchuk@gmail.com>
2022-10-10 00:21:23 +0300

    PMM-10849 fix pmm-agent log access issue

--- pmm/pmm2-testsuite.groovy
+++ pmm/pmm2-testsuite.groovy
@@ -102,11 +102,7 @@ void fetchAgentLog(String CLIENT_VERSION) {
                 set -o xtrace
                 export CLIENT_VERSION=${CLIENT_VERSION}
                 if [[ $CLIENT_VERSION != http* ]]; then
-                    journalctl -u pmm-agent.service > /var/log/pmm-agent.log
-                    sudo chmod 777 /var/log/pmm-agent.log
-                fi
-                if [[ -e /var/log/pmm-agent.log ]]; then
-                    cp /var/log/pmm-agent.log .
+                    journalctl -u pmm-agent.service > pmm-agent.log
                 fi
             '
             if [[ $CLIENT_VERSION != http* ]]; then
