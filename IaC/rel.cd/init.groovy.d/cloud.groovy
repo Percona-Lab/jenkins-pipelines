08bfad9f36d3725a18412b317715b3560c89d2f1
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2022-10-18 00:05:53 +0300

    ENG-1155 update cloud jenkins

--- IaC/rel.cd/init.groovy.d/cloud.groovy
+++ IaC/rel.cd/init.groovy.d/cloud.groovy
@@ -224,7 +224,8 @@ initMap['micro-amazon'] = '''
         sleep 1
         echo try again
     done
-    sudo yum -y install java-11-openjdk || :
+    sudo amazon-linux-extras install epel -y
+    sudo amazon-linux-extras install java-openjdk11 -y || :
     sudo yum -y install git || :
     sudo yum -y install aws-cli || :
     sudo install -o $(id -u -n) -g $(id -g -n) -d /mnt/jenkins
