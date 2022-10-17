e8122bc0baa1a71a598d9b1dc9ed305ab2a527cc
Alex Tymchuk <alexander.tymchuk@gmail.com>
2022-10-17 22:39:45 +0300

    PMM-10708 update AMI IDs

--- IaC/pmm.cd/init.groovy.d/cloud.groovy
+++ IaC/pmm.cd/init.groovy.d/cloud.groovy
@@ -21,49 +21,53 @@ netMap['us-east-2b'] = 'subnet-04356480646777b55'
 netMap['us-east-2c'] = 'subnet-00b3df129e7d8c658'
 
 
-// TODO We use rhel label here, but it's RHEL-compatible derivative.
+// TODO We use rhel label here, in reality it's a RHEL-compatible derivative.
 imageMap = [:]
-imageMap['us-east-2a.min-rhel-7-x64'] = 'ami-00f8e2c955f7ffa9b' //centos 7
-imageMap['us-east-2a.min-rhel-8-x64'] = 'ami-0c2c9b5652599cf35' // oracle linux 8
-imageMap['us-east-2a.min-rhel-9-x64'] = 'ami-028b1d77476075858' // almalinux 9
-imageMap['us-east-2a.min-focal-x64']    = 'ami-0eea504f45ef7a8f7'
-imageMap['us-east-2a.min-bionic-x64']   = 'ami-0b9ecb12083282d75'
-imageMap['us-east-2a.min-jammy-x64']    = 'ami-07a683b72d6bd7da3'
-imageMap['us-east-2a.min-buster-x64']   = 'ami-0d90bed76900e679a'
-imageMap['us-east-2a.min-bullseye-x64'] = 'ami-0c9e778f8faae5214'
+imageMap['us-east-2a.min-rhel-7-x64']   = 'ami-00f8e2c955f7ffa9b' //centos 7
+imageMap['us-east-2a.min-rhel-8-x64']   = 'ami-0ce24f7d9f52a2d88' // oracle linux 8
+imageMap['us-east-2a.min-rhel-9-x64']   = 'ami-028b1d77476075858' // almalinux 9
+imageMap['us-east-2a.min-focal-x64']    = 'ami-025aee7d3922c2191' // ubuntu 18
+imageMap['us-east-2a.min-bionic-x64']   = 'ami-07e3ebaa49bb2df97' // ubuntu 20
+imageMap['us-east-2a.min-jammy-x64']    = 'ami-01ef8c100091ba054' // ubuntu 22
+imageMap['us-east-2a.min-stretch-x64']  = 'ami-0a694f67ea86df8a7' // debian 9
+imageMap['us-east-2a.min-buster-x64']   = 'ami-0f89775886144ea25' // debian 10
+imageMap['us-east-2a.min-bullseye-x64'] = 'ami-0d89c8b5893c02db4' // debian 11
 
-imageMap['us-east-2b.min-rhel-7-x64'] = imageMap['us-east-2a.min-rhel-7-x64']
-imageMap['us-east-2b.min-rhel-8-x64'] = imageMap['us-east-2a.min-rhel-8-x64']
-imageMap['us-east-2b.min-rhel-9-x64'] = imageMap['us-east-2a.min-rhel-9-x64']
+imageMap['us-east-2b.min-rhel-7-x64']   = imageMap['us-east-2a.min-rhel-7-x64']
+imageMap['us-east-2b.min-rhel-8-x64']   = imageMap['us-east-2a.min-rhel-8-x64']
+imageMap['us-east-2b.min-rhel-9-x64']   = imageMap['us-east-2a.min-rhel-9-x64']
 imageMap['us-east-2b.min-focal-x64']    = imageMap['us-east-2a.min-focal-x64']
 imageMap['us-east-2b.min-bionic-x64']   = imageMap['us-east-2a.min-bionic-x64']
 imageMap['us-east-2b.min-jammy-x64']    = imageMap['us-east-2a.min-jammy-x64']
+imageMap['us-east-2b.min-stretch-x64']  = imageMap['us-east-2a.min-stretch-x64']
 imageMap['us-east-2b.min-buster-x64']   = imageMap['us-east-2a.min-buster-x64']
 imageMap['us-east-2b.min-bullseye-x64'] = imageMap['us-east-2a.min-bullseye-x64']
 
-imageMap['us-east-2c.min-rhel-7-x64'] = imageMap['us-east-2a.min-rhel-7-x64']
-imageMap['us-east-2c.min-rhel-8-x64'] = imageMap['us-east-2a.min-rhel-8-x64']
-imageMap['us-east-2c.min-rhel-9-x64'] = imageMap['us-east-2a.min-rhel-9-x64']
+imageMap['us-east-2c.min-rhel-7-x64']   = imageMap['us-east-2a.min-rhel-7-x64']
+imageMap['us-east-2c.min-rhel-8-x64']   = imageMap['us-east-2a.min-rhel-8-x64']
+imageMap['us-east-2c.min-rhel-9-x64']   = imageMap['us-east-2a.min-rhel-9-x64']
 imageMap['us-east-2c.min-focal-x64']    = imageMap['us-east-2a.min-focal-x64']
 imageMap['us-east-2c.min-bionic-x64']   = imageMap['us-east-2a.min-bionic-x64']
 imageMap['us-east-2c.min-jammy-x64']    = imageMap['us-east-2a.min-jammy-x64']
+imageMap['us-east-2c.min-stretch-x64']  = imageMap['us-east-2a.min-stretch-x64']
 imageMap['us-east-2c.min-buster-x64']   = imageMap['us-east-2a.min-buster-x64']
 imageMap['us-east-2b.min-bullseye-x64'] = imageMap['us-east-2a.min-bullseye-x64']
 
 
 priceMap = [:]
-priceMap['t2.large']  = '0.04'
-priceMap['t3.xlarge'] = '0.06'
-priceMap['t3.large']  = '0.03'
-priceMap['m4.large']  = '0.03'
+priceMap['t2.large']  = '0.045'
+priceMap['t3.xlarge'] = '0.065'
+priceMap['t3.large']  = '0.035'
+priceMap['m4.large']  = '0.035'
 
 userMap = [:]
-userMap['min-rhel-7-x64']  = 'centos'
-userMap['min-rhel-8-x64']  = 'rocky'
-userMap['min-rhel-9-x64']  = 'ec2-user'
+userMap['min-rhel-7-x64']    = 'centos'
+userMap['min-rhel-8-x64']    = 'rocky'
+userMap['min-rhel-9-x64']    = 'ec2-user'
 userMap['min-focal-x64']     = 'ubuntu'
 userMap['min-bionic-x64']    = 'ubuntu'
 userMap['min-jammy-x64']     = 'ubuntu'
+userMap['min-stretch-x64']   = 'admin'
 userMap['min-buster-x64']    = 'admin'
 userMap['min-bullseye-x64']  = 'admin'
 
@@ -73,7 +77,6 @@ initMap = [:]
 initMap['rpmMap'] = '''
     set -o xtrace
     RHVER=$(rpm --eval %rhel)
-    ARCH=$(uname -m)
     SYSREL=$(cat /etc/system-release | tr -dc '0-9.' | awk -F'.' {'print $1'})
 
     if ! mountpoint -q /mnt; then
@@ -148,29 +151,44 @@ initMap['debMap'] = '''
             sudo mount ${DEVICE} /mnt
         fi
     fi
+
+    echo '10.30.6.9 repo.ci.percona.com' | sudo tee -a /etc/hosts
+
     until sudo apt-get update; do
         sleep 1
         echo try again
     done
+
     until sudo apt-get install -y lsb-release; do
         sleep 1
         echo try again
     done
 
-    echo '10.30.6.9 repo.ci.percona.com' | sudo tee -a /etc/hosts
 
-    echo $(lsb_release -sc)
+    DEB_VERSION=$(lsb_release -sc)
+    JDK_PACKAGE=openjdk-11-jre-headless
 
-    sudo apt-get -y install openjdk-11-jre-headless git
+    if [ "$DEB_VERSION" == "stretch" ]; then
+        echo 'deb http://ftp.debian.org/debian stretch-backports main' | sudo tee /etc/apt/sources.list.d/stretch-backports.list
+        until sudo apt-get update; do
+            sleep 1
+            echo try again
+        done
+        JDK_PACKAGE=openjdk-11-jdk
+    fi
+
+    sudo apt-get -y install ${JDK_PACKAGE} git
     sudo install -o $(id -u -n) -g $(id -g -n) -d /mnt/jenkins
+
 '''
 
-initMap['min-rhel-7-x64'] = initMap['rpmMap']
-initMap['min-rhel-8-x64'] = initMap['rpmMap']
-initMap['min-rhel-9-x64'] = initMap['rpmMap']
+initMap['min-rhel-7-x64']   = initMap['rpmMap']
+initMap['min-rhel-8-x64']   = initMap['rpmMap']
+initMap['min-rhel-9-x64']   = initMap['rpmMap']
 initMap['min-focal-x64']    = initMap['debMap']
 initMap['min-bionic-x64']   = initMap['debMap']
 initMap['min-jammy-x64']    = initMap['debMap']
+initMap['min-stretch-x64']  = initMap['debMap']
 initMap['min-bullseye-x64'] = initMap['debMap']
 initMap['min-buster-x64']   = initMap['debMap']
 
@@ -187,6 +205,7 @@ typeMap['min-rhel-9-x64']  = typeMap['min-rhel-7-x64']
 typeMap['min-focal-x64']     = typeMap['min-rhel-7-x64']
 typeMap['min-bionic-x64']    = typeMap['min-rhel-7-x64']
 typeMap['min-jammy-x64']     = typeMap['min-rhel-7-x64']
+typeMap['min-stretch-x64']   = typeMap['min-rhel-7-x64']
 typeMap['min-buster-x64']    = typeMap['min-rhel-7-x64']
 typeMap['min-bullseye-x64']  = typeMap['min-rhel-7-x64']
 
@@ -197,6 +216,7 @@ execMap['min-rhel-9-x64']  = '1'
 execMap['min-focal-x64']     = '1'
 execMap['min-bionic-x64']    = '1'
 execMap['min-jammy-x64']     = '1'
+execMap['min-stretch-x64']   = '1'
 execMap['min-buster-x64']    = '1'
 execMap['min-bullseye-x64']  = '1'
 
@@ -207,6 +227,7 @@ devMap['min-rhel-9-x64']  = devMap['min-rhel-7-x64']
 devMap['min-focal-x64']     = devMap['min-rhel-7-x64']
 devMap['min-bionic-x64']    = devMap['min-rhel-7-x64']
 devMap['min-jammy-x64']     = devMap['min-rhel-7-x64']
+devMap['min-stretch-x64']   = '/dev/xvda=:80:true:gp2,/dev/xvdd=:20:true:gp2'
 devMap['min-buster-x64']    = '/dev/xvda=:80:true:gp2,/dev/xvdd=:20:true:gp2'
 devMap['min-bullseye-x64']  = '/dev/xvda=:80:true:gp2,/dev/xvdd=:20:true:gp2'
 
@@ -217,6 +238,7 @@ labelMap['min-rhel-9-x64']  = 'min-rhel-9-x64'
 labelMap['min-focal-x64']     = 'min-focal-x64'
 labelMap['min-bionic-x64']    = 'min-bionic-x64'
 labelMap['min-jammy-x64']     = 'min-jammy-x64'
+labelMap['min-stretch-x64']   = 'min-stretch-x64'
 labelMap['min-buster-x64']    = 'min-buster-x64'
 labelMap['min-bullseye-x64']  = 'min-bullseye-x64'
 
@@ -294,6 +316,7 @@ String region = 'us-east-2'
             getTemplate('min-focal-x64',        "${region}${it}"),
             getTemplate('min-bionic-x64',       "${region}${it}"),
             getTemplate('min-jammy-x64',        "${region}${it}"),
+            getTemplate('min-stretch-x64',      "${region}${it}"),
             getTemplate('min-buster-x64',       "${region}${it}"),
             getTemplate('min-bullseye-x64',     "${region}${it}"),
         ],                                       // List<? extends SlaveTemplate> templates
@@ -314,4 +337,3 @@ String region = 'us-east-2'
 jenkins.save()
 
 logger.info("Cloud init finished")
-
