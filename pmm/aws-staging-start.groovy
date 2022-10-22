cf2d8c34356dc7e24638feb34fd596b57001224f
Alex Tymchuk <alexander.tymchuk@gmail.com>
2022-10-23 01:14:30 +0300

    PMM-10279 fix owner name

--- pmm/aws-staging-start.groovy
+++ pmm/aws-staging-start.groovy
@@ -171,11 +171,12 @@ pipeline {
                         OWNER = (env.BUILD_USER_EMAIL ?: '').split('@')[0] ?: env.BUILD_USER_ID
                         OWNER_SLACK = slackUserIdFromEmail(botUser: true, email: env.BUILD_USER_EMAIL, tokenCredentialId: 'JenkinsCI-SlackBot-v2')
                         env.VM_NAME = 'pmm-' + OWNER.replaceAll("[^a-zA-Z0-9_.-]", "") + '-' + (new Date()).format("yyyyMMdd.HHmmss") + '-' + env.BUILD_NUMBER
-                        sh '''
+
+                        sh """
                             set -x
                             echo ${VM_NAME} > VN_NAME
                             echo \$OWNER > OWNER_FULL
-                        '''
+                        """
                     }
 
                     echo """
