3b5f62939d614609b829a7c3180b0112f4a2a1fe
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-10-22 12:00:22 +0300

    PMM-10729 refactor AMI staging start (#1812)
    * PMM-10729 refactor AMI staging start
    
    * PMM-10729 move wrap to the script
    
    * PMM-10729 make NOTIFY a choice param
    
    * PMM-10729 fix choice param
    
    * PMM-10729 move modify-attribute to later stage
    
    * PMM-10729 modify command
    
    * PMM-10729 apply w/o DeleteOnTerminate
    
    * PMM-10729 move attr mod to a lter stage
    
    * PMM-10729 provide aws creds
    
    * PMM-10729 try a different command to terminate ebs
    
    * PMM-10729 wait for the instance to initialize
    
    * PMM-10729 handle DeleteOnTermination
    
    * PMM-10729 wait for the instance
    
    * PMM-10729 remove long sleep
    
    * PMM-10729 fix typo
    
    * PMM-10729 exclude the failing mirror
    
    * PMM-10729 silence curl
    
    * PMM-10729 remove echo
    
    * PMM-10729 add sudo
    
    * PMM-10729 fix sudo with tee
    
    * PMM-10729 fix echo with pipe
    
    * PMM-10729 fix pipe to file
    
    * PMM-10729 refactor pmm2-ami-test
    
    * PMM-10729 move wrap to script
    
    * PM-10729 refactor pmm2-ami
    
    * PMM-10729 move tests out of post

--- pmm/pmm2-ami-test.groovy
+++ pmm/pmm2-ami-test.groovy
@@ -1,24 +1,10 @@
-void runUITests(CLIENT_VERSION, CLIENT_INSTANCE, SERVER_IP, AMI_TEST, AMI_INSTANCE_ID) {
-    stagingJob = build job: 'pmm2-ui-tests', parameters: [
-        string(name: 'CLIENT_VERSION', value: CLIENT_VERSION),
-        string(name: 'CLIENT_INSTANCE', value: CLIENT_INSTANCE),
-        string(name: 'SERVER_IP', value: SERVER_IP),
-        string(name: 'AMI_TEST', value: AMI_TEST),
-        string(name: 'AMI_INSTANCE_ID', value: AMI_INSTANCE_ID)
-    ]
-    env.VM_IP = stagingJob.buildVariables.IP
-    env.VM_NAME = stagingJob.buildVariables.VM_NAME
-    env.PMM_URL = "http://admin:admin@${SERVER_IP}"
-    env.PMM_UI_URL = "https://${SERVER_IP}"
-}
-
 void runAMIStagingStart(AMI_ID, SSH_KEY) {
     amiStagingJob = build job: 'pmm2-ami-staging-start', parameters: [
         string(name: 'AMI_ID', value: AMI_ID),
         string(name: 'SSH_KEY', value: SSH_KEY)
     ]
     env.AMI_INSTANCE_ID = amiStagingJob.buildVariables.INSTANCE_ID
-    env.AMI_INSTANCE_IP = amiStagingJob.buildVariables.IP
+    env.AMI_INSTANCE_IP = amiStagingJob.buildVariables.PUBLIC_IP
     env.PMM_URL = "http://admin:admin@${AMI_INSTANCE_IP}"
     env.PMM_UI_URL = "https://${AMI_INSTANCE_IP}"
 }
@@ -28,6 +14,9 @@ void runAMIStaginStop(INSTANCE_ID) {
         string(name: 'AMI_ID', value: INSTANCE_ID),
     ]
 }
+
+String OWNER = ''
+String OWNER_SLACK = ''
 
 pipeline {
     agent {
@@ -50,16 +39,9 @@ pipeline {
             defaultValue: '',
             description: 'AMI Image version',
             name: 'AMI_ID')
-        string(
-            defaultValue: 'true',
-            description: 'Use this AMI Setup as PMM-client',
-            name: 'SETUP_CLIENT')
-        string(
-            defaultValue: 'main',
-            description: 'Tag/Branch for percona-qa repository',
-            name: 'GIT_BRANCH')
     }
     options {
+        buildDiscarder(logRotator(numToKeepStr: '30'))
         skipDefaultCheckout()
         disableConcurrentBuilds()
     }
@@ -67,53 +49,48 @@ pipeline {
         stage('Prepare') {
             steps {
                 deleteDir()
-                withCredentials([usernamePassword(credentialsId: 'Jenkins API', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                    sh """
-                        curl -s -u ${USER}:${PASS} ${BUILD_URL}api/json \
-                            | python -c "import sys, json; print json.load(sys.stdin)['actions'][1]['causes'][0]['userId']" \
-                            | sed -e 's/@percona.com//' \
-                            > OWNER
-                        echo "pmm-\$(cat OWNER | cut -d . -f 1)-\$(date -u '+%Y%m%d%H%M')" \
-                            > VM_NAME
-                    """
-                }
-                stash includes: 'OWNER,VM_NAME', name: 'VM_NAME'
                 script {
-                    def OWNER = sh(returnStdout: true, script: "cat OWNER").trim()
-                    if ("${NOTIFY}" == "true") {
-                        slackSend botUser: true, channel: '#pmm-ci', color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
-                        slackSend botUser: true, channel: "@${OWNER}", color: '#FFFF00', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
+                    wrap([$class: 'BuildUser']) {
+                        OWNER = (env.BUILD_USER_EMAIL ?: '').split('@')[0] ?: env.BUILD_USER_ID
+                        OWNER_SLACK = slackUserIdFromEmail(botUser: true, email: env.BUILD_USER_EMAIL, tokenCredentialId: 'JenkinsCI-SlackBot-v2')
+                        env.VM_NAME = 'pmm-' + OWNER.replaceAll("[^a-zA-Z0-9_.-]", "") + '-' + (new Date()).format("yyyyMMdd.HHmmss") + '-' + env.BUILD_NUMBER
+                    }
+
+                    if (params.NOTIFY == "true") {
+                        slackSend botUser: true, channel: '#pmm-ci', color: '#0000FF', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
+                        if (OWNER_SLACK) {
+                            slackSend botUser: true, channel: "@${OWNER_SLACK}", color: '#0000FF', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
+                        }
                     }
                 }
             }
         }
         stage('Run PMM2 AMI Instance') {
             steps {
-                runAMIStagingStart(AMI_ID, SSH_KEY)
+                runAMIStagingStart(params.AMI_ID, params.SSH_KEY)
             }
         }
     }
     post {
-
         success {
-            runAMIStaginStop("${env.AMI_INSTANCE_ID}")
+            runAMIStaginStop(env.AMI_INSTANCE_ID)
             script {
-                if ("${NOTIFY}" == "true") {
-                    def OWNER = sh(returnStdout: true, script: "cat OWNER").trim()
-
+                if (params.NOTIFY == "true") {
                     slackSend botUser: true, channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build finished - https://${env.AMI_INSTANCE_IP}"
-                    slackSend botUser: true, channel: "@${OWNER}", color: '#00FF00', message: "[${JOB_NAME}]: build finished - https://${env.AMI_INSTANCE_IP}"
+                    if (OWNER_SLACK) {
+                        slackSend botUser: true, channel: "@${OWNER_SLACK}", color: '#00FF00', message: "[${JOB_NAME}]: build finished - https://${env.AMI_INSTANCE_IP}"
+                    }
                 }
             }
         }
         failure {
-            runAMIStaginStop("${env.AMI_INSTANCE_ID}")
+            runAMIStaginStop(env.AMI_INSTANCE_ID)
             script {
-                if ("${NOTIFY}" == "true") {
-                    def OWNER = sh(returnStdout: true, script: "cat OWNER").trim()
-
-                    slackSend botUser: true, channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build failed"
-                    slackSend botUser: true, channel: "@${OWNER}", color: '#FF0000', message: "[${JOB_NAME}]: build failed"
+                if (params.NOTIFY == "true") {
+                    slackSend botUser: true, channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${BUILD_URL} failed"
+                    if (OWNER_SLACK) {
+                        slackSend botUser: true, channel: "@${OWNER_SLACK}", color: '#FF0000', message: "[${JOB_NAME}]: build ${BUILD_URL} failed"
+                    }
                 }
             }
         }
