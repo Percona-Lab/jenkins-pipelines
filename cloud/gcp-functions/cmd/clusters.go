490649de59de5c854456d73b479b275d48e1d48e
Natalia Marukovich <natalia.marukovich@percona.com>
2022-10-24 13:00:12 +0400

    fix script

--- cloud/gcp-functions/cmd/clusters.go
+++ cloud/gcp-functions/cmd/clusters.go
@@ -9,51 +9,57 @@ import (
         "time"
         "strconv"
 
-        "google.golang.org/api/container/v1"
         "google.golang.org/api/compute/v1"
+        "google.golang.org/api/container/v1"
 )
 
-func CleanClusters(http.ResponseWriter, *http.Request) {
+func CleanClusters(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, World test!")
     ctx := context.Background()
+    computeService, err := compute.NewService(ctx)
+    if err != nil {
+        log.Fatal("Error: create service", err)
+    }
     containerService, errContainer := container.NewService(ctx)
     if errContainer != nil {
         log.Fatal("Error: create containerService", errContainer)
+        fmt.Fprintln(w, "Error: create containerService", errContainer)
     }
 
-    computeService, errCompute := compute.NewService(ctx)
-    if errCompute != nil {
-        log.Fatal("Error: create computeService", errCompute)
-    }
     project := os.Getenv("GCP_DEV_PROJECT")
     zonesReq := computeService.Zones.List(project)
     currentTime := time.Now().Unix()
+    fmt.Fprintln(w, "Сurrent time %s", currentTime)
+
     if err := zonesReq.Pages(ctx, func(page *compute.ZoneList) error {
         for _, zone := range page.Items {
-            clustersReq, err := containerService.Projects.Zones.Clusters.List(project, zone.Name).Context(ctx).Do()
+            clustersReq, err := containerService.Projects.Zones.Clusters.List(project, zone.Name).Do()
             if err != nil {
                 log.Fatal(err)
             }
+            fmt.Fprintln(w, "clusters: %v" ,clustersReq )
             for _, cluster := range clustersReq.Clusters {
+                fmt.Fprintln(w, "Iterate clusters %s", cluster.Name)
                 creationTime := cluster.ResourceLabels["creation-time"]
-                log.Println("This is stderr: %s", creationTime )
-                log.Println("This is stderr: %s", cluster.Name )
+
                 if len(creationTime) != 0 {
                     if creationTime, err := strconv.ParseInt(creationTime, 10, 64); err == nil {
-                        if (currentTime - creationTime) > 1 {
-                            log.Println("Try to delete")
+                        fmt.Fprintln(w, "Creation time %s", creationTime)
+                        if (currentTime - creationTime)/3600 > 6 {
+                            fmt.Fprint(w, "Try to delete")
                             resp, err := containerService.Projects.Zones.Clusters.Delete(project, zone.Name, cluster.Name).Context(ctx).Do()
                             if err != nil {
-                                log.Println("delete cluster: %v", err)
+                                fmt.Fprint(w, "%s\n", err)
                                 return fmt.Errorf("delete cluster: %v", err)
                             }
-                            fmt.Printf("%s\n", resp)
+
+                            fmt.Fprint(w, "%s\n", resp)
                             fmt.Printf("cluster: %s in zone %s was deleted\n", cluster.Name, zone.Name)
                         }
 
                     }
                 }
             }
-            return nil
         }
         return nil
     }); err != nil {
