d5a1a3b326b6cf5a4196d6a149e2c8e6acb8e063
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2022-11-15 11:27:02 +0200

    PSMDB-1093 full pipeline for tarballs, packages and docker images in the testing area

--- psmdb/jenkins/get-psmdb-branches-4.2.groovy
+++ psmdb/jenkins/get-psmdb-branches-4.2.groovy
@@ -80,7 +80,7 @@ pipeline {
                 }
                 slackNotify("#releases", "#00FF00", "[${JOB_NAME}]: new changes for branch ${BRANCH_NAME}[commit id: ${COMMIT_ID}] were detected, build will be started soon")
                 build job: 'psmdb42-autobuild-RELEASE', parameters: [string(name: 'GIT_BRANCH', value: BRANCH_NAME), string(name: 'PSMDB_VERSION', value: VERSION), string(name: 'PSMDB_RELEASE', value: RELEASE), string(name: 'COMPONENT', value: 'testing')]
-
+                build job: 'psmdb-multijob-testing', propagate: false, wait: false, parameters: [string(name: 'PSMDB_VERSION', value: VERSION), string(name: 'PSMDB_RELEASE', value: RELEASE)]
             }
         }
         stage('Build skipped') {
