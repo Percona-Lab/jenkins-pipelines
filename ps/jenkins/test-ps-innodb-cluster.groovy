f95c998d9718b0c56cfeba031686399cb29d68ce
Puneet Kaushik <puneet.kaushik@percona.com>
2022-11-16 00:27:11 +0530

    Restore Original repo

--- ps/jenkins/test-ps-innodb-cluster.groovy
+++ ps/jenkins/test-ps-innodb-cluster.groovy
@@ -1,6 +1,6 @@
 library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
 void installDependencies() {
@@ -16,7 +16,7 @@ void installDependencies() {
 
     sh '''
         rm -rf package-testing
-        git clone -b PS-8481 https://github.com/kaushikpuneet07/package-testing
+        git clone https://github.com/Percona-QA/package-testing
     '''
 }
 
