31a53b1894eb1760a26aebc6da5f549c07cacb68
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2022-11-18 15:51:27 +0200

    Update pmm2-submodules.groovy

--- pmm/pmm2-submodules.groovy
+++ pmm/pmm2-submodules.groovy
@@ -264,6 +264,14 @@ pipeline {
                                 "https://api.github.com/repos/\$(echo $CHANGE_URL | cut -d '/' -f 4-5)/actions/workflows/jenkins-dispatch.yml/dispatches" \
                                 -d '{"ref":"${CHANGE_BRANCH}","inputs":{"server_image":"${IMAGE}","client_image":"${CLIENT_IMAGE}","sha":"${FB_COMMIT_HASH}"}}'
                         """
+                        // trigger workflow in GH to run PMM binary cli tests
+                        sh """
+                            curl -v -X POST \
+                                -H "Accept: application/vnd.github.v3+json" \
+                                -H "Authorization: token ${GITHUB_API_TOKEN}" \
+                                "https://api.github.com/repos/\$(echo $CHANGE_URL | cut -d '/' -f 4-5)/actions/workflows/pmm-cli.yml/dispatches" \
+                                -d '{"ref":"${CHANGE_BRANCH}","inputs":{"client_tar_url":"${CLIENT_URL}","sha":"${FB_COMMIT_HASH}"}}'
+                        """
                     }
                 }
             }
