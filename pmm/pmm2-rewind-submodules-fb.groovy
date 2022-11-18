ce723210feb22238cce66413672d920afb08b757
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-11-18 06:44:37 +0300

    PMM-7 increase log retention

--- pmm/pmm2-rewind-submodules-fb.groovy
+++ pmm/pmm2-rewind-submodules-fb.groovy
@@ -10,7 +10,7 @@ pipeline {
         )
     }
     options {
-        buildDiscarder(logRotator(numToKeepStr: '10'))
+        buildDiscarder(logRotator(numToKeepStr: '30'))
         skipDefaultCheckout()
         disableConcurrentBuilds()
         skipStagesAfterUnstable()
