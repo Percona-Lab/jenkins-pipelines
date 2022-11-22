248298d93e834a1327b257a3fa335418fd4763b6
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-11-22 18:23:44 +0300

    PMM-6352 fix the TARBALL_TYPE variable

--- vars/uploadTarball.groovy
+++ vars/uploadTarball.groovy
@@ -7,7 +7,7 @@ def call(String TarballType) {
             withEnv(['TARBALL_TYPE=' + TarballType]){
                 sh '''
                     PATH_TO_BUILD=`cat uploadPath`
-                    TARBALL_TYPE="${TarballType}"
+                    echo "TarballType: ${TARBALL_TYPE}"
 
                     ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${KEY_PATH} ${USER}@repo.ci.percona.com "
                         mkdir -p ${PATH_TO_BUILD}/${TARBALL_TYPE}/tarball
@@ -15,7 +15,7 @@ def call(String TarballType) {
 
                     scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${KEY_PATH} \
                         `find . -name '*.tar.*'` \
-                        "${USER}@repo.ci.percona.com:${PATH_TO_BUILD}/${TARBALL_TYPE}/tarball/"
+                        ${USER}@repo.ci.percona.com:${PATH_TO_BUILD}/${TARBALL_TYPE}/tarball/
                 '''
             }
         }
