840129f113462e9b46f548c69119ff0aee1783fd
Peter <sirotnak.p@gmail.com>
2022-11-30 21:30:11 +0100

    Install python 3 yum way

--- pmm/pmm2-ovf-upgrade-tests.groovy
+++ pmm/pmm2-ovf-upgrade-tests.groovy
@@ -74,8 +74,7 @@ void checkUpgrade(String PMM_VERSION, String PRE_POST, String OVF_INSTANCE_NAME)
     node(env.OVF_INSTANCE_NAME) {
         withCredentials([sshUserPrivateKey(credentialsId: 'OVF_VM_TESTQA', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
             sh """
-                sudo apt-get update
-                sudo apt-get install python3.6
+                sudo yum install -y python3
                 ssh -i "${KEY_PATH}" -p 3022 -o ConnectTimeout=1 -o StrictHostKeyChecking=no admin@${OVF_INSTANCE_IP} '
                     export PMM_VERSION=${PMM_VERSION}
                     export OVF_INSTANCE_NAME=${OVF_INSTANCE_NAME}
