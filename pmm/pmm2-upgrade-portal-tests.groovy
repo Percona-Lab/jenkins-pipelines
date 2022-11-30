751485db172167daab185971b8e293f226b9ce89
Peter <sirotnak.p@gmail.com>
2022-11-30 17:31:59 +0100

    Fix portal upgrade

--- pmm/pmm2-upgrade-portal-tests.groovy
+++ pmm/pmm2-upgrade-portal-tests.groovy
@@ -164,11 +164,11 @@ pipeline {
             steps{
                 withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
                     sh """
-                        ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no ${USER}@${VM_IP} ' 
+                        ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no ${USER}@${VM_IP} "
                             set -o errexit
                             set -o xtrace
                             docker exec ${VM_NAME}-server sed -i 's/- nginx/- "nginx*"/' /usr/share/pmm-update/ansible/playbook/tasks/update.yml
-                        '
+                        "
                     """
                 }
             }
