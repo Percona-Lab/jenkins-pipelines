72c07e3d015ec5bc8efaa1893dcf5ab4fa21c861
Peter <sirotnak.p@gmail.com>
2022-11-30 16:04:21 +0100

    correct flags

--- pmm/pmm2-ami-upgrade-tests.groovy
+++ pmm/pmm2-ami-upgrade-tests.groovy
@@ -75,7 +75,7 @@ void checkUpgrade(String PMM_VERSION, String PRE_POST) {
             ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no admin@${VM_IP} '
                 export PMM_VERSION=${PMM_VERSION}
                 sudo chmod 755 /srv/pmm-qa/pmm-tests/check_upgrade.py
-                python3 /srv/pmm-qa/pmm-tests/check_upgrade.py --distribution=ami --prepost-upgrade=${PRE_POST} --pmm-version=${PMM_VERSION}
+                python3 /srv/pmm-qa/pmm-tests/check_upgrade.py --env=ami --pre_post=${PRE_POST} --version=${PMM_VERSION}
             '
         """
     }
