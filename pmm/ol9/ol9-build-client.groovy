8741497791c35b4aee3a8585c952b2e0cf7797c8
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-12-10 02:47:47 +0300

    PMM-6352 fix param extrapolation in dbl quotes

--- pmm/ol9/ol9-build-client.groovy
+++ pmm/ol9/ol9-build-client.groovy
@@ -83,6 +83,8 @@ pipeline {
                         withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
                             sh '''
                                 echo "${PASS}" | docker login -u "${USER}" --password-stdin
+                            '''
+                            sh """
                                 set -o xtrace
 
                                 export PUSH_DOCKER=1
@@ -97,7 +99,7 @@ pipeline {
                                 docker tag  ${DOCKER_CLIENT_TAG} perconalab/pmm-client:${DOCKER_LATEST_TAG}
                                 docker push ${DOCKER_CLIENT_TAG}
                                 docker push perconalab/pmm-client:${DOCKER_LATEST_TAG}
-                            '''
+                            """
                         }
                         stash includes: 'results/docker/CLIENT_TAG', name: 'CLIENT_IMAGE'
                         archiveArtifacts 'results/docker/CLIENT_TAG'
