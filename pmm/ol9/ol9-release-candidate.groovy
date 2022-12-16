4a659eeb678393f45b13699b915ec8bcf9f3f472
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-12-16 06:52:31 +0300

    PMM-6352 triple escape the backslash

--- pmm/ol9/ol9-release-candidate.groovy
+++ pmm/ol9/ol9-release-candidate.groovy
@@ -83,8 +83,11 @@ pipeline {
                     // ]
                     withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'GITHUB_API_TOKEN')]) {
                         sh """
+                            git config -f .gitmodules submodule.grafana.shallow true
+                            git config -f .gitmodules submodule.grafana-dashboards.shallow true
+
                             git submodule update --init --remote --jobs 10
-                            git submodule status | grep "^\\+" | sed -e "s/\\+//" | cut -d " " -f2 > remotes.txt
+                            git submodule status | grep "^\\\+" | sed -e "s/\\\+//" | cut -d " " -f2 > remotes.txt
                             cat remotes.txt
                             for sub in `cat remotes.txt`; do
                                 cd $sub
