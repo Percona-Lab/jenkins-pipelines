a53ea8110776b79d9261d9f912624f270f98fee7
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-12-16 13:56:02 +0300

    PMM-6352 configure git user

--- pmm/ol9/ol9-release-candidate.groovy
+++ pmm/ol9/ol9-release-candidate.groovy
@@ -83,9 +83,12 @@ pipeline {
                     // ]
                     withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'GITHUB_API_TOKEN')]) {
                         sh '''
+                            git config --global user.email "noreply@percona.com"
+                            git config --global user.name "PMM Jenkins"                            
                             git config -f .gitmodules submodule.grafana.shallow true
                             git config -f .gitmodules submodule.grafana-dashboards.shallow true
                             git config -f .gitmodules submodule.pmm-qa.shallow true
+                            git config -f .gitmodules submodule.percona-toolkit.shallow true
                             git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
                             git config push.default "current"
 
