4cb7b005b9ebac49cf06cd8f2eb59a5216119542
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-12-21 10:31:56 +0200

    Update defaults

--- pdpxc/pdpxc.groovy
+++ pdpxc/pdpxc.groovy
@@ -28,27 +28,27 @@ pipeline {
             ]
         )
         string(
-            defaultValue: '8.0.23',
+            defaultValue: '8.0.30',
             description: 'PXC version for test',
             name: 'VERSION'
          )
         string(
-            defaultValue: '2.0.18',
+            defaultValue: '2.4.4',
             description: 'Proxysql version for test',
             name: 'PROXYSQL_VERSION'
          )
         string(
-            defaultValue: '2.3.10',
+            defaultValue: '2.5.10',
             description: 'HAProxy version for test',
             name: 'HAPROXY_VERSION'
          )
         string(
-            defaultValue: '8.0.23',
+            defaultValue: '8.0.30',
             description: 'PXB version for test',
             name: 'PXB_VERSION'
          )
         string(
-            defaultValue: '3.3.1',
+            defaultValue: '3.5.0',
             description: 'Percona toolkit version for test',
             name: 'PT_VERSION'
          )
