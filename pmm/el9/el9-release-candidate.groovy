eb5be365b12ecaf4ef2f5136751dca2aa8289ef9
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-12-21 03:46:51 +0300

    PMM-6352 try tee vs redirect

--- pmm/el9/el9-release-candidate.groovy
+++ pmm/el9/el9-release-candidate.groovy
@@ -83,8 +83,7 @@ pipeline {
                             git submodule status | grep "^\\+" | sed -e "s/\\+//" | cut -d " " -f2 > remotes.txt
 
                             cat remotes.txt
-                            ls -la
-                            yq ea "select(fileIndex == 0) *d select(fileIndex == 1) | .deps" ci-default.yml ci.yml > branches.yml
+                            yq ea 'select(fileIndex == 0) *d select(fileIndex == 1) | .deps' ci-default.yml ci.yml | tee branches.yml
 
                             COUNT=0
                             for SUBMODULE in $(cat remotes.txt); do
