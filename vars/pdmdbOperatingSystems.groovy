0731a88de806965d737c29305551a192d31180b4
sandraromanchenko <sandra.romanchenko@percona.com>
2023-01-27 00:13:04 +0200

    Add different OS list for PSMDB 6.0

--- vars/pdmdbOperatingSystems.groovy
+++ vars/pdmdbOperatingSystems.groovy
@@ -1,3 +1,6 @@
-def call() {
-  return ['centos-7', 'debian-10', 'debian-11', 'ubuntu-focal', 'ubuntu-bionic', 'rhel8', 'ubuntu-jammy']
+def call(version = 'default') {
+    switch(version) {
+        case ~/6\.0\.*/: return ['centos-7', 'debian-10', 'debian-11', 'ubuntu-focal', 'ubuntu-bionic', 'rhel8', 'rhel9', 'ubuntu-jammy']
+        default:         return ['centos-7', 'debian-10', 'debian-11', 'ubuntu-focal', 'ubuntu-bionic', 'rhel8', 'ubuntu-jammy']
+    }
 }
