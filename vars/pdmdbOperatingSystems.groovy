64d4c812452689442257a2f72aadbdbd2eb925be
sandraromanchenko <sandra.romanchenko@percona.com>
2023-01-27 10:49:04 +0200

    PSMDB. Fix to parameter type

--- vars/pdmdbOperatingSystems.groovy
+++ vars/pdmdbOperatingSystems.groovy
@@ -1,4 +1,4 @@
-def call(version = 'default') {
+def call(String version='default') {
     switch(version) {
         case ~/6\.0\.*/: return ['centos-7', 'debian-10', 'debian-11', 'ubuntu-focal', 'ubuntu-bionic', 'rhel8', 'rhel9', 'ubuntu-jammy']
         default:         return ['centos-7', 'debian-10', 'debian-11', 'ubuntu-focal', 'ubuntu-bionic', 'rhel8', 'ubuntu-jammy']
