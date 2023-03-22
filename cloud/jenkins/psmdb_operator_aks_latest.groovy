6fc3714f68f5e629b43fa3c025f9fb0ff9ee6e3b
Tomislav Plavcic <tomislav.plavcic@percona.com>
2023-03-22 14:40:43 +0100

    K8SPSMDB-877 - Minor fixes in syntax

--- cloud/jenkins/psmdb_operator_aks_latest.groovy
+++ cloud/jenkins/psmdb_operator_aks_latest.groovy
@@ -359,7 +359,7 @@ EOF
             }
             withCredentials([azureServicePrincipal('PERCONA-OPERATORS-SP')]) {
                 sh """
-                    export CLUSTER_NAME=$(echo jenkins-lat-psmdb-$(git -C source rev-parse --short HEAD) | tr '[:upper:]' '[:lower:]')
+                    export CLUSTER_NAME=\$(echo jenkins-lat-psmdb-\$(git -C source rev-parse --short HEAD) | tr '[:upper:]' '[:lower:]')
                     az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" -t "$AZURE_TENANT_ID" --allow-no-subscriptions
                     az account set -s "$AZURE_SUBSCRIPTION_ID"
                     az aks list --query "[?starts_with(name, '$CLUSTER_NAME')].name" --output tsv | xargs az aks delete --resource-group percona-operators --yes --no-wait --name
