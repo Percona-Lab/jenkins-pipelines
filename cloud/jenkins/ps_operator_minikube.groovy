f96f74a2f61c184052a0cbb4ddd301e607569a8c
Tomislav Plavcic <tomislav.plavcic@percona.com>
2023-03-23 13:54:19 +0100

    Bump yq version for PS operator pipelines (#2034)

--- cloud/jenkins/ps_operator_minikube.groovy
+++ cloud/jenkins/ps_operator_minikube.groovy
@@ -261,7 +261,7 @@ pipeline {
 
                         curl -s https://get.helm.sh/helm-v3.9.4-linux-amd64.tar.gz \
                             | sudo tar -C /usr/local/bin --strip-components 1 -zvxpf -
-                        sudo sh -c "curl -s -L https://github.com/mikefarah/yq/releases/download/v4.16.2/yq_linux_amd64 > /usr/local/bin/yq"
+                        sudo sh -c "curl -s -L https://github.com/mikefarah/yq/releases/download/v4.29.1/yq_linux_amd64 > /usr/local/bin/yq"
                         sudo chmod +x /usr/local/bin/yq
 
 						cd "$(mktemp -d)"
