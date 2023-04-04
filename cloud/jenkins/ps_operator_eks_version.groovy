3430a820614f7612118a36fe51f165bf3a65f3ab
Natalia Marukovich <natalia.marukovich@percona.com>
2023-04-04 10:44:17 +0400

    fix

--- cloud/jenkins/ps_operator_eks_version.groovy
+++ cloud/jenkins/ps_operator_eks_version.groovy
@@ -110,8 +110,6 @@ void prepareNode() {
 
         curl -s https://get.helm.sh/helm-v3.9.4-linux-amd64.tar.gz \
             | sudo tar -C /usr/local/bin --strip-components 1 -zvxpf -
-        curl -s -L https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz \
-            | sudo tar -C /usr/local/bin --strip-components 1 --wildcards -zxvpf - '*/oc'
         
         sudo sh -c "curl -s -L https://github.com/mikefarah/yq/releases/download/v4.29.1/yq_linux_amd64 > /usr/local/bin/yq"
         sudo chmod +x /usr/local/bin/yq
