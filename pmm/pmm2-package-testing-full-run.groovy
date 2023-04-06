de7c57ffbc3e1cb912b86c1626912bf2b26b3b2d
Vadym Yarosh <vadym.yarosh@percona.com>
2023-04-06 20:04:27 +0200

    PMM-7 added hint for rc-testing

--- pmm/pmm2-package-testing-full-run.groovy
+++ pmm/pmm2-package-testing-full-run.groovy
@@ -3,13 +3,11 @@ library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
-void runStaging(String DOCKER_VERSION, CLIENTS) {
-    env.ADMIN_PASSWORD = 'admin'
+void runStaging(String DOCKER_VERSION, CLIENT_VERSION, CLIENTS) {
     stagingJob = build job: 'aws-staging-start', parameters: [
         string(name: 'DOCKER_VERSION', value: DOCKER_VERSION),
-        string(name: 'CLIENT_VERSION', value: 'pmm2-latest'),
-        string(name: 'DOCKER_ENV_VARIABLE', value: '-e DISABLE_TELEMETRY=true -e DATA_RETENTION=48h -e PERCONA_TEST_PLATFORM_ADDRESS=https://check-dev.percona.com:443 -e PERCONA_TEST_PLATFORM_PUBLIC_KEY=RWTg+ZmCCjt7O8eWeAmTLAqW+1ozUbpRSKSwNTmO+exlS5KEIPYWuYdX'),
-        string(name: 'ADMIN_PASSWORD', value: ${ADMIN_PASSWORD}),
+        string(name: 'CLIENT_VERSION', value: CLIENT_VERSION),
+        string(name: 'DOCKER_ENV_VARIABLE', value: '-e DISABLE_TELEMETRY=true -e DATA_RETENTION=48h'),
         string(name: 'CLIENTS', value: CLIENTS),
         string(name: 'NOTIFY', value: 'false'),
         string(name: 'DAYS', value: '1')
@@ -17,7 +15,7 @@ void runStaging(String DOCKER_VERSION, CLIENTS) {
     env.VM_IP = stagingJob.buildVariables.IP
     env.PMM_SERVER_IP = stagingJob.buildVariables.IP
     env.VM_NAME = stagingJob.buildVariables.VM_NAME
-    env.PMM_URL = "http://admin:${ADMIN_PASSWORD}@${VM_IP}"
+    env.PMM_URL = "http://admin:admin@${VM_IP}"
 }
 
 void destroyStaging(IP) {
@@ -80,30 +78,25 @@ pipeline {
         string(
             defaultValue: 'master',
             description: 'Tag/Branch for package-testing repository',
-            name: 'GIT_BRANCH',
-            trim: true)
+            name: 'GIT_BRANCH')
         string(
             defaultValue: '',
             description: 'Commit hash for the branch',
-            name: 'GIT_COMMIT_HASH',
-            trim: true)
+            name: 'GIT_COMMIT_HASH')
         string(
             defaultValue: 'perconalab/pmm-server:dev-latest',
             description: 'PMM Server docker container version (image-name:version-tag)',
-            name: 'DOCKER_VERSION',
-            trim: true)
+            name: 'DOCKER_VERSION')
+        string(
+            defaultValue: 'dev-latest',
+            description: 'PMM Client version(dev-latest|pmm2-rc|image-name:version-tag)',
+            name: 'CLIENT_VERSION')
         string(
             defaultValue: latestVersion,
             description: 'PMM Version for testing',
-            name: 'PMM_VERSION',
-            trim: true)
-        string(
-            defaultValue: 'pmm2-client',
-            description: 'Name of Playbook? ex: pmm2-client_integration, pmm2-client_integration_custom_path',
-            name: 'TESTS',
-            trim: true)
+            name: 'PMM_VERSION')
         choice(
-            choices: ['testing', 'experimental', 'main', 'pmm2-client-main'],
+            choices: ['testing', 'experimental', 'main', 'tools-main', 'pmm2-client-main'],
             description: 'Enable Repo for Client Nodes',
             name: 'INSTALL_REPO')
         choice(
@@ -117,9 +110,36 @@ pipeline {
     stages {
         stage('Setup Server Instance') {
             steps {
-                runStaging(DOCKER_VERSION, '--addclient=ps,1')
+                runStaging(DOCKER_VERSION, CLIENT_VERSION, '--addclient=ps,1')
             }
         }
+
+
+
+        stage('Integration Playbook'){
+            steps {
+                script {
+                    runPackageTestingJob(GIT_BRANCH, DOCKER_VERSION, CLIENT_VERSION, PMM_VERSION, 'pmm2-client_integration', METRICS_MODE, INSTALL_REPO);
+                }
+            }
+        }
+        stage('Integration Upgrade Playbook'){
+            steps {
+                script {
+                    runPackageTestingJob(GIT_BRANCH, DOCKER_VERSION, CLIENT_VERSION, PMM_VERSION, 'pmm2-client_integration_upgrade', METRICS_MODE, INSTALL_REPO);
+                }
+            }
+        }
+        stage('Integration Upgrade Playbook with Custom Path'){
+            steps {
+                script {
+                    runPackageTestingJob(GIT_BRANCH, DOCKER_VERSION, CLIENT_VERSION, PMM_VERSION, 'pmm2-client_integration_upgrade_custom_path', METRICS_MODE, INSTALL_REPO);
+                }
+            }
+        }
+
+
+
         stage('Execute Package Tests') {
             parallel {
                 stage('rhel-7-x64') {
@@ -150,23 +170,20 @@ pipeline {
                         }
                     }
                 }
-                stage('rhel-9-x64') {
-                    when {
-                        expression { env.TESTS == "pmm2-client" || env.TESTS == "pmm2-client_upgrade" }
-                    }
-                    agent {
-                        label 'min-rhel-9-x64'
-                    }
-                    steps{
-                        setup_rhel_package_tests()
-                        run_package_tests(GIT_BRANCH, TESTS, INSTALL_REPO)
-                    }
-                    post {
-                        always {
-                            deleteDir()
-                        }
-                    }
-                }
+//                 stage('rhel-9-x64') {
+//                     agent {
+//                         label 'min-rhel-9-x64'
+//                     }
+//                     steps{
+//                         setup_rhel_package_tests()
+//                         run_package_tests(GIT_BRANCH, TESTS, INSTALL_REPO)
+//                     }
+//                     post {
+//                         always {
+//                             deleteDir()
+//                         }
+//                     }
+//                 }
                 stage('focal-x64') {
                     agent {
                         label 'min-focal-x64'
