22d9746b0a87d09f2647e01f465479bf685ff282
Talha Bin Rizwan <talha.rizwan@percona.com>
2023-04-13 14:39:12 +0500

    PMM-11216-server-upgrade-side-container build as FB.

--- pmm/pmm2-submodules.groovy
+++ pmm/pmm2-submodules.groovy
@@ -215,7 +215,7 @@ pipeline {
 
                         DOCKER_SERVER_UPGRADE_TAG=perconalab/pmm-server-upgrade-fb:\${BRANCH_NAME}-\${GIT_COMMIT:0:7}
                         RPMBUILD_DOCKER_IMAGE=public.ecr.aws/e7j3v3n0/rpmbuild:2
-                        PMM_RELEASE_VERSION=$(cd ${PATH_TO_PMM} && git describe --always --dirty | cut -b2-)
+                        PMM_RELEASE_VERSION=$(git describe --always --dirty | cut -b2-)
 
                         docker run --rm -v ${PWD}:/pmm ${RPMBUILD_DOCKER_IMAGE} sh -c "cd /pmm && make -C admin release"
 
