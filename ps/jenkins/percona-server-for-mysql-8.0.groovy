2f0548328ae54e5489232dff72b4a5b82b459c45
Vadim Yalovets <vadim.yalovets@percona.com>
2023-04-20 16:24:09 +0300

    ENG-1124 Add stage to publish tarballs and docker images for tests

--- ps/jenkins/percona-server-for-mysql-8.0.groovy
+++ ps/jenkins/percona-server-for-mysql-8.0.groovy
@@ -47,7 +47,7 @@ void buildStage(String DOCKER_OS, String STAGE_PARAM) {
                 if [ -f ./test/percona-server-8.0.properties ]; then
                     . ./test/percona-server-8.0.properties
                 fi
-                sudo bash -x ./ps_builder.sh --builddir=\${build_dir}/test --install_deps=1
+                bash -x ./ps_builder.sh --builddir=\${build_dir}/test --install_deps=1
                 if [ ${BUILD_TOKUDB_TOKUBACKUP} = \"ON\" ]; then
                     bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --build_tokudb_tokubackup=1 --perconaft_branch=${PERCONAFT_BRANCH} --tokubackup_branch=${TOKUBACKUP_BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}
                 else
