f4a6b1406e3ea1b4c1d1259c9d5f8c863f8dccc4
Tomislav Plavcic <tomislav.plavcic@percona.com>
2023-05-25 15:54:45 +0200

    Fix PXC/PSMDB pipelines with cloud secret file

--- cloud/jenkins/psmdb_operator_aws_openshift-latest.groovy
+++ cloud/jenkins/psmdb_operator_aws_openshift-latest.groovy
@@ -363,6 +363,11 @@ pipeline {
                     curl -s -L https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$PLATFORM_VER/openshift-install-linux.tar.gz \
                         | sudo tar -C /usr/local/bin  --wildcards -zxvpf -
                 '''
+                withCredentials([file(credentialsId: 'cloud-secret-file', variable: 'CLOUD_SECRET_FILE')]) {
+                    sh """
+                        cp $CLOUD_SECRET_FILE ./source/e2e-tests/conf/cloud-secret.yml
+                    """
+                }
             }
         }
         stage('Build docker image') {
