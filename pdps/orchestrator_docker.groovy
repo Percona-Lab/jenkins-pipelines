d86cc6f32a496fb9ca9eb3e7f0b944dcd8424644
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-05-29 14:59:18 +0300

    Update defaults

--- pdps/orchestrator_docker.groovy
+++ pdps/orchestrator_docker.groovy
@@ -19,12 +19,12 @@ pipeline {
       ]
     )
     string(
-      defaultValue: '3.2.6-8',
+      defaultValue: '3.2.6-9',
       description: 'Full orchestrator version. Used as version and docker tag',
       name: 'ORCHESTRATOR_VERSION'
     )
     string(
-      defaultValue: '8.0.32-24',
+      defaultValue: '8.0.33-25',
       description: 'Full PS version to test with orchestrator',
       name: 'PS_VERSION'
     )
