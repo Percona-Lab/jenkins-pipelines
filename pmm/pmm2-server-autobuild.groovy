5586cb164964720e2e5897c01dbf3e2b71347819
Talha Bin Rizwan <talha.rizwan@percona.com>
2023-05-29 19:49:01 +0500

    PMM-12156 Update pmm/pmm2-server-autobuild.groovy

--- pmm/pmm2-server-autobuild.groovy
+++ pmm/pmm2-server-autobuild.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+library changelog: false, identifier: 'lib@PMM-el9-el7-build', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
@@ -19,12 +19,13 @@ pipeline {
             name: 'DESTINATION')
     }
     options {
+        buildDiscarder(logRotator(numToKeepStr: '30'))
         skipDefaultCheckout()
         disableConcurrentBuilds()
     }
-    triggers {
-        upstream upstreamProjects: 'pmm2-submodules-rewind', threshold: hudson.model.Result.SUCCESS
-    }
+    // triggers {
+    //     upstream upstreamProjects: 'pmm2-submodules-rewind', threshold: hudson.model.Result.SUCCESS
+    // }
     environment {
         PATH_TO_SCRIPTS = 'sources/pmm/src/github.com/percona/pmm/build/scripts'
     }
@@ -34,29 +35,34 @@ pipeline {
                 git poll: true,
                     branch: GIT_BRANCH,
                     url: 'http://github.com/Percona-Lab/pmm-submodules'
+                script {
+                    env.VERSION = sh(returnStdout: true, script: "cat VERSION").trim()
+                }
                 sh '''
                     set -o errexit
                     git submodule update --init --jobs 10
                     git submodule status
 
                     git rev-parse --short HEAD > shortCommit
-                    echo "UPLOAD/pmm2-components/yum/${DESTINATION}/${JOB_NAME}/pmm/\$(cat VERSION)/${GIT_BRANCH}/\$(cat shortCommit)/${BUILD_NUMBER}" > uploadPath
+                    echo "UPLOAD/pmm2-components/yum/${DESTINATION}/${JOB_NAME}/pmm/${VERSION}/${GIT_BRANCH}/$(cat shortCommit)/${BUILD_NUMBER}" > uploadPath
                 '''
 
                 script {
-                    def versionTag = sh(returnStdout: true, script: "cat VERSION").trim()
-                    if ("${DESTINATION}" == "testing") {
-                        env.DOCKER_LATEST_TAG = "${versionTag}-rc${BUILD_NUMBER}"
-                        env.DOCKER_RC_TAG = "${versionTag}-rc"
+                    if (params.DESTINATION == "testing") {
+                        env.DOCKER_LATEST_TAG     = "${VERSION}-rc${BUILD_NUMBER}"
+                        env.DOCKER_LATEST_TAG_EL7 = "${VERSION}-rc-el7${BUILD_NUMBER}"
+                        env.DOCKER_RC_TAG         = "${VERSION}-rc"
+                        env.DOCKER_RC_TAG_EL7     = "${VERSION}-rc-el7"
                     } else {
-                        env.DOCKER_LATEST_TAG = "dev-latest"
+                        env.DOCKER_LATEST_TAG     = "dev-latest"
+                        env.DOCKER_LATEST_TAG_EL7 = "dev-latest-el7"
                     }
                 }
 
                 archiveArtifacts 'uploadPath'
                 stash includes: 'uploadPath', name: 'uploadPath'
                 archiveArtifacts 'shortCommit'
-                slackSend botUser: true, channel: '#pmm-ci', color: '#0000FF', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
+                // slackSend botUser: true, channel: '#pmm-ci', color: '#0000FF', message: "[${JOB_NAME}]: build started - ${BUILD_URL}"
             }
         }
         stage('Build client source') {
@@ -74,77 +80,172 @@ pipeline {
             }
         }
         stage('Build client source rpm') {
-            steps {
-                sh "${PATH_TO_SCRIPTS}/build-client-srpm centos:7"
-                stash includes: 'results/srpm/pmm*-client-*.src.rpm', name: 'rpms'
-                uploadRPM()
+            parallel {
+                stage('Build client source rpm EL7') {
+                    steps {
+                        sh "${PATH_TO_SCRIPTS}/build-client-srpm centos:7"
+                    }
+                }
+                stage('Build client source rpm EL9') {
+                    steps {
+                        sh "${PATH_TO_SCRIPTS}/build-client-srpm public.ecr.aws/e7j3v3n0/rpmbuild:ol9"
+                    }
+                }
+            }
+            post {
+                success {
+                    stash includes: 'results/srpm/pmm*-client-*.src.rpm', name: 'rpms'
+                    uploadRPM()
+                }
             }
         }
-        stage('Build client binary rpm') {
-            steps {
-                sh """
-                    set -o errexit
+        stage('Build client binary rpms') {
+            parallel {
+                stage('Build client binary rpm EL7') {
+                    steps {
+                        sh """
+                            set -o errexit
 
-                    ${PATH_TO_SCRIPTS}/build-client-rpm centos:7
+                            ${PATH_TO_SCRIPTS}/build-client-rpm centos:7
 
-                    mkdir -p tmp/pmm-server/RPMS/
-                    cp results/rpm/pmm*-client-*.rpm tmp/pmm-server/RPMS/
-                """
-                stash includes: 'tmp/pmm-server/RPMS/*.rpm', name: 'rpms'
-                uploadRPM()
+                            mkdir -p tmp/pmm-server/RPMS/
+                            cp results/rpm/pmm*-client-*.rpm tmp/pmm-server/RPMS/
+                        """
+                    }
+                }
+                stage('Build client binary rpm EL9') {
+                    steps {
+                        sh """
+                            set -o errexit
+
+                            ${PATH_TO_SCRIPTS}/build-client-rpm public.ecr.aws/e7j3v3n0/rpmbuild:ol9
+
+                            mkdir -p tmp/pmm-server/RPMS/
+                            cp results/rpm/pmm*-client-*.rpm tmp/pmm-server/RPMS/
+                        """
+                    }
+                }
+            }
+            post {
+                success {
+                    stash includes: 'tmp/pmm-server/RPMS/*.rpm', name: 'rpms'
+                    uploadRPM()
+                }
             }
         }
         stage('Build server packages') {
-            steps {
-                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'pmm-staging-slave', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                    sh """
-                        set -o errexit
+            parallel {
+                stage('Build server packages EL7') {
+                    steps {
+                        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'pmm-staging-slave', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                            sh '''
+                                set -o errexit
 
-                        ${PATH_TO_SCRIPTS}/build-server-rpm-all
-                    """
+                                ${PATH_TO_SCRIPTS}/build-server-rpm-all
+                            '''
+                        }
+                    }
                 }
-                stash includes: 'tmp/pmm-server/RPMS/*/*/*.rpm', name: 'rpms'
-                uploadRPM()
+                stage('Build server packages EL9') {
+                    steps {
+                        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'pmm-staging-slave', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                            sh '''
+                                set -o errexit
+
+                                # These are used by `src/github.com/percona/pmm/build/scripts/vars`
+                                ## export ROOT_DIR=${WORKSPACE}
+                                export RPMBUILD_DOCKER_IMAGE=public.ecr.aws/e7j3v3n0/rpmbuild:ol9
+                                export RPMBUILD_DIST="el9"
+                                # Set this variable if we need to rebuils all rpms, for example to refresh stale assets stored in S3 build cache
+                                # export FORCE_REBUILD=1
+
+                                ${PATH_TO_SCRIPTS}/build-server-rpm-all
+                            '''
+                        }
+                    }
+                }
+            }
+            post {
+                success {
+                    stash includes: 'tmp/pmm-server/RPMS/*/*/*.rpm', name: 'rpms'
+                    uploadRPM()
+                }
             }
         }
-        stage('Build server docker') {
-            steps {
-                withCredentials([
-                    usernamePassword(credentialsId: 'hub.docker.com',
-                    passwordVariable: 'PASS',
-                    usernameVariable: 'USER'
-                    )]) {
-                    sh """
-                        echo "${PASS}" | docker login -u "${USER}" --password-stdin
-                    """
+        stage('Build server docker images') {
+            parallel {
+                stage('Build server docker EL7') {
+                    steps {
+                        withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                            sh '''
+                                echo "${PASS}" | docker login -u "${USER}" --password-stdin
+                            '''
+                        }
+                        withCredentials([aws(
+                            accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                            credentialsId: 'ECRRWUser',
+                            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                            )]) {
+                            sh '''
+                                set -o errexit
+
+                                #export PUSH_DOCKER=1
+                                export DOCKER_TAG=perconalab/pmm-server:$(date -u '+%Y%m%d%H%M')
+
+                                ${PATH_TO_SCRIPTS}/build-server-docker
+
+                                if [ -n ${DOCKER_RC_TAG_EL7} ]; then
+                                    docker tag ${DOCKER_TAG_EL7} perconalab/pmm-server:${DOCKER_RC_TAG_EL7}
+                                    ## docker push perconalab/pmm-server:${DOCKER_RC_TAG_EL7}
+                                fi
+                                docker tag ${DOCKER_TAG_EL7} perconalab/pmm-server:${DOCKER_LATEST_TAG_EL7}
+                                ## docker push ${DOCKER_TAG}
+                                ## docker push perconalab/pmm-server:${DOCKER_LATEST_TAG_EL7}
+                                echo "${DOCKER_LATEST_TAG_EL7}" > DOCKER_TAG_EL7
+                            '''
+                        }
+                        script {
+                            env.IMAGE_EL7 = sh(returnStdout: true, script: "cat DOCKER_TAG_EL7").trim()
+                        }
+                    }
                 }
-                withCredentials([aws(
-                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                    credentialsId: 'ECRRWUser',
-                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-                    )]) {
-                    sh """
-                        set -o errexit
+                stage('Build server docker EL9') {
+                    steps {
+                        withCredentials([
+                            usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                                sh '''
+                                    echo "${PASS}" | docker login -u "${USER}" --password-stdin
+                                '''
+                        }
+                        sh '''
+                            set -o errexit
 
-                        export PUSH_DOCKER=1
-                        export DOCKER_TAG=perconalab/pmm-server:\$(date -u '+%Y%m%d%H%M')
+                            # TODO: DOCKER_TAG for RC should be a real version, not a date
+                            if [ -n ${DOCKER_RC_TAG} ]; then
+                                export DOCKER_TAG=perconalab/pmm-server:${DOCKER_RC_TAG}
+                            else
+                                export DOCKER_TAG=perconalab/pmm-server:$(date -u '+%Y%m%d%H%M')
+                            fi
 
-                        ${PATH_TO_SCRIPTS}/build-server-docker
+                            export RPMBUILD_DOCKER_IMAGE=public.ecr.aws/e7j3v3n0/rpmbuild:ol9
+                            export DOCKERFILE=Dockerfile.el9
+                            # Build a docker image
+                            ${PATH_TO_SCRIPTS}/build-server-docker
 
-                        if [ ! -z \${DOCKER_RC_TAG+x} ]; then
-                            docker tag  \${DOCKER_TAG} perconalab/pmm-server:\${DOCKER_RC_TAG}
-                            docker push perconalab/pmm-server:\${DOCKER_RC_TAG}
-                            docker rmi perconalab/pmm-server:\${DOCKER_RC_TAG}
-                        fi
-                        docker tag \${DOCKER_TAG} perconalab/pmm-server:\${DOCKER_LATEST_TAG}
-                        docker push \${DOCKER_TAG}
-                        docker push perconalab/pmm-server:\${DOCKER_LATEST_TAG}
-                        docker rmi  \${DOCKER_TAG}
-                        docker rmi perconalab/pmm-server:\${DOCKER_LATEST_TAG}
-                    """
+                            if [ -n ${DOCKER_RC_TAG} ]; then
+                                docker tag ${DOCKER_TAG} perconalab/pmm-server:${DOCKER_RC_TAG}
+                                ## docker push perconalab/pmm-server:${DOCKER_RC_TAG}
+                            fi
+                            docker tag ${DOCKER_TAG} perconalab/pmm-server:${DOCKER_LATEST_TAG}
+                            ## docker push ${DOCKER_TAG}
+                            ## docker push perconalab/pmm-server:${DOCKER_LATEST_TAG}
+                            echo "${DOCKER_LATEST_TAG}" > DOCKER_TAG
+                        '''
+                        script {
+                            env.IMAGE = sh(returnStdout: true, script: "cat DOCKER_TAG").trim()
+                        }
+                    }
                 }
-                stash includes: 'results/docker/TAG', name: 'IMAGE'
-                archiveArtifacts 'results/docker/TAG'
             }
         }
         stage('Sign packages') {
@@ -158,23 +259,21 @@ pipeline {
             }
         }
     }
-    post {
-        always {
+    post {        
+        success {
             script {
-                if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
-                    unstash 'IMAGE'
-                    def IMAGE = sh(returnStdout: true, script: "cat results/docker/TAG").trim()
-                    slackSend botUser: true, channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build finished - ${IMAGE} - ${BUILD_URL}"
-                    slackSend botUser: true, channel: '@nailya.kutlubaeva', color: '#00FF00', message: "[${JOB_NAME}]: build finished - ${IMAGE}"
-                    if ("${DESTINATION}" == "testing")
-                    {
-                      currentBuild.description = "Release Candidate Build"
-                      slackSend botUser: true, channel: '#pmm-qa', color: '#00FF00', message: "[${JOB_NAME}]: ${BUILD_URL} Release Candidate build finished"
-                    }
-                } else {
-                    slackSend botUser: true, channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result} - ${BUILD_URL}"
-                    slackSend botUser: true, channel: '#pmm-qa', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result} - ${BUILD_URL}"
+                // slackSend botUser: true, channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build finished - ${IMAGE} - ${IMAGE_EL7} - ${BUILD_URL}"
+                if (params.DESTINATION == "testing") {
+                    currentBuild.description = "RC Build, Image(EL9):" + env.IMAGE + ", Image(EL7):" + env.IMAGE_EL7
+                    // slackSend botUser: true, channel: '#pmm-qa', color: '#00FF00', message: "[${JOB_NAME}]: RC build finished - ${IMAGE} - ${IMAGE_EL7} - ${BUILD_URL}"
                 }
+            }
+        }
+        failure {
+            script {
+                echo "Pipeline failed"
+                // slackSend botUser: true, channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result} - ${BUILD_URL}"
+                // slackSend botUser: true, channel: '#pmm-qa', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result} - ${BUILD_URL}"
             }
         }
     }
