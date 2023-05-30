b2cc788cab5d9c683ee7537916bcb717df728d8d
EvgeniyPatlan <evgeniy.patlan@percona.com>
2023-05-30 17:36:08 +0200

    Eng 1196 new repo (#2124)
    * ENG-1196 fix repo sync
    
    * ENG-7 fix sync task
    
    * ENG-7 fix sync

--- vars/sync2ProdPMMClientRepo.groovy
+++ vars/sync2ProdPMMClientRepo.groovy
@@ -43,9 +43,9 @@ def call(String DESTINATION, String SYNC_PMM_CLIENT) {
                             done
                             for dist in `ls -1 debian`; do
                                 for deb in `find debian/\${dist} -name '*.deb'`; do
-                                 pkg_fname=\$(basename \${deb})
+                                 pkg_fname=\$(basename \${deb} | awk -F'_' '{print \$2}' )
                                  EC=0
-                                 /usr/local/reprepro5/bin/reprepro --list-format '"'"'\${package}_\${version}_\${architecture}.deb\\n'"'"' -Vb /srv/repo-copy/pmm2-client/apt -C ${DESTINATION} list \${dist} | sed -re "s|[0-9]:||" | grep \${pkg_fname} > /dev/null || EC=\$?
+                                 /usr/local/reprepro5/bin/reprepro -Vb /srv/repo-copy/pmm2-client/apt -C ${DESTINATION} list \${dist} | sed -re "s|[0-9]:||" | grep \${pkg_fname} > /dev/null || EC=\$?
                                  REPOPUSH_ARGS=""
                                  if [ \${EC} -eq 0 ]; then
                                      REPOPUSH_ARGS=" --remove-package "
