f178caf12ac8c030be533e53b737e6cfa19e4091
Talha Bin Rizwan <talha.rizwan@percona.com>
2023-05-31 10:26:36 +0500

    PMM-12156 Update pmm/pmm2-server-autobuild-el7.groovy

--- pmm/pmm2-server-autobuild-el7.groovy
+++ pmm/pmm2-server-autobuild-el7.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: 'lib@PMM-el9-el7-build', retriever: modernSCM([
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
