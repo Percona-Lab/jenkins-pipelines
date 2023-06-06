4390c87c6f0adcebc2e6e7f7d9cbfcd6e71758f1
EvgeniyPatlan <evgeniy.patlan@percona.com>
2023-06-06 10:20:51 +0200

    PMM-7 fix sync methods (#2135)

--- vars/sync2ProdPMMClient.groovy
+++ vars/sync2ProdPMMClient.groovy
@@ -11,16 +11,9 @@ def call(String DESTINATION, String SYNC_PMM_CLIENT) {
                         set -o xtrace
 
                         pushd ${path_to_build}/binary
-                            rsync_exclude=" --exclude pmm2-client-*"
-                            find_exclude="! -name pmm2-client-*"
 
                             for rhel in \$(ls -1 redhat); do
-                                # skip synchronization of el8/el6 repos in case of pmm server rpms sync
-                                if [ "${SYNC_PMM_CLIENT}" == 'no' ] && [ "\${rhel}" -eq '8' -o "\${rhel}" -eq '6' ]; then
-                                    continue
-                                fi
-
-                                export dest_path=/srv/repo-copy/${DESTINATION}/\${rhel}
+                                export dest_path=/srv/repo-copy/percona/yum/${DESTINATION}/\${rhel}
 
                                 # RPMS
                                 mkdir -p \${dest_path}/RPMS
@@ -49,14 +42,24 @@ def call(String DESTINATION, String SYNC_PMM_CLIENT) {
                                 gpg --detach-sign --armor --passphrase ${SIGN_PASSWORD} \${dest_path}/SRPMS/repodata/repomd.xml 
                             done
 
+                            for dist in \$(ls -1 debian); do
+                                for deb in \$(find debian/\${dist} -name '*.deb'); do
+                                    repopush --remove-package --gpg-pass ${SIGN_PASSWORD} --package \${deb} --verbose --component ${DESTINATION} --codename \${dist} --repo-path /srv/repo-copy/apt
+                                done
+
+                                # source deb
+                                #for dsc in \$(find ../source -name '*.dsc'); do
+                                #    repopush --remove-package --gpg-pass ${SIGN_PASSWORD} --package \${dsc} --verbose --component ${DESTINATION} --codename \${dist} --repo-path /srv/repo-copy/apt
+                                #done
+                            done
                         popd
 
                         # Update /srv/repo-copy/version
                         date +%s > /srv/repo-copy/version
 
                         rsync -avt --bwlimit=50000 --delete --progress --exclude=rsync-* --exclude=*.bak \
-                            /srv/repo-copy/${DESTINATION}/ \
-                            10.10.9.209:/www/repo.percona.com/htdocs/${DESTINATION}/
+                            /srv/repo-copy/percona/yum/${DESTINATION}/ \
+                            10.10.9.209:/www/repo.percona.com/htdocs/percona/yum/${DESTINATION}/
                         rsync -avt --bwlimit=50000 --delete --progress --exclude=rsync-* --exclude=*.bak \
                             /srv/repo-copy/apt/ \
                             10.10.9.209:/www/repo.percona.com/htdocs/apt/
