dab27e038c2220e078f0f72a8fed0cb693bde041
Tomislav Plavcic <tomislav.plavcic@percona.com>
2023-06-26 11:13:03 +0200

    Change the version of urllib used in aks cloud pipeline

--- cloud/jenkins/psmdb_operator_aks_version.groovy
+++ cloud/jenkins/psmdb_operator_aks_version.groovy
@@ -235,7 +235,8 @@ pipeline {
                     
                     if ! command -v az &>/dev/null; then
                         curl -L https://azurecliprod.blob.core.windows.net/install.py -o install.py
-                        printf "/usr/azure-cli\\n/usr/bin" | sudo  python3 install.py
+                        printf "/usr/azure-cli\\n/usr/bin" | sudo python3 install.py
+                        sudo /usr/azure-cli/bin/python -m pip install "urllib3<2.0.0"
                     fi
                 '''
             }
