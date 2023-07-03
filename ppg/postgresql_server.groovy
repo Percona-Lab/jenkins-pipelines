943b5aade34544f31e6f997ec8ddef6fb29ce83b
EvgeniyPatlan <evgeniy.patlan@percona.com>
2023-07-03 14:27:03 +0300

    ENG-7 Add deb12

--- ppg/postgresql_server.groovy
+++ ppg/postgresql_server.groovy
@@ -224,6 +224,19 @@ pipeline {
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
+                stage('Debian bookworm(12)') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
+                        buildStage("debian:bookworm", "--build_deb=1")
+
+                        pushArtifactFolder("deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
+                    }
+                }
             }
         }
 
