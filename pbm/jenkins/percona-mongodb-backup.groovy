56f2e689a3dbefac9593a2ab57b5ef9762b8c2b1
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2023-07-10 15:37:32 +0300

    ENG-7 PBM deb src: change build OS stretch -> buster

--- pbm/jenkins/percona-mongodb-backup.groovy
+++ pbm/jenkins/percona-mongodb-backup.groovy
@@ -114,7 +114,7 @@ pipeline {
                     steps {
                         cleanUpWS()
                         popArtifactFolder("source_tarball/", AWS_STASH_PATH)
-                        buildStage("debian:stretch", "--build_src_deb=1")
+                        buildStage("debian:buster", "--build_src_deb=1")
 
                         pushArtifactFolder("source_deb/", AWS_STASH_PATH)
                         uploadDEBfromAWS("source_deb/", AWS_STASH_PATH)
