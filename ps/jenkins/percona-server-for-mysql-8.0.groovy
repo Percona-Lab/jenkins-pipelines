ca42ed85f4e9b88facfa956b4aa7fcaf1bd9015c
Vadim Yalovets <vadim.yalovets@percona.com>
2023-07-11 16:15:42 +0300

    Add bookworm to jobs

--- ps/jenkins/percona-server-for-mysql-8.0.groovy
+++ ps/jenkins/percona-server-for-mysql-8.0.groovy
@@ -593,7 +593,7 @@ parameters {
             script {
                 currentBuild.description = "Built on ${BRANCH}; path to packages: ${COMPONENT}/${AWS_STASH_PATH}"
                 REVISION = sh(returnStdout: true, script: "grep REVISION test/percona-server-8.0.properties | awk -F '=' '{ print\$2 }'").trim()
-                PS_RELEASE=$(echo ${BRANCH} | sed 's/release-//g')
+                PS_RELEASE=$(echo "${BRANCH}" | sed 's/release-//g')
                 echo "${REVISION} is the commit ID"
                 echo "${PS_RELEASE} is the PS_RELEASE Version"
 
