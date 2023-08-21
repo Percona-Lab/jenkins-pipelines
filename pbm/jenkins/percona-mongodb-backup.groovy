2a04bedccc1c03ca98a3c057073c58a83e91a589
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2023-08-21 21:11:24 +0300

    PBM-1170 pbm: create debian 12 packages

--- pbm/jenkins/percona-mongodb-backup.groovy
+++ pbm/jenkins/percona-mongodb-backup.groovy
@@ -228,6 +228,19 @@ pipeline {
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
+                stage('Debian Bookworm(12)') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
+                        buildStage("debian:bookworm", "--build_deb=1")
+
+                        pushArtifactFolder("deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
+                    }
+                }
                 stage('Centos 7 tarball') {
                     agent {
                         label 'docker'
