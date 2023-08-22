055806e32b037eb27bc7acf44e23fb24d3d1cacd
Vadim Yalovets <vadim.yalovets@percona.com>
2023-08-22 21:27:34 +0300

    ENG-1207 Create build pipeline for percona everest cli

--- vars/uploadTarballToDownloadsTesting.groovy
+++ vars/uploadTarballToDownloadsTesting.groovy
@@ -27,7 +27,7 @@ def call(String PRODUCT_NAME, String PRODUCT_VERSION) {
                     ssh -p 2222 jenkins-deploy.jenkins-deploy.web.r.int.percona.com mkdir -p /data/downloads/TESTING/${PRODUCT_NAME}-\${cutProductVersion}
 
                 ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com \
-                    rsync -avt -e "ssh -p 2222" --bwlimit=50000 --progress ${path_to_build}/binary/tarball/* jenkins-deploy.jenkins-deploy.web.r.int.percona.com:/data/downloads/TESTING/${PRODUCT_NAME}-\${cutProductVersion}/
+                    rsync -avt -e '"ssh -p 2222"' --bwlimit=50000 --progress ${path_to_build}/binary/tarball/* jenkins-deploy.jenkins-deploy.web.r.int.percona.com:/data/downloads/TESTING/${PRODUCT_NAME}-\${cutProductVersion}/
 
                 curl https://www.percona.com/admin/config/percona/percona_downloads/crawl_directory
             """
