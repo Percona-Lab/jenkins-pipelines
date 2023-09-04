529f6761b33af7b4bfe4e8b48f40ef8cde8adc22
Puneet Kaushik <puneet.kaushik@percona.com>
2023-09-04 14:42:37 +0530

    Add bookworm in PS innodb cluster job

--- ps/jenkins/test-ps-innodb-cluster.groovy
+++ ps/jenkins/test-ps-innodb-cluster.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+library changelog: false, identifier: 'lib@innodb-book', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
 ]) _
 
 void installDependencies() {
@@ -16,7 +16,7 @@ void installDependencies() {
 
     sh '''
         rm -rf package-testing
-        git clone https://github.com/Percona-QA/package-testing
+        git clone -b innodb-book https://github.com/kaushikpuneet07/package-testing
     '''
 }
 
@@ -106,6 +106,7 @@ pipeline {
                 'ubuntu-focal',
                 'ubuntu-bionic',
                 'ubuntu-jammy',
+                'debian-12',
                 'debian-11',
                 'debian-10',
                 'centos-7',
