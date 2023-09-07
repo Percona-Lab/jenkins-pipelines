ba4f01010ec51b6c92c3c4633fcf8ed6d362087a
Pavel Tankov <4014969+ptankov@users.noreply.github.com>
2023-09-07 11:28:44 +0300

    cloud-secret.yml is not neede anywhere - removing it's creation

--- cloud/jenkins/psmdb_operator_gke_latest.groovy
+++ cloud/jenkins/psmdb_operator_gke_latest.groovy
@@ -120,12 +120,6 @@ void initTests() {
             }
         }
     }
-
-    // withCredentials([file(credentialsId: 'cloud-secret-file', variable: 'CLOUD_SECRET_FILE')]) {
-    //     sh """
-    //         cp $CLOUD_SECRET_FILE source/e2e-tests/conf/cloud-secret.yml
-    //     """
-    // }
 }
 
 void clusterRunner(String cluster) {
