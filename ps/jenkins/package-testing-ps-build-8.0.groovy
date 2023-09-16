d4867bc01b3b639d3defd38d3974bf3979368160
Puneet Kaushik <puneet.kaushik@percona.com>
2023-09-16 13:55:56 +0530

    Restore original repo

--- ps/jenkins/package-testing-ps-build-8.0.groovy
+++ ps/jenkins/package-testing-ps-build-8.0.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: 'lib@ol8-pac', retriever: modernSCM([
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
 setup_rhel_package_tests = { ->
