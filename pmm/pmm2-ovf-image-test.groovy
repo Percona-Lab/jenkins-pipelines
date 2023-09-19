e0cb8d3a5df0e383685b9fc33fb121fa9d68d803
Talha Bin Rizwan <talha.rizwan@percona.com>
2023-09-19 22:45:37 +0500

    PMM-7 Fix OVF builds on DigitalOcean by using new auth token. (#2297)

--- pmm/pmm2-ovf-image-test.groovy
+++ pmm/pmm2-ovf-image-test.groovy
@@ -91,7 +91,7 @@ pipeline {
         stage('Prepare') {
             steps {
                 deleteDir()
-                withCredentials([string(credentialsId: '82c0e9e0-75b5-40ca-8514-86eca3a028e0', variable: 'DIGITALOCEAN_ACCESS_TOKEN')]) {
+                withCredentials([string(credentialsId: 'f5415992-e274-45c2-9eb9-59f9e8b90f43', variable: 'DIGITALOCEAN_ACCESS_TOKEN')]) {
                     sh '''
                         set -o xtrace
 
