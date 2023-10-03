b9fd79de0498a0bfc795b04f08215ce5dc3a958b
Vadym Yarosh <vadym.yarosh@percona.com>
2023-10-03 14:57:57 +0200

    PMM-7 debug getter

--- pmm/pmm2-ami-upgrade-tests-matrix.groovy
+++ pmm/pmm2-ami-upgrade-tests-matrix.groovy
@@ -31,18 +31,6 @@ def generateStage(VERSION) {
     }
 }
 
-//def getVer() {
-//    return sh(
-//            script: """
-//          sudo yum install -y wget jq
-//          rc_latest=\$(wget -q "https://registry.hub.docker.com/v2/repositories/perconalab/pmm-client/tags?page_size=25&name=rc" -O - | jq -r .results[].name  | grep 2.*.*-rc\$ | sort -V | tail -n1)
-//          rc_minor=\$(echo $rc_latest | awk -F. '{print \$2}')
-//          echo "2.\$((++rc_minor)).0"
-//      """,
-//            returnStdout: true
-//    ).trim()
-//}
-
 pipeline {
     agent {
         label 'cli'
@@ -74,7 +62,6 @@ pipeline {
                 script {
                     if ("${params.UPGRADE_TO}" == "dev-latest") {
                         enableTestingRepo = 'no'
-//                        pmmServerLatestVersion = getVer()
                         pmmServerLatestVersion = pmmVersion()
                     } else {
                         enableTestingRepo = 'yes'
