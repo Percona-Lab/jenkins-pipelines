bae0be0764e0ec2ad6d4294aeb92f475d3b0a1ac
Vadym Yarosh <vadym.yarosh@percona.com>
2023-10-11 18:39:13 +0200

    PMM-7 updated label

--- pmm/pmm2-ami-upgrade-tests-matrix.groovy
+++ pmm/pmm2-ami-upgrade-tests-matrix.groovy
@@ -18,7 +18,7 @@ def devLatestVersion = pmmVersion()
 def amiVersions = pmmVersion('ami').keySet() as List
 def versions = amiVersions[-5..-1]
 def parallelStagesMatrix = versions.collectEntries {
-    ["${it}" : generateStage(it)]
+    ["${it} -> ${PMM_SERVER_LATEST}" : generateStage(it)]
 }
 
 def generateStage(VERSION) {
