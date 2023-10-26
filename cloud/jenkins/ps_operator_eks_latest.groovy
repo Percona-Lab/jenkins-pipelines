fb3827160486dde6187116d6bde325360b379b36
Pavel Tankov <4014969+ptankov@users.noreply.github.com>
2023-10-26 10:05:07 +0300

    fixing the verision of kuttle to v0.15.0

--- cloud/jenkins/ps_operator_eks_latest.groovy
+++ cloud/jenkins/ps_operator_eks_latest.groovy
@@ -19,7 +19,12 @@ void prepareNode() {
         curl -fsSL https://github.com/kubernetes-sigs/krew/releases/latest/download/krew-linux_amd64.tar.gz | tar -xzf -
         ./krew-linux_amd64 install krew
         export PATH="\${KREW_ROOT:-\$HOME/.krew}/bin:\$PATH"
-        kubectl krew install kuttl assert
+
+        kubectl krew install assert
+
+        # v0.15.0 kuttl version
+        kubectl krew install --manifest-url https://raw.githubusercontent.com/kubernetes-sigs/krew-index/a67f31ecb2e62f15149ca66d096357050f07b77d/plugins/kuttl.yaml
+        echo $(kubectl kuttl --version) is installed
 
         curl -sL https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_\$(uname -s)_amd64.tar.gz | sudo tar -C /usr/local/bin -xzf - && sudo chmod +x /usr/local/bin/eksctl
     """
