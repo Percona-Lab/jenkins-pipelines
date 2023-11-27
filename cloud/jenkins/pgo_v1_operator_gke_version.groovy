756831ead64a64d482f0844f6ace05e3134ef141
Pavel Tankov <4014969+ptankov@users.noreply.github.com>
2023-11-27 16:17:35 +0200

    Update Google Cloud CLI repository configuration

--- cloud/jenkins/pgo_v1_operator_gke_version.groovy
+++ cloud/jenkins/pgo_v1_operator_gke_version.groovy
@@ -337,14 +337,14 @@ pipeline {
                         sudo chmod +x /usr/local/bin/jq
 
                         sudo tee /etc/yum.repos.d/google-cloud-sdk.repo << EOF
-                [google-cloud-cli]
-                name=Google Cloud CLI
-                baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
-                enabled=1
-                gpgcheck=1
-                repo_gpgcheck=0
-                gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-                EOF
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
                         sudo yum install -y google-cloud-cli google-cloud-cli-gke-gcloud-auth-plugin
                     """
                 }
