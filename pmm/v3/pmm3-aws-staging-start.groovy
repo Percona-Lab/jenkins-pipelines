72347eb0579d85f684d188a4b4cd099056d43bf2
Alex Demidoff <alexander.demidoff@percona.com>
2023-11-30 14:28:15 +0300

    PMM-12557 fix a faultering groovy script

--- pmm/v3/pmm3-aws-staging-start.groovy
+++ pmm/v3/pmm3-aws-staging-start.groovy
@@ -8,20 +8,6 @@ library changelog: false, identifier: 'lib@PMM-12557', retriever: modernSCM([
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
-def changeUserPasswordUtility(dockerImage) {
-    tag = dockerImage.split(":")[1]
-
-    if (tag.startsWith("PR") || tag.startsWith("dev"))
-        return "yes"
-
-    minorVersion = tag.split("\\.")[1].toInteger()
-
-    if (minorVersion < 27)
-        return "no"
-    else
-        return "yes"
-}
-
 def DEFAULT_SSH_KEYS = getSHHKeysPMM()
 
 pipeline {
@@ -257,7 +243,6 @@ pipeline {
             }
             steps {
                 script {
-                    env.CHANGE_USER_PASSWORD_UTILITY = changeUserPasswordUtility(DOCKER_VERSION)
                     withEnv(['JENKINS_NODE_COOKIE=dontKillMe']) {
                         node(env.VM_NAME){
                             withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
@@ -299,11 +284,7 @@ pipeline {
                                     docker logs ${VM_NAME}-server
 
                                     if [ ${ADMIN_PASSWORD} != admin ]; then
-                                        if [ ${CHANGE_USER_PASSWORD_UTILITY} == yes ]; then
-                                            docker exec ${VM_NAME}-server change-admin-password ${ADMIN_PASSWORD}
-                                        else
-                                            docker exec ${VM_NAME}-server grafana-cli --homepath /usr/share/grafana --configOverrides cfg:default.paths.data=/srv/grafana admin reset-admin-password ${ADMIN_PASSWORD}
-                                        fi
+                                        docker exec ${VM_NAME}-server change-admin-password ${ADMIN_PASSWORD}
                                     fi
                                 '''
                             }
