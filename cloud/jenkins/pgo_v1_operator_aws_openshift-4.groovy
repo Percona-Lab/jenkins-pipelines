f7539208bc5ca117af016f7de7989125cf4bd669
Pavel Tankov <4014969+ptankov@users.noreply.github.com>
2023-12-04 11:43:25 +0200

    Update AWS region in PGO v1 operator configuration

--- cloud/jenkins/pgo_v1_operator_aws_openshift-4.groovy
+++ cloud/jenkins/pgo_v1_operator_aws_openshift-4.groovy
@@ -76,7 +76,7 @@ networking:
   - 172.30.0.0/16
 platform:
   aws:
-    region: us-east-1
+    region: eu-west-3
     userTags:
       iit-billing-tag: openshift
       delete-cluster-after-hours: 8
