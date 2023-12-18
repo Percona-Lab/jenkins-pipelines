d47e0457604f38db73902720ab3de850a79b3bd4
Tomislav Plavcic <tplavcic@gmail.com>
2023-12-18 20:43:42 +0100

    K8SPG-358 - Fix OpenShift pipeline to not fail on unzip if re-run

--- cloud/jenkins/psmdb_operator_aws_openshift-4.groovy
+++ cloud/jenkins/psmdb_operator_aws_openshift-4.groovy
@@ -341,7 +341,7 @@ pipeline {
 
                 sh """
                     wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
-                    unzip terraform_0.11.14_linux_amd64.zip
+                    unzip -o terraform_0.11.14_linux_amd64.zip
                     sudo mv terraform /usr/local/bin/ && rm terraform_0.11.14_linux_amd64.zip
                 """
                 sh '''
