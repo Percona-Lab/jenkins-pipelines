7250473f6a36bda9e633ff323114662ce553e5dd
Natalia Marukovich <natalia.marukovich@percona.com>
2024-01-16 16:39:20 +0300

    update user deletion

--- cloud/aws-functions/orphaned_openshift_users.py
+++ cloud/aws-functions/orphaned_openshift_users.py
@@ -1,45 +1,70 @@
-# Remove unsused
+# Remove unsused users
 import logging
 import datetime
 import boto3
-from boto3.exceptions import NoSuchEntityException
-from utils import get_regions_list
 
+def is_user_to_terminate(client,user):
+    if 'openshift' not in user['UserName']:
+        return False
 
-def is_user_to_terminate(user_name):
-    if 'openshift' in user_name:
-        # поправить проверку
+    tags = client.list_user_tags(UserName=user['UserName'])['Tags']
+    tags_dict = {item['Key']: item['Value'] for item in tags}
+
+    for key in tags_dict.keys():
+        if 'kubernetes.io/cluster/openshift' not in key:
+            return False
+
+    current_time = datetime.datetime.now().timestamp()
+
+    creation_time = int(user['CreateDate'].timestamp())
+    if (current_time - creation_time) / 3600 > 24:
         return True
     return False
 
-def get_user_for_deletion(aws_region):
-    client = boto3.client('iam', aws_region)
+
+def get_user_for_deletion(client):
     users_for_deletion = []
 
     users = client.list_users()
     for user in users['Users']:
-        user_name=user['UserName']
-        if is_user_to_terminate(user_name):
-            users_for_deletion.append(user_name)
+        if is_user_to_terminate(client, user):
+            users_for_deletion.append(user['UserName'])
     if not users_for_deletion:
         logging.info(f"There are no users for deletion")
+
+    print(users_for_deletion)
     return users_for_deletion
 
-def delete_user(user_name, aws_region):
-    client = boto3.client('iam', aws_region)
+
+def delete_user(client, user_name):
     try:
-        client.delete_user(user_name)
-    except NoSuchEntityException as e:
+        client.delete_user(UserName=user_name)
+    except client.exceptions.NoSuchEntityException as e:
         logging.error(f"Delete of user failed with error: {e}")
 
-def lambda_handler(event, context):
-    aws_regions = get_regions_list()
+def delete_user_policies(client, user_name):
+    user_policies = client.list_user_policies(UserName=user_name)['PolicyNames']
+    print(user_policies)
+    if user_policies:
+        for policy in user_policies:
+            client.delete_user_policy(UserName=user_name, PolicyName=policy)
 
-    for aws_region in aws_regions:
-        logging.info(f"Searching for resources to remove in {aws_region}.")
-        users = get_user_for_deletion(aws_region)
-        for user in users:
-            logging.info(f"Deleting user {user}.")
-            delete_user(user, aws_region)
+def delete_user_access_keys(client, user_name):
+    user_access_keys = client.list_access_keys(UserName=user_name)['AccessKeyMetadata']
+    print(f"{user_name} {user_access_keys}")
+    if user_access_keys:
+        for key in user_access_keys:
+            client.delete_access_key(UserName=user_name, AccessKeyId=key['AccessKeyId'])
+
+
+def lambda_handler(event, context):
+    client = boto3.client('iam')
+    user_names = get_user_for_deletion(client)
+    for user_name in user_names:
+        logging.info(f"Deleting user {user_name}.")
+        delete_user_policies(client,user_name)
+        delete_user_access_keys(client, user_name)
+        delete_user(client, user_name)
+
 
 
