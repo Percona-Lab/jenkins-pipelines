bd625f612e686c849f81636853d5ca60853e3189
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-01-19 20:34:14 +0500

    Revert "Pull pipeline code from my repo."
    This reverts commit 98dceb2d75c5107a45a589d43f4f08d98f5978f0.

--- ppg/pg_tarballs.groovy
+++ ppg/pg_tarballs.groovy
@@ -1,6 +1,6 @@
 library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/maqeel75/jenkins-pipelines.git'
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
 void buildStage(String DOCKER_OS, String STAGE_PARAM) {
