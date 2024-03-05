664548b3df7a11b7522e46301b84561c847499e0
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-03-05 16:53:37 +0500

    Revert "Push .ddeb packages alongwith .deb packages to repository. (#2538)"
    This reverts commit de137e3ab4919868d80a4067c9e1c81dc62d6060.

--- vars/sync2ProdAutoBuild.groovy
+++ vars/sync2ProdAutoBuild.groovy
@@ -53,11 +53,10 @@ def call(String REPO_NAME, String DESTINATION) {
                                 DESTINATION=main
                             fi
                             for dist in `ls -1 debian`; do
-                                for deb in $(find debian/\${dist} -name '*.deb' -o -name '*.ddeb'); do
+                                for deb in `find debian/\${dist} -name '*.deb'`; do
                                  pkg_fname=\$(basename \${deb})
-                                 pkg_extension="\${pkg_fname##*.}"
                                  EC=0
-                                 /usr/local/reprepro5/bin/reprepro --list-format '"'"'\${package}_\${version}_\${architecture}.\${pkg_extension}\\n'"'"' -Vb /srv/repo-copy/${REPO_NAME}/apt -C ${DESTINATION} list \${dist} | sed -re "s|[0-9]:||" | grep ^\${pkg_fname} > /dev/null || EC=\$?
+                                 /usr/local/reprepro5/bin/reprepro --list-format '"'"'\${package}_\${version}_\${architecture}.deb\\n'"'"' -Vb /srv/repo-copy/${REPO_NAME}/apt -C ${DESTINATION} list \${dist} | sed -re "s|[0-9]:||" | grep ^\${pkg_fname} > /dev/null || EC=\$?
                                  REPOPUSH_ARGS=""
                                  if [ \${EC} -eq 0 ]; then
                                      REPOPUSH_ARGS=" --remove-package "
