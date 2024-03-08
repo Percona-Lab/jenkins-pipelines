85ff540078feda9a75c57ef2acd3e36119733e27
Vadim Yalovets <vadim.yalovets@percona.com>
2024-03-08 13:43:49 +0200

    RM-1364 PXB-8.3

--- ps/jenkins/percona-server-for-mysql-5.7.groovy
+++ ps/jenkins/percona-server-for-mysql-5.7.groovy
@@ -579,6 +579,7 @@ parameters {
                         sh """
                             pwd
                             ls -la /tmp
+                            echo "scp -o StrictHostKeyChecking=no -i ${KEY_PATH} percona-server-${PS_RELEASE}-${RPM_RELEASE}.docker.tar ${USER}@repo.ci.percona.com:${path_to_build}/binary/tarball/"
                         """
                     }
                     withCredentials([sshUserPrivateKey(credentialsId: 'repo.ci.percona.com', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
