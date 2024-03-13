44e0c6ff0fdba3ed74c8c82ed8d1343914ae69bb
Saikumar <saikumar.vs@percona.com>
2024-03-13 18:44:55 +0530

    Test Updates

--- pmm/aws-staging-start.groovy
+++ pmm/aws-staging-start.groovy
@@ -415,7 +415,7 @@ pipeline {
                                 export PMM_SERVER_IP=${IP}
                             fi
 
-			    sudo -E python /srv/qa-integration/pmm_qa/pmm-framework.py \
+			    sudo -E /srv/qa-integration/pmm_qa/pmm-framework.py \
                             --pmm-server-ip=$PMM_SERVER_IP \
                             ${CLIENTS}
                         fi
