1b72baefa6c6578d4fcb72805001d3b9049f85d7
Talha Bin Rizwan <talha.rizwan@percona.com>
2024-04-01 09:35:50 +0500

    PMM-7 Update PMM OVF pipelines for v3

--- pmm/v3/pmm3-ovf-staging-start.groovy
+++ pmm/v3/pmm3-ovf-staging-start.groovy
@@ -141,7 +141,7 @@ pipeline {
                     sh """
                         # This fails sometimes, so we want to isolate this step
                         sleep 60
-                        curl -k --user admin:admin https://${IP}/v1/Settings/Change --data '{"ssh_key": "'"\${OVF_PUBLIC_KEY}"'"}'
+                        curl -s --user admin:admin http://${IP}/v1/Settings/Change --data '{"ssh_key": "'"\${OVF_PUBLIC_KEY}"'"}'
                     """
                 }
             }
