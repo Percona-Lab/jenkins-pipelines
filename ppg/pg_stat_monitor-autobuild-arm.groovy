157fe716acbfccb8256c84ad59a5b18c6d8d3a63
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-05-10 00:47:39 +0500

    Passed PG branch and repo name.

--- ppg/pg_stat_monitor-autobuild-arm.groovy
+++ ppg/pg_stat_monitor-autobuild-arm.groovy
@@ -14,8 +14,8 @@ void buildStage(String DOCKER_OS, String STAGE_PARAM) {
         docker run -u root -v \${build_dir}:\${build_dir} ${DOCKER_OS} sh -c "
             set -o xtrace
             cd \${build_dir}
-            bash -x ./builder.sh --builddir=\${build_dir}/test --install_deps=1
-            bash -x ./builder.sh --builddir=\${build_dir}/test --branch=${BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}"
+            bash -x ./builder.sh --builddir=\${build_dir}/test --pg_release=\${PG_RELEASE} --ppg_repo_name=\${PPG_REPO} --install_deps=1
+            bash -x ./builder.sh --builddir=\${build_dir}/test --branch=${BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} --pg_release=\${PG_RELEASE} --ppg_repo_name=\${PPG_REPO} ${STAGE_PARAM}"
     """
 }
 
