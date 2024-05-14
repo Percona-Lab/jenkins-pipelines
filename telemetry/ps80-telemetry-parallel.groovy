0fd227a5c9234e70ba28828c86c0f01209acfe48
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2024-05-14 15:47:46 +0300

    Add telemetry

---
+++ telemetry/ps80-telemetry-parallel.groovy
@@ -0,0 +1,89 @@
+library changelog: false, identifier: "lib@telem_ph1", retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/eleo007/jenkins-pipelines.git'
+])
+
+pipeline {
+  agent {
+      label 'min-centos-7-x64'
+  }
+  environment {
+      PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin';
+      MOLECULE_DIR = "molecule/telemetry/${SCENARIO}";
+
+  }
+  parameters {
+        choice(
+            name: 'PLATFORM',
+            description: 'For what platform (OS) need to test',
+            choices: pdpsOperatingSystems()
+        )
+        string(
+            defaultValue: 'phase-0.1',
+            description: 'Telemetry Agent version',
+            name: 'VERSION'
+        )
+        string(
+            defaultValue: 'a620a1d8',
+            description: 'Telemetry Agent revision',
+            name: 'REVISION'
+        )
+        choice(
+            name: 'SCENARIO',
+            description: 'Scenario for test',
+            choices: ['ps-telemetry',]
+        )
+        string(
+            defaultValue: 'telemetry_ph1',
+            description: 'Branch for package-testing repository',
+            name: 'TESTING_BRANCH'
+        )
+        string(
+            defaultValue: 'eleo007',
+            description: 'Git account for package-testing repository',
+            name: 'TESTING_GIT_ACCOUNT'
+        )
+  }
+  options {
+          withCredentials(moleculePdpsJenkinsCreds())
+          disableConcurrentBuilds()
+  }
+    stages {
+        stage('Set build name'){
+          steps {
+                script {
+                    currentBuild.displayName = "${env.BUILD_NUMBER}-${env.SCENARIO}"
+                    currentBuild.description = "${env.VERSION}-${env.REPO}-${env.TESTING_BRANCH}-${env.MAJOR_REPO}"
+                }
+            }
+        }
+        stage('Check version param and checkout') {
+            steps {
+                deleteDir()
+                checkOrchVersionParam()
+                git poll: false, branch: TESTING_BRANCH, url: "https://github.com/${TESTING_GIT_ACCOUNT}/package-testing.git"
+            }
+        }
+        stage ('Prepare') {
+          steps {
+                script {
+                   installMolecule()
+             }
+           }
+        }
+        stage('Test') {
+          steps {
+                script {
+                    moleculeParallelTest(pdpsOperatingSystems(), env.MOLECULE_DIR)
+                }
+            }
+         }
+  }
+    post {
+        always {
+          script {
+              moleculeParallelPostDestroy(pdpsOperatingSystems(), env.MOLECULE_DIR)
+         }
+      }
+   }
+}
---
+++ telemetry/ps80-telemetry.groovy
@@ -0,0 +1,121 @@
+library changelog: false, identifier: "lib@telem_ph1", retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/eleo007/jenkins-pipelines.git'
+])
+
+
+pipeline {
+    agent {
+    label 'min-centos-7-x64'
+    }
+    environment {
+      PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin';
+      MOLECULE_DIR = "molecule/telemetry/${SCENARIO}";
+    }
+    parameters {
+        choice(
+            name: 'PLATFORM',
+            description: 'For what platform (OS) need to test',
+            choices: pdpsOperatingSystems()
+        )
+        string(
+            defaultValue: 'phase-0.1',
+            description: 'Telemetry Agent version',
+            name: 'VERSION'
+        )
+        string(
+            defaultValue: 'a620a1d8',
+            description: 'Telemetry Agent revision',
+            name: 'REVISION'
+        )
+        choice(
+            name: 'SCENARIO',
+            description: 'Scenario for test',
+            choices: ['ps-telemetry',]
+        )
+        string(
+            defaultValue: 'telemetry_ph1',
+            description: 'Branch for package-testing repository',
+            name: 'TESTING_BRANCH'
+        )
+        string(
+            defaultValue: 'eleo007',
+            description: 'Git account for package-testing repository',
+            name: 'TESTING_GIT_ACCOUNT'
+        )
+        choice(
+            name: 'DESTROY_ENV',
+            description: 'Destroy VM after tests',
+            choices: [
+                'yes',
+                'no'
+            ]
+        )
+    }
+    options {
+          withCredentials(moleculePdpsJenkinsCreds())
+          disableConcurrentBuilds()
+    }
+    stages {
+        stage('Set build name'){
+            steps {
+                script {
+                    currentBuild.displayName = "${env.BUILD_NUMBER}-${env.PLATFORM}-${env.SCENARIO}"
+                    currentBuild.description = "${env.VERSION}-${env.REPO}-${env.TESTING_BRANCH}-${env.MAJOR_REPO}"
+                }
+            }
+        }
+        stage('Check version param and checkout') {
+            steps {
+                deleteDir()
+                checkOrchVersionParam()
+                git poll: false, branch: TESTING_BRANCH, url: "https://github.com/${TESTING_GIT_ACCOUNT}/package-testing.git"
+            }
+        }
+        stage ('Prepare') {
+            steps {
+                script {
+                    installMolecule()
+                }
+            }
+        }
+        stage ('Create virtual machines') {
+            steps {
+                script{
+                    moleculeExecuteActionWithScenario(env.MOLECULE_DIR, "create", env.PLATFORM)
+                }
+            }
+        }
+        stage ('Run playbook for test') {
+            steps {
+                script{
+                    moleculeExecuteActionWithScenario(env.MOLECULE_DIR, "converge", env.PLATFORM)
+                }
+            }
+        }
+        stage ('Start testinfra tests') {
+            steps {
+                script{
+                    moleculeExecuteActionWithScenario(env.MOLECULE_DIR, "verify", env.PLATFORM)
+                }
+            }
+        }
+        stage ('Start Cleanup ') {
+            steps {
+                script {
+                    moleculeExecuteActionWithScenario(env.MOLECULE_DIR, "cleanup", env.PLATFORM)
+                }
+            }
+        }
+    }
+    post {
+        always {
+            script {
+                if (env.DESTROY_ENV == "yes") {
+                    moleculeExecuteActionWithScenario(env.MOLECULE_DIR, "destroy", env.PLATFORM)
+                    junit "${MOLECULE_DIR}/report.xml"
+                }
+            }
+        }
+    }
+}
