7683231230e4dbd35899f4b8f7e5164af5393fac
Naeem Akhter <naeem.akhter@percona.com>
2024-05-16 02:55:57 +0500

    Added new function to use python39 in set up that is used for PPG testing. Now using using agent min-ol-9-x64.

--- ppg/ppg.groovy
+++ ppg/ppg.groovy
@@ -5,7 +5,7 @@ library changelog: false, identifier: "lib@master", retriever: modernSCM([
 
 pipeline {
   agent {
-  label 'min-ol-8-x64'
+  label 'min-ol-9-x64'
   }
 
   parameters {
@@ -71,7 +71,7 @@ pipeline {
     stage ('Prepare') {
       steps {
           script {
-              installMoleculePPG()
+              installMoleculePython39()
             }
         }
     }
