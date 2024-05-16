7fa04220d5de98059e534cad22d2add3474ca7a9
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-05-16 16:41:11 +0500

    Removed debug build option

--- ppg/ppg-docker.groovy
+++ ppg/ppg-docker.groovy
@@ -14,7 +14,7 @@ pipeline {
         choice(name: 'PPG_REPO', choices: ['testing','release','experimental'], description: 'Percona-release repo')
         string(name: 'PPG_VERSION', defaultValue: '16.3-1', description: 'PPG version')
         choice(name: 'TARGET_REPO', choices: ['PerconaLab','AWS_ECR','DockerHub'], description: 'Target repo for docker image, use DockerHub for release only')
-        choice(name: 'DEBUG', choices: ['no','yes'], description: 'Additionally build debug image')
+//      choice(name: 'DEBUG', choices: ['no','yes'], description: 'Additionally build debug image')
     }
     options {
         disableConcurrentBuilds()
