57abc0a09eb09b4f18d5d0ca368856ba0d7ca787
Talha Bin Rizwan <talha.rizwan@percona.com>
2024-05-24 12:12:41 +0500

    PMM-12574 Add ARM support for pmm-client.

--- vars/uploadDEB.groovy
+++ vars/uploadDEB.groovy
@@ -15,20 +15,16 @@ def call() {
                         `dirname \${dsc}`/* \
                         ${USER}@repo.ci.percona.com:\${path_to_build}/source/debian/
                 fi
-
+                
                 for deb in \$(find . -name '*.deb'); do
-                    dist=`echo \${deb} | sed -re 's/.*\\.([^.]+)_(amd64|arm64).deb/\\1/'`
-                    path_to_dist=\${path_to_build}/binary/debian/\${dist}/x86_64
-                    ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com \
-                        mkdir -p \${path_to_dist}
-                    scp -o StrictHostKeyChecking=no -i ${KEY_PATH} \
-                        \${deb} \
-                        ${USER}@repo.ci.percona.com:\${path_to_dist}/
-                done
-
-                for deb in \$(find . -name '*.deb'); do
-                    dist=`echo \${deb} | sed -re 's/.*\\.([^.]+)_(amd64|arm64).deb/\\1/'`
-                    path_to_dist=\${path_to_build}/binary/debian/\${dist}/aarch64
+                    arch=`echo \${deb} | sed -re 's/.*_(amd64|arm64).deb/\\1/'`
+                    if [ "\${arch}" = "amd64" ]; then
+                        dist=`echo \${deb} | sed -re 's/.*\\.([^.]+)_amd64\\.deb/\\1/'`
+                        path_to_dist=\${path_to_build}/binary/debian/\${dist}/x86_64
+                    elif [ "\${arch}" = "arm64" ]; then
+                        dist=`echo \${deb} | sed -re 's/.*\\.([^.]+)_arm64\\.deb/\\1/'`
+                        path_to_dist=\${path_to_build}/binary/debian/\${dist}/aarch64
+                    fi
                     ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com \
                         mkdir -p \${path_to_dist}
                     scp -o StrictHostKeyChecking=no -i ${KEY_PATH} \
