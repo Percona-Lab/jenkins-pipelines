f09fc45da2169f784c4900bec59494d38b6d34c4
Talha Bin Rizwan <talha.rizwan@percona.com>
2024-05-24 17:33:14 +0500

    PMM-12574 Add ARM support for pmm-client.

--- pmm/infrastructure/rpm-build-3.groovy
+++ pmm/infrastructure/rpm-build-3.groovy
@@ -17,73 +17,47 @@ pipeline {
         DOCKER_TAG = "rpmbuild:3"
     }
     stages {
-        stage('Build rpmbuild image for v3') {
-                stages {
-                    stage('Prepare') {
-                        steps {
-                            checkout([$class: 'GitSCM', 
-                                    branches: [[name: "*/${PMM_GIT_BRANCH}"]],
-                                    extensions: [[$class: 'CloneOption',
-                                    noTags: true,
-                                    reference: '',
-                                    shallow: true]],
-                                    userRemoteConfigs: [[url: 'https://github.com/percona/pmm.git']]
-                            ])
-                        }
-                    }
-                    stage('Setup Buildx') {
-                        steps {
-                            script {
-                                sh '''
-                                    # Ensure Buildx is available and create a new builder with the docker-container driver
-                                    docker buildx create --use --name pmmbuilder --driver docker-container || true
-                                    docker buildx inspect pmmbuilder --bootstrap
-                                '''
-                            }
-                        }
-                    }
-                    // stage('Prepare') {
-                    //     steps {
-                    //         script {
-                    //             sh '''
-                    //                 git clone --single-branch --no-tags --branch ${PMM_GIT_BRANCH} --depth=1 https://github.com/percona/pmm.git .
-                    //             '''
-                    //         }
-                    //     }
-                    // }
-                    stage('Login to ECR') {
-                        steps {
-                            withCredentials([[
-                                $class: 'AmazonWebServicesCredentialsBinding',
-                                accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                                credentialsId: 'ECRRWUser',
-                                secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                                sh '''
-                                    aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin ${IMAGE_REGISTRY}
-                                '''
-                            }
-                        }
-                    }
-                    stage('Build') {
-                        steps {
-                            sh '''
-                                cd build/docker/rpmbuild/
-                                ## docker buildx build --pull --tag ${IMAGE_REGISTRY}/${DOCKER_TAG} -f Dockerfile.el9 .
-                                docker buildx build --pull --platform linux/amd64,linux/arm64 --tag ${IMAGE_REGISTRY}/${DOCKER_TAG} -f Dockerfile.el9 --push .
-                            '''
-                            // withCredentials([[
-                            //     $class: 'AmazonWebServicesCredentialsBinding',
-                            //     accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                            //     credentialsId: 'ECRRWUser',
-                            //     secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                            //     sh '''
-                            //         aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin ${IMAGE_REGISTRY}
-                            //         ## docker push ${IMAGE_REGISTRY}/${DOCKER_TAG}
-                            //     '''
-                            // }
-                        }
-                    }
+        stage('Prepare') {
+            steps {
+                deleteDir()
+                script {
+                    sh '''
+                        git clone --single-branch --no-tags --branch ${PMM_GIT_BRANCH} --depth=1 https://github.com/percona/pmm.git .
+                    '''
                 }
+            }
+        }
+        stage('Setup Buildx') {
+            steps {
+                script {
+                    sh '''
+                        # Ensure Buildx is available and create a new builder with the docker-container driver
+                        docker buildx create --use --name pmmbuilder --driver docker-container || true
+                        docker buildx inspect pmmbuilder --bootstrap
+                    '''
+                }
+            }
+        }
+        stage('Login to ECR') {
+            steps {
+                withCredentials([[
+                    $class: 'AmazonWebServicesCredentialsBinding',
+                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                    credentialsId: 'ECRRWUser',
+                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    sh '''
+                        aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin ${IMAGE_REGISTRY}
+                    '''
+                }
+            }
+        }
+        stage('Build') {
+            steps {
+                sh '''
+                    cd build/docker/rpmbuild/
+                    docker buildx build --pull --platform linux/amd64,linux/arm64 --tag ${IMAGE_REGISTRY}/${DOCKER_TAG} -f Dockerfile.el9 --push .
+                '''
+            }
         }
     }
 }
