9ff9e4a37b758391244844d0713dcc02c24905ba
Talha Bin Rizwan <talha.rizwan@percona.com>
2024-06-03 18:42:15 +0500

    PMM-12574 Add ARM support for pmm-client.

--- pmm/v3/pmm3-client-autobuild-arm.groovy
+++ pmm/v3/pmm3-client-autobuild-arm.groovy
@@ -87,33 +87,30 @@ pipeline {
                         uploadTarball('binary')
                     }
                 }
-                // stage('Build client docker') {
-                //     steps {
-                //         withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                //             withEnv(['PATH_TO_SCRIPTS=' + env.PATH_TO_SCRIPTS]) {
-                //                 sh '''
-                //                     echo "${PASS}" | docker login -u "${USER}" --password-stdin
-                //                     set -o xtrace
+                stage('Build client docker') {
+                    steps {
+                        withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                            withEnv(['PATH_TO_SCRIPTS=' + env.PATH_TO_SCRIPTS]) {
+                                sh '''
+                                    echo "${PASS}" | docker login -u "${USER}" --password-stdin
+                                    set -o xtrace
 
-                //                     export PUSH_DOCKER=1
-                //                     export DOCKER_CLIENT_TAG=perconalab/pmm-client:$(date -u '+%Y%m%d%H%M')
+                                    export DOCKER_CLIENT_TAG=perconalab/pmm-client:$(date -u '+%Y%m%d%H%M')
 
-                //                     ${PATH_TO_SCRIPTS}/build-client-docker
+                                    ${PATH_TO_SCRIPTS}/build-client-docker
 
-                //                     if [ -n "${DOCKER_RC_TAG}" ]; then
-                //                         docker tag $DOCKER_CLIENT_TAG perconalab/pmm-client:${DOCKER_RC_TAG}
-                //                         docker push perconalab/pmm-client:${DOCKER_RC_TAG}
-                //                     fi
-                //                     docker tag $DOCKER_CLIENT_TAG perconalab/pmm-client:${DOCKER_LATEST_TAG}
-                //                     docker push $DOCKER_CLIENT_TAG
-                //                     docker push perconalab/pmm-client:${DOCKER_LATEST_TAG}
-                //                 '''
-                //             }
-                //         }
-                //         stash includes: 'results/docker/CLIENT_TAG', name: 'CLIENT_IMAGE'
-                //         archiveArtifacts 'results/docker/CLIENT_TAG'
-                //     }
-                // }
+                                    if [ -n "${DOCKER_RC_TAG}" ]; then
+                                        docker buildx imagetools create --tag perconalab/pmm-client:${DOCKER_RC_TAG} ${DOCKER_CLIENT_TAG}
+                                    fi
+                                    docker buildx imagetools create --tag perconalab/pmm-client:${DOCKER_LATEST_TAG} ${DOCKER_CLIENT_TAG}
+                                    docker buildx imagetools create --tag ${DOCKER_CLIENT_TAG} ${DOCKER_CLIENT_TAG}
+                                '''
+                            }
+                        }
+                        stash includes: 'results/docker/CLIENT_TAG', name: 'CLIENT_IMAGE'
+                        archiveArtifacts 'results/docker/CLIENT_TAG'
+                    }
+                }
                 stage('Build client source rpm') {
                     parallel {
                         stage('Build client source rpm EL7') {
@@ -167,7 +164,7 @@ pipeline {
                     steps {
                         sh "${PATH_TO_SCRIPTS}/build-client-sdeb ubuntu:focal"
                         stash includes: 'results/source_deb/*', name: 'debs'
-                        // uploadDEB()
+                        uploadDEB()
                     }
                 }
                 stage('Build client binary debs') {
@@ -210,12 +207,12 @@ pipeline {
                         }
                     }
                 }
-                // stage('Sign packages') {
-                //     steps {
-                //         signRPM()
-                //         signDEB()
-                //     }
-                // }
+                stage('Sign packages') {
+                    steps {
+                        signRPM()
+                        signDEB()
+                    }
+                }
             }
         }
         // stage('Push to public repository') {
