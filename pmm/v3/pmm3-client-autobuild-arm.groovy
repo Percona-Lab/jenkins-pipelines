5a76e674c7ae31aecdc5436f37656a5ffb9dcffa
Talha Bin Rizwan <talha.rizwan@percona.com>
2024-06-06 17:36:09 +0500

    PMM-9242 Add ARM support for pmm-client.

--- pmm/v3/pmm3-client-autobuild-arm.groovy
+++ pmm/v3/pmm3-client-autobuild-arm.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: 'lib@PMM-12574-arm-build-for-client', retriever: modernSCM([
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
