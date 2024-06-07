aba81f14f4ac957832cf80a9c795e1c0b05dbebf
Vadim Yalovets <vadim.yalovets@percona.com>
2024-06-07 19:53:41 +0300

    Add noble (#2680)

--- pxb/jenkins/pxb-24.groovy
+++ pxb/jenkins/pxb-24.groovy
@@ -193,6 +193,19 @@ pipeline {
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
+                stage('Ubuntu Noble(24.04)') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
+                        buildStage("ubuntu:noble", "--build_deb=1")
+
+                        pushArtifactFolder("deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
+                    }
+                }
                 stage('Debian Buster(10)') {
                     agent {
                         label 'docker'
