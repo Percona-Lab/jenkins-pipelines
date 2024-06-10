450e6613f40df8710c89cf7a53648240459b6d87
panchal-yash <yash.panchal@percona.com>
2024-06-10 18:40:25 +0530

    add support for archiving artifacts for pxc and pxb rhel86-89 tests

--- pxb/jenkins/pxb-rhel-tarballs-86-89.groovy
+++ pxb/jenkins/pxb-rhel-tarballs-86-89.groovy
@@ -77,6 +77,7 @@ pipeline {
   post {
     always {
       script {
+        archiveArtifacts artifacts: "*.tar.gz" , followSymlinks: false
         moleculeParallelPostDestroy(pxbTarballRHEL8689(), env.MOLECULE_DIR)
       }
     }
