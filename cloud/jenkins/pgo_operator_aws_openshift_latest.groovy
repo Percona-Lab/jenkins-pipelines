938147d3b1234d8e061aa5f1ef4ccb2a9b373423
Pavel Tankov <4014969+ptankov@users.noreply.github.com>
2024-06-10 12:19:21 +0300

    making the install command similar to the others

--- cloud/jenkins/pgo_operator_aws_openshift_latest.groovy
+++ cloud/jenkins/pgo_operator_aws_openshift_latest.groovy
@@ -16,8 +16,7 @@ void prepareNode() {
         sudo sh -c "curl -s -L https://github.com/mikefarah/yq/releases/download/v4.35.1/yq_linux_amd64 > /usr/local/bin/yq"
         sudo chmod +x /usr/local/bin/yq
 
-        sudo sh -c "curl -s -L https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux64 > /usr/local/bin/jq"
-        sudo chmod +x /usr/local/bin/jq
+        sudo curl -fsSL https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux64 -o /usr/local/bin/jq && sudo chmod +x /usr/local/bin/jq
 
         sudo yum install -y https://repo.percona.com/yum/percona-release-latest.noarch.rpm || true
         sudo percona-release enable-only tools
