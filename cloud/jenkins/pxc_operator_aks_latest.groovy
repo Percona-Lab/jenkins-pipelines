5b7adddeb5fe138902c4dbe2bbf45b0713765bdb
Pavel Tankov <4014969+ptankov@users.noreply.github.com>
2024-06-10 12:38:39 +0300

    Upgrading yq from v4.35.1 to v4.44.1 for all pipelines

--- cloud/jenkins/pxc_operator_aks_latest.groovy
+++ cloud/jenkins/pxc_operator_aks_latest.groovy
@@ -10,9 +10,7 @@ void prepareNode() {
 
         curl -fsSL https://get.helm.sh/helm-v3.12.3-linux-amd64.tar.gz | sudo tar -C /usr/local/bin --strip-components 1 -xzf - linux-amd64/helm
 
-        sudo sh -c "curl -s -L https://github.com/mikefarah/yq/releases/download/v4.35.1/yq_linux_amd64 > /usr/local/bin/yq"
-        sudo chmod +x /usr/local/bin/yq
-
+        sudo curl -fsSL https://github.com/mikefarah/yq/releases/download/v4.44.1/yq_linux_amd64 -o /usr/local/bin/yq && sudo chmod +x /usr/local/bin/yq
         sudo curl -fsSL https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux64 -o /usr/local/bin/jq && sudo chmod +x /usr/local/bin/jq
 
         if ! command -v az &>/dev/null; then
