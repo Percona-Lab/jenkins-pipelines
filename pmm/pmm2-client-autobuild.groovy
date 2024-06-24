d910e2b5a18279993eeb565493a9f0d5bc522c3b
Talha Bin Rizwan <talha.rizwan@percona.com>
2024-06-24 20:28:37 +0500

    PMM-9242 Add ARM support for pmm client.

--- pmm/pmm2-client-autobuild.groovy
+++ pmm/pmm2-client-autobuild.groovy
@@ -20,24 +20,20 @@ pipeline {
     //     upstream upstreamProjects: 'pmm2-submodules-rewind', threshold: hudson.model.Result.SUCCESS
     // }
     stages {
-        stage('Start PMM2 Client Autobuilds') {
-            parallel {
-                stage('Build PMM2 Client for AMD64') {
-                    steps {
-                        build job: 'tbr-pmm2-client-autobuilds-amd', parameters: [
-                            string(name: 'GIT_BRANCH', value: params.GIT_BRANCH),
-                            string(name: 'DESTINATION', value: params.DESTINATION)
-                        ]
-                    }
-                }
-                stage('Build PMM2 Client for ARM64') {
-                    steps {
-                        build job: 'tbr-pmm2-client-autobuilds-arm', parameters: [
-                            string(name: 'GIT_BRANCH', value: params.GIT_BRANCH),
-                            string(name: 'DESTINATION', value: params.DESTINATION)
-                        ]
-                    }
-                }
+        stage('Build PMM2 Client for AMD64') {
+            steps {
+                build job: 'tbr-pmm2-client-autobuilds-amd', parameters: [
+                    string(name: 'GIT_BRANCH', value: params.GIT_BRANCH),
+                    string(name: 'DESTINATION', value: params.DESTINATION)
+                ]
+            }
+        }
+        stage('Build PMM2 Client for ARM64') {
+            steps {
+                build job: 'tbr-pmm2-client-autobuilds-arm', parameters: [
+                    string(name: 'GIT_BRANCH', value: params.GIT_BRANCH),
+                    string(name: 'DESTINATION', value: params.DESTINATION)
+                ]
             }
         }
     }
