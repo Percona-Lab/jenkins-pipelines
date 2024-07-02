917fde6bc95fed2aaa5bd069cd993beacff8bc5d
Pavel Tankov <4014969+ptankov@users.noreply.github.com>
2024-07-02 10:31:36 +0300

    restore  agent labels in Jenkins pipeline steps, because it is needed for separate nodes in parallel stages

--- cloud/jenkins/pgo_operator_eks_latest.groovy
+++ cloud/jenkins/pgo_operator_eks_latest.groovy
@@ -440,25 +440,41 @@ pipeline {
         stage('Run Tests') {
             parallel {
                 stage('cluster1') {
+                    agent {
+                        label 'docker'
+                    }
                     steps {
+                        prepareNode()
                         unstash "sourceFILES"
                         clusterRunner('cluster1')
                     }
                 }
                 stage('cluster2') {
+                    agent {
+                        label 'docker'
+                    }
                     steps {
+                        prepareNode()
                         unstash "sourceFILES"
                         clusterRunner('cluster2')
                     }
                 }
                 stage('cluster3') {
+                    agent {
+                        label 'docker'
+                    }
                     steps {
+                        prepareNode()
                         unstash "sourceFILES"
                         clusterRunner('cluster3')
                     }
                 }
                 stage('cluster4') {
+                    agent {
+                        label 'docker'
+                    }
                     steps {
+                        prepareNode()
                         unstash "sourceFILES"
                         clusterRunner('cluster4')
                     }
