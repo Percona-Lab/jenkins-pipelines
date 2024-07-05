b8a513b78de6ddd022b45e47d6d0a8435e923526
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2024-07-05 16:30:45 +0300

    PMM-12188 review changes about quotes

--- pmm/pmm2-release.groovy
+++ pmm/pmm2-release.groovy
@@ -550,20 +550,20 @@ ENDSSH
             steps{
                 script{
                     withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'GITHUB_API_TOKEN')]) {
-                        sh """
+                        sh '''
                             curl -v -X POST \
                                 -H "Accept: application/vnd.github.v3+json" \
                                 -H "Authorization: token ${GITHUB_API_TOKEN}" \
                                 "https://api.github.com/repos/percona/pmm-qa/actions/workflows/package-test-single.yml/dispatches" \
                                 -d '{"ref":"main","inputs":{"playbook": "pmm2-client", "package": "pmm2-client", "repository": "release", "metrics_mode": "auto"}}'
-                        """
-                        sh """
+                        '''
+                        sh '''
                             curl -v -X POST \
                                 -H "Accept: application/vnd.github.v3+json" \
                                 -H "Authorization: token ${GITHUB_API_TOKEN}" \
                                 "https://api.github.com/repos/percona/pmm-qa/actions/workflows/e2e-upgrade-tests-matrix-full.yml/dispatches" \
                                 -d '{"ref":"main","inputs":{"pmm_ui_tests_branch": "main", "pmm_qa_branch": "main", "repository": "release", "versions_range": 1}}'
-                        """
+                        '''
                     }
                 }
             }
