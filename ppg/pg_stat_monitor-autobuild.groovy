36a536326322026d25cc159628b7a987fede9625
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-07-09 16:10:58 +0500

    [PG-803]: Add 17 in PG versions list. (#2728)

--- ppg/pg_stat_monitor-autobuild.groovy
+++ ppg/pg_stat_monitor-autobuild.groovy
@@ -78,7 +78,7 @@ pipeline {
         choice(
             name: 'PG_RELEASE',
             description: 'PPG major version to test',
-            choices: ['11', '12', '13', '14', '15', '16']
+            choices: ['11', '12', '13', '14', '15', '16', '17']
         )
         string(
             defaultValue: 'ppg-16.0',
