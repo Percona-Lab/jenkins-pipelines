b50060cac4fe263386d92acce5724a034e410e22
Vadim Yalovets <vadim.yalovets@percona.com>
2024-07-22 09:55:03 +0300

    Fix ps docker build (#2745)

--- ps/jenkins/percona-server-for-mysql-8.0.groovy
+++ ps/jenkins/percona-server-for-mysql-8.0.groovy
@@ -825,7 +825,7 @@ parameters {
                         unstash 'properties'
                         sh '''
                             PS_RELEASE=$(echo ${BRANCH} | sed 's/release-//g')
-                            PS_MAJOR_RELEASE=$(echo ${BRANCH} | sed "s/release-//g" | sed "s/\\.//g" | awk '{print substr($0, 0, 3)}')
+                            PS_MAJOR_RELEASE=$(echo ${BRANCH} | sed "s/release-//g" | sed "s/\\.//g" | awk '{print substr($0, 0, 2)}')
                             if [ ${PS_MAJOR_RELEASE} != "80" ]; then
                                 MYSQL_SHELL_RELEASE=$(echo ${BRANCH} | sed 's/release-//g' | awk '{print substr($0, 0, 6)}' | sed 's/-//g')
                                 MYSQL_ROUTER_RELEASE=$(echo ${BRANCH} | sed 's/release-//g' | awk '{print substr($0, 0, 6)}' | sed 's/-//g')
