857914e43b0b96a5223aaaa7fa58fb29b1ddad0e
Saikumar <saikumar.vs@percona.com>
2024-07-24 19:26:35 +0530

    Update repo for testing

--- pmm/pmm2-ui-tests-nightly.groovy
+++ pmm/pmm2-ui-tests-nightly.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: 'lib@pmm7-fix_mysqlcheck', retriever: modernSCM([
+library changelog: false, identifier: 'lib@pmm7-fix-qarepo', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
