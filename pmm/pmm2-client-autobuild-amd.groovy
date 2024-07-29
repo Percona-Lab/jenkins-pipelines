e5feaf25b049febedcfd5c86419274f288cf6a48
Talha Bin Rizwan <talha.rizwan@percona.com>
2024-07-29 14:40:48 +0500

    PMM-9242 Update pipelines.

--- pmm/pmm2-client-autobuild-amd.groovy
+++ pmm/pmm2-client-autobuild-amd.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: 'lib@PMM-9242-arm-build-for-client', retriever: modernSCM([
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
