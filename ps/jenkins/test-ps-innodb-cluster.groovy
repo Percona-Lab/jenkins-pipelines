f5f417fc16caf0a9e2f2f9ff0f381bfab744cfcc
puneetkaushik <kaushik.puneet07@gmail.com>
2024-07-31 15:14:17 +0530

    update

--- ps/jenkins/test-ps-innodb-cluster.groovy
+++ ps/jenkins/test-ps-innodb-cluster.groovy
@@ -4,15 +4,18 @@ library changelog: false, identifier: 'lib@add-dist', retriever: modernSCM([
 ]) _
 
 void installDependencies() {
-    sh '''
-        export PATH=${PATH}:~/.local/bin
-        sudo yum install -y git python3-pip jq tar
-        sudo amazon-linux-extras install  python3.8 ansible2
-        python3 -m venv venv
-        source venv/bin/activate
-        python3 -m pip install --upgrade pip setuptools wheel
-        python3 -m pip install molecule==2.22 boto boto3 paramiko testinfra
-    '''
+    sh """
+        sudo apt update -y
+        sudo apt install -y python3 python3-pip python3-dev python3-venv
+        python3 -m venv virtenv
+        . virtenv/bin/activate
+        python3 --version
+        python3 -m pip install --upgrade pip
+        python3 -m pip install --upgrade setuptools
+        python3 -m pip install --upgrade setuptools-rust
+        python3 -m pip install --upgrade PyYaml==5.3.1 molecule==3.3.0 testinfra pytest molecule-ec2==0.3 molecule[ansible] "ansible<10.0.0" "ansible-lint>=5.1.1,<6.0.0" boto3 boto
+       
+       """
 
     sh '''
         rm -rf package-testing
@@ -72,7 +75,7 @@ void setInstancePrivateIPEnvironment() {
 
 pipeline {
     agent {
-        label 'micro-amazon'
+        label 'min-bookworm-x64'
     }
 
     options {
