6153cbb39f097b82981987a0124336747b869e49
Sandra Romanchenko <53295797+sandraromanchenko@users.noreply.github.com>
2024-08-01 09:34:25 +0300

    PSMDB. Add ubuntu noble for PSMDB 5.0 and 7.0 (#2781)

--- vars/pdmdbOperatingSystems.groovy
+++ vars/pdmdbOperatingSystems.groovy
@@ -1,9 +1,9 @@
 def call(String version = 'default', String newVersion = null) {
     def switchValues = [
         (~/(p.mdb-)?6(\.)?0.*/): ['centos-7', 'debian-10', 'debian-11', 'ubuntu-focal', 'rhel8', 'rhel9', 'ubuntu-jammy', 'ubuntu-noble', 'rhel8-arm', 'rhel9-arm', 'ubuntu-focal-arm', 'ubuntu-jammy-arm','ubuntu-noble-arm' ],
-        (~/(p.mdb-)?7(\.)?0.*/): ['centos-7', 'debian-11', 'debian-12', 'ubuntu-focal', 'rhel8', 'rhel9', 'ubuntu-jammy', 'rhel8-arm', 'rhel9-arm', 'ubuntu-focal-arm', 'ubuntu-jammy-arm' ],
+        (~/(p.mdb-)?7(\.)?0.*/): ['centos-7', 'debian-11', 'debian-12', 'ubuntu-focal', 'rhel8', 'rhel9', 'ubuntu-jammy', 'ubuntu-noble', 'rhel8-arm', 'rhel9-arm', 'ubuntu-focal-arm', 'ubuntu-jammy-arm', 'ubuntu-noble-arm' ],
         (~/(p.mdb-)?8(\.)?0.*/): ['debian-12', 'rhel8', 'rhel8-arm', 'rhel9', 'rhel9-arm', 'ubuntu-focal','ubuntu-focal-arm', 'ubuntu-jammy', 'ubuntu-jammy-arm'],
-        'default': ['centos-7', 'debian-10', 'debian-11', 'ubuntu-focal', 'rhel8', 'ubuntu-jammy']
+        'default': ['centos-7', 'debian-10', 'debian-11', 'ubuntu-focal', 'rhel8', 'ubuntu-jammy','ubuntu-noble']
     ]
 
     def versionValues = switchValues.find { key, value -> version ==~ key }?.value ?: switchValues['default']
