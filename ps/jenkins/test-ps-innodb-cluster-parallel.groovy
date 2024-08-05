a950de10d1e8ed50a6aad1d15cb3bf492ec35b7e
puneetkaushik <kaushik.puneet07@gmail.com>
2024-08-06 00:02:20 +0530

    update innodb cluster parrallel job

--- ps/jenkins/test-ps-innodb-cluster-parallel.groovy
+++ ps/jenkins/test-ps-innodb-cluster-parallel.groovy
@@ -14,7 +14,14 @@ List all_nodes = [
     'oracle-8',
     'oracle-9',
     'rhel-8',
-    'rhel-9'
+    'rhel-9',
+    'rhel-8-arm',
+    'rhel-9-arm',
+    'debian-11-arm',
+    'debian-12-arm',
+    'ubuntu-focal-arm',
+    'ubuntu-jammy-arm',
+    'ubuntu-noble-arm'
 ]
 
 List TEST_DISTS = []
