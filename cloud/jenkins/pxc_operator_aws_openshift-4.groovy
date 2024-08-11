abecb9619da73b1d834cc9e4d1989e4b44ef36e0
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2024-08-11 12:57:56 +0300

    fix proxy-protocol test

--- cloud/jenkins/pxc_operator_aws_openshift-4.groovy
+++ cloud/jenkins/pxc_operator_aws_openshift-4.groovy
@@ -166,7 +166,7 @@ compute:
   platform:
     aws:
       type: m5.2xlarge
-  replicas: 1
+  replicas: 3
 controlPlane:
   architecture: amd64
   hyperthreading: Enabled
@@ -203,7 +203,6 @@ EOF
         sshagent(['aws-openshift-41-key']) {
             sh """
                 /usr/local/bin/openshift-install create cluster --dir=openshift/$CLUSTER_SUFFIX
-                /usr/local/bin/openshift-install wait-for install-complete --dir=openshift/$CLUSTER_SUFFIX
                 export KUBECONFIG=openshift/$CLUSTER_SUFFIX/auth/kubeconfig
 
                 machineset=`oc get machineset  -n openshift-machine-api | awk 'NR==2 {print \$1; exit}'`
