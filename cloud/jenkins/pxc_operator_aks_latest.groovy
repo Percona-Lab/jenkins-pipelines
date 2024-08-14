5962c64b736484b5e6f538f3ba4a8f1767a9f39d
Natalia Marukovich <natalia.marukovich@percona.com>
2024-08-14 16:35:00 +0200

    test fix

--- cloud/jenkins/pxc_operator_aks_latest.groovy
+++ cloud/jenkins/pxc_operator_aks_latest.groovy
@@ -33,7 +33,8 @@ void prepareNode() {
     }
 
     if ("$PLATFORM_VER" == "latest") {
-        USED_PLATFORM_VER = sh(script: "az aks get-versions --location $location --output json | jq -r '.values | max_by(.patchVersions) | .patchVersions | keys[]' | sort --version-sort | tail -1", , returnStdout: true).trim()
+        USED_PLATFORM_VER = "1.30"
+//         sh(script: "az aks get-versions --location $location --output json | jq -r '.values | max_by(.patchVersions) | .patchVersions | keys[]' | sort --version-sort | tail -1", , returnStdout: true).trim()
     } else {
         USED_PLATFORM_VER="$PLATFORM_VER"
     }
@@ -176,8 +177,7 @@ void createCluster(String CLUSTER_SUFFIX) {
             --generate-ssh-keys \
             --enable-cluster-autoscaler \
             --outbound-type loadbalancer \
-            --kubernetes-version $USED_PLATFORM_VER \
-            -l $location
+            --kubernetes-version $USED_PLATFORM_VER
         az aks get-credentials --subscription eng-cloud-dev --resource-group percona-operators --name $CLUSTER_NAME-$CLUSTER_SUFFIX --overwrite-existing
     """
 }
