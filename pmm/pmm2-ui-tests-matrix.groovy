44f6813b23f03b183d9656555ce07b082599a544
Peter Sirotnak <sirotnak.p@gmail.com>
2024-08-14 10:50:49 +0200

    Fix runner in matrix

--- pmm/pmm2-ui-tests-matrix.groovy
+++ pmm/pmm2-ui-tests-matrix.groovy
@@ -167,7 +167,7 @@ pipeline {
                 }
                 stage('Run stanity tests for pmm-client docker container on arm64'){
                     steps {
-                        runUITestsJob(GIT_BRANCH, GIT_COMMIT_HASH, DOCKER_VERSION, CLIENT_VERSION, '@client-docker-multi-arch', MYSQL_IMAGE, POSTGRES_IMAGE, MONGO_IMAGE, PROXYSQL_IMAGE, PMM_QA_GIT_BRANCH, '', 'agent-arm64');
+                        runUITestsJob(GIT_BRANCH, GIT_COMMIT_HASH, DOCKER_VERSION, CLIENT_VERSION, '@client-docker-multi-arch', MYSQL_IMAGE, POSTGRES_IMAGE, MONGO_IMAGE, PROXYSQL_IMAGE, PMM_QA_GIT_BRANCH, '', 'agent-arm64-ol9');
                     }
                 }
             }
