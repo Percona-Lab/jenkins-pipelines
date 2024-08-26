fed828c29dc02b6adf0a5210562980602c64ec0a
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2024-08-26 15:20:52 +0300

    Deploy changes from main

--- vars/pdpsOperatingSystems.groovy
+++ vars/pdpsOperatingSystems.groovy
@@ -1,3 +1,3 @@
 def call() {
-  return ['centos-7', 'oracle-8', 'oracle-9', 'rhel-8', 'rhel-9', 'debian-10', 'debian-11', 'debian-12','ubuntu-focal', 'ubuntu-jammy', 'ubuntu-noble', 'amazon-2']
+  return ['centos-7', 'oracle-8', 'oracle-9', 'rhel-8', 'rhel-9', 'debian-11', 'debian-12','ubuntu-focal', 'ubuntu-jammy', 'ubuntu-noble', 'amazon-2']
 }
