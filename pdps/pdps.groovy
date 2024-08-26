ea8bc72e01b38a04c999cb2abad955ed5430e6ee
puneetkaushik <kaushik.puneet07@gmail.com>
2024-08-26 09:04:13 +0530

    update

--- pdps/pdps.groovy
+++ pdps/pdps.groovy
@@ -111,6 +111,7 @@ pipeline {
         stage('Check version param and checkout') {
             steps {
                 deleteDir()
+                checkOrchVersionParam()
                 git poll: false, branch: TESTING_BRANCH, url: "https://github.com/${TESTING_GIT_ACCOUNT}/package-testing.git"
             }
         }
