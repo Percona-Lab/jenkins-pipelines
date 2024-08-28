3dce23b92e3e309cea11be914b7472bc2379f610
Peter Sirotnak <sirotnak.p@gmail.com>
2024-08-28 22:13:53 +0200

    PMM-7: Not move chromium

--- pmm/pmm2-upgrade-tests.groovy
+++ pmm/pmm2-upgrade-tests.groovy
@@ -167,7 +167,6 @@ pipeline {
                         sudo git clone --single-branch --branch ${PMM_QA_GIT_BRANCH} https://github.com/percona/pmm-qa.git .
                         sudo git checkout ${PMM_QA_GIT_COMMIT_HASH}
                     popd
-                    sudo ln -s /usr/bin/chromium-browser /usr/bin/chromium
                 '''
             }
         }
