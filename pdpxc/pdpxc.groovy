fba451276eed5cfe2d4a2f099c7c673e0b6413b6
Yash Panchal <46398416+panchal-yash@users.noreply.github.com>
2024-09-10 11:25:00 +0530

    PXC80 PT mods for min_upgrade (#2864)

--- pdpxc/pdpxc.groovy
+++ pdpxc/pdpxc.groovy
@@ -1,12 +1,12 @@
-library changelog: false, identifier: "lib@master", retriever: modernSCM([
+library changelog: false, identifier: "lib@pdpxc-pt-noble", retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/panchal-yash/jenkins-pipelines.git'
 ])
 
 
 pipeline {
   agent {
-  label 'min-centos-7-x64'
+  label 'min-bookworm-x64'
   }
   environment {
       PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin';
@@ -112,7 +112,7 @@ pipeline {
     stage ('Prepare') {
       steps {
           script {
-              installMolecule()
+              installMoleculeBookworm()
             }
         }
     }
