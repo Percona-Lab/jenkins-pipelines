b37a35755b1133a4c5b72f9ab21308cdd59ff2e0
Vadim Yalovets <vadim.yalovets@percona.com>
2024-09-13 15:17:40 +0300

    RM-1442 PXC-8.4.0

--- pdpxc/percona-replication-manager.groovy
+++ pdpxc/percona-replication-manager.groovy
@@ -225,6 +225,19 @@ pipeline {
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
+                stage('Debian Bookworm(12)') {
+                    agent {
+                        label 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
+                        buildStage("debian:bookworm", "--build_deb=1")
+
+                        pushArtifactFolder("deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
+                    }
+                }
             }
         }
 
