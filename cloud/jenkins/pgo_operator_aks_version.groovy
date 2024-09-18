ae5dbb00c0fc160dceefb06457c0680246011e0b
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2024-09-18 10:33:07 +0300

    Use master branch and send slack notify

--- cloud/jenkins/pgo_operator_aks_version.groovy
+++ cloud/jenkins/pgo_operator_aks_version.groovy
@@ -436,9 +436,9 @@ pipeline {
             archiveArtifacts '*.xml'
 
             script {
-                // if (currentBuild.result != null && currentBuild.result != 'SUCCESS') {
-                //     slackSend channel: '#cloud-dev-ci', color: '#FF0000', message: "[$JOB_NAME]: build $currentBuild.result, $BUILD_URL"
-                // }
+                if (currentBuild.result != null && currentBuild.result != 'SUCCESS') {
+                    slackSend channel: '#cloud-dev-ci', color: '#FF0000', message: "[$JOB_NAME]: build $currentBuild.result, $BUILD_URL"
+                }
 
                 clusters.each { shutdownCluster(it) }
             }
