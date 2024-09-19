2659477659158679e9098914f69247e2e98da232
Talha Bin Rizwan <talha.rizwan@percona.com>
2024-09-19 18:00:47 +0500

    PMM-7 Install awscli to fix release pipeline. (#2885)

--- vars/installDocker.groovy
+++ vars/installDocker.groovy
@@ -3,7 +3,8 @@ def call() {
         ##SYSREL=$(cat /etc/system-release | tr -dc '0-9.'|awk -F'.' {'print $1'})
         sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
         sudo dnf install -y docker-ce docker-ce-cli containerd.io
-        sudo yum -y install git curl
+        sudo dnf install -y git curl python3 python3-pip
+        sudo pip3 install awscli
         sudo usermod -aG docker `id -u -n`
         sudo mkdir -p /etc/docker
         echo '{"experimental": true}' | sudo tee /etc/docker/daemon.json
