0a7e0409322005c53710959a99b17868e9051199
kaushikpuneet07 <puneet.kaushik@percona.com>
2024-10-04 14:12:43 +0530

    update in steps

--- pdps/pdps.groovy
+++ pdps/pdps.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: "lib@master", retriever: modernSCM([
+library changelog: false, identifier: "lib@fix-pd", retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
 ])
 
 
@@ -108,18 +108,18 @@ pipeline {
                 }
             }
         }
-        stage('Check version param and checkout') {
-            steps {
-                deleteDir()
-                checkOrchVersionParam()
-                git poll: false, branch: TESTING_BRANCH, url: "https://github.com/${TESTING_GIT_ACCOUNT}/package-testing.git"
-            }
-        }
         stage ('Prepare') {
             steps {
                 script {
                     installMoleculeBookworm()
                 }
+            }
+        }
+        stage('Check version param and checkout') {
+            steps {
+                deleteDir()
+                checkOrchVersionParam()
+                git poll: false, branch: TESTING_BRANCH, url: "https://github.com/${TESTING_GIT_ACCOUNT}/package-testing.git"
             }
         }
         stage ('Create virtual machines') {
