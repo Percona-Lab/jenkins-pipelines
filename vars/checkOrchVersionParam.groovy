c756184defa215005c5e41b7e4b8689067784bd3
kaushikpuneet07 <puneet.kaushik@percona.com>
2024-10-04 14:08:54 +0530

    update

--- vars/checkOrchVersionParam.groovy
+++ vars/checkOrchVersionParam.groovy
@@ -1,9 +1,9 @@
 def call() {
     sh """
-        if echo "${ORCHESTRATOR_VERSION}" | grep -qE '^[0-9]+(\\.[0-9]+)+\\-[0-9]+'; then
+        if echo "${ORCHESTRATOR_VERSION}" | grep -qE '^[0-9]+(\\.[0-9]+)+-[0-9]+'; then
             echo "ORCHESTRATOR_VERSION ${ORCHESTRATOR_VERSION} has expected pattern"
         else
-            echo "${ORCHESTRATOR_VERSION} is unexpected pattern for ORCHESTRATOR_VERSION. Please use version with the release information like 3.2.6-7"
+            echo "\${ORCHESTRATOR_VERSION} is unexpected pattern for ORCHESTRATOR_VERSION. Please use version with the release information like 3.2.6-7"
             exit 1
         fi
     """
