be32cfb6bf5477ce16436b75e0dbd1d6ba7f2818
kaushikpuneet07 <puneet.kaushik@percona.com>
2024-10-07 19:41:54 +0530

    update in OS

--- vars/pdpsOperatingSystems.groovy
+++ vars/pdpsOperatingSystems.groovy
@@ -1,3 +1,3 @@
 def call() {
-  return ['centos-7',  'oracle-8', 'oracle-9', 'rhel-8', 'rhel-9', 'debian-11', 'debian-12','ubuntu-focal', 'ubuntu-jammy', 'amazon-2', 'ubuntu-noble']
+  return ['oracle-8', 'oracle-9', 'rhel-8', 'rhel-9', 'debian-11', 'debian-12','ubuntu-focal', 'ubuntu-jammy', 'ubuntu-noble']
 }
