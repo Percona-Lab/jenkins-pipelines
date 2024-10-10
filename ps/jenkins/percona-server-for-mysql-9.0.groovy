c3edab9044e23f6d0577c4d9cfafca0fa97adf1e
Vadim Yalovets <vadim.yalovets@percona.com>
2024-10-10 12:26:49 +0300

    PS-9365 Analyze upstream release notes 9.0.0

--- ps/jenkins/percona-server-for-mysql-9.0.groovy
+++ ps/jenkins/percona-server-for-mysql-9.0.groovy
@@ -54,7 +54,7 @@ void buildStage(String DOCKER_OS, String STAGE_PARAM) {
                     . ./test/percona-server-9.0.properties
                 fi
                 bash -x ./ps_builder.sh --builddir=\${build_dir}/test --install_deps=1
-                bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}
+                bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}"
         fi
     """
 }
