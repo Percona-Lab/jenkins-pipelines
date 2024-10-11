89f04ff32fff4deff6c5ddd9474cd559ff580ce6
Naeem Akhter <naeem.akhter@percona.com>
2024-10-12 02:38:52 +0500

    Adding jobs for testing Percona Distribution for Postgres.

--- ppg/pdp-installcheck-world.groovy
+++ ppg/pdp-installcheck-world.groovy
@@ -11,55 +11,68 @@ pipeline {
   parameters {
         choice(
             name: 'PLATFORM',
-            description: 'For what platform (OS) need to test',
+            description: 'For which platform (OS) you want to test?',
             choices: ppgOperatingSystemsALL()
-        )
-        choice(
-            name: 'REPO',
-            description: 'Packages Repo for testing',
-            choices: [
-                'testing',
-                'experimental',
-                'release'
-            ]
-        )
-        string(
-            defaultValue: 'https://github.com/Percona-Lab/pg_tde.git',
-            description: 'TDE repo that we want to test, we could also use forked developer repo here.',
-            name: 'TDE_REPO'
-        )
-        string(
-            defaultValue: 'main',
-            description: 'TDE repo version/branch/tag to use; e.g main, 2.0.5',
-            name: 'TDE_BRANCH'
         )
         string(
             defaultValue: 'ppg-17.0',
             description: 'Server PG version for test, including major and minor version, e.g ppg-16.2, ppg-15.5',
             name: 'VERSION'
         )
-        booleanParam(
-            name: 'TDE_PACKAGE_INSTALL',
-            description: "Select if want to install TDE using native package from repo.percona.com, one shipped with VERSION above."
+        string(
+            defaultValue: 'https://github.com/percona/postgres',
+            description: 'PDP repo that we want to test, we could also use forked developer repo here.',
+            name: 'PDP_REPO'
+        )
+        string(
+            defaultValue: 'TDE_REL_17_STABLE',
+            description: 'PDP repo version/branch/tag to use; e.g main, TDE_REL_17_STABLE',
+            name: 'PDP_BRANCH'
         )
         string(
             defaultValue: 'main',
             description: 'Branch for ppg-testing testing repository',
             name: 'TESTING_BRANCH'
         )
+        choice(
+            name: 'TESTSUITE',
+            description: 'Testsuite to run',
+            choices: [
+                'installcheck',
+                'installcheck-world'
+            ]
+        )
+        choice(
+            name: 'ACCESS_METHOD',
+            description: 'Server access method to use',
+            choices: [
+                'heap',
+                'tde_heap',
+                'tde_heap_basic'
+            ]
+        )
+        booleanParam(
+            name: 'WITH_TDE_HEAP',
+            description: "Do you want TDE_HEAP build and test as part of this run?"
+        )
+        booleanParam(
+            name: 'CHANGE_TDE_BRANCH',
+            description: "Do you want to change TDE branch to other than default one given in PDP? It will only work if WITH_TDE_HEAP option is enabled."
+        )
+        string(
+            defaultValue: 'main',
+            description: 'pg_tde branch to use. It will only work if both options, WITH_TDE_HEAP and CHANGE_TDE_BRANCH, are enabled.',
+            name: 'TDE_BRANCH'
+        )
         string(
             defaultValue: 'yes',
             description: 'Destroy VM after tests',
             name: 'DESTROY_ENV'
         )
-        booleanParam(
-            name: 'MAJOR_REPO',
-            description: "Enable to use major (ppg-16) repo instead of ppg-16.2"
-        )
   }
   environment {
       PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin';
-      MOLECULE_DIR = "pg_tde/tde";
+      MOLECULE_DIR = "pdp/setup";
   }
   options {
           withCredentials(moleculeDistributionJenkinsCreds())
