04fa5aeef930fe743bf5f148c4d84e219fb8a278
Peter Sirotnak <sirotnak.p@gmail.com>
2024-10-23 16:15:54 +0200

    PMM-13422: Change browser for nightly tests

--- pmm/v3/pmm3-ui-tests-nightly.groovy
+++ pmm/v3/pmm3-ui-tests-nightly.groovy
@@ -326,7 +326,7 @@ pipeline {
                             sh """
                                 sed -i 's+http://localhost/+${PMM_UI_URL}/+g' pr.codecept.js
                                 export PWD=\$(pwd);
-                                npx codeceptjs run --reporter mocha-multi -c pr.codecept.js --grep '@qan|@nightly|@menu' --override '{ "helpers": { "Playwright": { "browser": "firefox" }}}'
+                                npx codeceptjs run --reporter mocha-multi -c pr.codecept.js --grep '@qan|@nightly|@menu'
                             """
                         }
                     }
