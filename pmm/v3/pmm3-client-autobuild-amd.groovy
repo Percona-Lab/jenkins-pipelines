b46069cae4dfdaac84d102a13ec937af0799b534
Talha Bin Rizwan <talha.rizwan@percona.com>
2024-10-25 22:27:37 +0500

    PMM-13464 fix pmm client tags (#2931)
    * PMM-13464 Fix pmm client container tags.
    
    * PMM-13464 Fix pmm client v3 container tags

--- pmm/v3/pmm3-client-autobuild-amd.groovy
+++ pmm/v3/pmm3-client-autobuild-amd.groovy
@@ -85,27 +85,25 @@ pipeline {
                 stage('Build client docker') {
                     steps {
                         withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                            withEnv(['PATH_TO_SCRIPTS=' + env.PATH_TO_SCRIPTS, 'DOCKER_RC_TAG=' + env.DOCKER_RC_TAG, 'DOCKER_LATEST_TAG=' + env.DOCKER_LATEST_TAG]) {
-                                sh '''
-                                    echo "${PASS}" | docker login -u "${USER}" --password-stdin
-                                    set -o xtrace
+                            sh '''
+                                echo "${PASS}" | docker login -u "${USER}" --password-stdin
+                                set -o xtrace
 
-                                    ##export PUSH_DOCKER=1
-                                    export DOCKER_CLIENT_TAG=perconalab/pmm-client:$(date -u '+%Y%m%d%H%M')-amd64
+                                ##export PUSH_DOCKER=1
+                                export DOCKER_CLIENT_TAG=perconalab/pmm-client:$(date -u '+%Y%m%d%H%M')-amd64
 
-                                    ${PATH_TO_SCRIPTS}/build-client-docker
+                                ${PATH_TO_SCRIPTS}/build-client-docker
 
-                                    if [ -n "${DOCKER_RC_TAG}" ]; then
-                                        docker tag $DOCKER_CLIENT_TAG perconalab/pmm-client:${DOCKER_RC_TAG}
-                                        docker push perconalab/pmm-client:${DOCKER_RC_TAG}
-                                    else
-                                        docker tag $DOCKER_CLIENT_TAG perconalab/pmm-client:${DOCKER_LATEST_TAG}
-                                        docker push perconalab/pmm-client:${DOCKER_LATEST_TAG}
-                                    fi
+                                if [ -n "${DOCKER_RC_TAG}" ]; then
+                                    docker tag $DOCKER_CLIENT_TAG perconalab/pmm-client:${DOCKER_RC_TAG}
+                                    docker push perconalab/pmm-client:${DOCKER_RC_TAG}
+                                else
+                                    docker tag $DOCKER_CLIENT_TAG perconalab/pmm-client:${DOCKER_LATEST_TAG}
+                                    docker push perconalab/pmm-client:${DOCKER_LATEST_TAG}
+                                fi
 
-                                    docker push $DOCKER_CLIENT_TAG
-                                '''
-                            }
+                                docker push $DOCKER_CLIENT_TAG
+                            '''
                         }
                     }
                 }
