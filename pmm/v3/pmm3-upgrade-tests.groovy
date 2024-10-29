f2d77c331b1c139943a4c0604288cd12d848ca5f
Peter Sirotnak <sirotnak.p@gmail.com>
2024-10-29 10:54:08 +0100

    PMM-7: Change watchtower tag

--- pmm/v3/pmm3-upgrade-tests.groovy
+++ pmm/v3/pmm3-upgrade-tests.groovy
@@ -209,7 +209,7 @@ pipeline {
                         -e WATCHTOWER_HTTP_API_UPDATE=1 \
                         --volume /var/run/docker.sock:/var/run/docker.sock \
                         --name watchtower \
-                        perconalab/watchtower:PMM-13202-fix-double-unlock
+                        perconalab/watchtower:latest
 
                     sleep 10
 
