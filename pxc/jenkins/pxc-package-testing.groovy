3b32afa713323692a08df9a00875be64cfde3e99
Yash Panchal <46398416+panchal-yash@users.noreply.github.com>
2024-11-04 23:01:20 +0530

    Fix typo in pxc-package-testing.groovy (#2941)

--- pxc/jenkins/pxc-package-testing.groovy
+++ pxc/jenkins/pxc-package-testing.groovy
@@ -516,7 +516,7 @@ def setup(){
                 installMoleculeBookworm()
                     sh '''
                         rm -rf package-testing                    
-                        git clone https://github.com/Percona-Lab/package-testing --branch master
+                        git clone https://github.com/Percona-QA/package-testing --branch master
                     '''
 }
 
