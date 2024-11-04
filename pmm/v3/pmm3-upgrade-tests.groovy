9bf6af52de4c9a15d251fd00f762ef2dc88c2a83
Peter Sirotnak <sirotnak.p@gmail.com>
2024-11-04 18:18:19 +0100

    PMM-7: upgrade job pipeline

---
+++ pmm/v3/pmm3-upgrade-tests.groovy
@@ -0,0 +1,82 @@
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+]) _
+
+void runUpgradeJob(String PMM_UI_GIT_BRANCH, PMM_VERSION, PMM_SERVER_LATEST, ENABLE_TESTING_REPO, ENABLE_EXPERIMENTAL_REPO, PERFORM_DOCKER_WAY_UPGRADE, PMM_SERVER_TAG) {
+    upgradeJob = build job: 'pmm3-upgrade-test-runner', parameters: [
+        string(name: 'PMM_UI_GIT_BRANCH', value: PMM_UI_GIT_BRANCH),
+        string(name: 'DOCKER_TAG', value: DOCKER_TAG),
+        string(name: 'CLIENT_VERSION', value: CLIENT_VERSION),
+        string(name: 'PMM_SERVER_LATEST', value: PMM_SERVER_LATEST),
+        string(name: 'PMM_QA_GIT_BRANCH', value: PMM_QA_GIT_BRANCH),
+        string(name: 'QA_INTEGRATION_GIT_BRANCH', value: QA_INTEGRATION_GIT_BRANCH),
+        string(name: 'UPGRADE_FLAG', value: UPGRADE_FLAG)
+    ]
+}
+
+pipeline {
+    agent {
+        label 'cli'
+    }
+    parameters {
+        string(
+            defaultValue: 'PMM-7-upgrade-job-pmm3',
+            description: 'Tag/Branch for UI Tests repository',
+            name: 'PMM_UI_GIT_BRANCH')
+        string(
+            defaultValue: 'perconalab/pmm-server:202410302129',
+            description: 'PMM Server Version to test for Upgrade',
+            name: 'DOCKER_TAG')
+        string(
+            defaultValue: "3-dev-latest",
+            description: 'PMM Client Version to test for Upgrade',
+            name: 'CLIENT_VERSION')
+        choice(
+            choices: ["3.0.0"],
+            description: 'latest PMM Server Version',
+            name: 'PMM_SERVER_LATEST')
+        string(
+            defaultValue: 'PMM-13481',
+            description: 'Tag/Branch for qa-integration repository',
+            name: 'PMM_QA_GIT_BRANCH')
+        string(
+            defaultValue: 'v3',
+            description: 'Tag/Branch for qa-integration repository',
+            name: 'QA_INTEGRATION_GIT_BRANCH')
+        choice(
+            choices: ["SSL", "EXTERNAL SERVICES", "MONGO BACKUP", "CUSTOM PASSWORD", "CUSTOM DASHBOARDS", "ANNOTATIONS-PROMETHEUS", "ADVISORS-ALERTING", "SETTINGS-METRICS"],
+            description: 'Subset of tests for the upgrade',
+            name: 'UPGRADE_FLAG')
+    }
+    options {
+        disableConcurrentBuilds()
+    }
+    triggers {
+        cron('0 3 * * *')
+    }
+    stages{
+        stage('UI tests Upgrade Matrix') {
+            parallel {
+                stage('Run SSL upgrade tests tests'){
+                    steps {
+                        script {
+                            runUITestsJob(GIT_BRANCH, GIT_COMMIT_HASH, DOCKER_VERSION, CLIENT_VERSION, '@ia', MYSQL_IMAGE, POSTGRES_IMAGE, MONGO_IMAGE, PROXYSQL_IMAGE, PMM_QA_GIT_BRANCH, '--addclient=ms,1');
+                        }
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            script {
+                if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
+                    slackSend channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build finished - ${BUILD_URL} "
+                } else {
+                    slackSend channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result} - ${BUILD_URL}"
+                }
+            }
+        }
+    }
+}
