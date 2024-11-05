acdcab443c097e59d425b7a2584e3a6b95612086
Pavel Tankov <4014969+ptankov@users.noreply.github.com>
2024-11-05 16:10:48 +0200

    Refactor code to remove cluster shutdown in psmdb_operator_minikube.groovy

--- cloud/jenkins/psmdb_operator_minikube.groovy
+++ cloud/jenkins/psmdb_operator_minikube.groovy
@@ -396,8 +396,6 @@ pipeline {
                 if (currentBuild.result != null && currentBuild.result != 'SUCCESS') {
                     slackSend channel: '#cloud-dev-ci', color: '#FF0000', message: "[$JOB_NAME]: build $currentBuild.result, $BUILD_URL"
                 }
-
-                clusters.each { shutdownCluster(it) }
             }
 
             sh """
