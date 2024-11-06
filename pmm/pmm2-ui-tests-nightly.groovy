6d18f6f5f0f241be142cb0c0a66c5554a16e7b8e
Peter Sirotnak <sirotnak.p@gmail.com>
2024-11-06 14:22:56 +0100

    PMM-13422: Remove debug commands

--- pmm/pmm2-ui-tests-nightly.groovy
+++ pmm/pmm2-ui-tests-nightly.groovy
@@ -365,9 +365,7 @@ pipeline {
         }
         stage('Sanity check') {
             steps {
-                sh """
-                    timeout 100 bash -c \'while [[ "$(curl -s -o /dev/null -w \'\'%{http_code}\'\' \${PMM_URL}/ping)" != "200" ]]; do sleep 5; done\' || false
-                """
+                sh 'timeout 100 bash -c \'while [[ "$(curl -s -o /dev/null -w \'\'%{http_code}\'\' \${PMM_URL}/ping)" != "200" ]]; do sleep 5; done\' || false'
             }
         }
         stage('Setup Node') {
