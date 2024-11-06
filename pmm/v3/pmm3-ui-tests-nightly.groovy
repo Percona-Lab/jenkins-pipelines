f72d92922039db6c1d729d4a259933e4efd09c97
Peter Sirotnak <sirotnak.p@gmail.com>
2024-11-06 10:25:23 +0100

    PMM-13422: Refactors nightly for AMI

--- pmm/v3/pmm3-ui-tests-nightly.groovy
+++ pmm/v3/pmm3-ui-tests-nightly.groovy
@@ -295,7 +295,7 @@ pipeline {
                     sh """
                         sed -i 's+http://localhost/+${PMM_UI_URL}/+g' pr.codecept.js
                         export PWD=\$(pwd);
-                        npx codeceptjs run --reporter mocha-multi -c pr.codecept.js --grep 'Test AMI login with ID.'
+                        npx codeceptjs run --reporter mocha-multi -c pr.codecept.js --grep 'Test AMI login with ID'
                     """
                 }
             }
