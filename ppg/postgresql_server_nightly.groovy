7bdb709cf1e8bec23182f041f531639f88a6b137
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-11-08 13:31:44 +0500

    Nightly build of PG 17  (#2955)
    * Revert "[PKG-250]: Build when there is a commit in pg_tde repo. (#2954)"
    
    This reverts commit 603fc6419ffa2ad19edd4e90224e18fd1cc79fe3.
    
    * [PKG-250]: Reverted changes to build pg_tde with server nightly builds. It was not required.

--- ppg/postgresql_server_nightly.groovy
+++ ppg/postgresql_server_nightly.groovy
@@ -8,7 +8,6 @@ void buildStage(String DOCKER_OS, String STAGE_PARAM) {
         set -o xtrace
         mkdir test
         wget \$(echo ${GIT_REPO} | sed -re 's|github.com|raw.githubusercontent.com|; s|\\.git\$||')/${GIT_BRANCH}/postgres/ppg_builder.sh -O ppg-server_builder.sh
-        sed -i 's|#git submodule update --init|git submodule update --init|g' ppg-server_builder.sh
         pwd -P
         ls -laR
         export build_dir=\$(pwd -P)
@@ -73,7 +72,7 @@ pipeline {
             steps {
                 script {
                     // Fetch the latest commit hash from the repository
-                    def latestCommit = sh(returnStdout: true, script: "git ls-remote https://github.com/percona/pg_tde.git main").split()[0]
+                    def latestCommit = sh(returnStdout: true, script: "git ls-remote https://github.com/Percona-Lab/postgres.git ${PG_BRANCH}").split()[0]
                     def lastBuildCommit = currentBuild.previousBuild?.description
 
                     if (latestCommit == lastBuildCommit) {
