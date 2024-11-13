b3b52433f0ed80ad7bac008c6ff515f5645145e9
Peter Sirotnak <sirotnak.p@gmail.com>
2024-11-13 08:05:26 +0100

    PMM-7: Newer version of pmm server

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -88,7 +88,7 @@ pipeline {
             description: 'Tag/Branch for UI Tests repository',
             name: 'PMM_UI_GIT_BRANCH')
         string(
-            defaultValue: 'perconalab/pmm-server:202410302129',
+            defaultValue: 'perconalab/pmm-server:202411121553',
             description: 'PMM Server Version to test for Upgrade',
             name: 'DOCKER_TAG')
         string(
