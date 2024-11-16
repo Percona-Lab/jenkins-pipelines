9c6b34d468c45f375ff1155a68a183d13c5f8438
Talha Bin Rizwan <talha.rizwan@percona.com>
2024-11-16 09:42:02 +0500

    PMM-13531 Replace Lacework with Snyk for image scanning in pipelines.

--- pmm/v3/pmm3-image-scanning.groovy
+++ pmm/v3/pmm3-image-scanning.groovy
@@ -1,5 +1,5 @@
 Jenkins.instance.getItemByFullName(env.JOB_NAME).description = '''
-This job helps run an image scan with LaceWork (security and best practice)
+This job helps run an image scan with Snyk (security and best practice)
 '''
 
 pipeline {
@@ -7,8 +7,8 @@ pipeline {
         label 'agent-amd64-ol9'
     }
     environment {
-        LW_ACCOUNT_NAME=credentials('LW_ACCOUNT_NAME')
-        LW_ACCESS_TOKEN=credentials('LW_ACCESS_TOKEN')
+        PATH = "${WORKSPACE}/node_modules/.bin:$PATH" // Add local npm bin to PATH
+        SNYK_TOKEN=credentials('SNYK_TOKEN')
     }
     parameters {
         string(
@@ -21,11 +21,27 @@ pipeline {
             name: 'TAG')
     }
     stages {
-        stage('Scan docker image') {
+        stage('Install Snyk CLI') {
             steps {
-                sh "lw-scanner image evaluate ${IMAGE} ${TAG} --html --html-file report.html --data-directory ."
-                archiveArtifacts 'report.html'
-                archiveArtifacts 'evaluations/**/**/evaluation_*.json'
+                script {
+                    sh '''
+                        curl -sL https://static.snyk.io/cli/latest/snyk-linux -o snyk
+                        chmod +x snyk
+                        npm install snyk-to-html
+                    '''
+                }
+            }
+        }
+        stage('Scan Image') {
+            steps {
+                script {
+                    def status = sh(
+                        script: "./snyk container test --severity-threshold=high --json-file-output=report.json ${params.IMAGE}:${params.TAG}",
+                        returnStatus: true
+                    )
+                    sh 'snyk-to-html -i report.json -o report.html'
+                    archiveArtifacts artifacts: 'report.html', allowEmptyArchive: true
+                }
             }
         }
     }
