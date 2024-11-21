a3963b36b82b23d9c3a82d09ebefefd0589bd6fb
kaushikpuneet07 <puneet.kaushik@percona.com>
2024-11-21 11:56:40 +0530

    update

---
+++ vars/installMoleculeBookworm_pro.groovy
@@ -0,0 +1,13 @@
+def call() {
+        sh """
+            sudo apt install -y gcc python3-pip python3-devel libselinux-python3
+            sudo apt remove ansible -y
+            python3 -m venv virtenv
+            . virtenv/bin/activate
+            python3 --version
+            python3 -m pip install --upgrade pip
+            python3 -m pip install --upgrade setuptools
+            python3 -m pip install --upgrade setuptools-rust
+            python3 -m pip install --upgrade molecule==3.3.0 testinfra pytest molecule-ec2==0.3 molecule[ansible] boto3 boto
+        """
+}
