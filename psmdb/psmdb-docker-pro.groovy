2633649c8977631b0c1c41491e8eef26cf1cc198
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2024-11-22 15:52:39 +0200

    PSMDB-1553 psmdb.cd: add push step for psmdb pro dockers

--- psmdb/psmdb-docker-pro.groovy
+++ psmdb/psmdb-docker-pro.groovy
@@ -5,15 +5,15 @@ library changelog: false, identifier: "lib@PSMDB-1553_build_psmdb_pro_dockers", 
 
 pipeline {
     agent {
-        label 'docker-32gb'
+        label 'docker'
     }
     environment {
         PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin'
     }
     parameters {
-        choice(name: 'PSMDB_REPO', choices: ['testing','release','experimental'], description: 'Percona-release repo')
-        string(name: 'PSMDB_VERSION', defaultValue: '6.0.2-1', description: 'PSMDB version')
-        choice(name: 'TARGET_REPO', choices: ['PerconaLab','AWS_ECR','DockerHub'], description: 'Target repo for docker image, use DockerHub for release only')
+        choice(name: 'PSMDB_REPO', choices: ['release','testing','experimental'], description: 'percona-release repo to take packages from')
+        string(name: 'PSMDB_VERSION', defaultValue: '6.0.18-15', description: 'PSMDB version, for example: 6.0.18-15 or 7.0.14-8')
+        string(name: 'TICKET_NAME', defaultValue: '', description: 'Target folder for docker image')
     }
     options {
         disableConcurrentBuilds()
@@ -43,8 +43,6 @@ pipeline {
                         sed -E "s|(enable psmdb-[0-9]{2}-pro)|\\1 --user_name='${USERNAME}' --repo_token='${PASSWORD}'|" -i Dockerfile
                         sed -E "s|(repo\\.percona\\.com/)(psmdb-[0-9]{2}-pro)|\\1private/'${USERNAME}'-'${PASSWORD}'/\\2|" -i Dockerfile
                         docker build . -t percona-server-mongodb-pro:${params.PSMDB_VERSION}
-                        docker save -o percona-server-mongodb-pro-${params.PSMDB_VERSION}.tar percona-server-mongodb-pro:${params.PSMDB_VERSION}
-                        gzip percona-server-mongodb-pro-${params.PSMDB_VERSION}.tar
                     """
                 }
             }
@@ -58,6 +56,7 @@ pipeline {
                     TRIVY_VERSION=\$(curl --silent 'https://api.github.com/repos/aquasecurity/trivy/releases/latest' | grep '"tag_name":' | tr -d '"' | sed -E 's/.*v(.+),.*/\\1/')
                     wget https://github.com/aquasecurity/trivy/releases/download/v\${TRIVY_VERSION}/trivy_\${TRIVY_VERSION}_Linux-64bit.tar.gz
                     sudo tar zxvf trivy_\${TRIVY_VERSION}_Linux-64bit.tar.gz -C /usr/local/bin/
+                    rm trivy_\${TRIVY_VERSION}_Linux-64bit.tar.gz
                     wget https://raw.githubusercontent.com/aquasecurity/trivy/v\${TRIVY_VERSION}/contrib/junit.tpl
                     curl https://raw.githubusercontent.com/Percona-QA/psmdb-testing/main/docker/trivyignore -o ".trivyignore"
                     if [ ${params.PSMDB_REPO} = "release" ]; then
@@ -80,6 +79,18 @@ pipeline {
                 always {
                     junit testResults: "*-junit.xml", keepLongStdio: true, allowEmptyResults: true, skipPublishingChecks: true
                 }
+            }
+        }
+        stage ('Push image to repo.ci') {
+            steps {
+                sh """
+                    docker save -o percona-server-mongodb-pro-${params.PSMDB_VERSION}.tar percona-server-mongodb-pro:${params.PSMDB_VERSION}
+                    gzip percona-server-mongodb-pro-${params.PSMDB_VERSION}.tar
+                    echo "UPLOAD/experimental/CUSTOM/${TICKET_NAME}/${JOB_NAME}/${BUILD_NUMBER}" > uploadPath
+                """
+                stash includes: 'uploadPath', name: 'uploadPath'
+                stash includes: '*.tar.*', name: 'docker.tarball'
+                uploadTarball('docker')
             }
         }
     }
