cf2be76c6f6bf012e23455dfa241723db43de50b
Vadim Yalovets <vadim.yalovets@percona.com>
2024-11-22 17:23:23 +0200

    PKG-242 Create packages for PS 5.7 EOL

--- ps/jenkins/percona-server-for-mysql-5.7.groovy
+++ ps/jenkins/percona-server-for-mysql-5.7.groovy
@@ -42,7 +42,7 @@ void buildStage(String DOCKER_OS, String STAGE_PARAM) {
             set -o xtrace
             mkdir -p test
             wget --header="Authorization: token ${TOKEN}" --header="Accept: application/vnd.github.v3.raw" -O ps_builder.sh \$(echo ${GIT_REPO} | sed -re 's|github.com|api.github.com/repos|; s|\\.git\$||')/contents/build-ps/percona-server-5.7_builder.sh?ref=${BRANCH}
-            sed -i "s|git clone \"\\$REPO\"|git clone \$(echo ${GIT_REPO}| sed -re 's|github.com|${TOKEN}@github.com|') percona-server|g" ps_builder.sh
+            sed -i "s|git clone \"\\\$REPO\"|git clone \$(echo ${GIT_REPO}| sed -re 's|github.com|${TOKEN}@github.com|') percona-server|g" ps_builder.sh
             grep "git clone" ps_builder.sh
             pwd -P
             export build_dir=\$(pwd -P)
@@ -70,8 +70,8 @@ pipeline {
         label 'docker'
     }
 parameters {
-        string(defaultValue: 'https://github.com/percona/percona-server.git', description: 'github repository for build', name: 'GIT_REPO')
-        string(defaultValue: 'release-5.7.32-35', description: 'Tag/Branch for percona-server repository', name: 'BRANCH')
+        string(defaultValue: 'https://github.com/percona/percona-server-private.git', description: 'github repository for build', name: 'GIT_REPO')
+        string(defaultValue: 'release-5.7.44-52', description: 'Tag/Branch for percona-server repository', name: 'BRANCH')
         string(defaultValue: '0', description: 'PerconaFT repository', name: 'PERCONAFT_REPO')
         string(defaultValue: 'Percona-Server-5.7.32-35', description: 'Tag/Branch for PerconaFT repository', name: 'PERCONAFT_BRANCH')
         string(defaultValue: '0', description: 'TokuBackup repository', name: 'TOKUBACKUP_REPO')
