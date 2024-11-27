1d5789b3ab60d3ff469c8bfb7f6657fa873b07bb
Saikumar <saikumar.vs@percona.com>
2024-11-27 10:07:34 +0530

    Updates syntax

--- pmm/pmm2-ui-tests.groovy
+++ pmm/pmm2-ui-tests.groovy
@@ -287,11 +287,11 @@ pipeline {
         stage('Setup Client for PMM-Server') {
             steps {
                 setupPMMClient(env.SERVER_IP, CLIENT_VERSION, 'pmm2', ENABLE_PULL_MODE, 'no', 'yes', 'compose_setup', ADMIN_PASSWORD)
+                env.SERVER_IP = sh(script: 'curl -s ifconfig.me', returnStdout: true).trim()
                 sh """
                     set -o errexit
                     set -o xtrace
                     export PATH=\$PATH:/usr/sbin
-                    export SERVER_IP=$(curl -s ifconfig.me)
                     if [[ \$CLIENT_VERSION != dev-latest ]]; then
                         export PATH="`pwd`/pmm2-client/bin:$PATH"
                     fi
