dd61402316a50bc63e5134cc31b891d69b57dda3
saikumar-vs <112166980+saikumar-vs@users.noreply.github.com>
2024-11-28 14:36:49 +0530

    Updates to fix server_ip (#3007)
    * Updates to fix server_ip

--- pmm/pmm2-ui-tests.groovy
+++ pmm/pmm2-ui-tests.groovy
@@ -287,6 +287,9 @@ pipeline {
         stage('Setup Client for PMM-Server') {
             steps {
                 setupPMMClient(env.SERVER_IP, CLIENT_VERSION, 'pmm2', ENABLE_PULL_MODE, 'no', 'yes', 'compose_setup', ADMIN_PASSWORD)
+                  script {
+                      env.SERVER_IP = "pmm-server"
+                  }
                 sh """
                     set -o errexit
                     set -o xtrace
