35e2eeee1a9279dc5d3b281dfbb51a2274bbe0fd
EvgeniyPatlan <evgeniy.patlan@percona.com>
2024-12-03 10:40:47 +0200

    Fix

--- pbm/pbm-docker-nightly.groovy
+++ pbm/pbm-docker-nightly.groovy
@@ -11,9 +11,9 @@ pipeline {
         PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin'
     }
     parameters {
-        choice(name: 'PBM_REPO_CH', choices: ['testing','release','experimental'], description: 'Percona-release repo')
+        string(name: 'PBM_REPO_CH', defaultValue: 'experimental', description: 'Percona-release repo')
         string(name: 'PBM_VERSION', defaultValue: '2.0.2-1', description: 'PBM version')
-        choice(name: 'TARGET_REPO', choices: ['PerconaLab','AWS_ECR'                                        ], description: 'Target repo for docker image, use DockerHub for release only')
+        string(name: 'TARGET_REPO', defaultValue: 'PerconaLab', description: 'Target repo for docker image, use DockerHub for release only')
     }
     options {
         disableConcurrentBuilds()
@@ -22,21 +22,15 @@ pipeline {
         stage('Set build name'){
             steps {
                 script {
-                    currentBuild.displayName = "${params.PBM_REPO_CH}-${params.PBM_VERSION}"
+                    currentBuild.displayName = "${params.PBM_REPO_CH}-nightly"
                 }
             }
         }
         stage ('Build image') {
             steps {
                 sh """
-                    MAJ_VER=\$(echo ${params.PBM_VERSION} | awk -F "." '{print \$1}')
-                    echo \$MAJ_VER
-                    MIN_VER=\$(echo ${params.PBM_VERSION} | awk -F "-" '{print \$1}')
-                    echo \$MIN_VER
                     git clone https://github.com/percona/percona-docker
                     cd percona-docker/percona-backup-mongodb
-                    sed -E "s/ENV PBM_VERSION (.+)/ENV PBM_VERSION ${params.PBM_VERSION}/" -i Dockerfile
-                    sed -E "s/ENV PBM_REPO_CH (.+)/ENV PBM_REPO_CH ${params.PBM_REPO_CH}/" -i Dockerfile
                     docker build . -t percona-backup-mongodb 
                     """
             }
@@ -49,36 +43,13 @@ pipeline {
                     sudo tar zxvf trivy_\${TRIVY_VERSION}_Linux-64bit.tar.gz -C /usr/local/bin/
                     wget https://raw.githubusercontent.com/aquasecurity/trivy/v\${TRIVY_VERSION}/contrib/junit.tpl
                     curl https://raw.githubusercontent.com/Percona-QA/psmdb-testing/main/docker/trivyignore -o ".trivyignore"
-                    if [ ${params.PBM_REPO_CH} = "release" ]; then
-                        /usr/local/bin/trivy -q image --format template --template @junit.tpl  -o trivy-hight-junit.xml \
-                                         --timeout 10m0s --ignore-unfixed --exit-code 1 --severity HIGH,CRITICAL percona-backup-mongodb
-                    else
                         /usr/local/bin/trivy -q image --format template --template @junit.tpl  -o trivy-hight-junit.xml \
                                          --timeout 10m0s --ignore-unfixed --exit-code 0 --severity HIGH,CRITICAL percona-backup-mongodb
-                    fi
                """
             }
             post {
                 always {
                     junit testResults: "*-junit.xml", keepLongStdio: true, allowEmptyResults: true, skipPublishingChecks: true
-                }
-            }
-        }
-        stage ('Push image to aws ecr') {
-            when {
-                environment name: 'TARGET_REPO', value: 'AWS_ECR'
-            }
-            steps {
-                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: '8468e4e0-5371-4741-a9bb-7c143140acea', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-                     sh """
-                         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-                         if [ -f "/usr/bin/yum" ] ; then sudo yum install -y unzip ; else sudo apt-get update && apt-get -y install unzip ; fi
-                         unzip -o awscliv2.zip
-                         sudo ./aws/install
-                         aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/e7j3v3n0
-                         docker tag percona-backup-mongodb public.ecr.aws/e7j3v3n0/psmdb-build:pbm-nightly
-                         docker push public.ecr.aws/e7j3v3n0/psmdb-build:pbm-nightly
-                     """
                 }
             }
         }
@@ -90,8 +61,6 @@ pipeline {
                 withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
                      sh """
                          docker login -u '${USER}' -p '${PASS}'
-                         MAJ_VER=\$(echo ${params.PBM_VERSION} | awk -F "." '{print \$1}')
-                         MIN_VER=\$(echo ${params.PBM_VERSION} | awk -F "-" '{print \$1}')
                          docker tag percona-backup-mongodb perconalab/percona-backup-mongodb:nightly
                          docker push perconalab/percona-backup-mongodb:nightly
                          docker tag percona-backup-mongodb perconalab/percona-backup-mongodb:nightly
@@ -110,13 +79,13 @@ pipeline {
             deleteDir()
         }
         success {
-            slackNotify("#mongodb_autofeed", "#00FF00", "[${JOB_NAME}]: Building of PBM ${PBM_VERSION} repo ${PBM_REPO_CH} succeed")
+            slackNotify("#mongodb_autofeed", "#00FF00", "[${JOB_NAME}]: Building of PBM nightly repo ${PBM_REPO_CH} succeed")
         }
         unstable {
-            slackNotify("#mongodb_autofeed", "#F6F930", "[${JOB_NAME}]: Building of PBM ${PBM_VERSION} repo ${PBM_REPO_CH} unstable - [${BUILD_URL}testReport/]")
+            slackNotify("#mongodb_autofeed", "#F6F930", "[${JOB_NAME}]: Building of PBM nightly repo ${PBM_REPO_CH} unstable - [${BUILD_URL}testReport/]")
         }
         failure {
-            slackNotify("#mongodb_autofeed", "#FF0000", "[${JOB_NAME}]: Building of PBM ${PBM_VERSION} repo ${PBM_REPO_CH} failed - [${BUILD_URL}]")
+            slackNotify("#mongodb_autofeed", "#FF0000", "[${JOB_NAME}]: Building of PBM nightly repo ${PBM_REPO_CH} failed - [${BUILD_URL}]")
         }
     }
 }
