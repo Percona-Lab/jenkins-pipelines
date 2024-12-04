f4ddaba5a5f814b7e346c785ed76074c24989705
Grishma123-Eng <grishmashah121@gmail.com>
2024-12-04 15:53:02 +0530

    adding parameter for versions

--- ps/jenkins/test-ps-innodb-cluster-parallel-test-grishma.groovy
+++ ps/jenkins/test-ps-innodb-cluster-parallel-test-grishma.groovy
@@ -56,6 +56,11 @@ pipeline {
 
     parameters {
         choice(
+            choices: ['PS80','PS84','PS_LTS_INN'],
+            description: 'Product for which the packages will be tested',
+            name: 'PRODUCT_TO_TEST'
+        )
+        choice(
             name: 'TEST_DIST',
             choices: [
                 'all',
