2646e1acd4cc88087c7bf98ccd1ee30945fe4a8c
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2024-12-10 20:40:29 +0200

    PSMDB-1553 build psmdb pro dockers
    1. get trivy according to arch
    2. add path to docker archive to build description

--- psmdb/psmdb-docker-pro.groovy
+++ psmdb/psmdb-docker-pro.groovy
@@ -3,6 +3,8 @@ library changelog: false, identifier: "lib@PSMDB-1553_build_psmdb_pro_dockers", 
     remote: 'https://github.com/vorsel/jenkins-pipelines.git'
 ])
 
+def REPO_CI_PATH
+
 pipeline {
     agent {
         label params.ARCH == 'x86_64' ? 'docker' : 'docker-64gb-aarch64'
@@ -46,8 +48,6 @@ pipeline {
                         echo \$OS_VER_NUM
                         PKG_URL="http://repo.percona.com/private/${USERNAME}-${PASSWORD}/psmdb-\$REPO_VER-pro/yum/${params.PSMDB_REPO}/\${OS_VER_NUM}/RPMS/${ARCH}/"
                         echo \$PKG_URL
-                        ARCH=\$(echo \$PKG_URL | awk -F "/" '{print \$(NF-1)}')
-                        echo \$ARCH
                         sed -E "/curl -Lf -o \\/tmp\\/Percona-Server-MongoDB-server.rpm/d" -i Dockerfile
                         sed -E "/percona-server-mongodb-mongos-\${FULL_PERCONA_VERSION}/d" -i Dockerfile
                         sed -E "/percona-server-mongodb-tools-\${FULL_PERCONA_VERSION}/d" -i Dockerfile
@@ -55,18 +55,15 @@ pipeline {
                         sed -E "s|rpm -iv /tmp/Percona-Server-MongoDB-server.rpm --nodeps;|dnf -y install /tmp/Percona-Server-MongoDB-mongos-pro.rpm /tmp/Percona-Server-MongoDB-tools.rpm; rpm -iv /tmp/Percona-Server-MongoDB-server-pro.rpm --nodeps;|" -i Dockerfile
                         sed -E "s|rm -rf /tmp/Percona-Server-MongoDB-server.rpm|rm -rf /tmp/*.rpm|" -i Dockerfile
                         sed -E "s|server/LICENSE|server-pro/LICENSE|" -i Dockerfile
-                        cat Dockerfile
 
-                        #sed -E "s|(psmdb-[0-9]{2})|\\1-pro|g" -i Dockerfile
-                        #sed -E 's:(percona-server-mongodb-(server|mongos)):\\1-pro:g' -i Dockerfile
-                        #sed -E "s|(enable psmdb-[0-9]{2}-pro)|\\1 --user_name='${USERNAME}' --repo_token='${PASSWORD}'|" -i Dockerfile
-                        #sed -E "s|(repo\\.percona\\.com/)(psmdb-[0-9]{2}-pro)|\\1private/'${USERNAME}'-'${PASSWORD}'/\\2|" -i Dockerfile
                         mkdir -p packages
                         curl -Lf -o ./packages/Percona-Server-MongoDB-server-pro.rpm \$PKG_URL/percona-server-mongodb-server-pro-${params.PSMDB_VERSION}.\$OS_VER.${ARCH}.rpm
                         curl -Lf -o ./packages/Percona-Server-MongoDB-mongos-pro.rpm \$PKG_URL/percona-server-mongodb-mongos-pro-${params.PSMDB_VERSION}.\$OS_VER.${ARCH}.rpm
                         curl -Lf -o ./packages/Percona-Server-MongoDB-tools.rpm \$PKG_URL/percona-server-mongodb-tools-${params.PSMDB_VERSION}.\$OS_VER.${ARCH}.rpm
                         sed -E '/^ARG PERCONA_TELEMETRY_DISABLE/a \\\nCOPY packages/*.rpm /tmp/' -i Dockerfile
+                        cat Dockerfile
                         docker build . -t percona-server-mongodb-pro:${params.PSMDB_VERSION}
+                        rm -rf packages
                     """
                 }
             }
@@ -78,9 +75,14 @@ pipeline {
                try {
                 sh """
                     TRIVY_VERSION=\$(curl --silent 'https://api.github.com/repos/aquasecurity/trivy/releases/latest' | grep '"tag_name":' | tr -d '"' | sed -E 's/.*v(.+),.*/\\1/')
-                    wget https://github.com/aquasecurity/trivy/releases/download/v\${TRIVY_VERSION}/trivy_\${TRIVY_VERSION}_Linux-64bit.tar.gz
-                    sudo tar zxvf trivy_\${TRIVY_VERSION}_Linux-64bit.tar.gz -C /usr/local/bin/
-                    rm trivy_\${TRIVY_VERSION}_Linux-64bit.tar.gz
+                    if [[ ${ARCH} == "x86_64" ]]; then
+                        TRIVY_ARCH="64bit"
+                    elif [[ ${ARCH} == "aarch64" ]]; then
+                        TRIVY_ARCH="ARM64"
+                    fi
+                    wget https://github.com/aquasecurity/trivy/releases/download/v\${TRIVY_VERSION}/trivy_\${TRIVY_VERSION}_Linux-\${TRIVY_ARCH}.tar.gz
+                    sudo tar zxvf trivy_\${TRIVY_VERSION}_Linux-\${TRIVY_ARCH}.tar.gz -C /usr/local/bin/
+                    rm trivy_\${TRIVY_VERSION}_Linux-\${TRIVY_ARCH}.tar.gz
                     wget https://raw.githubusercontent.com/aquasecurity/trivy/v\${TRIVY_VERSION}/contrib/junit.tpl
                     curl https://raw.githubusercontent.com/Percona-QA/psmdb-testing/main/docker/trivyignore -o ".trivyignore"
                     if [ ${params.PSMDB_REPO} = "release" ]; then
@@ -112,6 +114,9 @@ pipeline {
                     gzip percona-server-mongodb-pro-${params.PSMDB_VERSION}.tar
                     echo "UPLOAD/experimental/CUSTOM/${TICKET_NAME}/${JOB_NAME}/${BUILD_NUMBER}" > uploadPath
                 """
+                script {
+                    REPO_CI_PATH = sh(returnStdout: true, script: "cat uploadPath").trim()
+                }
                 stash includes: 'uploadPath', name: 'uploadPath'
                 stash includes: '*.tar.*', name: 'docker.tarball'
                 uploadTarball('docker')
@@ -128,12 +133,21 @@ pipeline {
         }
         success {
             slackNotify("@alex.miroshnychenko", "#00FF00", "[${JOB_NAME}]: Building of PSMDB ${PSMDB_VERSION} repo ${PSMDB_REPO} succeed")
+            script {
+                currentBuild.description = "Built on ${PSMDB_VERSION}. Path to packages: ${REPO_CI_PATH}"
+            }
         }
         unstable {
             slackNotify("@alex.miroshnychenko", "#F6F930", "[${JOB_NAME}]: Building of PSMDB ${PSMDB_VERSION} repo ${PSMDB_REPO} unstable - [${BUILD_URL}testReport/]")
+            script {
+                currentBuild.description = "Built on ${PSMDB_VERSION}. Path to packages: ${REPO_CI_PATH}"
+            }
         }
         failure {
             slackNotify("@alex.miroshnychenko", "#FF0000", "[${JOB_NAME}]: Building of PSMDB ${PSMDB_VERSION} repo ${PSMDB_REPO} failed - [${BUILD_URL}]")
+            script {
+                currentBuild.description = "Built on ${PSMDB_VERSION}. Path to packages: ${REPO_CI_PATH}"
+            }
         }
     }
 }
