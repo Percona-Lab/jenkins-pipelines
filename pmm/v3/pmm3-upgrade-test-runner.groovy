c6296babfd3958eee4bf7a4ccef9f577eea0fafa
Peter Sirotnak <sirotnak.p@gmail.com>
2024-12-10 07:38:52 +0100

    PMM-7: sh to python file

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -35,7 +35,8 @@ void checkClientBeforeUpgrade(String PMM_SERVER_VERSION, String CLIENT_VERSION) 
     def pmm_server_version = CLIENT_VERSION.trim();
     def pmm_client_version = CLIENT_VERSION.trim();
     sh """
-        sudo chmod 755 /srv/pmm-qa/pmm-tests/check_client_upgrade.sh
+        sudo chmod 755 /srv/pmm-qa/pmm-tests/check_client_upgrade.py
+        python3 /srv/pmm-qa/pmm-tests/check_client_upgrade.py
         bash -xe /srv/pmm-qa/pmm-tests/check_client_upgrade.sh ${pmm_server_version} ${pmm_client_version}
     """
 }
