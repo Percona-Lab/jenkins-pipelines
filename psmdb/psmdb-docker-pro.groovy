5b2ebefb0c714f40e0d0e7fe6de7e9e8e220abcc
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2024-12-10 15:53:55 +0200

    PSMDB-1553: build psmdb pro dockers rework approach

--- psmdb/psmdb-docker-pro.groovy
+++ psmdb/psmdb-docker-pro.groovy
@@ -1,11 +1,11 @@
-library changelog: false, identifier: "lib@master", retriever: modernSCM([
+library changelog: false, identifier: "lib@PSMDB-1553_build_psmdb_pro_dockers", retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/vorsel/jenkins-pipelines.git'
 ])
 
 pipeline {
     agent {
-        label 'docker'
+        label params.ARCH == 'x86_64' ? 'docker' : 'docker-64gb-aarch64'
     }
     environment {
         PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin'
@@ -14,6 +14,7 @@ pipeline {
         choice(name: 'PSMDB_REPO', choices: ['release','testing','experimental'], description: 'percona-release repo to take packages from')
         string(name: 'PSMDB_VERSION', defaultValue: '6.0.18-15', description: 'PSMDB version, for example: 6.0.18-15 or 7.0.14-8')
         string(name: 'TICKET_NAME', defaultValue: '', description: 'Target folder for docker image')
+        choice(name: 'ARCH', choices: ['x86_64','aarch64'], description: 'Docker arch')
     }
     options {
         disableConcurrentBuilds()
@@ -38,10 +39,33 @@ pipeline {
                         cd percona-docker/percona-server-mongodb-\$MAJ_VER
                         sed -E "s|ENV PSMDB_VERSION (.+)|ENV PSMDB_VERSION ${params.PSMDB_VERSION}|" -i Dockerfile
                         sed -E "s|ENV PSMDB_REPO (.+)|ENV PSMDB_REPO ${params.PSMDB_REPO}|" -i Dockerfile
-                        sed -E "s|(psmdb-[0-9]{2})|\\1-pro|g" -i Dockerfile
-                        sed -E 's:(percona-server-mongodb-(server|mongos)):\\1-pro:g' -i Dockerfile
-                        sed -E "s|(enable psmdb-[0-9]{2}-pro)|\\1 --user_name='${USERNAME}' --repo_token='${PASSWORD}'|" -i Dockerfile
-                        sed -E "s|(repo\\.percona\\.com/)(psmdb-[0-9]{2}-pro)|\\1private/'${USERNAME}'-'${PASSWORD}'/\\2|" -i Dockerfile
+                        REPO_VER=\$(echo ${params.PSMDB_VERSION} | awk -F "." '{print \$1\$2}')
+                        OS_VER=\$(grep -P "ENV OS_VER" Dockerfile | cut -d " " -f 3)
+                        echo \$OS_VER
+                        OS_VER_NUM=\$(echo \$OS_VER | sed "s|el||")
+                        echo \$OS_VER_NUM
+                        PKG_URL="http://repo.percona.com/private/${USERNAME}-${PASSWORD}/psmdb-\$REPO_VER-pro/yum/${params.PSMDB_REPO}/\${OS_VER_NUM}/RPMS/${ARCH}/"
+                        echo \$PKG_URL
+                        ARCH=\$(echo \$PKG_URL | awk -F "/" '{print \$(NF-1)}')
+                        echo \$ARCH
+                        sed -E "/curl -Lf -o \\/tmp\\/Percona-Server-MongoDB-server.rpm/d" -i Dockerfile
+                        sed -E "/percona-server-mongodb-mongos-\${FULL_PERCONA_VERSION}/d" -i Dockerfile
+                        sed -E "/percona-server-mongodb-tools-\${FULL_PERCONA_VERSION}/d" -i Dockerfile
+                        sed -E "s|rpmkeys --checksig /tmp/Percona-Server-MongoDB-server.rpm|rpmkeys --checksig /tmp/*.rpm|" -i Dockerfile
+                        sed -E "s|rpm -iv /tmp/Percona-Server-MongoDB-server.rpm --nodeps;|dnf -y install /tmp/Percona-Server-MongoDB-mongos-pro.rpm /tmp/Percona-Server-MongoDB-tools.rpm; rpm -iv /tmp/Percona-Server-MongoDB-server-pro.rpm --nodeps;|" -i Dockerfile
+                        sed -E "s|rm -rf /tmp/Percona-Server-MongoDB-server.rpm|rm -rf /tmp/*.rpm|" -i Dockerfile
+                        sed -E "s|server/LICENSE|server-pro/LICENSE|" -i Dockerfile
+                        cat Dockerfile
+
+                        #sed -E "s|(psmdb-[0-9]{2})|\\1-pro|g" -i Dockerfile
+                        #sed -E 's:(percona-server-mongodb-(server|mongos)):\\1-pro:g' -i Dockerfile
+                        #sed -E "s|(enable psmdb-[0-9]{2}-pro)|\\1 --user_name='${USERNAME}' --repo_token='${PASSWORD}'|" -i Dockerfile
+                        #sed -E "s|(repo\\.percona\\.com/)(psmdb-[0-9]{2}-pro)|\\1private/'${USERNAME}'-'${PASSWORD}'/\\2|" -i Dockerfile
+                        mkdir -p packages
+                        curl -Lf -o ./packages/Percona-Server-MongoDB-server-pro.rpm \$PKG_URL/percona-server-mongodb-server-pro-${params.PSMDB_VERSION}.\$OS_VER.${ARCH}.rpm
+                        curl -Lf -o ./packages/Percona-Server-MongoDB-mongos-pro.rpm \$PKG_URL/percona-server-mongodb-mongos-pro-${params.PSMDB_VERSION}.\$OS_VER.${ARCH}.rpm
+                        curl -Lf -o ./packages/Percona-Server-MongoDB-tools.rpm \$PKG_URL/percona-server-mongodb-tools-${params.PSMDB_VERSION}.\$OS_VER.${ARCH}.rpm
+                        sed -E '/^ARG PERCONA_TELEMETRY_DISABLE/a \\\nCOPY packages/*.rpm /tmp/' -i Dockerfile
                         docker build . -t percona-server-mongodb-pro:${params.PSMDB_VERSION}
                     """
                 }
@@ -103,13 +127,13 @@ pipeline {
             deleteDir()
         }
         success {
-            slackNotify("#releases-ci", "#00FF00", "[${JOB_NAME}]: Building of PSMDB ${PSMDB_VERSION} repo ${PSMDB_REPO} succeed")
+            slackNotify("@alex.miroshnychenko", "#00FF00", "[${JOB_NAME}]: Building of PSMDB ${PSMDB_VERSION} repo ${PSMDB_REPO} succeed")
         }
         unstable {
-            slackNotify("#releases-ci", "#F6F930", "[${JOB_NAME}]: Building of PSMDB ${PSMDB_VERSION} repo ${PSMDB_REPO} unstable - [${BUILD_URL}testReport/]")
+            slackNotify("@alex.miroshnychenko", "#F6F930", "[${JOB_NAME}]: Building of PSMDB ${PSMDB_VERSION} repo ${PSMDB_REPO} unstable - [${BUILD_URL}testReport/]")
         }
         failure {
-            slackNotify("#releases-ci", "#FF0000", "[${JOB_NAME}]: Building of PSMDB ${PSMDB_VERSION} repo ${PSMDB_REPO} failed - [${BUILD_URL}]")
+            slackNotify("@alex.miroshnychenko", "#FF0000", "[${JOB_NAME}]: Building of PSMDB ${PSMDB_VERSION} repo ${PSMDB_REPO} failed - [${BUILD_URL}]")
         }
     }
 }
