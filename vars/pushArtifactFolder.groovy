d80f83bc36f09c75c18c3f583e6c6417472f6947
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2024-12-18 20:56:59 +0200

    PKG-344 psmdb.cd: switch between hetzner/aws s3 obj storages

--- vars/pushArtifactFolder.groovy
+++ vars/pushArtifactFolder.groovy
@@ -1,10 +1,12 @@
-def call(String FOLDER_NAME, String AWS_STASH_PATH) {
-    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AWS_STASH', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+def call(String CLOUD_NAME, String FOLDER_NAME, String AWS_STASH_PATH) {
+    String S3_STASH = (CLOUD_NAME == 'Hetzner') ? 'HTZ_STASH' : 'AWS_STASH'
+    String S3_ENDPOINT = (CLOUD_NAME == 'Hetzner') ? '--endpoint-url https://fsn1.your-objectstorage.com' : '--endpoint-url https://s3.amazonaws.com'
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: S3_STASH, secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
         sh """
             pwd
             S3_PATH=s3://percona-jenkins-artifactory/${AWS_STASH_PATH}
-            aws s3 ls \$S3_PATH/${FOLDER_NAME} || :
-            aws s3 cp --quiet --recursive ${FOLDER_NAME} \$S3_PATH/${FOLDER_NAME} || :
+            aws s3 ls \$S3_PATH/${FOLDER_NAME} ${S3_ENDPOINT} || :
+            aws s3 cp --recursive ${FOLDER_NAME} \$S3_PATH/${FOLDER_NAME} ${S3_ENDPOINT} || :
         """
     }
 }
