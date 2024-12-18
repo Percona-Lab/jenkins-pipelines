97f1a29d286e8edd90ab7add0f02bb604217c9c3
Peter Sirotnak <sirotnak.p@gmail.com>
2024-12-18 12:00:58 +0100

    PMM-13607: Status check for agents

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -36,8 +36,7 @@ void checkClientBeforeUpgrade(String PMM_SERVER_VERSION, String CLIENT_VERSION) 
     def pmm_client_version = CLIENT_VERSION.trim();
     sh """
         echo ${CLIENT_VERSION}
-        export CLIENT_VERSION=${CLIENT_VERSION}
-        if [\${pmm_client_version} == "3-dev-latest"]; then
+        if [${CLIENT_VERSION} == "3-dev-latest"]; then
         GET_PMM_CLIENT_VERSION=$(wget -q https://raw.githubusercontent.com/Percona-Lab/pmm-submodules/v3/VERSION -O -)
         fi
         echo GET_PMM_CLIENT_VERSION
