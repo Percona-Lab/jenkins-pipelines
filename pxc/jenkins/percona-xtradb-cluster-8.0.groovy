1288227074797ef6f43ee0c32442d79d1d7bbc96
Vadim Yalovets <vadim.yalovets@percona.com>
2024-12-23 17:52:53 +0200

    RM-1471 PXB 8.0.35-32

--- pxc/jenkins/percona-xtradb-cluster-8.0.groovy
+++ pxc/jenkins/percona-xtradb-cluster-8.0.groovy
@@ -359,37 +359,6 @@ pipeline {
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
-                stage('Centos 7 tarball') {
-                    agent {
-                        label 'docker-32gb'
-                    }
-                    steps {
-                        cleanUpWS()
-                        unstash 'pxc-80.properties'
-                        popArtifactFolder("source_tarball/", AWS_STASH_PATH)
-                        buildStage("centos:7", "--build_tarball=1")
-
-                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
-                        pushArtifactFolder("test/tarball/", AWS_STASH_PATH)
-                        uploadTarballfromAWS("test/tarball/", AWS_STASH_PATH, 'binary')
-                    }
-                }
-/*
-                stage('Centos 7 debug tarball') {
-                    agent {
-                        label 'docker-32gb'
-                    }
-                    steps {
-                        cleanUpWS()
-                        unstash 'pxc-80.properties'
-                        popArtifactFolder("source_tarball/", AWS_STASH_PATH)
-                        buildStage("centos:7", "--build_tarball=1 --debug=1")
-
-                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
-                        pushArtifactFolder("debug/", AWS_STASH_PATH)
-                    }
-                }
-*/
                 stage('Centos 8 tarball') {
                     agent {
                         label 'docker-32gb'
