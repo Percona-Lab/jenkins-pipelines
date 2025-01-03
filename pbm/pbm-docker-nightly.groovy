dd92c8587ed3ac721d0bf57802a617c0e93d4a52
EvgeniyPatlan <evgeniy.patlan@percona.com>
2025-01-03 07:41:06 +0200

    PKG-41 Add nightly pbm docker build (#3027)
    * PKG-41 Add nightly pbm docker build
    
    * Fix
    
    * fix

---
+++ pbm/pbm-docker-nightly.groovy
@@ -0,0 +1,89 @@
+library changelog: false, identifier: "lib@master", retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+])
+
+pipeline {
+    agent {
+        label 'docker-32gb'
+    }
+    environment {
+        PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin'
+    }
+    parameters {
+        string(name: 'PBM_REPO_CH', defaultValue: 'experimental', description: 'Percona-release repo')
+        string(name: 'PBM_VERSION', defaultValue: '2.0.2-1', description: 'PBM version')
+        string(name: 'TARGET_REPO', defaultValue: 'PerconaLab', description: 'Target repo for docker image, use DockerHub for release only')
+    }
+    options {
+        disableConcurrentBuilds()
+    }
+    stages {
+        stage('Set build name'){
+            steps {
+                script {
+                    currentBuild.displayName = "${params.PBM_REPO_CH}-nightly"
+                }
+            }
+        }
+        stage ('Build image') {
+            steps {
+                sh """
+                    git clone https://github.com/percona/percona-docker
+                    cd percona-docker/percona-backup-mongodb
+                    docker build . -t percona-backup-mongodb 
+                    """
+            }
+        }
+        stage ('Run trivy analyzer') {
+            steps {
+                sh """
+                    TRIVY_VERSION=\$(curl --silent 'https://api.github.com/repos/aquasecurity/trivy/releases/latest' | grep '"tag_name":' | tr -d '"' | sed -E 's/.*v(.+),.*/\\1/')
+                    wget https://github.com/aquasecurity/trivy/releases/download/v\${TRIVY_VERSION}/trivy_\${TRIVY_VERSION}_Linux-64bit.tar.gz
+                    sudo tar zxvf trivy_\${TRIVY_VERSION}_Linux-64bit.tar.gz -C /usr/local/bin/
+                    wget https://raw.githubusercontent.com/aquasecurity/trivy/v\${TRIVY_VERSION}/contrib/junit.tpl
+                    curl https://raw.githubusercontent.com/Percona-QA/psmdb-testing/main/docker/trivyignore -o ".trivyignore"
+                        /usr/local/bin/trivy -q image --format template --template @junit.tpl  -o trivy-hight-junit.xml \
+                                         --timeout 10m0s --ignore-unfixed --exit-code 0 --severity HIGH,CRITICAL percona-backup-mongodb
+               """
+            }
+            post {
+                always {
+                    junit testResults: "*-junit.xml", keepLongStdio: true, allowEmptyResults: true, skipPublishingChecks: true
+                }
+            }
+        }
+        stage ('Push images to perconalab') {
+            when {
+                environment name: 'TARGET_REPO', value: 'PerconaLab'
+            }
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                     sh """
+                         docker login -u '${USER}' -p '${PASS}'
+                         docker tag percona-backup-mongodb perconalab/percona-backup-mongodb:nightly
+                         docker push perconalab/percona-backup-mongodb:nightly
+                     """
+                }
+            }
+        }
+    }
+    post {
+        always {
+            sh """
+                sudo docker rmi -f \$(sudo docker images -q | uniq) || true
+                sudo rm -rf ${WORKSPACE}/*
+            """
+            deleteDir()
+        }
+        success {
+            slackNotify("#mongodb_autofeed", "#00FF00", "[${JOB_NAME}]: Building of PBM nightly repo ${PBM_REPO_CH} succeed")
+        }
+        unstable {
+            slackNotify("#mongodb_autofeed", "#F6F930", "[${JOB_NAME}]: Building of PBM nightly repo ${PBM_REPO_CH} unstable - [${BUILD_URL}testReport/]")
+        }
+        failure {
+            slackNotify("#mongodb_autofeed", "#FF0000", "[${JOB_NAME}]: Building of PBM nightly repo ${PBM_REPO_CH} failed - [${BUILD_URL}]")
+        }
+    }
+}
