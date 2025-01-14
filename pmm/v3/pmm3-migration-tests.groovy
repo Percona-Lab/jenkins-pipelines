b1dda40db323d6de1d6ef399907ead4e99e2c648
Peter Sirotnak <sirotnak.p@gmail.com>
2025-01-14 12:02:02 +0100

    PMM-7: Skeleton for migration

--- pmm/v3/pmm3-migration-tests.groovy
+++ pmm/v3/pmm3-migration-tests.groovy
@@ -87,7 +87,7 @@ pipeline {
             description: 'Perform Docker-way Upgrade?',
             name: 'PERFORM_DOCKER_WAY_UPGRADE')
         text(
-            defaultValue: '--addclient=pgsql,1 --addclient=ps,1 --addclient=pdpgsql,1 --addclient=ms,1 --addclient=md,1 --mongo-replica-for-backup ',
+            defaultValue: '--addclient=pgsql,1 --addclient=ps,1 --addclient=pdpgsql,1 --addclient=ms,1 --addclient=md,1 --mongo-replica-for-backup',
             description: '''
             Configure PMM Clients
             ms - MySQL (ex. --addclient=ms,1),
