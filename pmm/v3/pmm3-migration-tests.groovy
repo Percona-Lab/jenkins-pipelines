73b1d5be4fa9351e2817a4cfce714b52db20529d
Peter Sirotnak <sirotnak.p@gmail.com>
2025-01-15 09:03:40 +0100

    PMM-7: Skeleton for migration

--- pmm/v3/pmm3-migration-tests.groovy
+++ pmm/v3/pmm3-migration-tests.groovy
@@ -250,7 +250,7 @@ pipeline {
                         echo "pgsql container name is: $PGSQL_CONTAINER_NAME"
                         PS_CONTAINER_NAME=$(docker ps -a --format "{{.Names}}" | grep ps_)
                         echo "ps container name is: $PS_CONTAINER_NAME"
-                        percona-release enable pmm3-client experimental
+                        sudo percona-release enable pmm3-client experimental
                         sudo yum install -y pmm-client
                     '''
                     env.SERVER_IP = "127.0.0.1"
