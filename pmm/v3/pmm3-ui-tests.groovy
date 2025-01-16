61615cd22bf8330d3b5ab76956d707630878412f
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2025-01-16 11:57:46 +0200

    PMM-7 no client on the host for ui-tests (#3104)
    * PMM-7 no client on the host for ui-tests
    
    * Update pmm3-ui-tests.groovy

--- pmm/v3/pmm3-ui-tests.groovy
+++ pmm/v3/pmm3-ui-tests.groovy
@@ -232,13 +232,13 @@ pipeline {
         }
         stage('Setup Clients for PMM-Server') {
             steps {
-                sh '''
-                  echo "started client setup"
-                '''
-                setupPMM3Client(SERVER_IP, CLIENT_VERSION.trim(), 'pmm', ENABLE_PULL_MODE, 'no', 'no', 'compose_setup', ADMIN_PASSWORD, 'no')
-                sh '''
-                  echo "installed local client"
-                '''
+//                 sh '''
+//                   echo "started client setup"
+//                 '''
+//                 setupPMM3Client(SERVER_IP, CLIENT_VERSION.trim(), 'pmm', ENABLE_PULL_MODE, 'no', 'no', 'compose_setup', ADMIN_PASSWORD, 'no')
+//                 sh '''
+//                   echo "installed local client"
+//                 '''
                 script {
                         env.PMM_REPO = params.CLIENT_VERSION == "pmm3-rc" ? "testing" : "experimental"
                 }
