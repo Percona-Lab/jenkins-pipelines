df4609fce472c711d0d4dfa3719034f8ddcf5426
Sandra <sandra.romanchenko@percona.com>
2025-01-23 13:03:48 +0200

    PSMDB. Use testing repo for PR and TA until PREL-283, TEL-60 are fixed

--- psmdb/psmdb-multijob-testing.groovy
+++ psmdb/psmdb-multijob-testing.groovy
@@ -23,13 +23,13 @@ pipeline {
                 script {
                     // path https://downloads.percona.com/downloads/TESTING/psmdb-4.4.17/percona-server-mongodb-4.4.17-17-x86_64.glibc2.17-minimal.tar.gz
                     def tarball = 'https://downloads.percona.com/downloads/TESTING/psmdb-' + params.PSMDB_VERSION + '/percona-server-mongodb-' + params.PSMDB_VERSION + '-' + params.PSMDB_RELEASE + '-x86_64.glibc2.17-minimal.tar.gz'
-                    build job: 'psmdb-tarball-functional', propagate: false, wait: true, parameters: [ string(name: 'TARBALL', value: tarball), string(name: 'TESTING_BRANCH', value: "main") ]
+                    build job: 'psmdb-tarball-functional', propagate: false, wait: true, parameters: [ string(name: 'TARBALL', value: tarball), string(name: 'TESTING_BRANCH', value: "PSMDB_disable_debug") ]
                 }
             }
         }
         stage ('Run functional tests on packages') {
             steps {
-                build job: 'psmdb-parallel', parameters: [ string(name: 'REPO', value: "testing"), string(name: 'PSMDB_VERSION', value: params.PSMDB_VERSION ), string(name: 'ENABLE_TOOLKIT', value: "false"), string(name: 'TESTING_BRANCH', value: "main") ]
+                build job: 'psmdb-parallel', parameters: [ string(name: 'REPO', value: "testing"), string(name: 'PSMDB_VERSION', value: params.PSMDB_VERSION ), string(name: 'ENABLE_TOOLKIT', value: "false"), string(name: 'TESTING_BRANCH', value: "PSMDB_disable_debug") ]
             }
         }
         stage ('Build docker images and check for vulnerabilities') { 
