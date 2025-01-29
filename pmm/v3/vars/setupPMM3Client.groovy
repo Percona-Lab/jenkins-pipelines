1c01bfa680bc45c8e43041cc23c00f59a7caa6ad
Talha Bin Rizwan <talha.rizwan@percona.com>
2025-01-29 15:10:46 +0500

    PMM-13714 Fix aws-staging-start pipeline for 3-dev-latest. (#3125)
    * PMM-13714 Fix aws-staging-start pipeline for 3-dev-latest.

--- pmm/v3/vars/setupPMM3Client.groovy
+++ pmm/v3/vars/setupPMM3Client.groovy
@@ -27,7 +27,6 @@ def call(String SERVER_IP, String CLIENT_VERSION, String PMM_VERSION, String ENA
 
             if [ "${CLIENT_VERSION}" = 3-dev-latest ]; then
                 sudo percona-release enable-only pmm3-client experimental
-                sudo yum -y update
                 sudo yum -y install pmm-client
             elif [ "${CLIENT_VERSION}" = pmm3-rc ]; then
                 sudo percona-release enable-only pmm3-client testing
@@ -35,7 +34,6 @@ def call(String SERVER_IP, String CLIENT_VERSION, String PMM_VERSION, String ENA
             elif [ "${CLIENT_VERSION}" = pmm3-latest ]; then
                 sudo percona-release enable-only pmm3-client experimental
                 sudo yum -y install pmm-client
-                sudo yum -y update
             elif [[ "${CLIENT_VERSION}" = 3* ]]; then
                 if [ "${ENABLE_TESTING_REPO}" = yes ]; then
                     sudo percona-release enable-only pmm3-client testing
