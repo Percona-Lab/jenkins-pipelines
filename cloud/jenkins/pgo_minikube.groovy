d3580471ca8230c9570b3472f786e4ead275405a
Pavel Tankov <4014969+ptankov@users.noreply.github.com>
2025-02-09 14:40:30 +0200

    Remove unnecessary echo statement from runTest function in pgo_minikube.groovy to streamline test execution

--- cloud/jenkins/pgo_minikube.groovy
+++ cloud/jenkins/pgo_minikube.groovy
@@ -165,7 +165,6 @@ void runTest(Integer TEST_ID) {
     waitUntil {
         def timeStart = new Date().getTime()
         try {
-            echo "The $testName test was started on cluster $clusterSuffix !"
             tests[TEST_ID]["result"] = "failure"
 
             timeout(time: 90, unit: 'MINUTES') {
