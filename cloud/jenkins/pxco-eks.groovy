13121166e51383148fb67f50aec6d7cc47080c2d
Pavel Tankov <4014969+ptankov@users.noreply.github.com>
2025-02-10 19:09:26 +0200

    CLOUD-888 Fix failing EKS pipelines because of a change how latest version is detected - 02 (#3151)

--- cloud/jenkins/pxco-eks.groovy
+++ cloud/jenkins/pxco-eks.groovy
@@ -63,7 +63,9 @@ void prepareNode() {
     """
 
     if ("$PLATFORM_VER" == "latest") {
-        PLATFORM_VER = sh(script: "aws eks describe-addon-versions --query 'addons[].addonVersions[].compatibilities[].clusterVersion' --output json | jq -r 'flatten | unique | sort | reverse | .[0]'", returnStdout: true).trim()
+        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+            USED_PLATFORM_VER = sh(script: "aws eks describe-addon-versions --query 'addons[].addonVersions[].compatibilities[].clusterVersion' --output json | jq -r 'flatten | unique | sort | reverse | .[0]'", , returnStdout: true).trim()
+        }
     }
 
     if ("$IMAGE_PXC") {
