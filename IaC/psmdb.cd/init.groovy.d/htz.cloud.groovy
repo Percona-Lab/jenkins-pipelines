3a3107898ed627340693e4a967fa9729ee0a5e22
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2025-02-17 12:01:45 +0200

    PKG-492 psmdb.cd: add "*-min" htz workers

--- IaC/psmdb.cd/init.groovy.d/htz.cloud.groovy
+++ IaC/psmdb.cd/init.groovy.d/htz.cloud.groovy
@@ -19,6 +19,12 @@ execMap['deb12-x64-fsn1']     = execMap['deb']
 execMap['deb12-aarch64-nbg1'] = execMap['deb']
 execMap['deb12-aarch64-hel1'] = execMap['deb']
 execMap['deb12-aarch64-fsn1'] = execMap['deb']
+execMap['deb12-x64-nbg1-min']     = execMap['deb']
+execMap['deb12-x64-hel1-min']     = execMap['deb']
+execMap['deb12-x64-fsn1-min']     = execMap['deb']
+execMap['deb12-aarch64-nbg1-min'] = execMap['deb']
+execMap['deb12-aarch64-hel1-min'] = execMap['deb']
+execMap['deb12-aarch64-fsn1-min'] = execMap['deb']
 execMap['launcher-x64-fsn1']  = 10
 
 bootDeadlineMap =[:]
@@ -29,6 +35,12 @@ bootDeadlineMap['deb12-x64-fsn1']     = bootDeadlineMap['default']
 bootDeadlineMap['deb12-aarch64-nbg1'] = bootDeadlineMap['default']
 bootDeadlineMap['deb12-aarch64-hel1'] = bootDeadlineMap['default']
 bootDeadlineMap['deb12-aarch64-fsn1'] = bootDeadlineMap['default']
+bootDeadlineMap['deb12-x64-nbg1-min']     = bootDeadlineMap['default']
+bootDeadlineMap['deb12-x64-hel1-min']     = bootDeadlineMap['default']
+bootDeadlineMap['deb12-x64-fsn1-min']     = bootDeadlineMap['default']
+bootDeadlineMap['deb12-aarch64-nbg1-min'] = bootDeadlineMap['default']
+bootDeadlineMap['deb12-aarch64-hel1-min'] = bootDeadlineMap['default']
+bootDeadlineMap['deb12-aarch64-fsn1-min'] = bootDeadlineMap['default']
 bootDeadlineMap['launcher-x64-fsn1']  = bootDeadlineMap['default']
 
 jvmOptsMap = [:]
@@ -39,12 +51,20 @@ jvmOptsMap['deb12-x64-fsn1']     = jvmOptsMap['deb12']
 jvmOptsMap['deb12-aarch64-nbg1'] = jvmOptsMap['deb12']
 jvmOptsMap['deb12-aarch64-hel1'] = jvmOptsMap['deb12']
 jvmOptsMap['deb12-aarch64-fsn1'] = jvmOptsMap['deb12']
+jvmOptsMap['deb12-x64-nbg1-min']     = jvmOptsMap['deb12']
+jvmOptsMap['deb12-x64-hel1-min']     = jvmOptsMap['deb12']
+jvmOptsMap['deb12-x64-fsn1-min']     = jvmOptsMap['deb12']
+jvmOptsMap['deb12-aarch64-nbg1-min'] = jvmOptsMap['deb12']
+jvmOptsMap['deb12-aarch64-hel1-min'] = jvmOptsMap['deb12']
+jvmOptsMap['deb12-aarch64-fsn1-min'] = jvmOptsMap['deb12']
 jvmOptsMap['launcher-x64-fsn1']  = jvmOptsMap['deb12']
 
 labelMap = [:]
-labelMap['deb12-x64']     = 'docker-x64 docker-deb12-x64 deb12-x64'
-labelMap['deb12-aarch64'] = 'docker-aarch64 docker-deb12-aarch64 deb12-aarch64'
-labelMap['launcher-x64']  = 'launcher-x64'
+labelMap['deb12-x64-min']     = 'docker-x64-min docker-deb12-x64-min deb12-x64-min'
+labelMap['deb12-aarch64-min'] = 'docker-aarch64-min docker-deb12-aarch64-min deb12-aarch64-min'
+labelMap['deb12-x64']         = 'docker-x64 docker-deb12-x64 deb12-x64'
+labelMap['deb12-aarch64']     = 'docker-aarch64 docker-deb12-aarch64 deb12-aarch64'
+labelMap['launcher-x64']      = 'launcher-x64'
 
 networkMap = [:]
 networkMap['percona-vpc-eu'] = '10442325' // percona-vpc-eu
@@ -109,21 +129,33 @@ initMap['deb12-x64-fsn1']     = initMap['deb-docker']
 initMap['deb12-aarch64-nbg1'] = initMap['deb-docker']
 initMap['deb12-aarch64-hel1'] = initMap['deb-docker']
 initMap['deb12-aarch64-fsn1'] = initMap['deb-docker']
+initMap['deb12-x64-nbg1-min']     = initMap['deb-docker']
+initMap['deb12-x64-hel1-min']     = initMap['deb-docker']
+initMap['deb12-x64-fsn1-min']     = initMap['deb-docker']
+initMap['deb12-aarch64-nbg1-min'] = initMap['deb-docker']
+initMap['deb12-aarch64-hel1-min'] = initMap['deb-docker']
+initMap['deb12-aarch64-fsn1-min'] = initMap['deb-docker']
 initMap['launcher-x64-fsn1']  = initMap['deb-docker']
 
 def templates = [
        /* new HetznerServerTemplate("ubuntu20-cx21", "java", "name=ubuntu20-docker", "fsn1", "cx21"), */
-        //                        tmplName              tmplLabels                 tmplImage                  region server type
-        new HetznerServerTemplate("deb12-x64-nbg1",     labelMap['deb12-x64'],     imageMap['deb12-x64'],     "nbg1", "cpx51"),
-        new HetznerServerTemplate("deb12-aarch64-nbg1", labelMap['deb12-aarch64'], imageMap['deb12-aarch64'], "nbg1", "cax41"),
-        new HetznerServerTemplate("deb12-x64-hel1",     labelMap['deb12-x64'],     imageMap['deb12-x64'],     "hel1", "cpx51"),
-        new HetznerServerTemplate("deb12-aarch64-hel1", labelMap['deb12-aarch64'], imageMap['deb12-aarch64'], "hel1", "cax41"),
-        new HetznerServerTemplate("deb12-x64-fsn1",     labelMap['deb12-x64'],     imageMap['deb12-x64'],     "fsn1", "cpx51"),
-        new HetznerServerTemplate("deb12-aarch64-fsn1", labelMap['deb12-aarch64'], imageMap['deb12-aarch64'], "fsn1", "cax41"),
-        new HetznerServerTemplate("launcher-x64-fsn1",  labelMap['launcher-x64'],  imageMap['launcher-x64'],  "fsn1", "cpx21")
+        //                        tmplName                  tmplLabels                     tmplImage                  region server type
+        new HetznerServerTemplate("deb12-x64-nbg1-min",     labelMap['deb12-x64-min'],     imageMap['deb12-x64'],     "nbg1", "cpx41"),
+        new HetznerServerTemplate("deb12-aarch64-nbg1-min", labelMap['deb12-aarch64-min'], imageMap['deb12-aarch64'], "nbg1", "cax31"),
+        new HetznerServerTemplate("deb12-x64-hel1-min",     labelMap['deb12-x64-min'],     imageMap['deb12-x64'],     "hel1", "cpx41"),
+        new HetznerServerTemplate("deb12-aarch64-hel1-min", labelMap['deb12-aarch64-min'], imageMap['deb12-aarch64'], "hel1", "cax31"),
+        new HetznerServerTemplate("deb12-x64-fsn1-min",     labelMap['deb12-x64-min'],     imageMap['deb12-x64'],     "fsn1", "cpx41"),
+        new HetznerServerTemplate("deb12-aarch64-fsn1-min", labelMap['deb12-aarch64-min'], imageMap['deb12-aarch64'], "fsn1", "cax31"),
+        new HetznerServerTemplate("deb12-x64-nbg1",         labelMap['deb12-x64'],         imageMap['deb12-x64'],     "nbg1", "cpx51"),
+        new HetznerServerTemplate("deb12-aarch64-nbg1",     labelMap['deb12-aarch64'],     imageMap['deb12-aarch64'], "nbg1", "cax41"),
+        new HetznerServerTemplate("deb12-x64-hel1",         labelMap['deb12-x64'],         imageMap['deb12-x64'],     "hel1", "cpx51"),
+        new HetznerServerTemplate("deb12-aarch64-hel1",     labelMap['deb12-aarch64'],     imageMap['deb12-aarch64'], "hel1", "cax41"),
+        new HetznerServerTemplate("deb12-x64-fsn1",         labelMap['deb12-x64'],         imageMap['deb12-x64'],     "fsn1", "cpx51"),
+        new HetznerServerTemplate("deb12-aarch64-fsn1",     labelMap['deb12-aarch64'],     imageMap['deb12-aarch64'], "fsn1", "cax41"),
+        new HetznerServerTemplate("launcher-x64-fsn1",      labelMap['launcher-x64'],      imageMap['launcher-x64'],  "fsn1", "cpx21")
 ]
 
-templates.each { it -> 
+templates.each { it ->
                        def sshConnector = new SshConnectorAsRoot("htz.cd.key")
                        sshConnector.setConnectionMethod(new PublicAddressOnly())  // Replace with the desired method
                        it.setConnector(sshConnector)
