f3dd39cf1ec25cb646feb3f2d797e322c77dda28
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2025-02-26 21:29:27 +0200

    CLOUD-875: Rename files and add missing scheduler job for pxc

--- cloud/jenkins/weekly_pxco.groovy
+++ cloud/jenkins/weekly_pxco.groovy
@@ -19,24 +19,24 @@ pipeline {
     stages {
         stage("Run parallel") {
             parallel {
-                stage('Trigger pgo-gke-1 job 3 times') {
+                stage('Trigger pxco-gke-1 job 3 times') {
                     steps {
-                        triggerJobMultiple("pgo-gke-1")
+                        triggerJobMultiple("pxco-gke-1")
                     }
                 }
-                stage('Trigger pgo-eks-1 job 3 times') {
+                stage('Trigger pxco-eks-1 job 3 times') {
                     steps {
-                        triggerJobMultiple("pgo-eks-1")
+                        triggerJobMultiple("pxco-eks-1")
                     }
                 }
-                stage('Trigger pgo-aks-1 job 3 times') {
+                stage('Trigger pxco-aks-1 job 3 times') {
                     steps {
-                        triggerJobMultiple("pgo-aks-1")
+                        triggerJobMultiple("pxco-aks-1")
                     }
                 }
-                stage('Trigger pgo-openshift-1 job 3 times') {
+                stage('Trigger pxco-openshift-1 job 3 times') {
                     steps {
-                        triggerJobMultiple("pgo-openshift-1")
+                        triggerJobMultiple("pxco-openshift-1")
                     }
                 }
             }
@@ -44,10 +44,10 @@ pipeline {
     }
     post {
         always {
-            copyArtifacts(projectName: 'pgo-gke-1', selector: lastCompleted(), target: 'pgo-gke-1')
-            copyArtifacts(projectName: 'pgo-eks-1', selector: lastCompleted(), target: 'pgo-eks-1')
-            copyArtifacts(projectName: 'pgo-aks-1', selector: lastCompleted(), target: 'pgo-aks-1')
-            copyArtifacts(projectName: 'pgo-openshift-1', selector: lastCompleted(), target: 'pgo-openshift-1')
+            copyArtifacts(projectName: 'pxco-gke-1', selector: lastCompleted(), target: 'pxco-gke-1')
+            copyArtifacts(projectName: 'pxco-eks-1', selector: lastCompleted(), target: 'pxco-eks-1')
+            copyArtifacts(projectName: 'pxco-aks-1', selector: lastCompleted(), target: 'pxco-aks-1')
+            copyArtifacts(projectName: 'pxco-openshift-1', selector: lastCompleted(), target: 'pxco-openshift-1')
             archiveArtifacts '*/*.xml'
             step([$class: 'JUnitResultArchiver', testResults: '*/*.xml', healthScaleFactor: 1.0])
         }
