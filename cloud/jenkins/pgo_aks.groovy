746be48723a5a58cc34dbc2eab3462f6fe40eb11
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2025-02-26 22:01:53 +0200

    Move image to runTest for AKS

--- cloud/jenkins/pgo_aks.groovy
+++ cloud/jenkins/pgo_aks.groovy
@@ -223,7 +223,7 @@ void runTest(Integer TEST_ID) {
                     cd source
 
                     [[ "$CLUSTER_WIDE" == "YES" ]] && export OPERATOR_NS=pg-operator
-                    export IMAGE=$IMAGE_OPERATOR
+                    [[ "$IMAGE_OPERATOR" ]] && export IMAGE=$IMAGE_OPERATOR || export IMAGE=perconalab/percona-postgresql-operator:$GIT_BRANCH
                     export PG_VER=$PG_VER
                     export IMAGE_POSTGRESQL=$IMAGE_POSTGRESQL
                     export IMAGE_PGBOUNCER=$IMAGE_PGBOUNCER
@@ -283,7 +283,7 @@ void makeReport() {
     pipelineParameters = """
         testsuite name=$JOB_NAME
         PG_VER=$PG_VER
-        [[ "$IMAGE_OPERATOR" ]] && export IMAGE=$IMAGE_OPERATOR || export IMAGE=perconalab/percona-postgresql-operator:$GIT_BRANCH
+        IMAGE_OPERATOR=$IMAGE_OPERATOR
         IMAGE_POSTGRESQL=$IMAGE_POSTGRESQL
         IMAGE_PGBOUNCER=$IMAGE_PGBOUNCER
         IMAGE_BACKREST=$IMAGE_BACKREST
