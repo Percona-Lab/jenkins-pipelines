6eb183757bc0b984ba0adeec840e4373a1217205
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2025-02-27 18:15:25 +0200

    CLOUD-875: Fix post for aks

--- cloud/jenkins/pgo_aks.groovy
+++ cloud/jenkins/pgo_aks.groovy
@@ -430,11 +430,11 @@ pipeline {
 
                 clusters.each { shutdownCluster(it) }
             }
-        }
         sh """
             sudo docker system prune --volumes -af
             sudo rm -rf *
         """
         deleteDir()
+        }
     }
 }
