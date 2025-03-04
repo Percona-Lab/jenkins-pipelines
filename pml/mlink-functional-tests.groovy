de0d874c2b05025d33567918d0f50adbcdb0661c
Keith Quinn <keith.quinn@percona.com>
2025-03-04 10:35:49 +0000

    PML Test for git clone

--- pml/mlink-functional-tests.groovy
+++ pml/mlink-functional-tests.groovy
@@ -58,15 +58,16 @@ pipeline {
                                     git poll: false, branch: params.PSMDB_TESTING_BRANCH, url: 'https://github.com/Percona-QA/psmdb-testing.git'
                                 }
 
-                                withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'GIT_TOKEN')]) {
-                                    dir('percona-mongolink') {
-                                        git url: "https://x-access-token:${GIT_TOKEN}@github.com/Percona-Lab/percona-mongolink.git",
-                                                branch: params.MLINK_BRANCH,
-                                                poll: false
-                                    }
-                                }
+//                                withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'GIT_TOKEN')]) {
+//                                    dir('percona-mongolink') {
+//                                        git url: "https://x-access-token:${GIT_TOKEN}@github.com/Percona-Lab/percona-mongolink.git",
+//                                                branch: params.MLINK_BRANCH,
+//                                                poll: false
+//                                    }
+//                                }
 
                                 sh """
+                                    git clone https://JNKPercona:${JNKPERCONA_TOKEN}@github.com/Percona-Lab/percona-mongolink.git
                                     cd psmdb-testing/mlink
                                     docker-compose build
                                     docker-compose up -d
