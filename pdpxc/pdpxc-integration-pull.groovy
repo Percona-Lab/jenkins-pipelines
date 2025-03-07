7eb46b7e5b3fc319767a2d0a3cacb26af2a76412
panchal-yash <yash.panchal@percona.com>
2025-03-07 18:06:55 +0530

    test

--- pdpxc/pdpxc-integration-pull.groovy
+++ pdpxc/pdpxc-integration-pull.groovy
@@ -1,6 +1,6 @@
 pipeline {
     agent {
-        label 'min-bookworm-x64'
+        label 'docker'
     }
     environment {
         S3_BUCKET = "s3://package-testing-status-test"
@@ -19,7 +19,7 @@ pipeline {
                 script {
                     echo "Fetching file from S3..."
                     
-                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: '5d78d9c7-2188-4b16-8e31-4d5782c6ceaa', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                         sh """
                             aws s3 cp ${S3_BUCKET}/${TRIGGER_FILE} ${LOCAL_TRIGGER_FILE}
                         """
@@ -35,20 +35,20 @@ pipeline {
                 script{
                     def pxcoValue = sh(script: "grep '^PXCO=' ${LOCAL_TRIGGER_FILE} | cut -d '=' -f2", returnStdout: true).trim()
                     
-                    if (pxcoValue) {
+                   // if (pxcoValue) {
                         echo "PXCO value: ${pxcoValue}"
                         
                         // Perform specific actions based on the PDPS_OPERATORS value
-                        if (pxcoValue == "0") {
+                       // if (pxcoValue == "0") {
                             echo "PXCO is set to 0. Setting build status to UNSTABLE."
                             currentBuild.result = 'UNSTABLE'  // Set build status to UNSTABLE
-                        } else if (pxcoValue == "1") {
+                       // } else if (pxcoValue == "1") {
                             echo "PXCO is set to 1. Keeping build status SUCCESS."
                             def jobId = env.BUILD_ID
                             
                             // Modify and upload the file back to S3
                             
-                            withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: '5d78d9c7-2188-4b16-8e31-4d5782c6ceaa', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                            withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AMI/OVF', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                             
                                 sh """
                                     sed -i 's/^PXCO=.*/PXCO=0/' ${LOCAL_TRIGGER_FILE}
@@ -56,18 +56,66 @@ pipeline {
                                 """
                             }
                             
-                            currentBuild.result = 'SUCCESS'  // Explicitly set build status to SUCCESS
+                       //     currentBuild.result = 'SUCCESS'  // Explicitly set build status to SUCCESS
                         
 
                             // Trigger the PXC Operator Jobs on cloud.cd jenkins
+                            def envFile = readFile("${LOCAL_TRIGGER_FILE}").trim()
+                            envFile.readLines().each { line ->
+                                def parts = line.split('=', 2)
+                                if (parts.size() == 2) {
+                                    def key = parts[0].trim()
+                                    def value = parts[1].trim()
+                                    env."${key}" = value  // Workaround to avoid putAt()
+                                }
+                            }
+                            echo "Environment variables set!"
+
+                            echo """
+                                GIT_BRANCH: ${PXCO_VERSION}
+                                GIT_REPO: ${node_to_test}
+                                PLATFORM_VER: ${params.test_repo}
+                                GKE_RELEASE_CHANNEL: ${test_type}
+                                IMAGE_OPERATOR: ${params.pxc57_repo}
+                                IMAGE_PXC: ${params.pxc57_repo}
+                                IMAGE_PROXY: ${params.pxc57_repo}
+                                IMAGE_HAPROXY: ${params.pxc57_repo}
+                                IMAGE_BACKUP: ${params.pxc57_repo}
+                                IMAGE_LOGCOLLECTOR: ${params.pxc57_repo}
+                                IMAGE_PMM_CLIENT: ${params.pxc57_repo}
+                                IMAGE_PMM_SERVER: ${params.pxc57_repo}
+                            """
+                            
+                            build(
+                                job: 'pxco-gke-1',
+                                parameters: [
+                                    string(name: "GIT_BRANCH", value: PXCO_VERSION),
+                                    string(name: "GIT_REPO", value: node_to_test),
+                                    string(name: "PLATFORM_VER", value: params.test_repo),
+                                    string(name: "GKE_RELEASE_CHANNEL", value: "${test_type}"),
+                                    string(name: "IMAGE_OPERATOR", value: params.pxc57_repo),
+                                    string(name: "IMAGE_PXC", value: params.pxc57_repo),
+                                    string(name: "IMAGE_PROXY", value: params.pxc57_repo),
+                                    string(name: "IMAGE_HAPROXY", value: params.pxc57_repo),
+                                    string(name: "IMAGE_BACKUP", value: params.pxc57_repo),
+                                    string(name: "IMAGE_LOGCOLLECTOR", value: params.pxc57_repo),
+                                    string(name: "IMAGE_PMM_CLIENT", value: params.pxc57_repo),
+                                    string(name: "IMAGE_PMM_SERVER", value: params.pxc57_repo)
+                                ],
+                                propagate: true,
+                                wait: true
+                            )
 
 
-                        } else {
-                            echo "PXCO has an unexpected value: ${pxcoValue}. No status change."
-                        }
-                    } else {
-                        echo "PXCO variable not found in the file."
-                    }
+
+                       // } else {
+                         //   echo "PXCO has an unexpected value: ${pxcoValue}. No status change."
+                      //  }
+                   // } else {
+                      
+                      //  echo "PXCO variable not found in the file."
+                    
+                    //}
                 }
             }
         }
