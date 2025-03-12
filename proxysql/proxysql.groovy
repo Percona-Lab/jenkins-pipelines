446189487e30e7184fae746bfadeec727b6e2a91
Muhammad Aqeel <muhammad.aqeel@percona.com>
2025-03-12 16:28:47 +0500

    [PKG-373]: Builds proxysql docker image only for AMD64.

--- proxysql/proxysql.groovy
+++ proxysql/proxysql.groovy
@@ -388,21 +388,12 @@ pipeline {
                         sudo systemctl stop apparmor
                         sudo systemctl disable apparmor
                         sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-                        export DOCKER_CLI_EXPERIMENTAL=enabled
-                        sudo mkdir -p /usr/libexec/docker/cli-plugins/
-                        sudo curl -L https://github.com/docker/buildx/releases/download/v0.21.2/buildx-v0.21.2.linux-amd64 -o /usr/libexec/docker/cli-plugins/docker-buildx
-                        sudo chmod +x /usr/libexec/docker/cli-plugins/docker-buildx
                         sudo systemctl restart docker
-                        sudo apt-get install -y qemu-system binfmt-support qemu-user-static
-                        sudo qemu-system-x86_64 --version
-                        sudo docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
                         git clone https://github.com/percona/percona-docker
                         cd percona-docker/proxysql
                         sed -i "s/ENV PROXYSQL_VERSION.*/ENV PROXYSQL_VERSION ${VERSION}-${RPM_RELEASE}/g" Dockerfile
                         sed -i "s/enable proxysql testing/enable proxysql ${COMPONENT}/g" Dockerfile
                         sudo docker build --no-cache --platform "linux/amd64" -t perconalab/proxysql:${VERSION}-${RPM_RELEASE} .
-
-                        sudo docker build --no-cache -t perconalab/proxysql:${VERSION}-${RPM_RELEASE}-aarch64 --platform="linux/arm64" -f Dockerfile .
 
                         sudo docker images
                     '''
@@ -414,35 +405,19 @@ pipeline {
                         sh '''
                             echo "${PASS}" | sudo docker login -u "${USER}" --password-stdin
                             sudo docker push perconalab/proxysql:${VERSION}-${RPM_RELEASE}
-                            sudo docker push perconalab/proxysql:${VERSION}-${RPM_RELEASE}-aarch64
 
                             PROXYSQL_MAJOR_VERSION=$(echo $VERSION | cut -d'.' -f1)
                             PROXYSQL_MINOR_VERSION=$(echo $VERSION | cut -d'.' -f2)
                             PROXYSQL_PATCH_VERSION=$(echo $VERSION | cut -d'.' -f3)
-                            sudo docker manifest create perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION}.${PROXYSQL_PATCH_VERSION} \
-                                perconalab/proxysql:${VERSION}-${RPM_RELEASE} \
-                                perconalab/proxysql:${VERSION}-${RPM_RELEASE}-aarch64
-                            sudo docker manifest annotate perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION}.${PROXYSQL_PATCH_VERSION} perconalab/proxysql:${VERSION}-${RPM_RELEASE}-aarch64 --os linux --arch arm64 --variant v8
-                            sudo docker manifest annotate perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION}.${PROXYSQL_PATCH_VERSION} perconalab/proxysql:${VERSION}-${RPM_RELEASE} --os linux --arch amd64
-                            sudo docker manifest inspect perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION}.${PROXYSQL_PATCH_VERSION}
 
-                            sudo docker manifest create perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION} \
-                                perconalab/proxysql:${VERSION}-${RPM_RELEASE} \
-                                perconalab/proxysql:${VERSION}-${RPM_RELEASE}-aarch64
-                            sudo docker manifest annotate perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION} perconalab/proxysql:${VERSION}-${RPM_RELEASE}-aarch64 --os linux --arch arm64 --variant v8
-                            sudo docker manifest annotate perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION} perconalab/proxysql:${VERSION}-${RPM_RELEASE} --os linux --arch amd64
-                            sudo docker manifest inspect perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION}
+                            docker tag perconalab/proxysql:${VERSION}-${RPM_RELEASE} perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION}.${PROXYSQL_PATCH_VERSION}
+                            docker tag perconalab/proxysql:${VERSION}-${RPM_RELEASE} perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION}
+                            docker tag perconalab/proxysql:${VERSION}-${RPM_RELEASE} perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}
 
-                            sudo docker manifest create perconalab/proxysql:${PROXYSQL_MAJOR_VERSION} \
-                                perconalab/proxysql:${VERSION}-${RPM_RELEASE} \
-                                perconalab/proxysql:${VERSION}-${RPM_RELEASE}-aarch64
-                            sudo docker manifest annotate perconalab/proxysql:${PROXYSQL_MAJOR_VERSION} perconalab/proxysql:${VERSION}-${RPM_RELEASE}-aarch64 --os linux --arch arm64 --variant v8
-                            sudo docker manifest annotate perconalab/proxysql:${PROXYSQL_MAJOR_VERSION} perconalab/proxysql:${VERSION}-${RPM_RELEASE} --os linux --arch amd64
-                            sudo docker manifest inspect perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}
+                            sudo docker push perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION}.${PROXYSQL_PATCH_VERSION}
+                            sudo docker push perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION}
+                            sudo docker push perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}
 
-                            sudo docker manifest push perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION}.${PROXYSQL_PATCH_VERSION}
-                            sudo docker manifest push perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}.${PROXYSQL_MINOR_VERSION}
-                            sudo docker manifest push perconalab/proxysql:${PROXYSQL_MAJOR_VERSION}
                         '''
                     }
                 }
