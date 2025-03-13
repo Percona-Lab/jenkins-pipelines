ebf27da266b69f9cf1ce5b176e5b90f33881563f
Natalia Marukovich <natalia.marukovich@percona.com>
2025-03-13 13:20:04 +0100

    test

--- cloud/jenkins/pso_openshift.groovy
+++ cloud/jenkins/pso_openshift.groovy
@@ -234,14 +234,13 @@ platform:
 publish: External
 EOF
             cat $OPENSHIFT_CONF_FILE >> openshift/$CLUSTER_SUFFIX/install-config.yaml
-            cat $OPENSHIFT_CONF_FILE
         """
 
         sshagent(['aws-openshift-41-key']) {
             sh """
                 /usr/local/bin/openshift-install create cluster --dir=openshift/$CLUSTER_SUFFIX
                 export KUBECONFIG=openshift/$CLUSTER_SUFFIX/auth/kubeconfig
-                cat "openshift/$CLUSTER_SUFFIX/auth/kubeconfig"
+                export KUBERNETES_MASTER=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
             """
         }
     }
