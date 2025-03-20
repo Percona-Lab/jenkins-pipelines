0ffc9fdb32ba09142f91f0178921485786da3081
Peter Sirotnak <sirotnak.p@gmail.com>
2025-03-20 18:00:14 +0100

    PMM-7: parallel

--- pmm/v3/pmm3-upgrade-tests.groovy
+++ pmm/v3/pmm3-upgrade-tests.groovy
@@ -4,6 +4,7 @@ library changelog: false, identifier: 'lib@master', retriever: modernSCM([
 ]) _
 
 void runUpgradeJob(String PMM_UI_GIT_BRANCH, DOCKER_TAG, DOCKER_TAG_UPGRADE, CLIENT_VERSION, CLIENT_REPOSITORY, PMM_SERVER_LATEST, PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH, UPGRADE_FLAG) {
+    println "Upgrade Variant is: $LABEL with variables $PMM_UI_GIT_BRANCH, $DOCKER_TAG, $DOCKER_TAG_UPGRADE, $CLIENT_VERSION, $CLIENT_REPOSITORY, $PMM_SERVER_LATEST, $PMM_QA_GIT_BRANCH, $QA_INTEGRATION_GIT_BRANCH"
     upgradeJob = build job: 'pmm3-upgrade-test-runner', parameters: [
         string(name: 'PMM_UI_GIT_BRANCH', value: PMM_UI_GIT_BRANCH),
         string(name: 'DOCKER_TAG', value: DOCKER_TAG),
