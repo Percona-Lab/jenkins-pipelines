37db14084327639efaca2aacd9bd6ea97a3a1610
Talha Bin Rizwan <talha.rizwan@percona.com>
2025-03-20 12:45:10 +0500

    PMM-13785 Build pmm-client rpm for AL

--- pmm/v3/pmm3-client-autobuild-amd.groovy
+++ pmm/v3/pmm3-client-autobuild-amd.groovy
@@ -3,7 +3,7 @@ library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
-library changelog: false, identifier: 'v3lib@PMM-13785-pmm-client-al', retriever: modernSCM(
+library changelog: false, identifier: 'v3lib@master', retriever: modernSCM(
   scm: [$class: 'GitSCMSource', remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'],
   libraryPath: 'pmm/v3/'
 )
