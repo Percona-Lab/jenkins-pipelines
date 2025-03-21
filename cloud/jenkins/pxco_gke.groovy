498f21e23e63c0cdc11dd4352457cca87d98a069
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2025-03-21 16:26:06 +0200

    CLOUD-877: add images list to build summary page (#3230)

--- cloud/jenkins/pxco_gke.groovy
+++ cloud/jenkins/pxco_gke.groovy
@@ -312,20 +312,24 @@ void makeReport() {
 
     echo "=========================[ Generating Parameters Report ]========================="
     pipelineParameters = """
-        testsuite name=$JOB_NAME
-        IMAGE_OPERATOR=$IMAGE_OPERATOR
-        IMAGE_PXC=$IMAGE_PXC
-        IMAGE_PROXY=$IMAGE_PROXY
-        IMAGE_HAPROXY=$IMAGE_HAPROXY
-        IMAGE_BACKUP=$IMAGE_BACKUP
-        IMAGE_LOGCOLLECTOR=$IMAGE_LOGCOLLECTOR
-        IMAGE_PMM_CLIENT=$IMAGE_PMM_CLIENT
-        IMAGE_PMM_SERVER=$IMAGE_PMM_SERVER
-        PLATFORM_VER=$PLATFORM_VER
-    """
+testsuite name=$JOB_NAME
+IMAGE_OPERATOR=${IMAGE_OPERATOR ?: 'e2e_defaults'}
+IMAGE_PXC=${IMAGE_PXC ?: 'e2e_defaults'}
+IMAGE_PROXY=${IMAGE_PROXY ?: 'e2e_defaults'}
+IMAGE_HAPROXY=${IMAGE_HAPROXY ?: 'e2e_defaults'}
+IMAGE_BACKUP=${IMAGE_BACKUP ?: 'e2e_defaults'}
+IMAGE_LOGCOLLECTOR=${IMAGE_LOGCOLLECTOR ?: 'e2e_defaults'}
+IMAGE_PMM_CLIENT=${IMAGE_PMM_CLIENT ?: 'e2e_defaults'}
+IMAGE_PMM_SERVER=${IMAGE_PMM_SERVER ?: 'e2e_defaults'}
+PLATFORM_VER=$PLATFORM_VER
+GKE_RELEASE_CHANNEL=$GKE_RELEASE_CHANNEL"""
 
     writeFile file: "TestsReport.xml", text: testsReport
     writeFile file: 'PipelineParameters.txt', text: pipelineParameters
+
+    addSummary(icon: 'symbol-aperture-outline plugin-ionicons-api',
+        text: "<pre>${pipelineParameters}</pre>"
+    )
 }
 
 void shutdownCluster(String CLUSTER_SUFFIX) {
