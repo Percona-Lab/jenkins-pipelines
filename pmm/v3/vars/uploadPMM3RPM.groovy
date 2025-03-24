3b21572a8379d8831f729ea55446ab7e07b629eb
Talha Bin Rizwan <talha.rizwan@percona.com>
2025-03-24 12:15:14 +0500

    PMM-13785 Build pmm-client rpm for AL

--- pmm/v3/vars/uploadPMM3RPM.groovy
+++ pmm/v3/vars/uploadPMM3RPM.groovy
@@ -4,57 +4,57 @@ def call() {
         unstash 'rpms'
         unstash 'uploadPath'
         withCredentials([sshUserPrivateKey(credentialsId: 'repo.ci.percona.com', keyFileVariable: 'KEY_PATH', usernameVariable: 'USER')]) {
-            sh """
+            sh '''
                 export path_to_build=`cat uploadPath`
 
                 # Upload source packages
                 if [ `find . -name '*.src.rpm' | wc -l` -gt 0 ]; then
                     ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com \
-                        mkdir -p \${path_to_build}/source/redhat
+                        mkdir -p ${path_to_build}/source/redhat
                     scp -o StrictHostKeyChecking=no -i ${KEY_PATH} \
                         `find . -name '*.src.rpm'` \
-                        ${USER}@repo.ci.percona.com:\${path_to_build}/source/redhat/
+                        ${USER}@repo.ci.percona.com:${path_to_build}/source/redhat/
                 fi
 
-                export arch_list=\$( find . -name '*.el[6-9].*.rpm' -o -name '*.amzn2023.*.rpm' -o -name '*.noarch.rpm' | awk -F'[.]' '{print \$(NF -1)}' | sort -n | uniq )
+                export arch_list=$( find . -name '*.el[6-9].*.rpm' -o -name '*.amzn2023.*.rpm' -o -name '*.noarch.rpm' | awk -F'[.]' '{print $(NF -1)}' | sort -n | uniq )
 
-                for arch in \${arch_list}; do
-                    if [ `find . -name "*.el8.\${arch}.rpm" | wc -l` -gt 0 ]; then
+                for arch in ${arch_list}; do
+                    if [ `find . -name "*.el8.${arch}.rpm" | wc -l` -gt 0 ]; then
                         ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com \
-                            mkdir -p \${path_to_build}/binary/redhat/8/\${arch}
+                            mkdir -p ${path_to_build}/binary/redhat/8/${arch}
                         scp -o StrictHostKeyChecking=no -i ${KEY_PATH} \
-                            `find . -name "*.el8.\${arch}.rpm"` \
-                            ${USER}@repo.ci.percona.com:\${path_to_build}/binary/redhat/8/\${arch}/
+                            `find . -name "*.el8.${arch}.rpm"` \
+                            ${USER}@repo.ci.percona.com:${path_to_build}/binary/redhat/8/${arch}/
                     fi
 
-                    if [ `find . -name "*.el9.\${arch}.rpm" | wc -l` -gt 0 ]; then
+                    if [ `find . -name "*.el9.${arch}.rpm" | wc -l` -gt 0 ]; then
                         ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com \
-                            mkdir -p \${path_to_build}/binary/redhat/9/\${arch}
+                            mkdir -p ${path_to_build}/binary/redhat/9/${arch}
                         scp -o StrictHostKeyChecking=no -i ${KEY_PATH} \
-                            `find . -name "*.el9.\${arch}.rpm"` \
-                            ${USER}@repo.ci.percona.com:\${path_to_build}/binary/redhat/9/\${arch}/
+                            `find . -name "*.el9.${arch}.rpm"` \
+                            ${USER}@repo.ci.percona.com:${path_to_build}/binary/redhat/9/${arch}/
                     fi
 
-                    if [ `find . -name "*.amzn2023.\${arch}.rpm" | wc -l` -gt 0 ]; then
+                    if [ `find . -name "*.amzn2023.${arch}.rpm" | wc -l` -gt 0 ]; then
                         ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com \
-                            mkdir -p \${path_to_build}/binary/redhat/2023/\${arch}
+                            mkdir -p ${path_to_build}/binary/redhat/2023/${arch}
                         scp -o StrictHostKeyChecking=no -i ${KEY_PATH} \
-                            `find . -name "*.amzn2023.\${arch}.rpm"` \
-                            ${USER}@repo.ci.percona.com:\${path_to_build}/binary/redhat/2023/\${arch}/
+                            `find . -name "*.amzn2023.${arch}.rpm"` \
+                            ${USER}@repo.ci.percona.com:${path_to_build}/binary/redhat/2023/${arch}/
                     fi
 
                     if [ `find . -name "*.noarch.rpm" | wc -l` -gt 0 ]; then
                         Vers=("8" "9" "2023")
-                        for osVer in "\${Vers[@]}"; do
+                        for osVer in "${Vers[@]}"; do
                             ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com \
-                                mkdir -p \${path_to_build}/binary/redhat/\${osVer}/\${arch}
+                                mkdir -p ${path_to_build}/binary/redhat/${osVer}/${arch}
                             scp -o StrictHostKeyChecking=no -i ${KEY_PATH} \
                                 `find . -name "*.noarch.rpm"` \
-                                ${USER}@repo.ci.percona.com:\${path_to_build}/binary/redhat/\${osVer}/\${arch}/
+                                ${USER}@repo.ci.percona.com:${path_to_build}/binary/redhat/${osVer}/${arch}/
                         done
                     fi
                 done
-            """
+            '''
         }
         deleteDir()
     }
