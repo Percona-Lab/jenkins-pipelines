021a7de20ed27637fd23937d3e19d6abf09406c3
Keith Quinn <keith.quinn@percona.com>
2025-04-02 12:43:55 +0100

    PML-104 Adding choice for layout for testing

--- pml/pml-manual.groovy
+++ pml/pml-manual.groovy
@@ -19,7 +19,7 @@ pipeline {
         choice(name: 'PSMDB',description: 'PSMDB for testing',choices: ['psmdb-70','psmdb-80','psmdb-60','psmdb-50'])
         string(name: 'PML_BRANCH',description: 'PML Branch for testing',defaultValue: 'main')
         choice(name: 'INSTANCE_TYPE',description: 'Ec2 instance type',choices: ['t2.micro','i3.large','i3en.large','i3.xlarge','i3en.xlarge'])
-        choice(name: 'LAYOUT',description: 'Layout',choices: ['replicaset','sharded','manual/aws'])
+        choice(name: 'LAYOUT',description: 'Layout',choices: ['replicaset','sharded','aws'])
         string(name: 'TIMEOUT',description: 'Timeout for the job',defaultValue: '3600')
         string(name: 'TESTING_BRANCH',description: 'Branch for testing repository',defaultValue: 'main')
         string(name: 'SSH_USER',description: 'User for debugging',defaultValue: 'none')
