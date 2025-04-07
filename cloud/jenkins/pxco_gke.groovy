134686f6640220023d907633f35107affb5c005d
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2025-04-07 23:13:06 +0300

    Remove extra space

--- cloud/jenkins/pxco_gke.groovy
+++ cloud/jenkins/pxco_gke.groovy
@@ -82,7 +82,7 @@ EOF
     }
 
     if ("$PLATFORM_VER" == "latest") {
-        PLATFORM_VER = sh(script: "gcloud container get-server-config --region= ${GKE_REGION} --flatten=channels --filter='channels.channel=RAPID' --format='value(channels.validVersions)' | cut -d- -f1", returnStdout: true).trim()
+        PLATFORM_VER = sh(script: "gcloud container get-server-config --region=${GKE_REGION} --flatten=channels --filter='channels.channel=RAPID' --format='value(channels.validVersions)' | cut -d- -f1", returnStdout: true).trim()
     }
 
     if ("$IMAGE_PXC") {
@@ -203,7 +203,7 @@ void createCluster(String CLUSTER_SUFFIX) {
             while [[ \$exitCode != 0 && \$maxRetries > 0 ]]; do
                 gcloud container clusters create $CLUSTER_NAME-$CLUSTER_SUFFIX \
                     --release-channel $GKE_RELEASE_CHANNEL \
-                    --zone  ${GKE_REGION} \
+                    --zone ${GKE_REGION} \
                     --cluster-version $PLATFORM_VER \
                     --preemptible \
                     --disk-size 30 \
@@ -228,7 +228,7 @@ void createCluster(String CLUSTER_SUFFIX) {
             FUTURE_TIME=\$(date -d '6 hours' --rfc-3339=seconds)
 
             gcloud container clusters update $CLUSTER_NAME-$CLUSTER_SUFFIX \
-                --zone  ${GKE_REGION} \
+                --zone ${GKE_REGION} \
                 --add-maintenance-exclusion-start "\$CURRENT_TIME" \
                 --add-maintenance-exclusion-end "\$FUTURE_TIME"
 
@@ -344,7 +344,7 @@ void shutdownCluster(String CLUSTER_SUFFIX) {
                 kubectl delete pods --all -n \$namespace --force --grace-period=0 || true
             done
             kubectl get svc --all-namespaces || true
-            gcloud container clusters delete --zone  ${GKE_REGION} $CLUSTER_NAME-$CLUSTER_SUFFIX --quiet || true
+            gcloud container clusters delete --zone ${GKE_REGION} $CLUSTER_NAME-$CLUSTER_SUFFIX --quiet || true
         """
     }
 }
