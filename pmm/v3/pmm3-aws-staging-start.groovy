1b21e55a6f5b8603a940db187d922620a2284540
Talha Bin Rizwan <talha.rizwan@percona.com>
2025-04-14 13:59:45 +0500

    PMM-13917 Add nomad security group to AWS staging instance.

--- pmm/v3/pmm3-aws-staging-start.groovy
+++ pmm/v3/pmm3-aws-staging-start.groovy
@@ -3,12 +3,12 @@ import hudson.slaves.*
 import jenkins.model.Jenkins
 import hudson.plugins.sshslaves.SSHLauncher
 
-library changelog: false, identifier: 'lib@PMM-13917-add-nomad-sg', retriever: modernSCM([
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
-library changelog: false, identifier: 'v3lib@PMM-13917-add-nomad-sg', retriever: modernSCM(
+library changelog: false, identifier: 'v3lib@master', retriever: modernSCM(
   scm: [$class: 'GitSCMSource', remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'],
   libraryPath: 'pmm/v3/'
 )
