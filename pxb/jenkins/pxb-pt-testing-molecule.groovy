82518187659adea8c3d81709374bbe3f1ba4ad5c
panchal-yash <yash.panchal@percona.com>
2025-05-13 20:13:08 +0530

    use pxb-tarball-job-addition branch package testing for pxb pt

--- pxb/jenkins/pxb-pt-testing-molecule.groovy
+++ pxb/jenkins/pxb-pt-testing-molecule.groovy
@@ -80,7 +80,7 @@
             stage('Checkout') {
                 steps {
                     deleteDir()
-                    git poll: false, branch: "master", url: "https://github.com/Percona-QA/package-testing.git"
+                    git poll: false, branch: "pxb-tarball-job-addition", url: "https://github.com/Percona-QA/package-testing.git"
                 }
             }
 
