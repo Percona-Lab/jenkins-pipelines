e106052c6d636859da39204e1fb3b9c1e4a22b1a
Keith Quinn <keith.quinn@percona.com>
2025-05-20 10:33:20 +0100

    Commenting out slack notifications

--- pml/pml-pkg-install-parallel.groovy
+++ pml/pml-pkg-install-parallel.groovy
@@ -85,12 +85,12 @@ pipeline {
          }
     }
     post {
-        success {
-            slackNotify("#mongodb_autofeed", "#00FF00", "[${JOB_NAME}]: package tests for PML(${PML_BRANCH}) with PSMDB Version(${psmdb_version}) finished succesfully - [${BUILD_URL}]")
-        }
-        failure {
-            slackNotify("#mongodb_autofeed", "#FF0000", "[${JOB_NAME}]: package tests for PML(${PML_BRANCH}) with PSMDB Version(${psmdb_version}) failed - [${BUILD_URL}]")
-        }
+//        success {
+//            slackNotify("#mongodb_autofeed", "#00FF00", "[${JOB_NAME}]: package tests for PML(${PML_BRANCH}) with PSMDB Version(${psmdb_version}) finished succesfully - [${BUILD_URL}]")
+//        }
+//        failure {
+//            slackNotify("#mongodb_autofeed", "#FF0000", "[${JOB_NAME}]: package tests for PML(${PML_BRANCH}) with PSMDB Version(${psmdb_version}) failed - [${BUILD_URL}]")
+//        }
         always {
             script {
                 moleculeParallelPostDestroy(pmlOperatingSystems("pml"), moleculeDir)
