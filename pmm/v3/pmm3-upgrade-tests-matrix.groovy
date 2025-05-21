1fbe1a37bad5823696edfe7fee6ce0848839bac9
Peter Sirotnak <sirotnak.p@gmail.com>
2025-05-21 12:05:37 +0200

    PMM-7: Refactors

--- pmm/v3/pmm3-upgrade-tests-matrix.groovy
+++ pmm/v3/pmm3-upgrade-tests-matrix.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: 'lib@PMM-7-PMM-v3-upgrade-job-matrix', retriever: modernSCM([
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
