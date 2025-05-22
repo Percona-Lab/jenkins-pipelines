59ecc0d4c19ca15be765b25d5c1bd9d9b8b75eca
Keith Quinn <keith.quinn@percona.com>
2025-05-22 10:30:05 +0100

    Debugging

--- vars/pmlOperatingSystems.groovy
+++ vars/pmlOperatingSystems.groovy
@@ -2,9 +2,8 @@ def call(String version = 'default', String newVersion = null, String gatedBuild
     Boolean isGatedBuild = gatedBuild.toLowerCase() == 'true'
 
     def switchValues = [
-//            ("pml"): ['debian-11', 'debian-12', 'ubuntu-focal', 'rhel8', 'rhel9', 'ubuntu-jammy', 'ubuntu-noble', 'rhel8-arm', 'rhel9-arm', 'ubuntu-focal-arm', 'ubuntu-jammy-arm', 'ubuntu-noble-arm', 'al2023', 'al2023-arm'],
-//            'default': ['centos-7', 'debian-10', 'debian-11', 'ubuntu-focal', 'rhel8', 'ubuntu-jammy'],
-            ("pml"): ['debian-11'],
+            ("pml"): ['debian-11', 'debian-12', 'ubuntu-focal', 'rhel8', 'rhel9', 'ubuntu-jammy', 'ubuntu-noble', 'rhel8-arm', 'rhel9-arm', 'ubuntu-focal-arm', 'ubuntu-jammy-arm', 'ubuntu-noble-arm', 'al2023', 'al2023-arm'],
+            'default': ['centos-7', 'debian-10', 'debian-11', 'ubuntu-focal', 'rhel8', 'ubuntu-jammy'],
     ]
 
     def versionValues = switchValues.find { key, value -> version ==~ key }?.value ?: switchValues['default']
