043b6c96cab9f6f301bd6c1ca5a5a0906cd86f77
Keith Quinn <keith.quinn@percona.com>
2025-05-22 14:56:09 +0100

    Removing arguments from OS call

--- vars/pmlOperatingSystems.groovy
+++ vars/pmlOperatingSystems.groovy
@@ -1,4 +1,4 @@
-def call(String version = 'default', String newVersion = null) {
+def call() {
         return [
             ("pml"): ['debian-11', 'debian-12', 'ubuntu-focal', 'rhel8', 'rhel9', 'ubuntu-jammy', 'ubuntu-noble', 'rhel8-arm', 'rhel9-arm', 'ubuntu-focal-arm', 'ubuntu-jammy-arm', 'ubuntu-noble-arm', 'al2023', 'al2023-arm']
     ]
