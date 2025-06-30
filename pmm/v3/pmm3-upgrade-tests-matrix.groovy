f1f27ce5ae034fda5c1d8b39e2b9a5c0a9a5723d
Peter Sirotnak <sirotnak.p@gmail.com>
2025-06-30 12:04:35 +0200

    PMM-7: Upgrade

--- pmm/v3/pmm3-upgrade-tests-matrix.groovy
+++ pmm/v3/pmm3-upgrade-tests-matrix.groovy
@@ -6,7 +6,7 @@ library changelog: false, identifier: 'lib@master', retriever: modernSCM([
 def pmmVersions = pmmVersion('v3')
 def latestVersion = pmmVersions[0]
 
-void runUpgradeJob(String PMM_UI_GIT_BRANCH, DOCKER_TAG, DOCKER_TAG_UPGRADE, CLIENT_VERSION, PMM_SERVER_LATEST, PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH) {
+void runUpgradeJob(String PMM_UI_GIT_BRANCH, DOCKER_TAG, DOCKER_TAG_UPGRADE, CLIENT_VERSION, CLIENT_REPOSITORY, PMM_SERVER_LATEST, PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH) {
     upgradeJob = build job: 'pmm3-upgrade-tests', parameters: [
         string(name: 'PMM_UI_GIT_BRANCH', value: PMM_UI_GIT_BRANCH),
         string(name: 'DOCKER_TAG', value: DOCKER_TAG),
@@ -76,7 +76,7 @@ pipeline {
         disableConcurrentBuilds()
     }
     triggers {
-        cron('0 3 * * *')
+        cron('0 1 * * *')
     }
     stages {
         stage('UI tests Upgrade Matrix') {
