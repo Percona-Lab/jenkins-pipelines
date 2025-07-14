19276442fbbb17b80f9d9759b7228d4c2c03a077
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-14 14:11:27 +0200

    PMM-7: Upgrade AMI

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -44,8 +44,7 @@ void runAMIStagingStart(String AMI_ID) {
   env.PMM_UI_URL = "https://${AMI_INSTANCE_IP}/"
   withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins-admin', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
     sh '''
-        ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no admin@${AMI_INSTANCE_IP} <<'ENDSSH'
-        cat <<EOF >> /home/admin/.config/systemd/user/pmm-server.env
+        ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no admin@${AMI_INSTANCE_IP} "bash -c 'cat <<EOF >> /home/admin/.config/systemd/user/pmm-server.env
         PMM_WATCHTOWER_HOST=http://watchtower:8080
         PMM_WATCHTOWER_TOKEN=123
         PMM_IMAGE=docker.io/perconalab/pmm-server:202506261956
@@ -55,10 +54,7 @@ void runAMIStagingStart(String AMI_ID) {
         PMM_DEV_PERCONA_PLATFORM_PUBLIC_KEY=RWTg+ZmCCjt7O8eWeAmTLAqW+1ozUbpRSKSwNTmO+exlS5KEIPYWuYdX
         PMM_DEV_PERCONA_PLATFORM_ADDRESS=https://check-dev.percona.com
         PMM_DEV_UPDATE_DOCKER_IMAGE=perconalab/pmm-server:3.2.0-rc
-        EOF
-
-        cat /home/admin/.config/systemd/user/pmm-server.env
-        ENDSSH
+        EOF'"
     '''
   }
 }
