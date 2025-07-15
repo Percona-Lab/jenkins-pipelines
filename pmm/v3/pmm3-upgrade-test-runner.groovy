db58ee5b5c7ca6fb66ed0fea1f6e8aaaab81bf0b
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-15 11:55:12 +0200

    PMM-7: Upgrade AMI

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -359,8 +359,8 @@ pipeline {
         stage('Sanity check') {
             steps {
                 sh '''
-                    curl \${PMM_URL}/ping
-                    timeout 100 bash -c \'while [[ "$(curl -s -o /dev/null -w \'\'%{http_code}\'\' \${PMM_URL}/ping)" != "200" ]]; do sleep 5; done\' || false
+                    curl -k \${PMM_URL}/ping
+                    timeout 100 bash -c \'while [[ "$(curl -k -s -o /dev/null -w \'\'%{http_code}\'\' \${PMM_URL}/ping)" != "200" ]]; do sleep 5; done\' || false
                 '''
             }
         }
