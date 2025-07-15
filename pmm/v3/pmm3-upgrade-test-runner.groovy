3264d4131a04b2f363476d680b5457966d2e8202
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-15 15:05:44 +0200

    PMM-7: Upgrade AMI

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -408,7 +408,7 @@ pipeline {
             steps {
                 script {
                     sh '''
-                        export PMM_VERSION=\$(curl --location -k --user admin:\${ADMIN_PASSWORD} \'\${PMM_UI_URL}\' | jq -r '.version' | awk -F "-" \'{print \$1}\')
+                        export PMM_VERSION=\$(curl --location -k --user admin:\${ADMIN_PASSWORD} \${PMM_UI_URL} | jq -r '.version' | awk -F "-" \'{print \$1}\')
                         sudo chmod 755 /srv/pmm-qa/pmm-tests/check_upgrade.py
                         python3 /srv/pmm-qa/pmm-tests/check_upgrade.py -v \$PMM_VERSION -p pre
                     '''
