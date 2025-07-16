3694d39a6c8b94fdbada6f8dd1f4f2c862f85898
Vadim Yalovets <vadim.yalovets@percona.com>
2025-07-16 13:58:12 +0300

    PKG-877 Create new param job to trigger tests for ProxySQL admin scrips v3

--- proxysql/qa-proxysql3-pipeline.groovy
+++ proxysql/qa-proxysql3-pipeline.groovy
@@ -1,6 +1,6 @@
 library changelog: false, identifier: 'lib@hetzner', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/adivinho/jenkins-pipelines.git'
 ])
 
 pipeline_timeout = 10
