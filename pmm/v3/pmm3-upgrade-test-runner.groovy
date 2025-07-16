0d6b6ac70e764ae947b314d8fcf0e037c5f9c86b
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-16 15:08:17 +0200

    PMM-7: Upgrade AMI

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -434,7 +434,8 @@ pipeline {
                              withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins-admin', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
                                 sh '''
                                     ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no admin@${AMI_INSTANCE_IP} "bash -c '
-                                        curl --location -k --user admin:\${ADMIN_PASSWORD} \${PMM_UI_URL}v1/server/version | jq -r \'.version\'
+                                        export=$(curl --location -k --user admin:\${ADMIN_PASSWORD} \${PMM_UI_URL}v1/server/version | jq -r \'.version\')
+                                        echo "PMM Version is: $PMM_VERSION"
                                         '
                                     "
                                 '''
