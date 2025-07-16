46a36c652a0187ee92b02c0179b837d99095fc7e
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-16 17:18:03 +0200

    PMM-7: Upgrade AMI

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -436,6 +436,9 @@ pipeline {
                                     ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no admin@${AMI_INSTANCE_IP} "bash -c '
                                         export PMM_VERSION=$(curl --location -k --user admin:\${ADMIN_PASSWORD} \${PMM_UI_URL}v1/server/version | jq -r \'.version\')
                                         echo \\${PMM_VERSION}
+                                        echo "PMM Version is: \\${PMM_VERSION}"
+                                        sudo chmod 755 /srv/pmm-qa/pmm-tests/check_upgrade.py
+                                        python3 /srv/pmm-qa/pmm-tests/check_upgrade.py -v \\$PMM_VERSION -p pre
                                         '
                                     "
                                 '''
