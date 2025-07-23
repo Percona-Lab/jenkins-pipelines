1c7c56d9caf558b58cb255915aab08abe4e8a16f
Vadim Yalovets <vadim.yalovets@percona.com>
2025-07-23 12:55:11 +0300

    js-lang-8.4.5

--- ps/jenkins/percona-server-for-mysql-8.0.groovy
+++ ps/jenkins/percona-server-for-mysql-8.0.groovy
@@ -336,9 +336,9 @@ parameters {
                         popArtifactFolder(params.CLOUD, "source_tarball/", AWS_STASH_PATH)
                         script {
                             if (env.FIPSMODE == 'YES') {
-                                buildStage("debian:buster", "--build_source_deb=1 --enable_fipsmode=1")
+                                buildStage("ubuntu:focal", "--build_source_deb=1 --enable_fipsmode=1")
                             } else {
-                                buildStage("debian:buster", "--build_source_deb=1")
+                                buildStage("ubuntu:focal", "--build_source_deb=1")
                             }
                         }
 
