4e8bc8accaca55af09741ed6753ed4521758bcc0
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-23 19:00:19 +0200

    PMM-7: Upgrade AMI

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -351,7 +351,7 @@ pipeline {
                         sh '''
                             curl -sL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
                             sudo bash nodesource_setup.sh
-                            sudo apt install nodejs
+                            sudo apt install -y nodejs
                             sudo apt-get install -y gettext
                             npm ci
                             sudo npx playwright install
