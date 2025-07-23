3bb9432321e962689f173a096f85df5a05e1152e
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-23 08:31:12 +0200

    PMM-7: Upgrade AMI

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -44,6 +44,8 @@ void runAMIStagingStart(String AMI_ID, PMM_QA_GIT_BRANCH) {
   env.VM_NAME = amiStagingJob.buildVariables.INSTANCE_ID
   env.PMM_URL = "https://admin:${ADMIN_PASSWORD}@${AMI_INSTANCE_IP}"
   env.PMM_UI_URL = "https://${AMI_INSTANCE_IP}/"
+  env.VM_IP = sh(script: "curl -s https://api.ipify.org", returnStdout: true).trim()
+  echo "Public IP: ${env.VM_IP}"
   withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins-admin', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
     sh """
         ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no admin@${AMI_INSTANCE_IP} 'bash -c "
