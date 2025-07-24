7e89e18fb68051d073c3967f22d7f29e676d6f75
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-24 08:51:01 +0200

    PMM-7: Upgrade AMI

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -366,6 +366,11 @@ pipeline {
                     set -o errexit
                     set -o xtrace
 
+                    curl -fsSL https://download.docker.com/linux/${OS_ID}/gpg | apt-key add -
+                    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/${OS_ID} ${OS_NAME} stable"
+                    sudo apt-get update
+                    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
                     sudo chown -R $(whoami):$(whoami) /srv/qa-integration
                     cd /srv/qa-integration/pmm_qa
                     echo "Setting docker based PMM clients"
