be1249f75efe60c7cb98b969b2db08fa8a7265a6
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-24 10:12:57 +0200

    PMM-7: Upgrade AMI

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -377,6 +377,8 @@ pipeline {
                         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
                     sudo apt-get update
                     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+                    sudo usermod -aG docker $(whoami)
+                    newgrp docker
 
                     sudo chown -R $(whoami):$(whoami) /srv/qa-integration
                     cd /srv/qa-integration/pmm_qa
