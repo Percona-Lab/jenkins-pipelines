909c210247f6501452cba47a4d04a0f412b7f89d
Yash <46398416+panchal-yash@users.noreply.github.com>
2025-07-25 09:13:38 -0400

    ps package testing: (#3445)
    add aws instance termination post completion
    
    pxc package testing:
     add aws instance termination post completion
     add al23 support
     update test_type option for pro and non-pro
     simplify the setup
     add git_repo and Branch in job [non-pro,pro,parallel,]
    
    ps-innodb-cluster jobs:
     add git_repo and git_branch selection for test-ps-innodb-cluster
     instance termination post completion
     add git_repo and Branch in job
    
    remove:
     old PS 57,80,81,innovation-lts package testing jobs
    
    pxb package testing:
     remove old jobs
     add aws instance termination post completion
    
    pxb-tarball package testing:
     add aws instance termination post completion

--- pxc/jenkins/pxc-package-testing-parallel.groovy
+++ pxc/jenkins/pxc-package-testing-parallel.groovy
@@ -3,7 +3,21 @@ library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
-List all_nodes = [
+List pro_nodes = [
+                'ubuntu-noble',
+                'ubuntu-jammy',
+                'ubuntu-noble-arm',
+                'ubuntu-jammy-arm',
+                'debian-12',
+                'debian-12-arm',
+                'ol-9',
+                'rhel-9',
+                'rhel-9-arm',
+                'amazon-linux-2023',
+                'amazon-linux-2023-arm'
+]   
+
+List non_pro_nodes = [
                 'ubuntu-noble',
                 'ubuntu-jammy',
                 'ubuntu-noble-arm',
@@ -13,21 +27,26 @@ List all_nodes = [
                 'debian-11',
                 'debian-12-arm',
                 'debian-11-arm',
-                'debian-10',
-                'centos-7',
                 'ol-8',
                 'ol-9',
                 'rhel-8',
                 'rhel-9',
-                'rhel-8-arm',
-                'rhel-9-arm'
+                'amazon-linux-2023',
+                'amazon-linux-2023-arm'
 ]
 
+List pxc57_nodes = [
+                'ubuntu-jammy',
+                'ubuntu-focal',
+                'debian-12',
+                'debian-11',
+                'ol-8',
+                'ol-9'
+]
+
+List all_possible_nodes = (pro_nodes + non_pro_nodes).unique()
+
 product_to_test = params.product_to_test
-
-List nodes_to_test = []
-
-nodes_to_test = all_nodes
 
 void runNodeBuild(String node_to_test) {
 
@@ -36,29 +55,93 @@ void runNodeBuild(String node_to_test) {
         echo "${test_type}"
     }
     else{
-    //    test_type = "install_and_upgrade"
         echo "${test_type}"
     }
 
-    build(
-        job: 'pxc-package-testing',
-        parameters: [
-            string(name: "product_to_test", value: params.product_to_test),
-            string(name: "node_to_test", value: node_to_test),
-            string(name: "test_repo", value: params.test_repo),
-            string(name: "test_type", value: "${test_type}"),
-            string(name: "pxc57_repo", value: params.pxc57_repo)
-        ],
-        propagate: true,
-        wait: true
-    )
+    if (params.product_to_test == "pxc-innovation-lts" || params.product_to_test == "pxc57") {
+        if (pro_repo == "yes") {
+            echo "PRO is not supported for PXC-5.7 or PXC-innovation-lts testing"
+        } else {
+            echo "Normal testing"
+            job = "pxc-package-testing"
+            env.JOB_TO_RUN = "${job}"
+            test_type = params.test_type
+
+            build(
+                job: "${job}",
+                parameters: [
+                    string(name: "product_to_test", value: params.product_to_test),
+                    string(name: "node_to_test", value: node_to_test),
+                    string(name: "test_repo", value: params.test_repo),
+                    string(name: "test_type", value: "${test_type}"),
+                    string(name: "pxc57_repo", value: params.pxc57_repo)
+                ],
+                propagate: true,
+                wait: true
+            )
+        }   
+    }
+    else if (params.product_to_test == "pxc84" || params.product_to_test == "pxc80") {
+
+        echo "Testing PXC-8.4 or PXC-8.0 PRO packages"
+
+        if (pro_repo == "yes") {
+            echo "Testing PRO packages"
+            job = "pxc-package-testing-pro"
+            env.JOB_TO_RUN = "${job}"
+            test_type = params.test_type_pro
+
+            build(
+                job: "${job}",
+                parameters: [
+                    string(name: "product_to_test", value: params.product_to_test),
+                    string(name: "node_to_test", value: node_to_test),
+                    string(name: "test_repo", value: params.test_repo),
+                    string(name: "test_type", value: "${test_type}"),
+                    string(name: "git_repo", value: params.git_repo),
+                    string(name: "BRANCH", value: params.BRANCH)
+                ],
+                propagate: true,
+                wait: true
+            )
+
+        } else {
+            echo "Testing Non Pro packages"
+            job = "pxc-package-testing"
+            env.JOB_TO_RUN = "${job}"
+            test_type = params.test_type
+
+            build(
+                job: "${job}",
+                parameters: [
+                    string(name: "product_to_test", value: params.product_to_test),
+                    string(name: "node_to_test", value: node_to_test),
+                    string(name: "test_repo", value: params.test_repo),
+                    string(name: "test_type", value: "${test_type}"),
+                    string(name: "pxc57_repo", value: params.pxc57_repo),
+                    string(name: "git_repo", value: params.git_repo),
+                    string(name: "BRANCH", value: params.BRANCH)
+                ],
+                propagate: true,
+                wait: true
+            )
+
+        }
+
+
+    } else {
+        error("Unsupported product_to_test for PRO testing: ${params.product_to_test}")
+    }
+    echo "inside runNodeBuild job_to_run is ${env.JOB_TO_RUN}"
 }
 
 pipeline {
     agent {
         label 'docker'
     }
-
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '100'))
+    }
     parameters {
         choice(
             name: 'product_to_test',
@@ -83,287 +166,112 @@ pipeline {
 
         choice(
             name: "pxc57_repo",
-            choices: ["original","pxc57","EOL"],
+            choices: ["EOL","original","pxc57"],
             description: "PXC-5.7 packages are located in 2 repos: pxc-57 and original and both should be tested. Choose which repo to use for test."
         )
 
         choice(
             name: 'test_type',
             choices: [
-                'install',
-                'install_and_upgrade',
-                'min_upgrade',
-                'maj_upgrade'
+                "install"
+                ,"min_upgrade_pxc57_eol_main_to_eol_testing"
+                ,"min_upgrade_pxc_80"
+                ,"min_upgrade_pxc_84"
+                ,"min_upgrade_pxc_innovation"
             ],
-            description: 'Set test type for testing'
+            description: 'Supports PXC57, PXC80, PXC84, PXC Innovation NON PRO Packages'
         )
+
+        choice(
+            name: 'test_type_pro',
+            choices: [
+                'install',
+                'min_upgrade_pro_pro',
+                'min_upgrade_nonpro_pro',
+                'min_upgrade_pro_nonpro',
+            ],
+            description: 'Supports PXC80 and PXC84 PRO packages'
+        )
+
+        choice(
+            name: 'pro_repo',
+            choices: [
+                'no',
+                'yes'
+            ],
+            description: 'Set if PRO packages should be tested or not (PXC 80 and PXC 84)'
+        )
+
+        string(
+            name: 'git_repo',
+            defaultValue: "Percona-QA/package-testing",
+            description: 'Git repository to use for testing'
+        )
+        
+        string(
+            name: 'BRANCH',
+            defaultValue: 'master',
+            description: 'Git branch to use for testing'
+        )
+
     }
 
     stages {
         stage("Prepare") {
             steps {
                 script {
-                    currentBuild.displayName = "#${BUILD_NUMBER}-${product_to_test}-${params.test_repo}-all"
-                    currentBuild.description = "action: ${params.action_to_test} node: ${params.node_to_test}"
+                    if (params.pro_repo == "yes") {
+                        currentBuild.displayName = "#${BUILD_NUMBER}-${product_to_test}-${params.test_repo}-pro=${params.pro_repo}-${params.test_type_pro}"
+                    }else {
+                        currentBuild.displayName = "#${BUILD_NUMBER}-${product_to_test}-${params.test_repo}-pro=${params.pro_repo}-${params.test_type}"
+                    }
                 }
             }
         }
 
         stage("Run parallel") {
-            parallel {
-                stage("Debian-10") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("debian-10")
+            steps {
+                script {
 
-                            }
-                        }
+                    def selectedNodes
+                    def jobType
+                    def allPossibleNodes
+                    
+                    if (params.product_to_test == "pxc57") {
+                        selectedNodes = pxc57_nodes
+                        jobType = "PXC 5.7"
+                        allPossibleNodes = pxc57_nodes
+                    } else {
+                        selectedNodes = (params.pro_repo == "yes") ? pro_nodes : non_pro_nodes
+                        jobType = (params.pro_repo == "yes") ? "PRO" : "Non Pro"
+                        allPossibleNodes = all_possible_nodes
                     }
 
-                    steps {
-                        runNodeBuild("debian-10")
-                    }
-                }
-
-                stage("Debian-11") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("debian-11")
-
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("debian-11")
-                    }
-                }
-
-                stage("Debian-12") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("debian-12")
-
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("debian-12")
-                    }
-                }
-
-                stage("Debian-11-arm") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("debian-11-arm")
-
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("debian-11-arm")
-                    }
-                }
-
-                stage("Debian-12-arm") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("debian-12-arm")
-
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("debian-12-arm")
-                    }
-                }
-
-                stage("Centos 7") {
-                    when {
-                        expression {
-                            allOf{                            
-                                nodes_to_test.contains("centos-7")
-
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("centos-7")
-                    }
-                }
-
-                stage("ol-8") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("ol-8")
-
-                            }
-                        }
-                    }
-                    steps {
-                        runNodeBuild("ol-8")
-                    }
-                }
-
-                stage("ol-9") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("ol-9")
-                            
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("ol-9")
-                    }
-                }
-
-                stage("rhel-8") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("rhel-8")
-
-                            }
-                        }
-                    }
-                    steps {
-                        runNodeBuild("rhel-8")
-                    }
-                }
-
-                stage("rhel-9") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("rhel-9")
-                            
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("rhel-9")
-                    }
-                }
-
-                stage("rhel-8-arm") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("rhel-8-arm")
-
-                            }
-                        }
-                    }
-                    steps {
-                        runNodeBuild("rhel-8-arm")
-                    }
-                }
-
-                stage("rhel-9-arm") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("rhel-9-arm")
-                            
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("rhel-9-arm")
-                    }
-                }
-
-                stage("ubuntu-noble") {
-                    when {
-                        expression {
-                            allOf{                            
-                                nodes_to_test.contains("ubuntu-noble")
-
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("ubuntu-noble")
-                    }
-                }
-
-                stage("ubuntu-jammy") {
-                    when {
-                        expression {
-                            allOf{                            
-                                nodes_to_test.contains("ubuntu-jammy")
-
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("ubuntu-jammy")
-                    }
-                }
-
-                stage("ubuntu-noble-arm") {
-                    when {
-                        expression {
-                            allOf{                            
-                                nodes_to_test.contains("ubuntu-noble-arm")
-
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("ubuntu-noble-arm")
-                    }
-                }
-
-                stage("ubuntu-jammy-arm") {
-                    when {
-                        expression {
-                            allOf{                            
-                                nodes_to_test.contains("ubuntu-jammy-arm")
-
-                            }
-                        }
-                    }
-
-                    steps {
-                        runNodeBuild("ubuntu-jammy-arm")
-                    }
-                }
+                    echo "Testing ${selectedNodes.size()}/${all_possible_nodes.size()} nodes for ${jobType} job"
+                    
+                    def parallelStages = [:]
+                    
+                    all_possible_nodes.each { node ->
+                        parallelStages[node] = {
                 
-                stage("ubuntu-focal") {
-                    when {
-                        expression {
-                            allOf{
-                                nodes_to_test.contains("ubuntu-focal")
-
+                            stage("${node}") {
+                                if (selectedNodes.contains(node)) {
+                                    echo "Running ${jobType} tests for: ${node}"
+                                    runNodeBuild(node)
+                                } else {
+                                    echo "SKIPPED: ${node} - not in ${jobType} job"
+                                    echo "This OS is not part of the ${jobType} testing suite"
+                                }
                             }
                         }
                     }
-
-                    steps {
-                        runNodeBuild("ubuntu-focal")
-                    }
+                    
+                    parallel parallelStages
                 }
-
             }
         }
     }
+
 }
+
+
