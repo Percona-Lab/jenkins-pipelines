948344a7d8eb4fb92932f7a770f5cc4f57ee547d
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-28 14:56:54 +0200

    PMM-7: Fixes

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -285,6 +285,10 @@ pipeline {
                 stage('Install dependencies') {
                     steps {
                         sh '''
+                            curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+                            sudo bash nodesource_setup.sh
+                            sudo apt install nodejs
+                            sudo apt-get install -y gettext
                             npm ci
                             npx playwright install
                             envsubst < env.list > env.generated.list
