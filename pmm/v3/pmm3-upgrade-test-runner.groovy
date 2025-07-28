df824a305aba8c3835defa13dc42e6defceaf836
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-28 14:06:10 +0200

    PMM-7: Fixes

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -213,7 +213,7 @@ pipeline {
                         $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
                         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
                     sudo apt-get update
-                    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+                    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
                     docker network create pmm-qa
                     docker volume create pmm-volume
