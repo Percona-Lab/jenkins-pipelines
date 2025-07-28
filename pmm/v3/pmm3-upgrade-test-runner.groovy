981160d229aab9ec4232f6c3f17282940c343c51
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-28 18:24:06 +0200

    PMM-7: Fixes

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -30,7 +30,8 @@ void checkClientBeforeUpgrade(String PMM_SERVER_VERSION, String CLIENT_VERSION) 
     } else {
         sh '''
             sudo su
-            ls -l $(which pmm-admin)
+            $(which pmm-admin)
+            ls -l
             sudo chmod 755 /srv/pmm-qa/pmm-tests/check_client_upgrade.py
             python3 /srv/pmm-qa/pmm-tests/check_client_upgrade.py ${PMM_VERSION}
         '''
