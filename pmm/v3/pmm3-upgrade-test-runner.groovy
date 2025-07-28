41ee22aedd07b88c77ec06f7f862b9dd58ce8010
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-28 14:01:07 +0200

    PMM-7: Fixes

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -199,7 +199,21 @@ pipeline {
                     sudo mkdir -p /srv/qa-integration || true
                     cd /srv/qa-integration
                     sudo git clone --single-branch --branch \${QA_INTEGRATION_GIT_BRANCH} https://github.com/Percona-Lab/qa-integration.git .
-                    sudo chown $(whoami) -R /srv/qa-integration
+                    sudo chown $(id -u):$(id -u) -R /srv/qa-integration
+
+                    sudo apt-get update
+                    sudo apt-get install ca-certificates curl
+                    sudo install -m 0755 -d /etc/apt/keyrings
+                    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+                    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+                    # Add the repository to Apt sources:
+                    echo \
+                        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+                        $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+                        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                    sudo apt-get update
+                    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
                     docker network create pmm-qa
                     docker volume create pmm-volume
