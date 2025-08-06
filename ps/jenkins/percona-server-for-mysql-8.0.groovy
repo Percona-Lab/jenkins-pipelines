f7452df9df9db696fb0fa3040dfab8315a5a33d0
Vadim Yalovets <vadim.yalovets@percona.com>
2025-08-06 18:05:27 +0300

    PKG-870 Packaging tasks for release - PS 8.0.43-34

--- ps/jenkins/percona-server-for-mysql-8.0.groovy
+++ ps/jenkins/percona-server-for-mysql-8.0.groovy
@@ -450,46 +450,6 @@ parameters {
                         pushArtifactFolder(params.CLOUD, "rpm/", AWS_STASH_PATH)
                     }
                 }
-                stage('Oracle Linux 9') {
-                    agent {
-                        label params.CLOUD == 'Hetzner' ? 'docker-x64' : 'docker-32gb'
-                    }
-                    steps {
-                        cleanUpWS()
-                        installCli("rpm")
-                        unstash 'properties'
-                        popArtifactFolder(params.CLOUD, "srpm/", AWS_STASH_PATH)
-                        script {
-                            if (env.FIPSMODE == 'YES') {
-                                buildStage("oraclelinux:9", "--build_rpm=1 --with_zenfs=1 --enable_fipsmode=1")
-                            } else {
-                                buildStage("oraclelinux:9", "--build_rpm=1 --with_zenfs=1")
-                            }
-                        }
-
-                        pushArtifactFolder(params.CLOUD, "rpm/", AWS_STASH_PATH)
-                    }
-                }
-                stage('Oracle Linux 9 ARM') {
-                    agent {
-                        label params.CLOUD == 'Hetzner' ? 'docker-aarch64' : 'docker-32gb-aarch64'
-                    }
-                    steps {
-                        cleanUpWS()
-                        installCli("rpm")
-                        unstash 'properties'
-                        popArtifactFolder(params.CLOUD, "srpm/", AWS_STASH_PATH)
-                        script {
-                            if (env.FIPSMODE == 'YES') {
-                                buildStage("oraclelinux:9", "--build_rpm=1 --enable_fipsmode=1")
-                            } else {
-                                buildStage("oraclelinux:9", "--build_rpm=1")
-                            }
-                        }
-
-                        pushArtifactFolder(params.CLOUD, "rpm/", AWS_STASH_PATH)
-                    }
-                }
                 stage('Oracle Linux 10') {
                     agent {
                         label params.CLOUD == 'Hetzner' ? 'docker-x64' : 'docker-32gb'
