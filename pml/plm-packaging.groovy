ba15e12303663b3d984c0faeff2ba593b6bc2985
Keith Quinn <keith.quinn@percona.com>
2025-08-08 15:00:59 +0100

    Testing

--- pml/plm-packaging.groovy
+++ pml/plm-packaging.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: "lib@master", retriever: modernSCM([
+library changelog: false, identifier: "lib@PKG-876", retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ])
