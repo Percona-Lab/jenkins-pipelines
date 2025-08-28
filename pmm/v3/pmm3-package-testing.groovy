3f02a0f26cc24d835aedb8bab0e53ed6faecf4e4
Peter Sirotnak <sirotnak.p@gmail.com>
2025-08-28 12:19:12 +0200

    PMM-7: Add support for rhel 10

--- pmm/v3/pmm3-package-testing.groovy
+++ pmm/v3/pmm3-package-testing.groovy
@@ -40,7 +40,7 @@ void setup_rhel_10_package_tests()
     sh '''
         sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
         sudo dnf -y update
-        sudo dnf install -y ansible-core git wget dpkg
+        sudo dnf install -y ansible-core git wget
     '''
 }
 
