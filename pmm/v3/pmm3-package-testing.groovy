caedbe7f27122c63a69ff2ca3ae3230a4b2c665a
Peter Sirotnak <sirotnak.p@gmail.com>
2025-08-28 10:55:31 +0200

    PMM-7: Add support for rhel 10

--- pmm/v3/pmm3-package-testing.groovy
+++ pmm/v3/pmm3-package-testing.groovy
@@ -174,6 +174,20 @@ pipeline {
                         }
                     }
                 }
+                stage('rhel-10-arm64') {
+                    agent {
+                        label 'min-rhel-10-arm64'
+                    }
+                    steps{
+                        setup_rhel_package_tests()
+                        run_package_tests(GIT_BRANCH, TESTS, INSTALL_REPO)
+                    }
+                    post {
+                        always {
+                            deleteDir()
+                        }
+                    }
+                }
                 stage('jammy-arm64') {
                     agent {
                         label 'min-jammy-arm64'
