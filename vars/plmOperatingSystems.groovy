98fd149be707c031fc0e48793dd397f4d7da115c
Sandra Romanchenko <53295797+sandraromanchenko@users.noreply.github.com>
2025-09-02 13:57:20 +0300

    PKG-1083. Temporarily remove PLM support for EL10, v2 (#3523)

--- vars/plmOperatingSystems.groovy
+++ vars/plmOperatingSystems.groovy
@@ -1,3 +1,3 @@
 def call() {
-        return ['debian-11', 'debian-11-arm', 'debian-12', 'debian-12-arm', 'rhel8', 'rhel9', 'ubuntu-jammy', 'ubuntu-noble', 'rhel8-arm', 'rhel9-arm', 'rhel10-arm', 'ubuntu-jammy-arm', 'ubuntu-noble-arm', 'al2023', 'al2023-arm']
+        return ['debian-11', 'debian-11-arm', 'debian-12', 'debian-12-arm', 'rhel8', 'rhel9', 'ubuntu-jammy', 'ubuntu-noble', 'rhel8-arm', 'rhel9-arm', 'ubuntu-jammy-arm', 'ubuntu-noble-arm', 'al2023', 'al2023-arm']
 }
