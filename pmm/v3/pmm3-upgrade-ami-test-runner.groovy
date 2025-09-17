db136ff5832a06606df82a04dab9edc1ca82145c
Peter Sirotnak <sirotnak.p@gmail.com>
2025-09-17 14:42:44 +0200

    PMM-7: Upgrade

--- pmm/v3/pmm3-upgrade-ami-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-ami-test-runner.groovy
@@ -7,22 +7,34 @@ void checkClientBeforeUpgrade(String PMM_SERVER_VERSION, String CLIENT_VERSION) 
     def PMM_VERSION = CLIENT_VERSION.trim();
     env.PMM_VERSION = PMM_VERSION;
     if (PMM_VERSION == '3-dev-latest') {
-        sh '''
-            GET_PMM_CLIENT_VERSION=$(wget -q https://raw.githubusercontent.com/Percona-Lab/pmm-submodules/v3/VERSION -O -)
-            sudo chmod 755 /srv/pmm-qa/pmm-tests/check_client_upgrade.py
-            python3 /srv/pmm-qa/pmm-tests/check_client_upgrade.py ${GET_PMM_CLIENT_VERSION}
-        '''
+        withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins-admin', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
+            sh """
+                ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no admin@${env.CLIENT_IP} 'bash -c "
+                    GET_PMM_CLIENT_VERSION=$(wget -q https://raw.githubusercontent.com/Percona-Lab/pmm-submodules/v3/VERSION -O -)
+                    sudo chmod 755 /srv/pmm-qa/pmm-tests/check_client_upgrade.py
+                    python3 /srv/pmm-qa/pmm-tests/check_client_upgrade.py ${GET_PMM_CLIENT_VERSION}
+                 "'
+            """
+        }
     } else if (PMM_VERSION == 'pmm3-rc') {
-        sh '''
-            GET_PMM_CLIENT_VERSION=$(wget -q "https://registry.hub.docker.com/v2/repositories/perconalab/pmm-client/tags?page_size=25&name=rc" -O - | jq -r .results[].name  | grep 3.*.*-rc$ | sort -V | tail -n1)
-            sudo chmod 755 /srv/pmm-qa/pmm-tests/check_client_upgrade.py
-            python3 /srv/pmm-qa/pmm-tests/check_client_upgrade.py ${GET_PMM_CLIENT_VERSION}
-        '''
+        withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins-admin', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
+            sh """
+                ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no admin@${env.CLIENT_IP} 'bash -c "
+                    GET_PMM_CLIENT_VERSION=$(wget -q "https://registry.hub.docker.com/v2/repositories/perconalab/pmm-client/tags?page_size=25&name=rc" -O - | jq -r .results[].name  | grep 3.*.*-rc$ | sort -V | tail -n1)
+                    sudo chmod 755 /srv/pmm-qa/pmm-tests/check_client_upgrade.py
+                    python3 /srv/pmm-qa/pmm-tests/check_client_upgrade.py ${GET_PMM_CLIENT_VERSION}
+                 "'
+            """
+        }
     } else {
-        sh '''
-            sudo chmod 755 /srv/pmm-qa/pmm-tests/check_client_upgrade.py
-            python3 /srv/pmm-qa/pmm-tests/check_client_upgrade.py ${PMM_VERSION}
-        '''
+        withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins-admin', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
+            sh """
+                ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no admin@${env.CLIENT_IP} 'bash -c "
+                    sudo chmod 755 /srv/pmm-qa/pmm-tests/check_client_upgrade.py
+                    python3 /srv/pmm-qa/pmm-tests/check_client_upgrade.py ${PMM_VERSION}
+                "'
+            """
+        }
     }
 }
 
