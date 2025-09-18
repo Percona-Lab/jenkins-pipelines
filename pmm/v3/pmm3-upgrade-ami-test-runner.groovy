732fb5c891f2bfdc4a375f500ff5001d1ed31ee9
Peter Sirotnak <sirotnak.p@gmail.com>
2025-09-18 12:38:08 +0200

    PMM-7: Upgrade

--- pmm/v3/pmm3-upgrade-ami-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-ami-test-runner.groovy
@@ -200,23 +200,6 @@ pipeline {
                     sudo chmod -R 755 /srv/qa-integration
                     sudo chown $(id -u):$(id -u) -R /srv/qa-integration
                     sudo ln -s /usr/bin/chromium-browser /usr/bin/chromium
-
-                    sudo apt-get update
-                    sudo apt-get install ca-certificates curl
-                    sudo install -m 0755 -d /etc/apt/keyrings
-                    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-                    sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-                     # Add the repository to Apt sources:
-                    echo \
-                        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-                        $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-                        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-                    sudo apt-get update
-                    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin ansible
-                    sudo usermod -aG docker $(whoami)
-                    newgrp docker
-                    sudo chmod 666 /var/run/docker.sock
                 '''
             }
         }
