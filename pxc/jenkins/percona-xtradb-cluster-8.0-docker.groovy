8ba0efb2330a6385efa7346713b9e9e6cbe3cc4b
Vadim Yalovets <vadim.yalovets@percona.com>
2025-09-25 16:44:47 +0300

    PKG-1026 Packaging tasks for release - PXC 8.4.6

--- pxc/jenkins/percona-xtradb-cluster-8.0-docker.groovy
+++ pxc/jenkins/percona-xtradb-cluster-8.0-docker.groovy
@@ -129,7 +129,7 @@ pipeline {
                             sed -i "s:yum/release:yum/testing:g" Dockerfile
                             if [ ${PXC_MAJOR_RELEASE} != "80" ]; then
                                 #sed -i "s/ENV PXB_VERSION.*/ENV PXB_VERSION ${MYSQL_VERSION_MAJOR}.${MYSQL_VERSION_MINOR}.${MYSQL_VERSION_PATCH}${MYSQL_VERSION_EXTRA}.${RPM_RELEASE}/g" Dockerfile
-                                sed -i "s/ENV PXB_VERSION.*/ENV PXB_VERSION 8.4.0-2.1/g" Dockerfile
+                                sed -i "s/ENV PXB_VERSION.*/ENV PXB_VERSION 8.4.0-4.1/g" Dockerfile
                                 sed -i "s/ENV PS_VERSION.*/ENV PS_VERSION ${MYSQL_VERSION_MAJOR}.${MYSQL_VERSION_MINOR}.${MYSQL_VERSION_PATCH}${MYSQL_VERSION_EXTRA}.${RPM_RELEASE}/g" Dockerfile
                                 if [ ${PXC_MAJOR_RELEASE} != "84" ]; then
                                     sed -i "s/tools/pxb-8x-innovation/g" Dockerfile
