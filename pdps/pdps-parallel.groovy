1b2ef1fdbaa7bf61ba959ab121c8929eb7c8c512
kaushikpuneet07 <puneet.kaushik@percona.com>
2025-09-28 23:07:34 +0530

    restore repo

--- pdps/pdps-parallel.groovy
+++ pdps/pdps-parallel.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: "lib@rhel10-ps", retriever: modernSCM([
+library changelog: false, identifier: "lib@master", retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ])
 
 pipeline {
