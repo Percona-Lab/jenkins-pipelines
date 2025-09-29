f7a5d011d7451c8a25c088e9bf1710bed5bbd99f
Peter Sirotnak <sirotnak.p@gmail.com>
2025-09-29 12:11:54 +0200

    PMM-7: Refactor

--- pmm/v3/pmm3-upgrade-ami-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-ami-test-runner.groovy
@@ -296,6 +296,7 @@ pipeline {
                 withCredentials([aws(accessKeyVariable: 'BACKUP_LOCATION_ACCESS_KEY', credentialsId: 'BACKUP_E2E_TESTS', secretKeyVariable: 'BACKUP_LOCATION_SECRET_KEY'), aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'PMM_AWS_DEV', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
                     sh '''
                         ./node_modules/.bin/codeceptjs run-multiple parallel --reporter mocha-multi -c pr.codecept.js --steps --grep '@ami-ovf-pre-upgrade'
+                        export ADMIN_PASSWORD="pmm3admin!"
                     '''
                 }
             }
