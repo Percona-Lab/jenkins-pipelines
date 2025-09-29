81e450bc1325bf396627b9a8e95ec4f70b1644bb
Peter Sirotnak <sirotnak.p@gmail.com>
2025-09-29 19:42:14 +0200

    PMM-7: Refactor

--- pmm/v3/pmm3-upgrade-ami-tests.groovy
+++ pmm/v3/pmm3-upgrade-ami-tests.groovy
@@ -5,6 +5,7 @@ library changelog: false, identifier: 'lib@master', retriever: modernSCM([
 
 def versionsList = pmmVersion('v3-ami')
 def amiVersions = versionsList.values()
+def versions = versionsList.keySet()
 def latestVersion = versions[versions.size() - 1]
 
 void runUpgradeJob(String PMM_UI_GIT_BRANCH, AMI_TAG, DOCKER_TAG_UPGRADE, CLIENT_VERSION, CLIENT_REPOSITORY, PMM_SERVER_LATEST, PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH) {
