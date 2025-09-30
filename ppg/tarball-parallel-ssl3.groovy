b4ac07670749d4c8daa4abfeb973d34217bf52bd
Naeem Akhter <40981522+Naeem-Akhter@users.noreply.github.com>
2025-09-30 21:53:38 +0500

    Adding Debian-13 (Trixie) as a supported platform in ppg test workflows. Clean up and update of versions for pg-18. (#3566)

--- ppg/tarball-parallel-ssl3.groovy
+++ ppg/tarball-parallel-ssl3.groovy
@@ -29,7 +29,7 @@ pipeline {
             ]
         )
         string(
-            defaultValue: 'ppg-17.6',
+            defaultValue: 'ppg-18.0',
             description: 'PG version for test',
             name: 'VERSION'
         )
