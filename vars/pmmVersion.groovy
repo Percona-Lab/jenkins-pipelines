54021d251186e12eede7fc6c1ed5777eaa85cca4
Peter Sirotnak <sirotnak.p@gmail.com>
2025-09-30 14:02:50 +0200

    PMM-7: Refactor

--- vars/pmmVersion.groovy
+++ vars/pmmVersion.groovy
@@ -73,6 +73,7 @@ def call(String type='dev-latest') {
     '3.3.0': 'ami-0569db88919e78ee2',
     '3.3.1': 'ami-009c9a577432158a3',
     '3.4.0': 'ami-0f26e28bb71f9bafd',
+    '3.5.0': 'ami-037000c4d14b70792',
   ]
   List<String> v3 = v3Versions.keySet() as List<String>;
 
