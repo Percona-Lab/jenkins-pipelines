5efd851723decfa94e498bdcb6d4bd2d4f575d1e
Peter Sirotnak <sirotnak.p@gmail.com>
2025-09-30 12:28:53 +0200

    PMM-7: Refactor

--- pmm/v3/pmm3-upgrade-ami-tests.groovy
+++ pmm/v3/pmm3-upgrade-ami-tests.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+library changelog: false, identifier: 'lib@PMM-14156', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
