49a6035ca84d6809b5bdadf108ceeffc080576d5
Peter Sirotnak <sirotnak.p@gmail.com>
2025-10-01 20:30:04 +0200

    PMM-7: Fix debian prepare

--- pmm/v3/pmm3-package-testing.groovy
+++ pmm/v3/pmm3-package-testing.groovy
@@ -60,10 +60,10 @@ void setup_debian_package_tests()
 void setup_debian_trixie_package_tests()
 {
     sh '''
-        sudo apt-get install -y git wget python3-pip dirmngr gnupg2
+        wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/ansible.list
         sudo apt update -y
-        pip3 install --user ansible
-        export PATH=$HOME/.local/bin:$PATH
+        sudo apt-get install -y git wget dirmngr gnupg2 ansible
         ansible --version
     '''
 }
