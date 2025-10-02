736d9f1a06c42a844362526634a504cbbba5b3f1
Peter Sirotnak <sirotnak.p@gmail.com>
2025-10-02 12:30:34 +0200

    PMM-7: Move package test

--- pmm/v3/pmm3-package-testing.groovy
+++ pmm/v3/pmm3-package-testing.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: 'lib@PMM-7-fix-package-tests-nightly', retriever: modernSCM([
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
