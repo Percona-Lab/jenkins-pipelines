49b545e13178abe1f6bd3ebdc5a406b891e16419
Peter Sirotnak <sirotnak.p@gmail.com>
2025-10-08 09:39:10 +0200

    PMM-7: Log versions

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -138,6 +138,10 @@ pipeline {
                     println CLIENT_VERSION.trim()
                     println clientRepoAvailableVersions
                     println clientRepoAvailableVersions.contains(CLIENT_VERSION.trim())
+                    if (!clientRepoAvailableVersions.contains(CLIENT_VERSION.trim())) {
+                        env.CLIENT_VERSION = "https://downloads.percona.com/downloads/pmm3/3.0.0/binary/tarball/pmm-client-${CLIENT_VERSION}-x86_64.tar.gz"
+                    }
+                    println CLIENT_VERSION
                     env.ADMIN_PASSWORD = params.ADMIN_PASSWORD
                     currentBuild.description = "${env.UPGRADE_FLAG} - Upgrade for PMM from ${env.DOCKER_TAG.split(":")[1]} to ${env.PMM_SERVER_LATEST}."
                 }
