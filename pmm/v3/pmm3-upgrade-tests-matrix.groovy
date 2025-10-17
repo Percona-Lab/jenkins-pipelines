91f1427fef9dadb7fe96bbfdd2386bad617d75dc
Peter Sirotnak <sirotnak.p@gmail.com>
2025-10-17 09:11:15 +0200

    PMM-7: Remove latest version

--- pmm/v3/pmm3-upgrade-tests-matrix.groovy
+++ pmm/v3/pmm3-upgrade-tests-matrix.groovy
@@ -3,7 +3,7 @@ library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
-def pmmVersions = pmmVersion('v3')
+def pmmVersions = pmmVersion('v3')[0..-2]
 def latestVersion = pmmVersions[0]
 
 void runUpgradeJob(String PMM_UI_GIT_BRANCH, DOCKER_TAG, DOCKER_TAG_UPGRADE, CLIENT_VERSION, CLIENT_REPOSITORY, PMM_SERVER_LATEST, PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH) {
@@ -23,6 +23,7 @@ def generateVariants(String PMM_UI_GIT_BRANCH, PMM_SERVER_LATEST, PMM_QA_GIT_BRA
     def results = new HashMap<>();
     def latestVersion = PMM_SERVER_LATEST;
 
+// 3.0.0, 3.1.0, 3.2.0, 3.3.0, 3.3.1, 3.4.0, 3.4.1, 3.5.0
     for (pmmVersion in pmmVersions) {
         if(pmmVersion == latestVersion) {
             results.put(
