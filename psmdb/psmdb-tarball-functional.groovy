0b9896973183833cfc217abae0c561301c837c61
Keith Quinn <keith.quinn@percona.com>
2025-10-20 11:48:09 +0100

    Adding env var for vault trial license

--- psmdb/psmdb-tarball-functional.groovy
+++ psmdb/psmdb-tarball-functional.groovy
@@ -55,11 +55,13 @@ pipeline {
                 sh """
                   echo ${params.TARBALL} | sed -E 's/(.+mongodb-)([0-9].[0-9])(.+)/\\2/' > VERSION
                 """
-                script {
-                    def PSMDB_VER = sh(returnStdout: true, script: "cat VERSION").trim()
-                    def os = pdmdbOperatingSystems("${PSMDB_VER}")
-                    os.removeAll { it.contains('-arm') }
-                    moleculeParallelTest(os, moleculeDir)
+                withCredentials([string(credentialsId: 'VAULT_TRIAL_LICENSE', variable: 'VAULT_TRIAL_LICENSE')]) {
+                    script {
+                        def PSMDB_VER = sh(returnStdout: true, script: "cat VERSION").trim()
+                        def os = pdmdbOperatingSystems("${PSMDB_VER}")
+                        os.removeAll { it.contains('-arm') }
+                        moleculeParallelTest(os, moleculeDir)
+                    }
                 }
             }
         }
