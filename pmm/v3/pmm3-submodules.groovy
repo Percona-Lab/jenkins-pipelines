c806b21885e370b48b6af583e90642f3345053de
yurkovychv <vasyl.yurkovych@percona.com>
2025-10-27 10:36:22 +0200

    PMM-14427 remove source

--- pmm/v3/pmm3-submodules.groovy
+++ pmm/v3/pmm3-submodules.groovy
@@ -86,7 +86,7 @@ pipeline {
                     echo "$(pwd)"
                     echo "$(git submodule status)" || true
 
-                    launchable record build --name "pmm3-submodules-${PMM_VERSION}-${FB_COMMIT}" --source submodules=./sources
+                    launchable record build --name "pmm3-submodules-${PMM_VERSION}-${FB_COMMIT}"
                 '''
                 }
                 stash includes: 'apiBranch', name: 'apiBranch'
