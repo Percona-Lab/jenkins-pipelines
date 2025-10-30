84ca45abc24a83bb39a5a689aac31a7caeae9ea7
Peter Sirotnak <sirotnak.p@gmail.com>
2025-10-30 13:06:34 +0100

    PMM-7: Fix upgrade

--- pmm/v3/pmm3-upgrade-ami-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-ami-test-runner.groovy
@@ -64,7 +64,7 @@ void runAMIStagingStart(String AMI_ID, PMM_QA_GIT_BRANCH, SSH_KEY) {
 def versionsList = pmmVersion('v3-ami')
 def versionsListParameter = versionsList.collect { k, v -> "${k} - ${v}" }
 def amiVersions = versionsList.values()[-5..-1]
-def versions = versionsList.keySet()[-5..-1]
+def versions = versionsList.keySet().toList()[-5..-1]
 def upgradeAmiVersion = amiVersions[1]
 def latestVersion = versions[versions.size() - 1]
 def upgradeVersion = versions[versions.size() - 2]
