4b43076abc2b4c859d00ba2d8a1f3f5ce27b6445
Peter Sirotnak <sirotnak.p@gmail.com>
2025-11-03 14:47:02 +0100

    PMM-7: Fix OVF

--- pmm/v3/pmm3-upgrade-ovf-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-ovf-test-runner.groovy
@@ -141,10 +141,6 @@ pipeline {
             defaultValue: 'PMM-14156-ami-ovf',
             description: 'Tag/Branch for qa-integration repository',
             name: 'QA_INTEGRATION_GIT_BRANCH')
-        choice(
-            choices: ["SSL", "EXTERNAL SERVICES", "MONGO BACKUP", "CUSTOM PASSWORD", "CUSTOM DASHBOARDS", "ANNOTATIONS-PROMETHEUS", "ADVISORS-ALERTING", "SETTINGS-METRICS"],
-            description: 'Subset of tests for the upgrade',
-            name: 'UPGRADE_FLAG')
     }
     options {
         skipDefaultCheckout()
@@ -156,7 +152,6 @@ pipeline {
                 script {
                     env.ADMIN_PASSWORD = params.ADMIN_PASSWORD
                     env.PMM_UI_URL = params.PMM_UI_URL
-//                     currentBuild.description = "${env.UPGRADE_FLAG} - Upgrade for PMM from ${env.DOCKER_TAG.split(":")[1]} to ${env.PMM_SERVER_LATEST}."
                 }
                 git poll: false,
                     branch: PMM_UI_GIT_BRANCH,
@@ -173,45 +168,6 @@ pipeline {
                     sudo chown $(id -u):$(id -u) -R /srv/qa-integration
                     sudo ln -s /usr/bin/chromium-browser /usr/bin/chromium
                 '''
-            }
-        }
-        stage('Select subset of tests') {
-            steps {
-                script {
-                    if (env.UPGRADE_FLAG == "SSL") {
-                        env.PRE_UPGRADE_FLAG = "@pre-ssl-upgrade"
-                        env.POST_UPGRADE_FLAG = "@post-ssl-upgrade"
-                        env.PMM_CLIENTS = "--database ssl_psmdb --database ssl_mysql --database ssl_pdpgsql"
-                    } else if (env.UPGRADE_FLAG == "EXTERNAL SERVICES") {
-                        env.PRE_UPGRADE_FLAG = "@pre-external-upgrade"
-                        env.POST_UPGRADE_FLAG = "@post-external-upgrade"
-                        env.PMM_CLIENTS = "--database external --database ps --database pdpgsql --database psmdb --database pxc"
-                    } else if (env.UPGRADE_FLAG == "MONGO BACKUP") {
-                        env.PRE_UPGRADE_FLAG = "@pre-mongo-backup-upgrade"
-                        env.POST_UPGRADE_FLAG = "@post-mongo-backup-upgrade"
-                        env.PMM_CLIENTS = "--database psmdb,SETUP_TYPE=pss"
-                    } else if (env.UPGRADE_FLAG == "CUSTOM PASSWORD") {
-                        env.PRE_UPGRADE_FLAG = "@pre-custom-password-upgrade"
-                        env.POST_UPGRADE_FLAG = "@post-custom-password-upgrade"
-                        env.PMM_CLIENTS = "--database ps --database pgsql --database psmdb"
-                    } else if (env.UPGRADE_FLAG == "CUSTOM DASHBOARDS") {
-                        env.PRE_UPGRADE_FLAG = "@pre-dashboards-upgrade"
-                        env.POST_UPGRADE_FLAG = "@post-dashboards-upgrade"
-                        env.PMM_CLIENTS = "--help"
-                    } else if (env.UPGRADE_FLAG == "ANNOTATIONS-PROMETHEUS") {
-                        env.PRE_UPGRADE_FLAG = "@pre-annotations-prometheus-upgrade"
-                        env.POST_UPGRADE_FLAG = "@post-annotations-prometheus-upgrade"
-                        env.PMM_CLIENTS = "--database ps --database pgsql --database psmdb"
-                    } else if (env.UPGRADE_FLAG == "ADVISORS-ALERTING") {
-                        env.PRE_UPGRADE_FLAG = "@pre-advisors-alerting-upgrade"
-                        env.POST_UPGRADE_FLAG = "@post-advisors-alerting-upgrade"
-                        env.PMM_CLIENTS = "--help"
-                    } else if (env.UPGRADE_FLAG == "SETTINGS-METRICS") {
-                        env.PRE_UPGRADE_FLAG = "@pre-settings-metrics-upgrade"
-                        env.POST_UPGRADE_FLAG = "@post-settings-metrics-upgrade"
-                        env.PMM_CLIENTS = "--database pgsql --database ps --database psmdb"
-                    }
-                }
             }
         }
         stage('Start OVF server Instance') {
@@ -270,43 +226,6 @@ pipeline {
                         \${PMM_CLIENTS}
                     popd
                 '''
-            }
-        }
-        stage('Setup Custom queries') {
-            steps {
-                script {
-                    if (env.UPGRADE_FLAG == "SETTINGS-METRICS") {
-                        sh '''
-                            containers=$(docker ps -a)
-                            echo $containers
-                            psContainerName=$(docker ps -a --format "{{.Names}}" | grep "ps_pmm")
-                            echo "$psContainerName"
-                            pgsqlContainerName=$(docker ps -a --format "{{.Names}}" | grep "pgsql_pg")
-                            echo "$pgsqlContainerName"
-                            echo "Creating Custom Queries"
-                            git clone https://github.com/Percona-Lab/pmm-custom-queries
-                            docker cp pmm-custom-queries/mysql/. $psContainerName:/usr/local/percona/pmm/collectors/custom-queries/mysql/high-resolution/
-                            echo "Adding Custom Queries for postgres"
-                            docker cp pmm-custom-queries/postgresql/. $pgsqlContainerName:/usr/local/percona/pmm/collectors/custom-queries/postgresql/high-resolution/
-                            echo 'node_role{role="my_monitored_server_1"} 1' > node_role.prom
-                            sudo cp node_role.prom /usr/local/percona/pmm/collectors/textfile-collector/high-resolution/
-                            docker exec $psContainerName pkill -f mysqld_exporter
-                            docker exec $pgsqlContainerName pkill -f postgres_exporter
-                            docker exec $pgsqlContainerName pmm-admin list
-                            docker exec $psContainerName pmm-admin list
-                            sudo pkill -f node_exporter
-                            sleep 5
-                            echo "Setup for Custom Queries Completed along with custom text file collector Metrics"
-                            docker ps -a --format "{{.Names}}"
-
-                            docker exec $psContainerName pmm-admin list | grep mysqld_exporter
-
-                            psAgentId=$(docker exec $psContainerName pmm-admin list | grep mysqld_exporter | awk -F' ' '{ print $4 }')
-                            psAgentPort=$(docker exec $psContainerName pmm-admin list | grep mysqld_exporter | awk -F' ' '{ print $6 }')
-                            echo $psAgentPort
-                        '''
-                    }
-                }
             }
         }
         stage('Sleep') {
@@ -377,6 +296,9 @@ pipeline {
             }
         }
         stage('Upgrade PMM client') {
+            environment {
+                ADMIN_PASSWORD = "pmm3admin!"
+            }
             steps {
                 sh """
                    containers=\$(docker ps --format "{{ .Names }}")
@@ -422,10 +344,13 @@ pipeline {
             }
         }
         stage('Run post pmm client upgrade UI tests') {
+            environment {
+                ADMIN_PASSWORD = "pmm3admin!"
+            }
             steps {
                 withCredentials([aws(accessKeyVariable: 'BACKUP_LOCATION_ACCESS_KEY', credentialsId: 'BACKUP_E2E_TESTS', secretKeyVariable: 'BACKUP_LOCATION_SECRET_KEY'), aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'PMM_AWS_DEV', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
                     sh '''
-                        ./node_modules/.bin/codeceptjs run-multiple parallel --reporter mocha-multi -c pr.codecept.js --steps --grep \${POST_UPGRADE_FLAG}
+                        ./node_modules/.bin/codeceptjs run-multiple parallel --reporter mocha-multi -c pr.codecept.js --steps --grep '@ami-ovf-post-upgrade'
                     '''
                 }
             }
