37785039ac764cff9a18546a9749da7b22b91c2b
Peter Sirotnak <sirotnak.p@gmail.com>
2025-11-08 10:24:17 +0100

    PMM-7: Fix OVF

--- pmm/v3/pmm3-ovf-staging-start.groovy
+++ pmm/v3/pmm3-ovf-staging-start.groovy
@@ -102,9 +102,9 @@ pipeline {
                 }
                 node(env.VM_NAME){
                     sh '''
-                        if [[ "$v" == *https* ]]; then
+                        if [[ "${OVA_VERSION}" == *https* ]]; then
                             wget -nv -O pmm-server.ova ${OVA_VERSION}
-                        elif [[ "$v" == "3.0.0" ]]; then
+                        elif [[ "${OVA_VERSION}" == "3.0.0" ]]; then
                             wget -nv -O pmm-server.ova  https://downloads.percona.com/downloads/pmm3/${OVA_VERSION}/ova/pmm-server-${OVA_VERSION}.ova
                         else
                             wget -nv -O pmm-server.ova  https://downloads.percona.com/downloads/pmm3/${OVA_VERSION}/ova/PMM3-Server-${OVA_VERSION}-1.ova
