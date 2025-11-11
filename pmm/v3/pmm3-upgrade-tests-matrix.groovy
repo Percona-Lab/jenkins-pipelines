fbe14cd6c1a57a3ea18a2143e543e6b17f2a9aee
Peter Sirotnak <sirotnak.p@gmail.com>
2025-11-11 10:40:30 +0100

    PMM-7: Fix versions

--- pmm/v3/pmm3-upgrade-tests-matrix.groovy
+++ pmm/v3/pmm3-upgrade-tests-matrix.groovy
@@ -3,7 +3,7 @@ library changelog: false, identifier: 'lib@3.4.0-peter-upgrade', retriever: mode
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
-def pmmVersions = pmmVersion('v3').subList(Math.max(0, pmmVersion('v3').size() - 5), pmmVersion('v3').size());
+def pmmVersions = pmmVersion('v3')[-5..-1]
 def latestVersion = pmmVersion('v3').last()
 
 void runUpgradeJob(String PMM_UI_GIT_BRANCH, DOCKER_TAG, DOCKER_TAG_UPGRADE, CLIENT_VERSION, CLIENT_REPOSITORY, PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH, latestVersion) {
