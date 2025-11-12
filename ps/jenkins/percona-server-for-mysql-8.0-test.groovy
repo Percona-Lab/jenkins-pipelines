e379302b6d22db5ea5e4cd07f8d768d5910c0423
Grishma123-eng <grishmashah121@gmail.com>
2025-11-12 12:05:57 +0530

    created separate block for post success

--- ps/jenkins/percona-server-for-mysql-8.0-test.groovy
+++ ps/jenkins/percona-server-for-mysql-8.0-test.groovy
@@ -451,7 +451,7 @@ parameters {
             //    uploadTarballfromAWS(params.CLOUD, "source_tarball/", AWS_STASH_PATH, 'source')
             }
         } 
-        stage('Build PS generic source packages') {
+    /*    stage('Build PS generic source packages') {
             parallel {
                 stage('Build PS generic source rpm') {
                     agent {
@@ -773,7 +773,7 @@ parameters {
                         pushArtifactFolder(params.CLOUD, "deb/", AWS_STASH_PATH)
                     }
                 } */
-                stage('Ubuntu Focal(20.04) ARM') {
+            /*    stage('Ubuntu Focal(20.04) ARM') {
                     when {
                         expression { env.FIPSMODE == 'NO' }
                     }
@@ -891,7 +891,7 @@ parameters {
                         pushArtifactFolder(params.CLOUD, "deb/", AWS_STASH_PATH)
                     }
                 } */
-                stage('Oracle Linux 8 binary tarball') {
+         /*       stage('Oracle Linux 8 binary tarball') {
                     when {
                         expression { env.FIPSMODE == 'NO' }
                     }
@@ -1175,148 +1175,49 @@ parameters {
                           wait: false
                 }
             }
-        } 
+        } */
     } 
+}
     post {
-        success {
-            script {
-                if (env.FIPSMODE == 'YES') {
-                 //   slackNotify("${SLACKNOTIFY}", "#00FF00", "[${JOB_NAME}]: PRO -> build has been finished successfully for ${BRANCH} - [${BUILD_URL}]")
-                } else {
-               //     slackNotify("${SLACKNOTIFY}", "#00FF00", "[${JOB_NAME}]: build has been finished successfully for ${BRANCH} - [${BUILD_URL}]")
-                }
-            }
-            unstash 'properties'
-            script {
-                currentBuild.description = "Built on ${BRANCH}; path to packages: ${COMPONENT}/${AWS_STASH_PATH}"
-                env.PS_REVISION = sh(returnStdout: true, script: "grep REVISION test/percona-server-8.0.properties | awk -F '=' '{ print\$2 }'").trim()
-                sh "cat test/percona-server-8.0.properties"
-                echo "Revision is: ${env.PS_REVISION}"
-                echo "PS_RELEASE is: ${PS_RELEASE}"
-                echo "PS_VERSION_SHORT_KEY is: ${PS_VERSION_SHORT_KEY}"
-                echo "Value is : ${PS_VERSION_SHORT}"
-                echo "DOCKER account is : ${DOCKER_ACC}"
 
-                if (env.product_to_test == 'PS80') {
-                    echo "Running PS80-specific steps"
-                } else if (env.product_to_test == 'PS84') {
-                    echo "Running PS84-specific steps"
-                } else {
-                    echo "Running client test"
-                }
-                 if("${PS_VERSION_SHORT}"){
-                    echo "Executing MINITESTS as VALID VALUES FOR PS8_RELEASE_VERSION:${PS_VERSION_SHORT}"
-                    echo "Checking for the Github Repo VERSIONS file changes..."
-                    withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'TOKEN')]) {
-                    sh '''#!/bin/bash
-                        set -e -x
-                        git clone https://jenkins-pxc-cd:$TOKEN@github.com/Percona-QA/package-testing.git
-                        cd package-testing
-                        git config user.name "jenkins-pxc-cd"
-                        git config user.email "it+jenkins-pxc-cd@percona.com"
-                        git checkout testing-branch
-                        echo "${PS_VERSION_SHORT} is the VALUE!!@!"
-                        export RELEASE_VER_VAL="${PS_VERSION_SHORT}"
-                        # Use bash regex check
-                        if [[ "$RELEASE_VER_VAL" =~ ^PS8[0-9]{1}$ ]]; then
-                            echo "$RELEASE_VER_VAL is a valid version"
-                            OLD_REV=$(grep ${PS_VERSION_SHORT}_REV VERSIONS | cut -d '=' -f2-)
-                            OLD_VER=$(grep ${PS_VERSION_SHORT}_VER VERSIONS | cut -d '=' -f2-)
-                            sed -i s/${PS_VERSION_SHORT}_REV=$OLD_REV/${PS_VERSION_SHORT}_REV='"'${PS_REVISION}'"'/g VERSIONS
-                            sed -i s/${PS_VERSION_SHORT}_VER=$OLD_VER/${PS_VERSION_SHORT}_VER='"'${PS_RELEASE}'"'/g VERSIONS
-                        else
-                            echo "INVALID PS8_RELEASE_VERSION VALUE: $RELEASE_VER_VAL"
-                        fi
-                        git diff
-                        if [[ -z $(git diff) ]]; then
-                            echo "No changes"
-                        else
-                            echo "There are changes"
-                            git add -A
-                            git commit -m "Autocommit: add ${PS_REVISION} and ${PS_RELEASE} for ${PS_VERSION_SHORT} package testing VERSIONS file."
-                            git push
-                        fi
-                        '''
-                    }
-                    parallel(
-                        "Start Minitests for PS": {
-                             try {
-                                package_tests_ps80(minitestNodes)
-                                echo "Minitests completed successfully. Triggering next stages."
-                             //   slackNotify("${SLACKNOTIFY}", "#FF0000", "[${JOB_NAME}]: minitest sucessfully run for ${BRANCH} - [${BUILD_URL}]")
-                                echo "TRIGGERING THE PACKAGE TESTING JOB!!!"
-                                withCredentials([string(credentialsId: 'JNKPERCONA_PS80_TOKEN', variable: 'TOKEN')]) {
-                                    def jenkinsServerUrl = 'https://ps80.cd.percona.com'
-                                    def jobName = 'ps-package-testing-molecule'
-                                    def response = sh(script: """
-                                        curl -X POST \\
-                                        -u ${TOKEN} \\
-                                        "${jenkinsServerUrl}/job/${jobName}/buildWithParameters" \\
-                                        --data-urlencode "product_to_test=${product_to_test}" \\
-                                        --data-urlencode "install_repo=testing" \\
-                                        --data-urlencode "action_to_test=install" \\
-                                        --data-urlencode "check_warnings=yes" \\
-                                        --data-urlencode "install_mysql_shell=no"
-                                    """, returnStdout: true).trim()
-                                    echo "PS job triggered on ${jenkinsServerUrl}/job/${jobName}"
-                                    echo "Response: ${response}"  
-                                    }          
-        
-                                echo "Trigger PMM_PS Github Actions Workflow"
-                                withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'GITHUB_API_TOKEN')]) {
-                                    sh """
-                                    curl -i -v -X POST \
-                                    -H "Accept: application/vnd.github.v3+json" \
-                                    -H "Authorization: token ${GITHUB_API_TOKEN}" \
-                                    "https://api.github.com/repos/Percona-Lab/qa-integration/actions/workflows/PMM_PS.yaml/dispatches" \
-                                    -d '{"ref":"main","inputs":{"ps_version":"${PS_RELEASE}"}}'
-                                    """ 
-                                    }
-                            //    slackNotify("${SLACKNOTIFY}", "#FF0000", "[${JOB_NAME}]: PMM sucessfully run for ${BRANCH} - [${BUILD_URL}]")
-                            } catch (err) {
-                                    echo " Minitests block failed: ${err}"
-                                    currentBuild.result = 'FAILURE'
-                                    throw err
-                                }
-                        },
-                        "Start docker job": {
-                            try {
-                                docker_test()
-                                echo "DOCKER images run successfully."
-                            }   catch (err) {
-                                echo "Docker test block failed: ${err}"
-                                currentBuild.result = 'FAILURE'
-                                throw err
-                            }
-                            }
-                    )          
-                }    
-                    else{
-                            error "Skipping MINITESTS and Other Triggers as invalid RELEASE VERSION FOR THIS JOB"
-                           // slackNotify("${SLACKNOTIFY}", "#00FF00", "[${JOB_NAME}]: Skipping MINITESTS and Other Triggers as invalid RELEASE VERSION FOR THIS JOB ${BRANCH} - [${BUILD_URL}]")
-                        }
-                         deleteDir()
+    success {
+        script {
+            if (env.FIPSMODE == 'YES') {
+                // slackNotify("${SLACKNOTIFY}", "#00FF00", "[${JOB_NAME}]: PRO -> build finished successfully for ${BRANCH} - [${BUILD_URL}]")
+            } else {
+                // slackNotify("${SLACKNOTIFY}", "#00FF00", "[${JOB_NAME}]: build finished successfully for ${BRANCH} - [${BUILD_URL}]")
             }
-           
-           // slackNotify("${SLACKNOTIFY}", "#00FF00", "[${JOB_NAME}]: Triggering Builds for Package Testing for ${BRANCH} - [${BUILD_URL}]")
-            deleteDir()
+
+            unstash 'properties'
+
+            // ✅ Call external shared function
+            MinitestPostSuccess(
+                product_to_test: product_to_test,
+                PS_RELEASE: PS_RELEASE,
+                PS_VERSION_SHORT: PS_VERSION_SHORT,
+                PS_VERSION_SHORT_KEY: PS_VERSION_SHORT_KEY,
+                minitestNodes: minitestNodes,
+                SLACKNOTIFY: SLACKNOTIFY,
+                BRANCH: BRANCH,
+                DOCKER_ACC: DOCKER_ACC
+            )
         }
-        failure {
-            deleteDir()
-        }
-        always {
-            sh '''
-                sudo rm -rf ./*
-            '''
-            script {
-                if (env.FIPSMODE == 'YES') {
-                    currentBuild.description = "Pro -> Build on ${BRANCH}"
-                } else {
-                    currentBuild.description = "Build on ${BRANCH}"
-                }
+    }
+
+    failure {
+        deleteDir()
+    }
+
+    always {
+        sh 'sudo rm -rf ./*'
+        script {
+            if (env.FIPSMODE == 'YES') {
+                currentBuild.description = "Pro -> Build on ${BRANCH}"
+            } else {
+                currentBuild.description = "Build on ${BRANCH}"
             }
-            deleteDir()
         }
+        deleteDir()
     }
 }
 
---
+++ vars/MinitestPostSucess.groovy
@@ -0,0 +1,120 @@
+def call(Map args = [:]) {
+    def product_to_test  = args.get('product_to_test', '')
+    def PS_RELEASE       = args.get('PS_RELEASE', '')
+    def PS_VERSION_SHORT = args.get('PS_VERSION_SHORT', '')
+    def PS_VERSION_SHORT_KEY = args.get('PS_VERSION_SHORT_KEY', '')
+    def minitestNodes    = args.get('minitestNodes', [])
+    def SLACKNOTIFY      = args.get('SLACKNOTIFY', '')
+    def BRANCH           = args.get('BRANCH', '')
+    def DOCKER_ACC       = args.get('DOCKER_ACC', '')
+
+    echo "Starting post-success logic..."
+    echo "Revision: ${env.PS_REVISION}"
+    echo "PS_RELEASE: ${PS_RELEASE}"
+    echo "PS_VERSION_SHORT_KEY: ${PS_VERSION_SHORT_KEY}"
+    echo "PS_VERSION_SHORT: ${PS_VERSION_SHORT}"
+    echo "Docker Account: ${DOCKER_ACC}"
+
+    if (product_to_test == 'PS80') {
+        echo "Running PS80-specific steps"
+    } else if (product_to_test == 'PS84') {
+        echo "Running PS84-specific steps"
+    } else {
+        echo "Running client test"
+    }
+
+    if ("${PS_VERSION_SHORT}") {
+        echo "Executing MINITESTS as VALID VALUE: ${PS_VERSION_SHORT}"
+        echo "Checking for GitHub Repo VERSIONS file changes..."
+
+        withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'TOKEN')]) {
+            sh '''#!/bin/bash
+                set -e -x
+                git clone https://jenkins-pxc-cd:$TOKEN@github.com/Percona-QA/package-testing.git
+                cd package-testing
+                git config user.name "jenkins-pxc-cd"
+                git config user.email "it+jenkins-pxc-cd@percona.com"
+                git checkout testing-branch
+                echo "${PS_VERSION_SHORT} is the VALUE!!@!"
+                export RELEASE_VER_VAL="${PS_VERSION_SHORT}"
+                if [[ "$RELEASE_VER_VAL" =~ ^PS8[0-9]{1}$ ]]; then
+                    echo "$RELEASE_VER_VAL is a valid version"
+                    OLD_REV=$(grep ${PS_VERSION_SHORT}_REV VERSIONS | cut -d '=' -f2-)
+                    OLD_VER=$(grep ${PS_VERSION_SHORT}_VER VERSIONS | cut -d '=' -f2-)
+                    sed -i s/${PS_VERSION_SHORT}_REV=$OLD_REV/${PS_VERSION_SHORT}_REV='"'${PS_REVISION}'"'/g VERSIONS
+                    sed -i s/${PS_VERSION_SHORT}_VER=$OLD_VER/${PS_VERSION_SHORT}_VER='"'${PS_RELEASE}'"'/g VERSIONS
+                else
+                    echo "INVALID PS8_RELEASE_VERSION VALUE: $RELEASE_VER_VAL"
+                fi
+                git diff
+                if [[ -z $(git diff) ]]; then
+                    echo "No changes"
+                else
+                    echo "There are changes"
+                    git add -A
+                    git commit -m "Autocommit: add ${PS_REVISION} and ${PS_RELEASE} for ${PS_VERSION_SHORT} package testing VERSIONS file."
+                    git push
+                fi
+            '''
+        }
+
+        parallel(
+            "Start Minitests for PS": {
+                try {
+                    package_tests_ps80(minitestNodes)
+                    echo "Minitests completed successfully. Triggering next stages."
+
+                    // Trigger PS package-testing job on another Jenkins
+                    withCredentials([string(credentialsId: 'JNKPERCONA_PS80_TOKEN', variable: 'TOKEN')]) {
+                        def jenkinsServerUrl = 'https://ps80.cd.percona.com'
+                        def jobName = 'ps-package-testing-molecule'
+                        def response = sh(script: """
+                            curl -X POST \\
+                            -u ${TOKEN} \\
+                            "${jenkinsServerUrl}/job/${jobName}/buildWithParameters" \\
+                            --data-urlencode "product_to_test=${product_to_test}" \\
+                            --data-urlencode "install_repo=testing" \\
+                            --data-urlencode "action_to_test=install" \\
+                            --data-urlencode "check_warnings=yes" \\
+                            --data-urlencode "install_mysql_shell=no"
+                        """, returnStdout: true).trim()
+                        echo "PS job triggered on ${jenkinsServerUrl}/job/${jobName}"
+                        echo "Response: ${response}"
+                    }
+
+                    // Trigger GitHub workflow
+                    echo "Trigger PMM_PS GitHub Actions Workflow"
+                    withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'GITHUB_API_TOKEN')]) {
+                        sh """
+                            curl -i -v -X POST \
+                            -H "Accept: application/vnd.github.v3+json" \
+                            -H "Authorization: token ${GITHUB_API_TOKEN}" \
+                            "https://api.github.com/repos/Percona-Lab/qa-integration/actions/workflows/PMM_PS.yaml/dispatches" \
+                            -d '{"ref":"main","inputs":{"ps_version":"${PS_RELEASE}"}}'
+                        """
+                    }
+
+                } catch (err) {
+                    echo " Minitests block failed: ${err}"
+                    currentBuild.result = 'FAILURE'
+                    throw err
+                }
+            },
+            "Start Docker job": {
+                try {
+                    docker_test()
+                    echo "Docker images run successfully."
+                } catch (err) {
+                    echo " Docker test block failed: ${err}"
+                    currentBuild.result = 'FAILURE'
+                    throw err
+                }
+            }
+        )
+
+    } else {
+        error "Skipping MINITESTS — invalid RELEASE VERSION FOR THIS JOB"
+    }
+
+    deleteDir()
+}
