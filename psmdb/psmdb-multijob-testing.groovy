99cfda33e8544190d0c4d10102323bb3acff1312
Keith Quinn <keith.quinn@percona.com>
2025-11-24 16:14:55 +0000

    Updating parameters for multijob

--- psmdb/psmdb-multijob-testing.groovy
+++ psmdb/psmdb-multijob-testing.groovy
@@ -12,7 +12,7 @@ pipeline {
     }
     parameters {
         choice(name: 'CLOUD', choices: [ 'Hetzner','AWS' ], description: 'Cloud infra for build')
-        string(name: 'PSMDB_VERSION', defaultValue: '4.4.17', description: 'PSMDB Version')
+        string(name: 'PSMDB_VERSION', defaultValue: '7.0.26-14', description: 'PSMDB Version')
         string(name: 'PSMDB_RELEASE', defaultValue: '17', description: 'PSMDB Release')
     }
     options {
@@ -22,15 +22,13 @@ pipeline {
         stage ('Run functional tests on tarballs') {
             steps {
                 script {
-                    // path https://downloads.percona.com/downloads/TESTING/psmdb-4.4.17/percona-server-mongodb-4.4.17-17-x86_64.glibc2.17-minimal.tar.gz
-                    def tarball = 'https://downloads.percona.com/downloads/TESTING/psmdb-' + params.PSMDB_VERSION + '/percona-server-mongodb-' + params.PSMDB_VERSION + '-' + params.PSMDB_RELEASE + '-x86_64.glibc2.17-minimal.tar.gz'
-                    build job: 'psmdb-tarball-functional', propagate: false, wait: true, parameters: [ string(name: 'TARBALL', value: tarball), string(name: 'TESTING_BRANCH', value: "main") ]
+                    build job: 'psmdb-tarball-functional', propagate: false, wait: true, parameters: [ string(name: 'PSMDB_VERSION', value: params.PSMDB_VERSION), string(name: 'TESTING_BRANCH', value: "main") ]
                 }
             }
         }
         stage ('Run functional tests on packages') {
             steps {
-                build job: 'psmdb-parallel', parameters: [ string(name: 'REPO', value: "testing"), string(name: 'PSMDB_VERSION', value: params.PSMDB_VERSION ), string(name: 'ENABLE_TOOLKIT', value: "false"), string(name: 'TESTING_BRANCH', value: "main") ]
+                build job: 'psmdb-parallel', parameters: [ string(name: 'REPO', value: "testing"), string(name: 'PSMDB_VERSION', value: params.PSMDB_VERSION.replaceFirst(/-\d+$/, '') ), string(name: 'ENABLE_TOOLKIT', value: "false"), string(name: 'TESTING_BRANCH', value: "main") ]
             }
         }
         stage ('Build docker images and check for vulnerabilities') {
