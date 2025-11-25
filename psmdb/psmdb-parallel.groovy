8b3e93b2489ec9ac2242fc19f72205c452c78789
Keith Quinn <keith.quinn@percona.com>
2025-11-25 11:33:28 +0000

    Cleaning up variables and slack message

--- psmdb/psmdb-parallel.groovy
+++ psmdb/psmdb-parallel.groovy
@@ -102,10 +102,10 @@ pipeline {
     }
     post {
         success {
-            slackNotify("#mongodb_autofeed", "#00FF00", "[${JOB_NAME}]: package tests for PSMDB ${PSMDB_VERSION}, repo ${REPO}, private repo - finished succesfully - [${BUILD_URL}]")
+            slackNotify("#mongodb_autofeed", "#00FF00", "[${JOB_NAME}]: package tests for PSMDB ${PSMDB_VERSION}, repo ${REPO} - finished succesfully - [${BUILD_URL}]")
         }
         failure {
-            slackNotify("#mongodb_autofeed", "#FF0000", "[${JOB_NAME}]: package tests for PSMDB ${PSMDB_VERSION}, repo ${REPO}, private repo - failed - [${BUILD_URL}]")
+            slackNotify("#mongodb_autofeed", "#FF0000", "[${JOB_NAME}]: package tests for PSMDB ${PSMDB_VERSION}, repo ${REPO} - failed - [${BUILD_URL}]")
         }
         always {
             script {
