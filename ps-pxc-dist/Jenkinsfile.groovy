4923e43933e5be4761988b072a8d9562827fc001
Vadim Yalovets <vadim.yalovets@percona.com>
2025-11-27 10:21:22 +0200

    PKG-1095 Packaging tasks for release - PS 8.0.44

--- ps-pxc-dist/Jenkinsfile.groovy
+++ ps-pxc-dist/Jenkinsfile.groovy
@@ -105,6 +105,11 @@ pipeline {
             description: "Skips refresh-downloads-area stage",
             name: 'SKIP_PRODUCTION_REFRESH'
         )
+        choice(
+            choices: 'YES\nNO',
+            description: 'Push amazonelinux 2023 packages by default',
+            name: 'PUSHAMAZONLINUX'
+        )
     }
     options {
         skipDefaultCheckout()
@@ -129,6 +134,7 @@ pipeline {
                             echo "COMPONENT=\${COMPONENT}" >> args_pipeline
                             echo "REMOVE_LOCKFILE=\${REMOVE_LOCKFILE}" >> args_pipeline
                             echo "REMOVE_BEFORE_PUSH=\${REMOVE_BEFORE_PUSH}" >> args_pipeline
+                            echo "PUSHAMAZONLINUX=\${PUSHAMAZONLINUX}" >> args_pipeline
                             echo "\$(awk '{\$1="export" OFS \$1} 1' args_pipeline)" > args_pipeline
                             rsync -aHv --delete -e "ssh -o StrictHostKeyChecking=no -i \$KEY_PATH" args_pipeline \$USER@repo.ci.percona.com:/tmp/args_pipeline
                             ssh -o StrictHostKeyChecking=no -i \$KEY_PATH \$USER@repo.ci.percona.com " \
