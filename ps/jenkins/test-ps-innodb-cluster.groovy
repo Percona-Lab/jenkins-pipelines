d84775817679543fce61d6203d5256cb7cc60f07
kaushikpuneet07 <puneet.kaushik@percona.com>
2025-12-04 13:01:56 +0530

    revert originals

--- ps/jenkins/test-ps-innodb-cluster.groovy
+++ ps/jenkins/test-ps-innodb-cluster.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: 'lib@ps84-inno', retriever: modernSCM([
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
 void installDependencies() {
