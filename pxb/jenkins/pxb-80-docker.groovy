3ce67f4f0a5d4fed3c02d8b38f6cd67832747f7c
Vadim Yalovets <vadim.yalovets@percona.com>
2025-12-09 07:56:50 +0200

    PKG-1198 Packaging tasks for release - PXB 8.4.0-5

--- pxb/jenkins/pxb-80-docker.groovy
+++ pxb/jenkins/pxb-80-docker.groovy
@@ -96,6 +96,7 @@ pipeline {
                             sed -i "s/ENV PS_VERSION.*/ENV PS_VERSION ${MYSQL_VERSION_MAJOR}.${MYSQL_VERSION_MINOR}.${MYSQL_VERSION_PATCH}${MYSQL_VERSION_EXTRA}.1/g" Dockerfile
                             sed -i "s/ARG PXB_REPO=release/ARG PXB_REPO=${COMPONENT}/g" Dockerfile
                             sed -i "s/ARG PS_REPO=release/ARG PS_REPO=${COMPONENT}/g" Dockerfile
+                            sed -i "s/ARG TOOLS_REPO=release/ARG TOOLS_REPO=${COMPONENT}/g" Dockerfile
                             if [ ${ORGANIZATION} != "percona" ]; then
                                 sudo docker build --provenance=false --no-cache -t perconalab/percona-xtrabackup:${XB_VERSION_MAJOR}.${XB_VERSION_MINOR}.${XB_VERSION_PATCH}${XB_VERSION_EXTRA}.${RPM_RELEASE}-amd64 --platform "linux/amd64" .
                                 sudo docker build --provenance=false --no-cache -t perconalab/percona-xtrabackup:${XB_VERSION_MAJOR}.${XB_VERSION_MINOR}.${XB_VERSION_PATCH}${XB_VERSION_EXTRA}.${RPM_RELEASE}-arm64 --platform="linux/arm64" .
