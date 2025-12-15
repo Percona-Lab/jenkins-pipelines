bcc557c3984c866530c447eee32673da88cf1797
kaushikpuneet07 <puneet.kaushik@percona.com>
2025-12-15 12:30:08 +0530

    update

--- pxb/jenkins/pxb-tarball-molecule.groovy
+++ pxb/jenkins/pxb-tarball-molecule.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: "lib@add-di", retriever: modernSCM([
+library changelog: false, identifier: "lib@master", retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ])
 
 def PXBskipOSPRO() {
