70a4a1e9126bdadfe68b8255674f2840227003d9
Peter Sirotnak <sirotnak.p@gmail.com>
2026-01-26 14:03:04 +0100

    PMM-7: Fix upgrade

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -269,8 +269,7 @@ pipeline {
                     steps {
                         sh '''
                             npm ci
-                            npx playwright install-deps
-                            npx playwright install
+                            npx playwright install --with-deps
                             envsubst < env.list > env.generated.list
                             sed -i 's+http://localhost/+${PMM_UI_URL}/+g' pr.codecept.js
                             export PWD=$(pwd)
