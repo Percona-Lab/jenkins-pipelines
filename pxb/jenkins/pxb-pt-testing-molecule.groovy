21954f9826b29dd257d97841b2018cfdf07ca7de
kaushikpuneet07 <puneet.kaushik@percona.com>
2026-02-03 18:58:56 +0530

    update

--- pxb/jenkins/pxb-pt-testing-molecule.groovy
+++ pxb/jenkins/pxb-pt-testing-molecule.groovy
@@ -1,7 +1,7 @@
 
-    library changelog: false, identifier: "lib@add-rocky", retriever: modernSCM([
+    library changelog: false, identifier: "lib@master", retriever: modernSCM([
         $class: 'GitSCMSource',
-        remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
+        remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
     ])
 
     properties([
