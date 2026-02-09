1ca7c04c614de9ed22746d117bc12c82cc5d5078
Peter Sirotnak <sirotnak.p@gmail.com>
2026-02-09 15:50:35 +0100

    PMM-7: AMI Upgrade jenkins

--- pmm/v3/pmm3-upgrade-ami-tests.groovy
+++ pmm/v3/pmm3-upgrade-ami-tests.groovy
@@ -28,8 +28,8 @@ def generateVariants(String PMM_UI_GIT_BRANCH, PMM_QA_GIT_BRANCH, QA_INTEGRATION
     versionsList.each { key, value ->
         println key
         println value
-        print latestVersion
-        if(pmmVersion == latestVersion) {
+        print "Key value is: $key and latest version is: $latestVersion"
+        if(key == latestVersion) {
             println "Version are equal!"
             results.put(
                 "Upgrade AMI PMM from ${key} (AMI tag: ${value}) to: 'perconalab/pmm-server:3-dev-latest'",
