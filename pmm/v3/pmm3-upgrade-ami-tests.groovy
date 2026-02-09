8db90ec2b563609233da75add7bbe14253c8a647
Peter Sirotnak <sirotnak.p@gmail.com>
2026-02-09 15:26:43 +0100

    PMM-7: AMI Upgrade jenkins

--- pmm/v3/pmm3-upgrade-ami-tests.groovy
+++ pmm/v3/pmm3-upgrade-ami-tests.groovy
@@ -9,53 +9,49 @@ def versions = versionsList.keySet().toList()[-5..-1]
 def latestVersion = versions[versions.size() - 1]
 
 void runUpgradeJob(String PMM_UI_PRE_UPGRADE_GIT_BRANCH, PMM_UI_GIT_BRANCH, AMI_TAG, DOCKER_TAG_UPGRADE, CLIENT_VERSION, CLIENT_REPOSITORY, PMM_SERVER_LATEST, PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH) {
-    upgradeJob = build job: 'pmm3-upgrade-ami-test-runner', parameters: [\
-        string(name: 'PMM_UI_PRE_UPGRADE_GIT_BRANCH', value: PMM_UI_PRE_UPGRADE_GIT_BRANCH),
-        string(name: 'PMM_UI_GIT_BRANCH', value: PMM_UI_GIT_BRANCH),
-        string(name: 'AMI_TAG', value: AMI_TAG),
-        string(name: 'DOCKER_TAG_UPGRADE', value: DOCKER_TAG_UPGRADE),
-        string(name: 'CLIENT_VERSION', value: CLIENT_VERSION),
-        string(name: 'CLIENT_REPOSITORY', value: CLIENT_REPOSITORY),
-        string(name: 'PMM_SERVER_LATEST', value: PMM_SERVER_LATEST),
-        string(name: 'PMM_QA_GIT_BRANCH', value: PMM_QA_GIT_BRANCH),
-        string(name: 'QA_INTEGRATION_GIT_BRANCH', value: QA_INTEGRATION_GIT_BRANCH),
-    ]
+//     upgradeJob = build job: 'pmm3-upgrade-ami-test-runner', parameters: [\
+//         string(name: 'PMM_UI_PRE_UPGRADE_GIT_BRANCH', value: PMM_UI_PRE_UPGRADE_GIT_BRANCH),
+//         string(name: 'PMM_UI_GIT_BRANCH', value: PMM_UI_GIT_BRANCH),
+//         string(name: 'AMI_TAG', value: AMI_TAG),
+//         string(name: 'DOCKER_TAG_UPGRADE', value: DOCKER_TAG_UPGRADE),
+//         string(name: 'CLIENT_VERSION', value: CLIENT_VERSION),
+//         string(name: 'CLIENT_REPOSITORY', value: CLIENT_REPOSITORY),
+//         string(name: 'PMM_SERVER_LATEST', value: PMM_SERVER_LATEST),
+//         string(name: 'PMM_QA_GIT_BRANCH', value: PMM_QA_GIT_BRANCH),
+//         string(name: 'QA_INTEGRATION_GIT_BRANCH', value: QA_INTEGRATION_GIT_BRANCH),
+//     ]
 }
 
 def generateVariants(String PMM_UI_GIT_BRANCH, PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH, CLIENT_REPOSITORY, versionsList, latestVersion) {
     def results = new HashMap<>();
     def upgradeVersions = versionsList.keySet().toList()[-6..-1];
 
-    def iterator = 0;
-
-    for (version in upgradeVersions) {
+    for (pmmVersion in pmmVersions) {
         def upgradeVersion = versionsList[version];
-
-
-        if(version == latestVersion && CLIENT_REPOSITORY == 'experimental') {
-            results.put("Upgrade AMI PMM from ${version} (AMI tag: ${upgradeVersion}) to repo: experimental.", generateStage(PMM_UI_GIT_BRANCH, upgradeVersion, 'perconalab/pmm-server:3-dev-latest', 'pmm3-rc', 'experimental', versionsList.keySet().last(), PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH, iterator++))
+        if(pmmVersion == pmmVersions.last()) {
+            results.put(
+                "Run matrix upgrade tests from version: \"$pmmVersion\"",
+                generateStage(PMM_UI_GIT_BRANCH, "perconalab/pmm-server:${pmmVersion}-rc", 'perconalab/pmm-server:3-dev-latest', 'pmm3-rc', 'testing', PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH, latestVersion)
+            )
         } else {
-            if(CLIENT_REPOSITORY != 'experimental') {
-                latestVersion = versionsList.keySet()[versionsList.keySet().size() - 1]
-                if(version == versionsList.keySet()[versionsList.keySet().size() - 1]) {
-                    continue;
-                }
-            }
-
-            def upgradeDockerTag;
-            def latestUpgradeVersion;
-
-            if(CLIENT_REPOSITORY == 'experimental') {
-                upgradeDockerTag = 'perconalab/pmm-server:3-dev-latest'
-                latestUpgradeVersion = versionsList.keySet()[versionsList.keySet().size() - 1]
-            } else if (CLIENT_REPOSITORY == 'testing') {
-                upgradeDockerTag = "perconalab/pmm-server:${latestVersion}-rc"
-                latestUpgradeVersion = versionsList.keySet()[versionsList.keySet().size() - 2]
-            }
-
-            results.put("Upgrade AMI PMM from ${version} (AMI tag: ${upgradeVersion}) to repo: testing.", generateStage(PMM_UI_GIT_BRANCH, upgradeVersion, upgradeDockerTag, version, CLIENT_REPOSITORY, latestUpgradeVersion, PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH, iterator++))
+            results.put(
+                "Upgrade AMI PMM from ${version} (AMI tag: ${upgradeVersion}) to: perconalab/pmm-server:${pmmVersions.last()}-rc.",
+                generateStage(
+                    PMM_UI_GIT_BRANCH,
+                    upgradeVersion,
+                    "perconalab/pmm-server:${pmmVersions.last()}-rc",
+                    pmmVersion,
+                    CLIENT_REPOSITORY,
+                    latestUpgradeVersion,
+                    PMM_QA_GIT_BRANCH,
+                    QA_INTEGRATION_GIT_BRANCH,
+                    iterator++
+                )
+            )
         }
     }
+
+    println results
 
     return results;
 }
