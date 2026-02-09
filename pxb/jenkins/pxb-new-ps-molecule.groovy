6ca4bfa2489b07970f2a0a37ab64c1742ffa774b
Tomislav Plavcic <tplavcic@gmail.com>
2026-02-09 16:31:50 +0100

    Use different branch for loading libraries

--- pxb/jenkins/pxb-new-ps-molecule.groovy
+++ pxb/jenkins/pxb-new-ps-molecule.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: "lib@master", retriever: modernSCM([
+library changelog: false, identifier: "lib@tomislav-pxb-python", retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ])
