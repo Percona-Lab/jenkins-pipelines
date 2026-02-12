3f7d23a3ad58a8f48eae457ac1464b5811e89977
kaushikpuneet07 <puneet.kaushik@percona.com>
2026-02-12 11:59:10 +0530

    update

--- ps/jenkins/test-ps-innodb-cluster.groovy
+++ ps/jenkins/test-ps-innodb-cluster.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+library changelog: false, identifier: 'lib@fix-pkg', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
 ]) _
 
 void installDependencies() {
@@ -11,9 +11,9 @@ void installDependencies() {
         . virtenv/bin/activate
         python3 --version
         python3 -m pip install --upgrade pip
-        python3 -m pip install --upgrade setuptools
+        python3 -m pip install --upgrade "setuptools>81"
         python3 -m pip install --upgrade setuptools-rust
-        python3 -m pip install --upgrade PyYaml==5.3.1 molecule==3.3.0 testinfra pytest molecule-ec2==0.3 molecule[ansible] "ansible<10.0.0" "ansible-lint>=5.1.1,<6.0.0" boto3 boto
+        python3 -m pip install --upgrade PyYaml==5.3.1 molecule==25.12.0 testinfra pytest molecule-ec2==0.3 molecule[ansible] "ansible<10.0.0" "ansible-lint>=5.1.1,<6.0.0" boto3 boto
        
        """
 
