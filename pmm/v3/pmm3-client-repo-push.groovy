9ac78b4a27063bee6fc699a02ff0837449600917
Alex Demidoff <alexander.demidoff@percona.com>
2026-02-18 13:31:52 +0300

    PMM-13396 Parallel PMM Client builds (#3780)
    * PMM-13396 Create a deidcated pipeline to push PMM Client binaries
    
    * PMM-13396 Parallelize client stages.
    
    * PMM-13396 Parallelize client stages.
    
    * PMM-13396 Move agent definition to pipeline level in client autobuild.
    
    * PMM-13396 Enable concurrent builds in client autobuild pipelines.
    
    * Revert "PMM-13396 Enable concurrent builds in client autobuild pipelines."
    
    This reverts commit 3b49d429049591b052e68d0113e0348f7d2c905e.
    
    * PMM-13396 Update pipelines and library branch after testing.
    
    * PMM-13396 Remove a key
    
    ---------
    
    Co-authored-by: Talha Bin Rizwan <talha.rizwan@percona.com>

---
+++ pmm/v3/pmm3-client-repo-push.groovy
@@ -0,0 +1,57 @@
+library changelog: false, identifier: 'v3lib@master', retriever: modernSCM(
+  scm: [$class: 'GitSCMSource', remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'],
+  libraryPath: 'pmm/v3/'
+)
+
+pipeline {
+    agent {
+        label 'master'
+    }
+    parameters {
+        choice(
+            choices: ['experimental', 'testing'],
+            description: 'Publish packages to repositories: testing for RC, experimental for 3-dev-latest',
+            name: 'DESTINATION'
+        )
+        string(
+            description: 'Upload path for the packages',
+            name: 'UPLOAD_PATH'
+        )
+    }
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '30'))
+        skipDefaultCheckout()
+        disableConcurrentBuilds()
+        parallelsAlwaysFailFast()
+    }
+    stages {
+        stage('Push to public repository') {
+            steps {
+                // Upload packages to the repo defined in `DESTINATION`
+                sync2ProdPMMClientRepo(params.DESTINATION, params.UPLOAD_PATH, 'pmm3-client')
+                withCredentials([sshUserPrivateKey(credentialsId: 'repo.ci.percona.com', keyFileVariable: 'KEY_PATH', usernameVariable: 'USER')]) {
+                    script {
+                        sh """
+                            ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i \${KEY_PATH} \${USER}@repo.ci.percona.com "
+                                scp -P 2222 -o ConnectTimeout=1 -o StrictHostKeyChecking=no ${params.UPLOAD_PATH}/binary/tarball/*.tar.gz jenkins@jenkins-deploy.jenkins-deploy.web.r.int.percona.com:/data/downloads/TESTING/pmm/
+                            "
+                        """
+                    }  
+                }
+            }
+        }
+    }
+    post {
+        success {
+            script {
+                slackSend botUser: true, channel: '#pmm-notifications', color: '#00FF00', message: "[${JOB_NAME}]: packages pushed to ${params.DESTINATION} repo - ${BUILD_URL}"
+            }
+        }
+        failure {
+            script {
+                echo "Pipeline failed"
+                slackSend botUser: true, channel: '#pmm-notifications', color: '#FF0000', message: "[${JOB_NAME}]: build ${currentBuild.result} - ${BUILD_URL}"
+            }
+        }
+    }
+}
