75fe0ed6ec039b8ee287d8adab5d569f5a53f7ca
Grishma123-Eng <60029598+Grishma123-Eng@users.noreply.github.com>
2026-02-20 16:59:45 +0530

    adding support for rocky 8 and rocky 9 (#3826)
    * adding support for rocky 8 and rocky 9
    
    * adding changes in pxc testing parallel job

--- pxc/jenkins/pxc-package-testing-parallel.groovy
+++ pxc/jenkins/pxc-package-testing-parallel.groovy
@@ -47,6 +47,10 @@ List non_pro_pxc80 = [
                 'rhel-9',
                 'rhel-8-arm',
                 'rhel-9-arm',
+                'rocky-linux-8',
+                'rocky-linux-8-arm',
+                'rocky-linux-9',
+                'rocky-linux-9-arm',
                 'amazon-linux-2023',
                 'amazon-linux-2023-arm'
 ]
@@ -70,6 +74,10 @@ List non_pro_pxc84 = [
                 'rhel-8-arm',
                 'rhel-9-arm',
                 'rhel-10-arm',
+                'rocky-linux-8',
+                'rocky-linux-8-arm',
+                'rocky-linux-9',
+                'rocky-linux-9-arm',
                 'amazon-linux-2023',
                 'amazon-linux-2023-arm'
 ]
