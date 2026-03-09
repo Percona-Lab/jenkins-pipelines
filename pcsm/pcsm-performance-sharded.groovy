5bcf0bb3a6585062eb9b1aa3f45a4b7f8a00e93a
Keith Quinn <keith.quinn@percona.com>
2026-03-09 14:58:47 +0000

    Updating sharded variables

--- pcsm/pcsm-performance-sharded.groovy
+++ pcsm/pcsm-performance-sharded.groovy
@@ -12,7 +12,8 @@ pipeline {
     }
     parameters {
         choice(name: 'OPERATING_SYSTEM',description: 'Operating System you want to test on',choices: ['ubuntu24', 'redhat8', 'ubuntu22'])
-        choice(name: 'PSMDB',description: 'PSMDB used for testing',choices: ['6', '7','8'])
+        choice(name: 'MONGODB_VERSION',description: 'PSMDB used for testing',choices: ['6', '7','8'])
+        booleanParam(name: 'MONGODB_COMMUNITY', defaultValue: false, description: 'Do you want to use Mongodb Community Edition?')
         string(name: 'PCSM_BRANCH',description: 'PCSM Branch for testing',defaultValue: 'main')
         string(name: 'TESTING_BRANCH',description: 'Branch for testing repository',defaultValue: 'main')
         string(name: 'GO_VERSION',description: 'Version of Golang used',defaultValue: '1.24.1')
