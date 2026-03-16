e717e9a8306bfc072a12f0bc2f9048af33a90b16
Peter Sirotnak <sirotnak.p@gmail.com>
2026-03-16 16:25:24 +0100

    PMM-7: PMM Version

--- pmm/v3/pmm3-upgrade-tests-matrix.groovy
+++ pmm/v3/pmm3-upgrade-tests-matrix.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+library changelog: false, identifier: 'lib@PMM-7-fix-new-navigation-upgrade', retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
