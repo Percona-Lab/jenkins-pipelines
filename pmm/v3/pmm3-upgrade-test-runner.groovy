a0a6ff251a82b0a4d0ac3c270886ca0341f65471
Peter Sirotnak <sirotnak.p@gmail.com>
2026-03-17 10:01:38 +0100

    PMM-7: PMM Version

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -14,22 +14,19 @@ void checkClientBeforeUpgrade(String PMM_SERVER_VERSION, String CLIENT_VERSION) 
     if (PMM_VERSION == '3-dev-latest') {
         sh '''
             GET_PMM_CLIENT_VERSION=$(wget -q https://raw.githubusercontent.com/Percona-Lab/pmm-submodules/v3/VERSION -O -)
-            CLIENT_CONTAINER_NAME=$(docker ps --format '{{.Names}}' | grep ps_pmm)
-            docker cp /srv/pmm-qa/support_scripts/check_client_upgrade.py ${CLIENT_CONTAINER_NAME}:/check_client_upgrade.py
-            docker exec ${CLIENT_CONTAINER_NAME} python3 /check_client_upgrade.py ${GET_PMM_CLIENT_VERSION}
+            sudo chmod 755 /srv/pmm-qa/support_scripts/check_client_upgrade.py
+            python3 /srv/pmm-qa/support_scripts/check_client_upgrade.py ${GET_PMM_CLIENT_VERSION}
         '''
     } else if (PMM_VERSION == 'pmm3-rc') {
         sh '''
             GET_PMM_CLIENT_VERSION=$(wget -q "https://registry.hub.docker.com/v2/repositories/perconalab/pmm-client/tags?page_size=25&name=rc" -O - | jq -r .results[].name  | grep 3.*.*-rc$ | sort -V | tail -n1)
-            CLIENT_CONTAINER_NAME=$(docker ps --format '{{.Names}}' | grep ps_pmm)
-            docker cp /srv/pmm-qa/support_scripts/check_client_upgrade.py ${CLIENT_CONTAINER_NAME}:/check_client_upgrade.py
-            docker exec ${CLIENT_CONTAINER_NAME} python3 /check_client_upgrade.py ${GET_PMM_CLIENT_VERSION}
+            sudo chmod 755 /srv/pmm-qa/support_scripts/check_client_upgrade.py
+            python3 /srv/pmm-qa/support_scripts/check_client_upgrade.py ${GET_PMM_CLIENT_VERSION}
         '''
     } else {
         sh '''
-            CLIENT_CONTAINER_NAME=$(docker ps --format '{{.Names}}' | grep ps_pmm)
-            docker cp /srv/pmm-qa/support_scripts/check_client_upgrade.py ${CLIENT_CONTAINER_NAME}:/check_client_upgrade.py
-            docker exec ${CLIENT_CONTAINER_NAME} python3 /check_client_upgrade.py ${PMM_VERSION}
+            sudo chmod 755 /srv/pmm-qa/support_scripts/check_client_upgrade.py
+            python3 /srv/pmm-qa/support_scripts/check_client_upgrade.py ${PMM_VERSION}
         '''
     }
 }
@@ -118,7 +115,7 @@ pipeline {
             description: 'PMM client repository',
             name: 'CLIENT_REPOSITORY')
         string(
-            defaultValue: 'main',
+            defaultValue: 'PMM-7-fix-check-client-upgrade',
             description: 'Tag/Branch for pmm qa repository',
             name: 'PMM_QA_GIT_BRANCH')
         string(
