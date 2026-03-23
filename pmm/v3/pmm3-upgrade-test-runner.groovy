6f6da89f2355a90985b331929264b97a7cbd4c59
Peter Sirotnak <sirotnak.p@gmail.com>
2026-03-23 17:01:16 +0100

    PMM-7: Refactors pipeline

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -132,7 +132,7 @@ pipeline {
             description: 'Tag/Branch for pmm qa repository',
             name: 'PMM_QA_GIT_BRANCH')
         string(
-            defaultValue: 'PMM-7-fix-rc-setup-psmdb',
+            defaultValue: 'main',
             description: 'Tag/Branch for qa-integration repository',
             name: 'QA_INTEGRATION_GIT_BRANCH')
         choice(
