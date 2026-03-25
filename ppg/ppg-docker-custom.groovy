3e6e38bde1dc22a03a1e46331e329d5f1e7bc165
Surabhi Bhat <surabhi.bhat@percona.com>
2026-03-25 21:27:36 +0530

    Fix ppg-docker-custom build

--- ppg/ppg-docker-custom.groovy
+++ ppg/ppg-docker-custom.groovy
@@ -35,6 +35,16 @@ pipeline {
                     echo \$MAJ_VER
                     MIN_VER=\$(echo ${params.PPG_VERSION} | cut -f1 -d'-' | cut -f2 -d'.')
                     echo \$MIN_VER
+                    export DOCKER_CLI_EXPERIMENTAL=enabled
+                    sudo mkdir -p /usr/libexec/docker/cli-plugins/
+                    sudo curl -L https://github.com/docker/buildx/releases/download/v0.30.0/buildx-v0.30.0.linux-amd64 -o /usr/libexec/docker/cli-plugins/docker-buildx
+                    sudo chmod +x /usr/libexec/docker/cli-plugins/docker-buildx
+                    sudo systemctl restart docker
+                    sudo apt-get install -y qemu-system binfmt-support qemu-user-static
+                    sudo qemu-system-x86_64 --version
+                    sudo lscpu | grep -q 'sse4_2' && grep -q 'popcnt' /proc/cpuinfo && echo "Supports x86-64-v2" || echo "Does NOT support x86-64-v2"
+                    sudo docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
                     git clone https://github.com/Manika-Percona/percona-docker
                     cd percona-docker/percona-distribution-postgresql-\$MAJ_VER-extras
                     sed -E "s/ENV PPG_VERSION (.+)/ENV PPG_VERSION ${params.PPG_VERSION}/" -i Dockerfile-postgis
