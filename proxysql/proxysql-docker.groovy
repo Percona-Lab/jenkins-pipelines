f3e7d79714134adebfc0109c4c0a177445c90a1a
Vadim Yalovets <vadim.yalovets@percona.com>
2026-04-02 14:54:07 +0300

    PS-10559 Build Tasks for ProxySQL 3.0.6

--- proxysql/proxysql-docker.groovy
+++ proxysql/proxysql-docker.groovy
@@ -77,7 +77,7 @@ parameters {
             steps {
                 script {
                         sh '''
-                            Dockerfile="Dockerfile-proxysql2"
+                            Dockerfile="Dockerfile-proxysql${VERSION%%.*}"
                             sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
                             sudo apt-get -y install apparmor
                             sudo aa-status
