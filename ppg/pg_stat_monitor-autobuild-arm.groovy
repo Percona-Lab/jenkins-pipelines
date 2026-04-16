421a77e46c122d4c78ec3cc936fa7e8a06096556
Surabhi Bhat <surabhi.bhat@percona.com>
2026-04-16 16:27:12 +0530

    Use sync2ProdAutoBuildPG for PG builds

--- ppg/pg_stat_monitor-autobuild-arm.groovy
+++ ppg/pg_stat_monitor-autobuild-arm.groovy
@@ -228,7 +228,7 @@ pipeline {
         stage('Push to public repository') {
             steps {
                 // sync packages
-                sync2ProdAutoBuild(params.CLOUD, PPG_REPO, COMPONENT)
+                sync2ProdAutoBuildPG(params.CLOUD, PPG_REPO, COMPONENT)
             }
         }
     } //stages
--- ppg/pgpool2-build.groovy
+++ ppg/pgpool2-build.groovy
@@ -337,7 +337,7 @@ pipeline {
         stage('Push to public repository') {
             steps {
                 // sync packages
-                sync2ProdAutoBuild(params.CLOUD, PPG_REPO, COMPONENT)
+                sync2ProdAutoBuildPG(params.CLOUD, PPG_REPO, COMPONENT)
             }
         }
 
