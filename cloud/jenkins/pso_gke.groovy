0d5f5c8b33f6e78ad9fa0fec6d1cbea82db77e5f
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2026-04-17 13:34:51 +0300

    Add nodes for cluster

--- cloud/jenkins/pso_gke.groovy
+++ cloud/jenkins/pso_gke.groovy
@@ -490,6 +490,30 @@ pipeline {
                         clusterRunner('cluster5')
                     }
                 }
+                stage('cluster6') {
+                    agent { label params.JENKINS_AGENT == 'Hetzner' ? 'docker-x64-min' : 'docker' }
+                    steps {
+                        prepareAgent()
+                        unstash "sourceFILES"
+                        clusterRunner('cluster6')
+                    }
+                }
+                stage('cluster7') {
+                    agent { label params.JENKINS_AGENT == 'Hetzner' ? 'docker-x64-min' : 'docker' }
+                    steps {
+                        prepareAgent()
+                        unstash "sourceFILES"
+                        clusterRunner('cluster7')
+                    }
+                }
+                stage('cluster8') {
+                    agent { label params.JENKINS_AGENT == 'Hetzner' ? 'docker-x64-min' : 'docker' }
+                    steps {
+                        prepareAgent()
+                        unstash "sourceFILES"
+                        clusterRunner('cluster8')
+                    }
+                }
             }
         }
     }
