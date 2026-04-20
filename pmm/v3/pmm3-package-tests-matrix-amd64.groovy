7bd8485da983c499ed790eaba7bc70d591ab252e
Peter Sirotnak <sirotnak.p@gmail.com>
2026-04-20 10:37:36 +0200

    PMM-14987: Fix for correct repository

--- pmm/v3/pmm3-package-tests-matrix-amd64.groovy
+++ pmm/v3/pmm3-package-tests-matrix-amd64.groovy
@@ -24,7 +24,7 @@ pipeline {
     }
     parameters {
         string(
-            defaultValue: 'master',
+            defaultValue: 'PMM-14987',
             description: 'Tag/Branch for package-testing repository',
             name: 'GIT_BRANCH',
             trim: true)
