7a8ad5483a0874d77d2f28237507c7a43644b076
Peter Sirotnak <sirotnak.p@gmail.com>
2026-04-20 11:18:16 +0200

    PMM-7: Add docker way upgrade

--- pmm/v3/pmm3-docker-way-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-docker-way-upgrade-test-runner.groovy
@@ -358,6 +358,7 @@ pipeline {
             steps {
                 withCredentials([aws(accessKeyVariable: 'BACKUP_LOCATION_ACCESS_KEY', credentialsId: 'BACKUP_E2E_TESTS', secretKeyVariable: 'BACKUP_LOCATION_SECRET_KEY'), aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'PMM_AWS_DEV', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
                     sh '''
+                        export DOCKER_TAG_UPGRADE=\${DOCKER_TAG_UPGRADE}
                         docker stop pmm-server
                         docker pull \${DOCKER_TAG_UPGRADE}
                         docker rename pmm-server pmm-server-old
