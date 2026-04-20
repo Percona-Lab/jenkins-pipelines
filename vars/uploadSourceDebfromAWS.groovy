a1d50b8fbf2ca0c05072693fd923ef8ee58858aa
Manika Singhal <manika.singhal@percona.com>
2026-04-20 15:52:49 +0530

    Refactor variable assignment syntax in Groovy script

--- vars/uploadSourceDebfromAWS.groovy
+++ vars/uploadSourceDebfromAWS.groovy
@@ -13,14 +13,14 @@ def call(String CLOUD_NAME, String FOLDER_NAME, String AWS_STASH_PATH) {
                 echo '10.30.6.9 repo.ci.percona.com' >> ./hosts
                 sudo cp ./hosts /etc/ || true
 
-                dsc=$(find . -name '*.dsc' | head -n 1)
+                dsc=`find . -name '*.dsc' | head -n 1`
                 if [ -f "\${dsc}" ]; then
                     ssh -o StrictHostKeyChecking=no -i ${KEY_PATH} ${USER}@repo.ci.percona.com \
                         mkdir -p \${path_to_build}/source/debian
-                    dir=$(dirname "\${dsc}")
-                    scp -o StrictHostKeyChecking=no -i "$KEY_PATH" \
+                    dir=`dirname \${dsc}`
+                    scp -o StrictHostKeyChecking=no -i ${KEY_PATH} \
                         \${dir}/* \
-                        \${USER}@repo.ci.percona.com:\${path_to_build}/source/debian/
+                        ${USER}@repo.ci.percona.com:\${path_to_build}/source/debian/
                 fi
 
                 for deb in \$(find . -name '*.deb'); do
