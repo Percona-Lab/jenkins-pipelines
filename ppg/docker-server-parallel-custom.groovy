fe4a51943eb7f1288acd9c6197f70303ba0a8e72
Naeem Akhter <naeem.akhter@percona.com>
2026-04-23 00:40:14 +0500

    Changing file and job names to align wih policy.

--- ppg/docker-server-parallel-custom.groovy
+++ ppg/docker-server-parallel-custom.groovy
@@ -4,18 +4,15 @@ library changelog: false, identifier: "lib@master", retriever: modernSCM([
 ])
 
 // Helper function
-def sendSlackNotification(repo, old_server_version, new_server_version, old_version_docker_tag, new_version_docker_tag, upgrade_docker_tag, with_postgis, milestone) {
+def sendSlackNotification(repo, version, tag, with_postgis, milestone) {
     // Note: Result might be null if the build fails early, default to FAILURE
     def status = currentBuild.result ?: "FAILURE"
     def color = (status == "SUCCESS") ? "good" : "danger"
-
+    
     def buildSummary = """Job: ${env.JOB_NAME}
-Old Server Version: ${old_server_version}
-New Server Version: ${new_server_version}
-Old Version Docker Tag: ${old_version_docker_tag}
-New Version Docker Tag: ${new_version_docker_tag}
-Upgrade Docker Tag: ${upgrade_docker_tag}
+Version: ${version}
 Repo: ${repo}
+Docker-tag: ${tag}
 With_PostGIS: ${with_postgis}
 Milestone: ${milestone}
 Status: *${status}*
@@ -26,36 +23,34 @@ Build Report: ${env.BUILD_URL}"""
 
 pipeline {
     agent { label 'min-ol-9-x64' }
-
+    
     parameters {
-        string(name: 'OLD_SERVER_VERSION', defaultValue: '16.13', description: 'Old server version that needs to be upgraded: 16.13, 17.9, 18.3, etc.')
-        string(name: 'NEW_SERVER_VERSION', defaultValue: '17.9', description: 'New server version to upgrade to: 17.9, 18.3. etc.')
-        string(name: 'OLD_VERSION_DOCKER_TAG', defaultValue: '16.13-v2', description: 'Old version docker tag to test: 16.13-v2, 17.9-v2, 18.3-v2. etc.')
-        string(name: 'NEW_VERSION_DOCKER_TAG', defaultValue: '17.9-v2', description: 'New version docker tag to test: 16.13-v2, 17.9-v2, 18.3-v2. etc.')
-        string(name: 'UPGRADE_DOCKER_TAG', defaultValue: '18.3-17.9-16.13-1', description: 'Upgrade docker tag to use: 18.3-17.9-16.13-1, 18.3-17.9-16.13-2. etc.')
+        string(name: 'DOCKER_TAG', defaultValue: '18.3', description: 'TAG of the docker to test.')
+        string(name: 'SERVER_VERSION', defaultValue: '18.3', description: 'Docker PG version to test.')
         string(name: 'TESTING_BRANCH', defaultValue: 'main', description: 'Branch for testing repository')
         choice(name: 'REPOSITORY', choices: ['perconalab', 'percona'], description: 'Docker hub repository.')
-        choice(name: 'MILESTONE', choices: ['1', '2', '3'], description: 'DO Milestone.')
+        choice(name: 'MILESTONE', choices: ['1', '2', '3'], defaultValue: '3', description: 'Custom image Milestone.')
         booleanParam(name: 'WITH_POSTGIS', defaultValue: true, description: "Enable PostGIS testing.")
         booleanParam(name: 'DESTROY_ENV', defaultValue: true, description: 'Destroy VM after tests')
     }
 
     environment {
         PATH = "/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:${env.HOME}/.local/bin"
-        MOLECULE_DIR = "docker/ppg-docker-DO-upgrade"
+        MOLECULE_DIR = "docker/ppg-docker-custom"
     }
 
     options {
         // Ensure this shared library function returns the correct wrapper
         withCredentials(moleculeDistributionJenkinsCreds())
-        timestamps()
+        timestamps() 
     }
 
     stages {
         stage('Set build name') {
             steps {
                 script {
-                    currentBuild.displayName = "#${env.BUILD_NUMBER}-docker-DO-upgrade-${params.OLD_SERVER_VERSION}-to-${params.NEW_SERVER_VERSION}"
+                    def suffix = params.WITH_POSTGIS ? "with-postgis" : "no-postgis"
+                    currentBuild.displayName = "#${env.BUILD_NUMBER}-docker-custom-${suffix}-${params.SERVER_VERSION}"
                 }
             }
         }
@@ -92,11 +87,12 @@ pipeline {
             }
         }
     }
-
+    
     post {
         always {
             script {
                 if (params.DESTROY_ENV) {
+                    // List of distros to test
                     def distros = [
                         'rocky-9',
                         'rhel-10',
@@ -112,7 +108,7 @@ pipeline {
                     moleculeParallelPostDestroyPPG(distros, env.MOLECULE_DIR)
                 }
                 // FIX: Passing params explicitly
-                sendSlackNotification(params.REPOSITORY, params.OLD_SERVER_VERSION, params.NEW_SERVER_VERSION, params.OLD_VERSION_DOCKER_TAG, params.NEW_VERSION_DOCKER_TAG, params.UPGRADE_DOCKER_TAG, params.WITH_POSTGIS, params.MILESTONE)
+                sendSlackNotification(params.REPOSITORY, params.SERVER_VERSION, params.DOCKER_TAG, params.WITH_POSTGIS, params.MILESTONE)
             }
         }
     }
