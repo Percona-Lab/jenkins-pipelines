f3ae346ad60a43d65a394f0d9c3bc1eb5939f7f0
Peter Sirotnak <sirotnak.p@gmail.com>
2026-04-27 11:51:08 +0200

    PMM-7: Add PMM Version 3.7.1

--- vars/pmmVersion.groovy
+++ vars/pmmVersion.groovy
@@ -77,6 +77,7 @@ def call(String type='dev-latest') {
     '3.5.0': 'ami-0b818149fe2015f98',
     '3.6.0': 'ami-08d17ac27fab9f3f1',
     '3.7.0': 'ami-08414cea86cc5611e',
+    '3.7.1': 'ami-07fe22a2d9e2e5672',
   ]
   List<String> v3 = v3Versions.keySet() as List<String>;
 
