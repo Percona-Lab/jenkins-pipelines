a17f345401d9719e991504f3ac68231b57d15b1b
travagliad <215686151+travagliad@users.noreply.github.com>
2026-05-05 16:22:59 +0100

    PMM-15041: Simplify valkey+psmdb-inmemory stage (single runClientWithRetry)

--- pmm/v3/pmm3-deploy-services.groovy
+++ pmm/v3/pmm3-deploy-services.groovy
@@ -288,16 +288,7 @@ pipeline {
         stage('valkey-psmdb-inmemory') {
           when { expression { return params.DEPLOY_VALKEY.toBoolean() || params.DEPLOY_MONGO_GROUP.toBoolean() } }
           steps {
-            script {
-              def parts = []
-              if (params.DEPLOY_VALKEY.toBoolean()) {
-                parts << '--database valkey'
-              }
-              if (params.DEPLOY_MONGO_GROUP.toBoolean()) {
-                parts << '--database psmdb,SETUP_TYPE=pss,STORAGE_ENGINE=inMemory'
-              }
-              runClientWithRetry(parts.join(' '), 'valkey-psmdb-inmemory')
-            }
+            runClientWithRetry('--database valkey --database psmdb,SETUP_TYPE=pss,STORAGE_ENGINE=inMemory', 'valkey-psmdb-inmemory')
           }
         }
       }
