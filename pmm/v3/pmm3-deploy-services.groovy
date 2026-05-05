380efb17ff4212d6ca29c0296af8e97676607939
travagliad <215686151+travagliad@users.noreply.github.com>
2026-05-05 14:28:58 +0100

    PMM-15041: Run Mongo inMemory setup on dedicated client stage
    Address review feedback by moving the inMemory Mongo setup out of the combined ps-single stage into a separate client node stage.

--- pmm/v3/pmm3-deploy-services.groovy
+++ pmm/v3/pmm3-deploy-services.groovy
@@ -260,7 +260,14 @@ pipeline {
         stage('ps-single+psmdb-pss') {
           when { expression { return params.DEPLOY_MYSQL_GROUP.toBoolean() || params.DEPLOY_MONGO_GROUP.toBoolean() } }
           steps {
-            runClientWithRetry('--database ps,QUERY_SOURCE=slowlog,MY_ROCKS=true --database psmdb,SETUP_TYPE=pss --database psmdb,SETUP_TYPE=pss,STORAGE_ENGINE=inMemory', 'ps-single-psmdb')
+            runClientWithRetry('--database ps,QUERY_SOURCE=slowlog,MY_ROCKS=true --database psmdb,SETUP_TYPE=pss', 'ps-single-psmdb')
+          }
+        }
+
+        stage('psmdb-pss-inmemory') {
+          when { expression { return params.DEPLOY_MONGO_GROUP.toBoolean() } }
+          steps {
+            runClientWithRetry('--database psmdb,SETUP_TYPE=pss,STORAGE_ENGINE=inMemory', 'psmdb-pss-inmemory')
           }
         }
 
