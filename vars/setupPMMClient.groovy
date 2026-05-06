703483c995a3a3085407f22c534c4deb3bd61a3b
travagliad <215686151+travagliad@users.noreply.github.com>
2026-05-06 02:06:01 +0100

    Enhance build reliability by adding retry logic for staging jobs and cleaning package manager caches

--- vars/setupPMMClient.groovy
+++ vars/setupPMMClient.groovy
@@ -44,13 +44,17 @@ def call(String SERVER_IP, String CLIENT_VERSION, String PMM_VERSION, String ENA
                 sudo yum -y update
                 sudo percona-release enable-only pmm2-client experimental
             elif [[ "$CLIENT_VERSION" = 2* ]]; then
-                sudo yum -y install "pmm2-client-$CLIENT_VERSION-6.el9.x86_64"
                 if [[ "$ENABLE_TESTING_REPO" = yes ]]; then
                     sudo percona-release enable-only pmm2-client testing
                 elif [[ "$ENABLE_TESTING_REPO" = no ]] && [[ "$ENABLE_EXPERIMENTAL_REPO" = yes ]]; then
                     sudo percona-release enable-only pmm2-client experimental
                 else
                     sudo percona-release enable-only pmm2-client release
+                fi
+                sudo yum clean all || true
+                sudo yum makecache
+                if ! sudo yum -y install "pmm2-client-${CLIENT_VERSION}*.el9.x86_64"; then
+                    sudo yum -y install pmm2-client
                 fi
                 sleep 10
             else
