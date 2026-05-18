768d21694512855e7c065262f4197f0ade4119c8
Peter Sirotnak <sirotnak.p@gmail.com>
2026-05-18 10:19:23 +0200

    PMM-7: Revert pmm qa related changes

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -361,9 +361,7 @@ pipeline {
             steps {
                 withCredentials([aws(accessKeyVariable: 'BACKUP_LOCATION_ACCESS_KEY', credentialsId: 'BACKUP_E2E_TESTS', secretKeyVariable: 'BACKUP_LOCATION_SECRET_KEY'), aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'PMM_AWS_DEV', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
                     sh '''
-                        pushd /srv/pmm-qa/codeceptjs-e2e
-                            ./node_modules/.bin/codeceptjs run --reporter mocha-multi -c pr.codecept.js --steps --grep '@pmm-upgrade'
-                        popd
+                        ./node_modules/.bin/codeceptjs run --reporter mocha-multi -c pr.codecept.js --steps --grep '@pmm-upgrade'
                     '''
                 }
             }
