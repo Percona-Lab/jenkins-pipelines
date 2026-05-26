471e9500defdde36da634313c39968578b483615
Peter Sirotnak <sirotnak.p@gmail.com>
2026-05-26 15:26:42 +0200

    PMM-7: Fix ami upgrade

--- pmm/v3/pmm3-upgrade-ami-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-ami-test-runner.groovy
@@ -340,11 +340,16 @@ pipeline {
             }
             steps {
                 withCredentials([aws(accessKeyVariable: 'BACKUP_LOCATION_ACCESS_KEY', credentialsId: 'BACKUP_E2E_TESTS', secretKeyVariable: 'BACKUP_LOCATION_SECRET_KEY'), aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'PMM_AWS_DEV', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-                    dir('codeceptjs-e2e') {
-                        sh '''
-                            ./node_modules/.bin/codeceptjs run-multiple parallel --reporter mocha-multi -c pr.codecept.js --steps --grep '@ami-ovf-post-upgrade'
-                        '''
-                    }
+                    sh '''
+                        pushd /srv/pmm-qa/codeceptjs-e2e
+                            npm ci
+                        popd
+                    '''
+                    sh '''
+                        pushd /srv/pmm-qa/codeceptjs-e2e
+                            ./node_modules/.bin/codeceptjs run --reporter mocha-multi -c pr.codecept.js --steps --grep '@ami-ovf-post-upgrade'
+                        popd
+                    '''
                 }
             }
         }
