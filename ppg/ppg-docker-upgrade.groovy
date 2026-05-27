0c02ab56a1ed33f6028a6a4414403624fba3e604
Surabhi Bhat <surabhi.bhat@percona.com>
2026-05-27 18:24:54 +0530

    Build ubi8 docker images for PG

--- ppg/ppg-docker-upgrade.groovy
+++ ppg/ppg-docker-upgrade.groovy
@@ -16,6 +16,8 @@ pipeline {
         string(name: 'IMAGE_TAG', defaultValue: '18.3-17.9-16.13-15.17-14.22-1', description: 'Image tag in the form <MAJ.MIN>-<MAJ.MIN>-...-<BUILD_NUMBER> (e.g. 18.3-17.9-16.13-15.17-14.22-1). The leading MAJ is used as PG_MAJOR build arg.')
         choice(name: 'TARGET_REPO', choices: ['PerconaLab','DockerHub'], description: 'Target repo for the regular upgrade docker image (custom image always goes to PerconaLab). Use DockerHub for release only')
         choice(name: 'IMAGE_TYPE', choices: ['both','regular','custom'], description: 'Which upgrade image(s) to build: regular only, custom only, or both (default)')
+        booleanParam(name: 'BUILD_UBI9', defaultValue: true, description: 'Build UBI9 image')
+        booleanParam(name: 'BUILD_UBI8', defaultValue: true, description: 'Build UBI8 image (regular only, no custom variant)')
     }
     options {
         disableConcurrentBuilds()
@@ -31,8 +33,6 @@ pipeline {
         stage ('Build image') {
             steps {
                 sh """
-                    PG_MAJOR=\$(echo ${params.IMAGE_TAG} | cut -f1 -d'-' | cut -f1 -d'.')
-                    echo "PG_MAJOR=\$PG_MAJOR"
                     export DOCKER_CLI_EXPERIMENTAL=enabled
                     sudo mkdir -p /usr/libexec/docker/cli-plugins/
                     sudo curl -L https://github.com/docker/buildx/releases/download/v0.30.0/buildx-v0.30.0.linux-amd64 -o /usr/libexec/docker/cli-plugins/docker-buildx
@@ -42,48 +42,89 @@ pipeline {
                     sudo qemu-system-x86_64 --version
                     sudo lscpu | grep -q 'sse4_2' && grep -q 'popcnt' /proc/cpuinfo && echo "Supports x86-64-v2" || echo "Does NOT support x86-64-v2"
                     sudo docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-
-                    git clone https://github.com/percona/percona-docker
-                    cd percona-docker/postgresql-containers
-                    sed -i "s|ppg-\\\${PG_MAJOR} release|ppg-\\\${PG_MAJOR} ${params.PPG_REPO}|g" build/upgrade/Dockerfile
-                    sed -i "s|ppg-\\\${pg_version} release|ppg-\\\${pg_version} ${params.PPG_REPO}|g" build/upgrade/Dockerfile
-                    sed -i "s|ppg-\\\${PG_MAJOR} release|ppg-\\\${PG_MAJOR} ${params.PPG_REPO}|g" build/upgrade/Dockerfile-custom
-                    sed -i "s|ppg-\\\${pg_version} release|ppg-\\\${pg_version} ${params.PPG_REPO}|g" build/upgrade/Dockerfile-custom
                     export DOCKER_BUILDKIT=1
+                """
+                script {
+                    if (params.BUILD_UBI9) {
+                        sh """
+                            PG_MAJOR=\$(echo ${params.IMAGE_TAG} | cut -f1 -d'-' | cut -f1 -d'.')
+                            echo "PG_MAJOR=\$PG_MAJOR"
+                            git clone https://github.com/percona/percona-docker
+                            cd percona-docker/percona-distribution-postgresql-upgrade
+                            sed -i "s|ppg-\\\${PG_MAJOR} release|ppg-\\\${PG_MAJOR} ${params.PPG_REPO}|g" Dockerfile
+                            sed -i "s|ppg-\\\${pg_version} release|ppg-\\\${pg_version} ${params.PPG_REPO}|g" Dockerfile
+                            sed -i "s|ppg-\\\${PG_MAJOR} release|ppg-\\\${PG_MAJOR} ${params.PPG_REPO}|g" Dockerfile-custom
+                            sed -i "s|ppg-\\\${pg_version} release|ppg-\\\${pg_version} ${params.PPG_REPO}|g" Dockerfile-custom
 
-                    if [ "${params.IMAGE_TYPE}" = "both" ] || [ "${params.IMAGE_TYPE}" = "regular" ]; then
-                        docker build --platform=linux/amd64 --no-cache --provenance=false \\
-                            --build-arg PG_MAJOR=\$PG_MAJOR \\
-                            -t percona-distribution-postgresql-upgrade:${params.IMAGE_TAG} \\
-                            -f build/upgrade/Dockerfile .
-                    fi
-                    if [ "${params.IMAGE_TYPE}" = "both" ] || [ "${params.IMAGE_TYPE}" = "custom" ]; then
-                        docker build --platform=linux/amd64 --no-cache --provenance=false \\
-                            --build-arg PG_MAJOR=\$PG_MAJOR \\
-                            -t percona-distribution-postgresql-upgrade-custom:${params.IMAGE_TAG} \\
-                            -f build/upgrade/Dockerfile-custom .
-                    fi
-                    """
+                            if [ "${params.IMAGE_TYPE}" = "both" ] || [ "${params.IMAGE_TYPE}" = "regular" ]; then
+                                docker build --platform=linux/amd64 --no-cache --provenance=false \\
+                                    --build-arg PG_MAJOR=\$PG_MAJOR \\
+                                    -t percona-distribution-postgresql-upgrade:${params.IMAGE_TAG} \\
+                                    -f Dockerfile .
+                            fi
+                            if [ "${params.IMAGE_TYPE}" = "both" ] || [ "${params.IMAGE_TYPE}" = "custom" ]; then
+                                docker build --platform=linux/amd64 --no-cache --provenance=false \\
+                                    --build-arg PG_MAJOR=\$PG_MAJOR \\
+                                    -t percona-distribution-postgresql-upgrade-custom:${params.IMAGE_TAG} \\
+                                    -f Dockerfile-custom .
+                            fi
+                        """
+                    }
+                    if (params.BUILD_UBI8) {
+                        sh """
+                            PG_MAJOR=\$(echo ${params.IMAGE_TAG} | cut -f1 -d'-' | cut -f1 -d'.')
+                            echo "PG_MAJOR=\$PG_MAJOR"
+                            git clone https://github.com/percona/percona-docker percona-docker-ubi8
+                            cd percona-docker-ubi8/percona-distribution-postgresql-upgrade
+                            sed -i "s|ppg-\\\${PG_MAJOR} release|ppg-\\\${PG_MAJOR} ${params.PPG_REPO}|g" Dockerfile-ubi8
+                            sed -i "s|ppg-\\\${pg_version} release|ppg-\\\${pg_version} ${params.PPG_REPO}|g" Dockerfile-ubi8
+
+                            if [ "${params.IMAGE_TYPE}" = "both" ] || [ "${params.IMAGE_TYPE}" = "regular" ]; then
+                                docker build --platform=linux/amd64 --no-cache --provenance=false \\
+                                    --build-arg PG_MAJOR=\$PG_MAJOR \\
+                                    -t percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-ubi8 \\
+                                    -f Dockerfile-ubi8 .
+                            fi
+                        """
+                    }
+                }
             }
         }
         stage ('Run trivy analyzer') {
             steps {
                 installTrivy(method: 'binary', junitTpl: true)
-                sh """
-                    if [ ${params.PPG_REPO} = "release" ]; then
-                        TRIVY_EXIT=1
-                    else
-                        TRIVY_EXIT=0
-                    fi
-                    if [ "${params.IMAGE_TYPE}" = "both" ] || [ "${params.IMAGE_TYPE}" = "regular" ]; then
-                        /usr/local/bin/trivy -q image --format template --template @junit.tpl  -o trivy-hight-junit.xml \\
-                                         --timeout 10m0s --ignore-unfixed --exit-code \$TRIVY_EXIT --severity HIGH,CRITICAL percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}
-                    fi
-                    if [ "${params.IMAGE_TYPE}" = "both" ] || [ "${params.IMAGE_TYPE}" = "custom" ]; then
-                        /usr/local/bin/trivy -q image --format template --template @junit.tpl  -o trivy-hight-custom-junit.xml \\
-                                         --timeout 10m0s --ignore-unfixed --exit-code \$TRIVY_EXIT --severity HIGH,CRITICAL percona-distribution-postgresql-upgrade-custom:${params.IMAGE_TAG}
-                    fi
-               """
+                script {
+                    if (params.BUILD_UBI9) {
+                        sh """
+                            if [ ${params.PPG_REPO} = "release" ]; then
+                                TRIVY_EXIT=1
+                            else
+                                TRIVY_EXIT=0
+                            fi
+                            if [ "${params.IMAGE_TYPE}" = "both" ] || [ "${params.IMAGE_TYPE}" = "regular" ]; then
+                                /usr/local/bin/trivy -q image --format template --template @junit.tpl  -o trivy-hight-junit.xml \\
+                                                 --timeout 10m0s --ignore-unfixed --exit-code \$TRIVY_EXIT --severity HIGH,CRITICAL percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}
+                            fi
+                            if [ "${params.IMAGE_TYPE}" = "both" ] || [ "${params.IMAGE_TYPE}" = "custom" ]; then
+                                /usr/local/bin/trivy -q image --format template --template @junit.tpl  -o trivy-hight-custom-junit.xml \\
+                                                 --timeout 10m0s --ignore-unfixed --exit-code \$TRIVY_EXIT --severity HIGH,CRITICAL percona-distribution-postgresql-upgrade-custom:${params.IMAGE_TAG}
+                            fi
+                        """
+                    }
+                    if (params.BUILD_UBI8) {
+                        sh """
+                            if [ ${params.PPG_REPO} = "release" ]; then
+                                TRIVY_EXIT=1
+                            else
+                                TRIVY_EXIT=0
+                            fi
+                            if [ "${params.IMAGE_TYPE}" = "both" ] || [ "${params.IMAGE_TYPE}" = "regular" ]; then
+                                /usr/local/bin/trivy -q image --format template --template @junit.tpl  -o trivy-hight-ubi8-junit.xml \\
+                                                 --timeout 10m0s --ignore-unfixed --exit-code \$TRIVY_EXIT --severity HIGH,CRITICAL percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-ubi8
+                            fi
+                        """
+                    }
+                }
             }
             post {
                 always {
@@ -100,11 +141,21 @@ pipeline {
             }
             steps {
                 withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                     sh """
-                         docker login -u '${USER}' -p '${PASS}'
-                         docker tag percona-distribution-postgresql-upgrade:${params.IMAGE_TAG} perconalab/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-amd64
-                         docker push perconalab/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-amd64
-                     """
+                    script {
+                        sh "docker login -u '${USER}' -p '${PASS}'"
+                        if (params.BUILD_UBI9) {
+                            sh """
+                                docker tag percona-distribution-postgresql-upgrade:${params.IMAGE_TAG} perconalab/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-amd64
+                                docker push perconalab/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-amd64
+                            """
+                        }
+                        if (params.BUILD_UBI8) {
+                            sh """
+                                docker tag percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-ubi8 perconalab/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-ubi8-amd64
+                                docker push perconalab/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-ubi8-amd64
+                            """
+                        }
+                    }
                 }
             }
         }
@@ -117,11 +168,21 @@ pipeline {
             }
             steps {
                 withCredentials([usernamePassword(credentialsId: 'hub.docker.com', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                     sh """
-                         docker login -u '${USER}' -p '${PASS}'
-                         docker tag percona-distribution-postgresql-upgrade:${params.IMAGE_TAG} percona/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-amd64
-                         docker push percona/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-amd64
-                     """
+                    script {
+                        sh "docker login -u '${USER}' -p '${PASS}'"
+                        if (params.BUILD_UBI9) {
+                            sh """
+                                docker tag percona-distribution-postgresql-upgrade:${params.IMAGE_TAG} percona/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-amd64
+                                docker push percona/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-amd64
+                            """
+                        }
+                        if (params.BUILD_UBI8) {
+                            sh """
+                                docker tag percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-ubi8 percona/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-ubi8-amd64
+                                docker push percona/percona-distribution-postgresql-upgrade:${params.IMAGE_TAG}-ubi8-amd64
+                            """
+                        }
+                    }
                 }
             }
         }
