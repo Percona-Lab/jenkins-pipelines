7d0d7bc6c3b513d38cbf90f21685b768353aa62a
Vadim Yalovets <vadim.yalovets@percona.com>
2026-05-27 13:24:32 +0300

    PS-11201 PS installation in RHEL-10 fails because of maridb

--- ps/jenkins/get-ps-branches-84.groovy
+++ ps/jenkins/get-ps-branches-84.groovy
@@ -23,52 +23,57 @@ pipeline {
         stage('Get release branches') {
             steps {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AWS_STASH', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                    sh """
-                        EC=0
-                        aws s3 ls s3://percona-jenkins-artifactory/percona-server/branch_commit_id_8.4.properties || EC=\$?
+            sh """
+              EC=0
+              aws s3 ls s3://percona-jenkins-artifactory/percona-server/branch_commit_id_8.4.properties || EC=\$?
 
-            if [ \${EC} = 1 ]; then
-              LATEST_RELEASE_BRANCH=\$(git -c 'versionsort.suffix=-' ls-remote --heads --sort='v:refname' ${GIT_REPO} 8.4 | tail -1)
-              BRANCH_NAME="8.4"
-              COMMIT_ID=\$(echo \${LATEST_RELEASE_BRANCH} | cut -d " " -f 1)
+              if [ \${EC} = 1 ]; then
+                LATEST_RELEASE_BRANCH=\$(git -c 'versionsort.suffix=-' ls-remote --heads --sort='v:refname' ${GIT_REPO} 8.4 | tail -1)
+                BRANCH_NAME="8.4"
+                COMMIT_ID=\$(echo \${LATEST_RELEASE_BRANCH} | cut -d " " -f 1)
 
-              echo "BRANCH_NAME=\${BRANCH_NAME}" > branch_commit_id_8.4.properties
-              echo "COMMIT_ID=\${COMMIT_ID}" >> branch_commit_id_8.4.properties
+                echo "BRANCH_NAME=\${BRANCH_NAME}" > branch_commit_id_8.4.properties
+                echo "COMMIT_ID=\${COMMIT_ID}" >> branch_commit_id_8.4.properties
 
-              aws s3 cp branch_commit_id_8.4.properties s3://percona-jenkins-artifactory/percona-server/
-                          echo "START_NEW_BUILD=NO" > startBuild
-            else
-                          aws s3 cp s3://percona-jenkins-artifactory/percona-server/branch_commit_id_8.4.properties .
-              source ./branch_commit_id_8.4.properties
-              cat ./branch_commit_id_8.4.properties
-
-              LATEST_RELEASE_BRANCH=\$(git -c 'versionsort.suffix=-' ls-remote --heads --sort='v:refname' ${GIT_REPO} 8.4 | tail -1)
-              LATEST_BRANCH_NAME="8.4"
-              LATEST_COMMIT_ID=\$(echo \${LATEST_RELEASE_BRANCH} | cut -d " " -f 1)
-              VERSION=\$(echo \${LATEST_RELEASE_BRANCH} | cut -d - -f 2-3)
-
-              if [ "x\${COMMIT_ID}" != "x\${LATEST_COMMIT_ID}" ] || [ "x\${BRANCH_NAME}" != "x\${LATEST_BRANCH_NAME}" ]; then
-                echo "START_NEW_BUILD=YES" > startBuild
-              else
+                aws s3 cp branch_commit_id_8.4.properties s3://percona-jenkins-artifactory/percona-server/
                 echo "START_NEW_BUILD=NO" > startBuild
+              else
+                aws s3 cp s3://percona-jenkins-artifactory/percona-server/branch_commit_id_8.4.properties .
+                source ./branch_commit_id_8.4.properties
+                cat ./branch_commit_id_8.4.properties
+
+                LATEST_RELEASE_BRANCH=\$(git -c 'versionsort.suffix=-' ls-remote --heads --sort='v:refname' ${GIT_REPO} 8.4 | tail -1)
+                LATEST_BRANCH_NAME="8.4"
+                LATEST_COMMIT_ID=\$(echo \${LATEST_RELEASE_BRANCH} | cut -d " " -f 1)
+                VERSION=$(curl -s "https://raw.githubusercontent.com/percona/percona-server/${BRANCH}/MYSQL_VERSION" | \
+                    awk -F= '
+                       /^MYSQL_VERSION_MAJOR/  { major=$2 }
+                       /^MYSQL_VERSION_MINOR/  { minor=$2 }
+                       /^MYSQL_VERSION_PATCH/  { patch=$2 }
+                       /^MYSQL_VERSION_EXTRA/{ extra=$2 }
+                       END { printf "%s.%s.%s%s\n", major, minor, patch, extra }
+                    ')
+
+                if [ "x\${COMMIT_ID}" != "x\${LATEST_COMMIT_ID}" ] || [ "x\${BRANCH_NAME}" != "x\${LATEST_BRANCH_NAME}" ]; then
+                  echo "START_NEW_BUILD=YES" > startBuild
+                else
+                  echo "START_NEW_BUILD=NO" > startBuild
+                fi
+
+                echo "BRANCH_NAME=\${LATEST_BRANCH_NAME}" > branch_commit_id_8.4.properties
+                echo "COMMIT_ID=\${LATEST_COMMIT_ID}" >> branch_commit_id_8.4.properties
+                echo "VERSION=\${VERSION}" >> branch_commit_id_8.4.properties
+                aws s3 cp branch_commit_id_8.4.properties s3://percona-jenkins-artifactory/percona-server/
               fi
-
-              echo "BRANCH_NAME=\${LATEST_BRANCH_NAME}" > branch_commit_id_8.4.properties
-              echo "COMMIT_ID=\${LATEST_COMMIT_ID}" >> branch_commit_id_8.4.properties
-              echo "VERSION=\${VERSION}" >> branch_commit_id_8.4.properties
-                          aws s3 cp branch_commit_id_8.4.properties s3://percona-jenkins-artifactory/percona-server/
-                        fi
-                    """
-                }
-                script {
-                    START_NEW_BUILD = sh(returnStdout: true, script: "source ./startBuild; echo \${START_NEW_BUILD}").trim()
-                    BRANCH_NAME = sh(returnStdout: true, script: "source ./branch_commit_id_8.4.properties; echo \${BRANCH_NAME}").trim()
-                    COMMIT_ID = sh(returnStdout: true, script: "source ./branch_commit_id_8.4.properties; echo \${COMMIT_ID}").trim()
-                    VERSION = sh(returnStdout: true, script: "source ./branch_commit_id_8.4.properties; echo \${BRANCH_NAME} | cut -d - -f 2-3 ").trim()
-                }
-
-            }
-
+            """
+          }
+          script {
+               START_NEW_BUILD = sh(returnStdout: true, script: "source ./startBuild; echo \${START_NEW_BUILD}").trim()
+               BRANCH_NAME = sh(returnStdout: true, script: "source ./branch_commit_id_8.4.properties; echo \${BRANCH_NAME}").trim()
+               COMMIT_ID = sh(returnStdout: true, script: "source ./branch_commit_id_8.4.properties; echo \${COMMIT_ID}").trim()
+               VERSION = sh(returnStdout: true, script: "source ./branch_commit_id_8.4.properties; echo \${BRANCH_NAME} | cut -d - -f 2-3 ").trim()
+              }
+          }    
         }
         stage('Build needed') {
             when {
