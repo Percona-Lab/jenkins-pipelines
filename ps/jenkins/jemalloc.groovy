66ae900945072e695443995a41e0a4d9a7a2f506
Vadim Yalovets <vadim.yalovets@percona.com>
2026-06-02 14:30:52 +0300

    PS-10967 Packaging tasks for release - PS 8.0.46-37

--- ps/jenkins/jemalloc.groovy
+++ ps/jenkins/jemalloc.groovy
@@ -112,12 +112,12 @@ void buildStage(String DOCKER_OS, String STAGE_PARAM) {
                     DEBIAN_FRONTEND=noninteractive apt-get -y purge eatmydata || true
                     if [ \\\$DEBIAN_VERSION = focal -o  \\\$DEBIAN_VERSION = bullseye -o \\\$DEBIAN_VERSION = jammy -o  \\\$DEBIAN_VERSION = noble ]; then
                         PKGLIST=\\"gcc-9\\"
-                    elif [ \\\$DEBIAN_VERSION = trixie ]; then
+                    elif [ \\\$DEBIAN_VERSION = trixie -o  \\\$DEBIAN_VERSION = resolute ]; then
                         PKGLIST=\\"gcc-13\\"
                     else
                         PKGLIST=\\"gcc-11\\"
                     fi
-                    if [ \\\$DEBIAN_VERSION = focal -o  \\\$DEBIAN_VERSION = bullseye -o \\\$DEBIAN_VERSION = jammy -o  \\\$DEBIAN_VERSION = bookworm -o  \\\$DEBIAN_VERSION = noble -o  \\\$DEBIAN_VERSION = trixie ]; then
+                    if [ \\\$DEBIAN_VERSION = focal -o  \\\$DEBIAN_VERSION = bullseye -o \\\$DEBIAN_VERSION = jammy -o  \\\$DEBIAN_VERSION = bookworm -o  \\\$DEBIAN_VERSION = noble -o  \\\$DEBIAN_VERSION = trixie \\\$DEBIAN_VERSION = resolute ]; then
                         PKGLIST=\\"\\\${PKGLIST} python3-mysqldb\\"
                     else
                         PKGLIST=\\"\\\${PKGLIST} python-mysqldb\\"
@@ -575,6 +575,42 @@ pipeline {
                         cleanUpWS()
                         popArtifactFolder(params.CLOUD, "source_tarball/", AWS_STASH_PATH)
                         buildStage("ubuntu:noble", "DEB")
+                        sh '''
+                            pwd
+                            ls -la test/deb
+                            cp -r test/deb .
+                        '''
+
+                        pushArtifactFolder(params.CLOUD, "deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS(params.CLOUD, "deb/", AWS_STASH_PATH)
+                    }
+                }
+                stage('Ubuntu Resolute (26.04)') {
+                    agent {
+                        label params.CLOUD == 'Hetzner' ? 'docker-x64-min' : 'docker'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder(params.CLOUD, "source_tarball/", AWS_STASH_PATH)
+                        buildStage("ubuntu:resolute", "DEB")
+                        sh '''
+                            pwd
+                            ls -la test/deb
+                            cp -r test/deb .
+                        '''
+
+                        pushArtifactFolder(params.CLOUD, "deb/", AWS_STASH_PATH)
+                        uploadDEBfromAWS(params.CLOUD, "deb/", AWS_STASH_PATH)
+                    }
+                }
+                stage('Ubuntu Resolute (26.04) ARM') {
+                    agent {
+                        label params.CLOUD == 'Hetzner' ? 'docker-aarch64' : 'docker-32gb-aarch64'
+                    }
+                    steps {
+                        cleanUpWS()
+                        popArtifactFolder(params.CLOUD, "source_tarball/", AWS_STASH_PATH)
+                        buildStage("ubuntu:resolute", "DEB")
                         sh '''
                             pwd
                             ls -la test/deb
