2ef2de1cf196ea2d18baaf0ceb431d781477d391
Keith Quinn <keith.quinn@percona.com>
2026-06-03 13:32:11 +0100

    Fixing variable

--- pcsm/pcsm-release-test-run.groovy
+++ pcsm/pcsm-release-test-run.groovy
@@ -24,11 +24,11 @@ pipeline {
 //                build job: 'hetzner-pcsm-functional-tests', propagate: false, parameters: [ string(name: 'CLOUD', value: params.CLOUD), string(name: 'PCSM_BRANCH', value: params.PCSM_BRANCH )]
 //            }
 //        }
-        stage ('Run PCSM package tests') {
-            steps {
-                build job: 'pcsm-packaging', parameters: [ string(name: 'pcsm_version', value: params.PCSM_VERSION), string(name: 'install_repo', value: "testing" )]
-            }
-        }
+//        stage ('Run PCSM package tests') {
+//            steps {
+//                build job: 'pcsm-packaging', parameters: [ string(name: 'pcsm_version', value: params.PCSM_VERSION), string(name: 'install_repo', value: "testing" )]
+//            }
+//        }
         stage ('Create single-arch (amd64) docker image') {
             steps {
                 script {
@@ -40,7 +40,7 @@ pipeline {
         stage ('Create multi-arch (amd64+arm) docker image') {
             steps {
                 script {
-                    def version = params.PBM_VERSION + '-1'
+                    def version = params.PCSM_VERSION + '-1'
                     build job: 'pcsm-docker-arm', parameters: [string(name: 'CLOUD', value: params.CLOUD), string(name: 'PCSM_REPO_CH', value: 'testing' ), string(name: 'PCSM_VERSION', value: version), string(name: 'LATEST', value: "no") ]
                 }
             }
