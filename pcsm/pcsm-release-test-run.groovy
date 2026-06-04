7a35e55483943d99b002746d0d078f2efbbd426c
Keith Quinn <keith.quinn@percona.com>
2026-06-04 10:45:56 +0100

    Uncommenting jobs

--- pcsm/pcsm-release-test-run.groovy
+++ pcsm/pcsm-release-test-run.groovy
@@ -19,32 +19,24 @@ pipeline {
         disableConcurrentBuilds()
     }
     stages {
-//        stage ('Run PCSM functional tests') {
-//            steps {
-//                build job: 'hetzner-pcsm-functional-tests', propagate: false, parameters: [ string(name: 'CLOUD', value: params.CLOUD), string(name: 'PCSM_BRANCH', value: params.PCSM_BRANCH )]
-//            }
-//        }
-//        stage ('Run PCSM package tests') {
-//            steps {
-//                build job: 'pcsm-packaging', parameters: [ string(name: 'pcsm_version', value: params.PCSM_VERSION), string(name: 'install_repo', value: "testing" )]
-//            }
-//        }
-//        stage ('Create single-arch (amd64) docker image') {
-//            steps {
-//                script {
-//                    def version = params.PCSM_VERSION + '-1'
-//                    build job: 'pcsm-docker', parameters: [string(name: 'CLOUD', value: params.CLOUD), string(name: 'PCSM_REPO_CH', value: 'testing'), string(name: 'PCSM_VERSION', value: version), string(name: 'LATEST', value: "no") ]
-//                }
-//            }
-//        }
-//        stage ('Create multi-arch (amd64+arm) docker image') {
-//            steps {
-//                script {
-//                    def version = params.PCSM_VERSION + '-1'
-//                    build job: 'hetzner-pcsm-docker-multiarch', parameters: [string(name: 'CLOUD', value: params.CLOUD), string(name: 'PCSM_REPO_CH', value: 'testing' ), string(name: 'PCSM_VERSION', value: version), string(name: 'LATEST', value: "no") ]
-//                }
-//            }
-//        }
+        stage ('Run PCSM functional tests') {
+            steps {
+                build job: 'hetzner-pcsm-functional-tests', propagate: false, parameters: [ string(name: 'CLOUD', value: params.CLOUD), string(name: 'PCSM_BRANCH', value: params.PCSM_BRANCH )]
+            }
+        }
+        stage ('Run PCSM package tests') {
+            steps {
+                build job: 'pcsm-packaging', parameters: [ string(name: 'pcsm_version', value: params.PCSM_VERSION), string(name: 'install_repo', value: "testing" )]
+            }
+        }
+        stage ('Create multi-arch (amd64+arm) docker image') {
+            steps {
+                script {
+                    def version = params.PCSM_VERSION + '-1'
+                    build job: 'hetzner-pcsm-docker-multiarch', parameters: [string(name: 'CLOUD', value: params.CLOUD), string(name: 'PCSM_REPO_CH', value: 'testing' ), string(name: 'PCSM_VERSION', value: version), string(name: 'LATEST', value: "no") ]
+                }
+            }
+        }
         stage ('Run PCSM tarball/docker SBOM tests') {
             steps {
                 script {
