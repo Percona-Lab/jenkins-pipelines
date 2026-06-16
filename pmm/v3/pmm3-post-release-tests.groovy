ca6b9d79d4754d22185801d8701434333aa8c5d8
travagliad <215686151+travagliad@users.noreply.github.com>
2026-06-16 10:28:04 +0100

    Alex's comments

--- pmm/v3/pmm3-post-release-tests.groovy
+++ pmm/v3/pmm3-post-release-tests.groovy
@@ -32,10 +32,14 @@ pipeline {
                 script {
                     env.ADMIN_PASSWORD = params.ADMIN_PASSWORD
                     def tags = sh(
-                        script: '''wget -q "https://registry.hub.docker.com/v2/repositories/percona/pmm-server/tags?page_size=100" -O - \
-                            | jq -r '.results[].name' | grep -E '^[0-9]+\\.[0-9]+\\.[0-9]+$' | sort -V''',
+                        script: '''set -e
+curl -sf "https://registry.hub.docker.com/v2/repositories/percona/pmm-server/tags?page_size=5" \
+    | jq -r '.results[].name' | grep -E '^3\\.[0-9]+\\.[0-9]+$' | sort -V''',
                         returnStdout: true
                     ).trim().split('\n').findAll { it }
+                    if (tags.size() < 2) {
+                        error('Need at least two released PMM 3.x percona/pmm-server tags on Docker Hub')
+                    }
                     def prevVersion = tags[-2]
                     def latestVersion = tags[-1]
                     env.PMM_SERVER_LATEST = latestVersion
