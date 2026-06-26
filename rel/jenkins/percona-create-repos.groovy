ef251668ff3008850aa4af3e00ff9be39fe5848d
Vadim Yalovets <vadim.yalovets@percona.com>
2026-06-26 17:08:35 +0300

    PS-10983 Packaging tasks for release - PS 9.7.1-1

--- rel/jenkins/percona-create-repos.groovy
+++ rel/jenkins/percona-create-repos.groovy
@@ -28,6 +28,18 @@ pipeline {
             defaultValue: '5',
             description: 'Limit',
             name: 'LIMIT')
+        choice(
+            choices: 'NO\nYES',
+            description: 'PRO build repo',
+            name: 'PROBUILD')
+        choice(
+            choices: 'YES\nNO',
+            description: 'Create YUM repo',
+            name: 'CREATEYUM')
+        choice(
+            choices: 'YES\nNO',
+            description: 'Create APT repo',
+            name: 'CREATEAPT')
     }
     options {
         skipDefaultCheckout()
@@ -37,18 +49,24 @@ pipeline {
     }
     stages {
         stage('Create YUM repo') {
+            when {
+                expression { env.CREATEYUM == 'YES' }
+            }
             steps {
-                createRepo(REPO_NAME, 'yum', COMPONENTS, CENTOS_VERSIONS, DEB_CODE_NAMES, LIMIT)
+                createRepo(REPO_NAME, 'yum', COMPONENTS, CENTOS_VERSIONS, DEB_CODE_NAMES, LIMIT, PROBUILD)
             }
         }
         stage('Create APT repo') {
+            when {
+                expression { env.CREATEAPT == 'YES' }
+            }
             steps {
-                createRepo(REPO_NAME, 'apt', COMPONENTS, CENTOS_VERSIONS, DEB_CODE_NAMES, LIMIT)
+                createRepo(REPO_NAME, 'apt', COMPONENTS, CENTOS_VERSIONS, DEB_CODE_NAMES, LIMIT, PROBUILD)
             }
         }
         stage('Sync repo to production') {
             steps {
-                syncRepo(REPO_NAME)
+                syncRepo(REPO_NAME, PROBUILD)
             }
         }
 
