5c9849843542b46895af2da2b6fd5ccb27172214
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2026-07-07 14:28:09 +0300

    PSMDB-2032 add package tests for PS4M

---
+++ ps4m/ps4m-package-parallel.groovy
@@ -0,0 +1,93 @@
+library changelog: false, identifier: "lib@master", retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+])
+
+def moleculeDir = "ps4m/ps4m-package"
+
+pipeline {
+    agent {
+        label 'min-bookworm-x64'
+    }
+    environment {
+        PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin'
+        ANSIBLE_DISPLAY_SKIPPED_HOSTS = false
+    }
+    parameters {
+        choice(
+            name: 'REPO',
+            description: 'Percona release repo channel for package install',
+            choices: [
+                'testing',
+                'release',
+                'experimental'
+            ]
+        )
+        string(
+            defaultValue: 'psmdb-83',
+            description: 'Percona release repo name, e.g. psmdb-83',
+            name: 'PSMDB_REPO'
+        )
+        string(
+            defaultValue: '0.51.0',
+            description: 'mongot package version, e.g. 0.51.0',
+            name: 'MONGOT_VERSION'
+        )
+        string(
+            defaultValue: 'main',
+            description: 'Branch for testing repository',
+            name: 'TESTING_BRANCH')
+    }
+    options {
+        withCredentials(moleculePbmJenkinsCreds())
+        disableConcurrentBuilds()
+    }
+    stages {
+        stage('Set build name'){
+            steps {
+                script {
+                    currentBuild.displayName = "${params.REPO}-${params.PSMDB_REPO}-mongot-${params.MONGOT_VERSION}"
+                    currentBuild.description = "mongot ${params.MONGOT_VERSION}"
+                }
+            }
+        }
+        stage('Checkout') {
+            steps {
+                deleteDir()
+                git poll: false, branch: TESTING_BRANCH, url: 'https://github.com/Percona-QA/psmdb-testing.git'
+            }
+        }
+        stage ('Prepare') {
+            steps {
+                script {
+                    installMoleculeBookworm()
+                }
+            }
+        }
+        stage('Test') {
+            steps {
+                script {
+                    moleculeParallelTestPSMDB(pdmdbOperatingSystems(PSMDB_REPO), moleculeDir)
+                }
+            }
+            post {
+                always {
+                    junit testResults: "**/*-report.xml", keepLongStdio: true, allowEmptyResults: true, skipPublishingChecks: true
+                }
+            }
+        }
+    }
+    post {
+        success {
+            slackNotify("#mongodb_autofeed", "#00FF00", "[${JOB_NAME}]: PS4M package tests for mongot ${MONGOT_VERSION} with ${PSMDB_REPO} repo ${REPO} - finished succesfully - [${BUILD_URL}]")
+        }
+        failure {
+            slackNotify("#mongodb_autofeed", "#FF0000", "[${JOB_NAME}]: PS4M package tests for mongot ${MONGOT_VERSION} with ${PSMDB_REPO} repo ${REPO} - failed - [${BUILD_URL}]")
+        }
+        always {
+            script {
+                moleculeParallelPostDestroy(pdmdbOperatingSystems(PSMDB_REPO), moleculeDir)
+            }
+        }
+    }
+}
