6d78e7dfaa6f8849c9a07047c1992df7db256907
Vadim Yalovets <vadim.yalovets@percona.com>
2026-07-09 12:31:27 +0300

    PXC-4935 Packaging tasks for release - PXC 8.4.10

--- vars/psBuildMatrix.groovy
+++ vars/psBuildMatrix.groovy
@@ -82,10 +82,10 @@ void buildStage(String DOCKER_OS, String STAGE_PARAM) {
                   . ./test/percona-server-8.0.properties
               fi
               sudo bash -x ./ps_builder.sh --builddir=\${build_dir}/test --install_deps=1
-              if [ ${BUILD_TOKUDB_TOKUBACKUP} = "ON" ]; then
-                  bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --build_tokudb_tokubackup=1 --perconaft_branch=${PERCONAFT_BRANCH} --tokubackup_branch=${TOKUBACKUP_BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}
+              if [ \${BUILD_TOKUDB_TOKUBACKUP} = "ON" ]; then
+                  bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --build_tokudb_tokubackup=1 --perconaft_branch=\${PERCONAFT_BRANCH} --tokubackup_branch=\${TOKUBACKUP_BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}
               else
-                  bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --perconaft_branch=${PERCONAFT_BRANCH} --tokubackup_branch=${TOKUBACKUP_BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}
+                  bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --perconaft_branch=\${PERCONAFT_BRANCH} --tokubackup_branch=\${TOKUBACKUP_BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}
               fi
           else
               docker run -u root --shm-size=16g --cap-add=SYS_NICE -v \${build_dir}:\${build_dir} ${DOCKER_OS} sh -c "
@@ -100,10 +100,10 @@ void buildStage(String DOCKER_OS, String STAGE_PARAM) {
                       . ./test/percona-server-8.0.properties
                   fi
                   bash -x ./ps_builder.sh --builddir=\${build_dir}/test --install_deps=1
-                  if [ ${BUILD_TOKUDB_TOKUBACKUP} = \"ON\" ]; then
-                      bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --build_tokudb_tokubackup=1 --perconaft_branch=${PERCONAFT_BRANCH} --tokubackup_branch=${TOKUBACKUP_BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}
+                  if [ \${BUILD_TOKUDB_TOKUBACKUP} = \"ON\" ]; then
+                      bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --build_tokudb_tokubackup=1 --perconaft_branch=\${PERCONAFT_BRANCH} --tokubackup_branch=\${TOKUBACKUP_BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}
                   else
-                      bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --perconaft_branch=${PERCONAFT_BRANCH} --tokubackup_branch=${TOKUBACKUP_BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}
+                      bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --perconaft_branch=\${PERCONAFT_BRANCH} --tokubackup_branch=\${TOKUBACKUP_BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}
                   fi"
           fi
       """
