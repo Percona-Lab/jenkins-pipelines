901088f0baa7ec0f7e0d560d89787b1919f0f226
Vadim Yalovets <vadim.yalovets@percona.com>
2026-07-16 08:50:22 +0300

    PXB-3744 Packaging tasks for release - PXB 9.7.1-rc1

--- pxc/jenkins/percona-xtradb-cluster-5.7.groovy
+++ pxc/jenkins/percona-xtradb-cluster-5.7.groovy
@@ -89,7 +89,7 @@ pipeline {
             description: 'Repo component to push packages to',
             name: 'COMPONENT')
         choice(
-            choices: '#releases\n#releases-ci',
+            choices: '#releases-ci\n#releases',
             description: 'Channel for notifications',
             name: 'SLACKNOTIFY')
     }
@@ -102,7 +102,7 @@ pipeline {
     stages {
         stage('Create PXC source tarball') {
             steps {
-               // slackNotify("${SLACKNOTIFY}", "#00FF00", "[${JOB_NAME}]: starting build for ${GIT_BRANCH} - [${BUILD_URL}]")
+               // slackNotify("${SLACKNOTIFY}", "#00FF00", "🥷 [${JOB_NAME}]: starting build for ${GIT_BRANCH} - [${BUILD_URL}]")
                 cleanUpWS()
                 buildStage("centos:7", "--get_sources=1")
                 sh '''
@@ -549,11 +549,11 @@ pipeline {
     }
     post {
         success {
-           // slackNotify("${SLACKNOTIFY}", "#00FF00", "[${JOB_NAME}]: build has been finished successfully for ${GIT_BRANCH} - [${BUILD_URL}]")
+            slackNotify("${SLACKNOTIFY}", "#00FF00", "🥷 [${JOB_NAME}]: build has been finished successfully for ${GIT_BRANCH} - [${BUILD_URL}]")
             deleteDir()
         }
         failure {
-            //slackNotify("${SLACKNOTIFY}", "#FF0000", "[${JOB_NAME}]: build failed for ${GIT_BRANCH} - [${BUILD_URL}]")
+            slackNotify("${SLACKNOTIFY}", "#FF0000", "🥷 [${JOB_NAME}]: build failed for ${GIT_BRANCH} - [${BUILD_URL}]")
             deleteDir()
         }
         always {
