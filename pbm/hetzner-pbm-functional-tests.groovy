f7dca6273bbd9481da7fac92a25f6e0b937ad701
Keith Quinn <keith.quinn@percona.com>
2026-07-17 08:02:49 +0100

    Test

--- pbm/hetzner-pbm-functional-tests.groovy
+++ pbm/hetzner-pbm-functional-tests.groovy
@@ -70,7 +70,7 @@ pipeline {
                                     docker compose build easyrsa
                                     docker compose build
                                     docker compose up -d
-                                    KMS_ID="${KMS_ID}" docker compose run test pytest -s --junitxml=junit.xml --shard-id=${SHARD} --num-shards=10 \$JENKINS_FLAG ${params.PYTEST_PARAMS} || true
+                                    KMS_ID="${KMS_ID}" docker compose run test pytest -s --junitxml=junit.xml --shard-id=${SHARD} --num-shards=1 \$JENKINS_FLAG ${params.PYTEST_PARAMS} || true
                                     docker compose down -v --remove-orphans
                                     curl -H "Content-Type:multipart/form-data" -H "Authorization: Bearer ${ZEPHYR_TOKEN}" -F "file=@junit.xml;type=application/xml" 'https://api.zephyrscale.smartbear.com/v2/automations/executions/junit?projectKey=PBM' -F 'testCycle={"name":"${JOB_NAME}-${BUILD_NUMBER}","customFields": { "PBM branch": "${PBM_BRANCH}","PSMDB docker image": "${PSMDB}","instance": "${instance}"}};type=application/json' -i || true
                                 """
