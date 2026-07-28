3ab6229562afa64e5af4eb116598afb5940f8fcc
Peter Sirotnak <sirotnak.p@gmail.com>
2026-07-28 12:01:18 +0200

    PMM-7: Fix AMI Upgrade

--- pmm/v3/pmm3-upgrade-ami-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-ami-test-runner.groovy
@@ -265,7 +265,7 @@ pipeline {
             steps {
                 withCredentials([aws(accessKeyVariable: 'BACKUP_LOCATION_ACCESS_KEY', credentialsId: 'BACKUP_E2E_TESTS', secretKeyVariable: 'BACKUP_LOCATION_SECRET_KEY'), aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'PMM_AWS_DEV', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
                     sh '''
-                        sed -i "s/PMM_IMAGE=.*/PMM_IMAGE=docker.io/\$DOCKER_TAG_UPGRADE/g" ~/.config/systemd/user/pmm-server.env
+                        sed -i "s|PMM_IMAGE=.*|PMM_IMAGE=docker.io/\$DOCKER_TAG_UPGRADE|g" ~/.config/systemd/user/pmm-server.env
                         cat ~/.config/systemd/user/pmm-server.env
                         source ~/.config/systemd/user/pmm-server.env podman pull ${PMM_IMAGE}
                         systemctl --user restart pmm-server
