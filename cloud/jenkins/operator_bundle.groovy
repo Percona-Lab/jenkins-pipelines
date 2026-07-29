3ce79f227380d585c1676b9e27001a3ad9560c8e
Julio Pasinatto <julio.pasinatto@percona.com>
2026-07-29 18:29:17 -0300

    fix comments and add push to repos

--- cloud/jenkins/operator_bundle.groovy
+++ cloud/jenkins/operator_bundle.groovy
@@ -5,12 +5,12 @@ pipeline {
             description: 'Which operator to generate the bundle for',
             name: 'OPERATOR')
         choice(
-            choices: ['community', 'redhat', 'all'],
+            choices: ['community', 'redhat'],
             description: 'Which bundle to generate',
             name: 'BUNDLE_TYPE')
         string(
             defaultValue: '',
-            description: 'The version of the bundle to build',
+            description: 'The version of the operator to build',
             name: 'VERSION')
         string(
             defaultValue: '',
@@ -22,8 +22,12 @@ pipeline {
             name: 'GIT_BRANCH')
         booleanParam(
             defaultValue: false,
-            description: 'Build and push the generated community bundle image',
+            description: 'Build and push the generated bundle image',
             name: 'PUSH_BUNDLE')
+        booleanParam(
+            defaultValue: false,
+            description: 'Create branch and push bundle to community/certified operator repos',
+            name: 'PUSH_TO_REPOS')
     }
     agent {
         label 'docker-x64-min'
@@ -44,28 +48,95 @@ pipeline {
         stage('Generate bundle') {
             steps {
                 script {
-                    if (params.BUNDLE_TYPE == 'all') {
+                    if (params.OPERATOR == 'percona-postgresql-operator') {
                         sh """
-                            cd source/installers/olm
-                            make bundles \
-                                VERSION=${params.VERSION} \
-                                OPENSHIFT_VERSIONS=${params.OPENSHIFT_VERSIONS}
-                        """
-                    } else {
-                        sh """
-                            cd source/installers/olm
-                            make bundles/${params.BUNDLE_TYPE} \
-                                VERSION=${params.VERSION} \
-                                OPENSHIFT_VERSIONS=${params.OPENSHIFT_VERSIONS}
+                            cd source
+                            make generate-crd-without-description
                         """
                     }
-                    archiveArtifacts artifacts: 'source/installers/olm/bundles/**', allowEmptyArchive: true
+                    sh """
+                        cd source/installers/olm
+                        make bundles/${params.BUNDLE_TYPE} \
+                            VERSION=${params.VERSION} \
+                            OPENSHIFT_VERSIONS=${params.OPENSHIFT_VERSIONS}
+                    """
+                }
+            }
+        }
+        stage('Prepare version folder') {
+            steps {
+                sh """
+                    BUNDLE_DIR=source/installers/olm/bundles/${params.BUNDLE_TYPE}
+                    rm -rf ${params.VERSION}
+                    mkdir -p ${params.VERSION}
+                    cp -a \${BUNDLE_DIR}/manifests ${params.VERSION}/
+                    cp -a \${BUNDLE_DIR}/metadata ${params.VERSION}/
+                """
+                archiveArtifacts artifacts: "${params.VERSION}/**", allowEmptyArchive: false
+            }
+        }
+        stage('Create hub branches') {
+            when {
+                expression { return params.PUSH_TO_REPOS }
+            }
+            steps {
+                script {
+                    def packageName = params.BUNDLE_TYPE == 'redhat' ? "${params.OPERATOR}-certified" : params.OPERATOR
+                    def branchName = "${packageName}-${params.VERSION}"
+                    def repos = params.BUNDLE_TYPE == 'community' ? [
+                        'redhat-openshift-ecosystem/community-operators-prod',
+                        'k8s-operatorhub/community-operators'
+                    ] : [
+                        'redhat-openshift-ecosystem/certified-operators'
+                    ]
+                    def branchLinks = []
+
+                    withCredentials([string(credentialsId: 'GITHUB_API_TOKEN', variable: 'GITHUB_TOKEN')]) {
+                        repos.each { repo ->
+                            def dirName = "hub-${repo.tokenize('/').last()}"
+                            sh """
+                                rm -rf ${dirName}
+                                git clone --depth=1 https://github.com/${repo}.git ${dirName}
+                            """
+                            dir(dirName) {
+                                sh """
+                                    git config user.email "jenkins@percona.com"
+                                    git config user.name "JNKPercona"
+                                    git checkout -b ${branchName}
+                                    mkdir -p operators/${packageName}
+                                    rm -rf operators/${packageName}/${params.VERSION}
+                                    cp -a ../${params.VERSION} operators/${packageName}/${params.VERSION}
+                                    git add operators/${packageName}/${params.VERSION}
+                                    git commit -m "operator ${packageName} (${params.VERSION})"
+                                    git remote set-url origin https://x-access-token:\${GITHUB_TOKEN}@github.com/${repo}.git
+                                    git push -u origin ${branchName}
+                                """
+                            }
+                            branchLinks.add("https://github.com/${repo}/tree/${branchName}")
+                        }
+                    }
+
+                    def linksText = branchLinks.collect { "- ${it}" }.join('\n')
+                    slackSend(
+                        channel: '#cloud-dev-ci',
+                        color: 'good',
+                        message: """
+                            :white_check_mark: *Operator Bundle Hub Branches Created*
+                            *Operator:* ${params.OPERATOR}
+                            *Package:* ${packageName}
+                            *Version:* ${params.VERSION}
+                            *Bundle Type:* ${params.BUNDLE_TYPE}
+                            *Build:* ${env.BUILD_URL}
+                            *Branches:*
+                            ${linksText}
+                        """.stripIndent()
+                    )
                 }
             }
         }
         stage('Build') {
             when {
-                expression { return params.PUSH_BUNDLE && params.BUNDLE_TYPE == 'community' }
+                expression { return params.PUSH_BUNDLE }
             }
             steps {
                 sh """
@@ -77,13 +148,14 @@ pipeline {
         }
         stage('Push') {
             when {
-                expression { return params.PUSH_BUNDLE && params.BUNDLE_TYPE == 'community' }
+                expression { return params.PUSH_BUNDLE }
             }
             steps {
                  sh """
                     cd source/installers/olm
                     make push \
-                        VERSION=${params.VERSION}
+                        VERSION=${params.VERSION} \
+                        CONFIRM_PUSH=0
                 """
             }
         }
