8261cff7eccdc8225ce8980b899ae93c92b75574
Alex Demidoff <alexander.demidoff@percona.com>
2026-08-22 01:08:46 +0300

    PMM-15349 Remove Watchtower from PMM v3 build and release pipelines (#4353)
    Watchtower was removed from PMM Server in 3.9.0 (PMM-14969), but the v3
    build and release pipelines kept building, tagging and publishing it on
    every feature build, nightly rewind, RC and GA release.
    
    - pmm3-submodules: drop the three Watchtower stages, PATH_TO_WATCHTOWER
      and the Watchtower line from the feature build comment
    - pmm3-watchtower-autobuild: delete the pipeline
    - pmm3-release-candidate: drop the Watchtower autobuild stage and the
      WATCHTOWER_IMAGE parameter passed to pmm3-ami
    - pmm3-ami: drop the WATCHTOWER_IMAGE parameter, the pmm-ami make target
      never consumed it
    - pmm3-release: stop publishing percona/watchtower tags
    
    Staging and upgrade-test pipelines keep Watchtower for now, they still
    need it to exercise upgrades from pre-3.9 versions.

--- pmm/v3/pmm3-ami.groovy
+++ pmm/v3/pmm3-ami.groovy
@@ -11,10 +11,6 @@ pipeline {
             defaultValue: 'docker.io/perconalab/pmm-server:3-dev-latest',
             description: 'Docker image for PMM Server running in the AMI',
             name: 'PMM_SERVER_IMAGE')
-        string(
-            defaultValue: 'docker.io/perconalab/watchtower:dev-latest',
-            description: 'Docker image for Watchtower running in the AMI',
-            name: 'WATCHTOWER_IMAGE')
         choice(
             choices: ['no', 'yes'],
             description: "Build a Release Candidate?",
@@ -49,7 +45,7 @@ pipeline {
         stage('Build PMM AMI Image') {
             steps {
                 dir("build") {
-                    sh "PMM_SERVER_IMAGE=${PMM_SERVER_IMAGE} WATCHTOWER_IMAGE=${WATCHTOWER_IMAGE} make pmm-ami"
+                    sh "PMM_SERVER_IMAGE=${PMM_SERVER_IMAGE} make pmm-ami"
                 }
                 script {
                     env.AMI_ID = sh(script: "jq -r '.builds[-1].artifact_id' build/manifest.json | cut -d ':' -f2", returnStdout: true)
