f09ecd2b332a59fcaa43dd0f89e0678ad684fa82
Tomislav Plavcic <tomislav.plavcic@percona.com>
2026-08-31 10:57:04 +0200

    PS-10483 - Bump molecule version (#4354)
    * PS-10483 - Bump Molecule version in installMoleculeBookworm function
    
    * Temp change package testing repo branch
    
    * Fix ansible versions to support python 3.11
    
    * Fix ansible-core version
    
    * Temp change package testing repo branch for PS package testing
    
    * Bump molecule version in ps-package-testing-molecule
    
    * Fix ansible stack in ps-package-testing-molecule
    
    * Lower ansible version
    
    * Lower version of ansible in installMoleculeBookworm
    
    * Change molecule version in shared functions
    
    * Revert jenkins-pipelines temp test branch
    
    * Revert changes in installMolecule and installMoleculeBookworm
    
    * Switch mysql jobs to installMoleculeBookwormMysql
    
    * Temp test change - revert before merge
    
    * Revert "Temp test change - revert before merge"
    
    This reverts commit bb6706287ddabb630cf835b5fe1ab94b9d04ed68.

--- ps/jenkins/ps-package-testing-molecule.groovy
+++ ps/jenkins/ps-package-testing-molecule.groovy
@@ -228,20 +228,6 @@ def deleteBuildInstances(){
     }
 }
 
-def installMolecule() {
-        sh """
-            sudo apt update -y
-            sudo apt install -y python3 python3-pip python3-dev python3-venv
-            python3 -m venv virtenv
-            . virtenv/bin/activate
-            python3 --version
-            python3 -m pip install --upgrade pip
-            python3 -m pip install --upgrade "setuptools<81"
-            python3 -m pip install --upgrade setuptools-rust
-            python3 -m pip install --upgrade PyYaml==5.3.1 molecule==3.3.0 testinfra pytest molecule-ec2==0.3 molecule[ansible] "ansible<10.0.0" "ansible-lint>=5.1.1,<6.0.0" boto3 boto
-        """
-}
-
 def loadEnvFile(envFilePath) {
     def envMap = []
     def envFileContent = readFile(file: envFilePath).trim().split('\n')
@@ -502,7 +488,7 @@ pipeline {
             stage('Prepare') {
                 steps {
                     script {
-                        installMolecule()
+                        installMoleculeBookwormMysql()
                     }
                 }
             }
--- pxb/jenkins/pxb-tarball-molecule.groovy
+++ pxb/jenkins/pxb-tarball-molecule.groovy
@@ -169,7 +169,7 @@ pipeline {
       steps {
         script {
           echo "PXB_RELEASE is ${env.PXB_RELEASE}"
-          installMoleculeBookworm()
+          installMoleculeBookwormMysql()
         }
       }
     }
