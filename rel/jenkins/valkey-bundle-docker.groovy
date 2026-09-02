3628245c572acdaeefd91f2f5a195d4ea1f7b93c
EvgeniyPatlan <evgeniy.patlan@percona.com>
2026-09-02 18:27:35 +0300

    Update qemu-aarch64 registration format in Groovy script

--- rel/jenkins/valkey-bundle-docker.groovy
+++ rel/jenkins/valkey-bundle-docker.groovy
@@ -173,7 +173,7 @@ pipeline {
                         echo -1 | sudo tee /proc/sys/fs/binfmt_misc/qemu-aarch64 >/dev/null
                     fi
 
-                    printf '%s\\n' ':qemu-aarch64:M::\\x7fELF\\x02\\x01\\x01\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x02\\x00\\xb7\\x00:\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\x00\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xfe\\xff\\xff\\xff:/usr/local/bin/qemu-aarch64:OCF' \
+                    printf '%s\\n' ':qemu-aarch64:M::\\x7fELF\\x02\\x01\\x01\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x02\\x00\\xb7\\x00:\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\x00\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xfe\\xff\\xff\\xff:/usr/local/bin/qemu-aarch64:POCF' \
                         | sudo tee /proc/sys/fs/binfmt_misc/register >/dev/null
 
                     cat /proc/sys/fs/binfmt_misc/qemu-aarch64
