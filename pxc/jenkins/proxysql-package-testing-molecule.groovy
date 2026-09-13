a9507f2bd31bde15bd7f58a575c9f296a29739a4
kaushikpuneet07 <puneet.kaushik@percona.com>
2026-09-13 08:18:12 +0530

    update

--- pxc/jenkins/proxysql-package-testing-molecule.groovy
+++ pxc/jenkins/proxysql-package-testing-molecule.groovy
@@ -75,7 +75,7 @@
             stage('Checkout') {
                 steps {
                     deleteDir()
-                    git poll: false, branch: "master", url: "https://github.com/Percona-Lab/package-testing.git"
+                    git poll: false, branch: "master", url: "https://github.com/Percona-QA/package-testing.git"
                 }
             }
 
--- pxc/jenkins/pxc-package-testing.groovy
+++ pxc/jenkins/pxc-package-testing.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: 'lib@fix-molecule-ssh-key-workspace-path', retriever: modernSCM([
+library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
+    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ]) _
 
 import groovy.transform.Field
