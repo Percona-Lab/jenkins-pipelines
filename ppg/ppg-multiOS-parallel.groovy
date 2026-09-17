b3aa9d2666cab80350a6dc8a8759aaa7bf9b5754
Naeem Akhter <naeem.akhter@percona.com>
2026-09-17 21:06:26 +0500

    Change library to obs-work branch

--- ppg/ppg-multiOS-parallel.groovy
+++ ppg/ppg-multiOS-parallel.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: "lib@master", retriever: modernSCM([
+library changelog: false, identifier: "lib@obs-work", retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ])
