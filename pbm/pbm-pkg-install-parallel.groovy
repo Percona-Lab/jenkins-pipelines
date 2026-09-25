cb158a0d1b2dc977a489338b43d06cb3fc968745
Keith Quinn <keith.quinn@percona.com>
2026-09-25 12:34:18 +0100

    Removing test changes

--- pbm/pbm-pkg-install-parallel.groovy
+++ pbm/pbm-pkg-install-parallel.groovy
@@ -1,4 +1,4 @@
-library changelog: false, identifier: "lib@PSMDB-2214", retriever: modernSCM([
+library changelog: false, identifier: "lib@master", retriever: modernSCM([
     $class: 'GitSCMSource',
     remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
 ])
@@ -66,17 +66,17 @@ pipeline {
             }
         }
     }
-//    post {
-//        success {
-//            slackNotify("#mongodb_autofeed", "#00FF00", "[${JOB_NAME}]: package tests for PBM ${VERSION} repo ${install_repo} finished succesfully - [${BUILD_URL}]")
-//        }
-//        failure {
-//            slackNotify("#mongodb_autofeed", "#FF0000", "[${JOB_NAME}]: package tests for PBM ${VERSION} repo ${install_repo} failed - [${BUILD_URL}]")
-//        }
-//        always {
-//            script {
-//                moleculeParallelPostDestroy(pdmdbOperatingSystems(psmdb_to_test), moleculeDir)
-//            }
-//        }
-//    }
+    post {
+        success {
+            slackNotify("#mongodb_autofeed", "#00FF00", "[${JOB_NAME}]: package tests for PBM ${VERSION} repo ${install_repo} finished succesfully - [${BUILD_URL}]")
+        }
+        failure {
+            slackNotify("#mongodb_autofeed", "#FF0000", "[${JOB_NAME}]: package tests for PBM ${VERSION} repo ${install_repo} failed - [${BUILD_URL}]")
+        }
+        always {
+            script {
+                moleculeParallelPostDestroy(pdmdbOperatingSystems(psmdb_to_test), moleculeDir)
+            }
+        }
+    }
 }
